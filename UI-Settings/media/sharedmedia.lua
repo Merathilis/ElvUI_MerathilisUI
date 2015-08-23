7e5bc5ebb1b3a9f671d15b73c0a27b42944235ff
Merathilis <neo0608@gmail.com>
2015-08-23 15:48:21 +0200

    Added Font, Textures, etc.

---
+++ UI-Settings/media/sharedmedia.lua
@@ -0,0 +1,18 @@
+local LSM = LibStub('LibSharedMedia-3.0')
+
+if LSM == nil then return end
+
+LSM:Register('font','Bui Prototype', [[Interface\AddOns\ElvUI_BenikUI\media\fonts\PROTOTYPE.TTF]])
+LSM:Register('font','Bui Visitor1', [[Interface\AddOns\ElvUI_BenikUI\media\fonts\visitor1.ttf]])
+LSM:Register('font','Bui Visitor2', [[Interface\AddOns\ElvUI_BenikUI\media\fonts\visitor2.ttf]])
+LSM:Register('font','Bui Tukui', [[Interface\AddOns\ElvUI_BenikUI\media\fonts\uf_font.ttf]])
+LSM:Register('font','Bui Kait', [[Interface\AddOns\ElvUI_BenikUI\media\fonts\heavy_dock_kait.ttf]])
+LSM:Register('font','Bui ArialN', [[Interface\AddOns\ElvUI_BenikUI\media\fonts\ARIALN.ttf]])
+LSM:Register('font','Bui Roadway', [[Interface\AddOns\ElvUI_BenikUI\media\fonts\ROADWAY.ttf]])
+LSM:Register('statusbar','BuiFlat', [[Interface\AddOns\ElvUI_BenikUI\media\textures\Flat.tga]])
+LSM:Register('statusbar','BuiOnePixel', [[Interface\AddOns\ElvUI_BenikUI\media\textures\BuiOnePixel.tga]])
+LSM:Register('statusbar','BuiMelli', [[Interface\AddOns\ElvUI_BenikUI\media\textures\Melli.tga]])
+LSM:Register('statusbar','BuiMelliDark', [[Interface\AddOns\ElvUI_BenikUI\media\textures\MelliDark.tga]])
+LSM:Register('statusbar','BuiEmpty', [[Interface\AddOns\ElvUI_BenikUI\media\textures\Empty.tga]])
+LSM:Register('statusbar','BuiKait1', [[Interface\AddOns\ElvUI_BenikUI\media\textures\Kait1.tga]])
+LSM:Register('statusbar','BuiKait2', [[Interface\AddOns\ElvUI_BenikUI\media\textures\Kait2.tga]])
