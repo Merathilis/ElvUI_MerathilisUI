50f1d3aef70cd36a980756d41272e4e4292ce6c4
Benik <benik@otenet.gr>
2015-08-25 15:34:05 +0300

    more lines

--- UI-Settings/core.lua
+++ UI-Settings/core.lua
@@ -4,3 +4,9 @@ local MER = E:NewModule('MerathilisUI', "AceConsole-3.0");
 local LSM = LibStub('LibSharedMedia-3.0')
 local EP = LibStub('LibElvUIPlugin-1.0')
 local addon, ns = ...
+
+-- Profile (if this gets big, move it to a seperate file but load before your core.lua. Put it in the .toc file)
+-- In case you create options, also add them here
+P['Merathilis Eule'] = {
+	['installed'] = nil,
+}
