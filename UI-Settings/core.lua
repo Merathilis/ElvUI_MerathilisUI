1fd54e76610badad2695e0983681ab44dead83e3
Benik <benik@otenet.gr>
2015-08-25 15:33:15 +0300

    5 lines

--- UI-Settings/core.lua
+++ UI-Settings/core.lua
@@ -1,1 +1,6 @@
 local E, L, V, P, G, _ = unpack(ElvUI);
+local MER = E:NewModule('MerathilisUI', "AceConsole-3.0");
+
+local LSM = LibStub('LibSharedMedia-3.0')
+local EP = LibStub('LibElvUIPlugin-1.0')
+local addon, ns = ...
