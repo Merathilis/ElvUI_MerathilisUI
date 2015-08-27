808ea39b426491a1a0f253b935067e8b2554e9c0
Merathilis <neo0608@gmail.com>
2015-08-27 09:27:46 +0200

    Add Hover Effect for UnitFrames Add VAT Settings

--- MerathilisUI/core.lua
+++ MerathilisUI/core.lua
@@ -84,11 +84,6 @@ local function SetupUI() -- this cannot be local when using the module name (MER
 		E.db.actionbar.bar6.buttonsPerRow = 1
 		E.db.actionbar.bar6.mousover = true
 		E.db.actionbar.bar6.buttons = 4
-		-- ExtraActionButtons
-		--E.db.actionbar.bar8.buttons = 6
-		--E.db.actionbar.bar8.mousover = true
-		--E.db.actionbar.bar8.buttonsPerRow = 1
-		--E.db.actionbar.bar8.buttonsize = 24
 		E.db.actionbar.barPet.point = 'RIGHT'
 		E.db.actionbar.barPet.buttonspacing = 4
 		E.db.actionbar.stanceBar.point = 'BOTTOMLEFT'
@@ -816,6 +811,42 @@ local function SetupUI() -- this cannot be local when using the module name (MER
 					}, -- [2]
 				},		
 			}
+		end
+	end
+	
+	-- ElvUI_VisualAuraTimer
+	if IsAddOnLoaded('ElvUI_VisualAuraTimers') then
+		do
+			E.private.VAT.staticColor.r = 1
+			E.private.VAT.staticColor.g = 0.5
+			E.private.VAT.staticColor.b = 0
+			E.private.VAT.noduration = false
+			E.private.VAT.spacing = -1
+			E.private.VAT.backdropTexture = 'MerathilisFlat'
+			E.private.VAT.enableStaticColor = true
+			E.private.VAT.threshold.debuffsvalue = 20
+			E.private.VAT.threshold.buffsvalue = 10
+			E.private.VAT.threshold.buffs = true
+			E.private.VAT.threshold.tempenchants = true
+			E.private.VAT.colors.hoursIndicator.g = 0.490196078431373
+			E.private.VAT.colors.hoursIndicator.b = 0.0392156862745098
+			E.private.VAT.colors.minutesIndicator.r = 1
+			E.private.VAT.colors.minutesIndicator.g = 0.5
+			E.private.VAT.colors.minutesIndicator.b = 0
+			E.private.VAT.colors.hourminutesIndicator.r = 1
+			E.private.VAT.colors.hourminutesIndicator.g = 0.5
+			E.private.VAT.colors.hourminutesIndicator.b = 0
+			E.private.VAT.colors.expireIndicator.r = 1
+			E.private.VAT.colors.expireIndicator.g = 0.5
+			E.private.VAT.colors.expireIndicator.b = 0
+			E.private.VAT.colors.secondsIndicator.r = 1
+			E.private.VAT.colors.secondsIndicator.g = 0.5
+			E.private.VAT.colors.secondsIndicator.b = 0
+			E.private.VAT.colors.daysIndicator.r = 1
+			E.private.VAT.colors.daysIndicator.g = 0.5
+			E.private.VAT.colors.daysIndicator.b = 0
+			E.private.VAT.decimalThreshold = 5
+			E.private.VAT.statusbarTexture = 'MerathilisFlat'
 		end
 	end
 	
---
+++ MerathilisUI/modules/unithover.lua
@@ -0,0 +1,23 @@
+local E, L, V, P, G, _ = unpack(ElvUI);
+local UF = E:GetModule('UnitFrames');
+
+-- mouseover classcolor
+local function HoverClassColor(self, frame, db)
+    if frame.isMouseOverHooked then return; end
+    local health = frame.Health
+    frame:HookScript("OnEnter", function(self)
+        if (not UnitIsConnected(self.unit) or UnitIsDead(self.unit) or UnitIsGhost(self.unit)) then return; end
+        local hover = RAID_CLASS_COLORS[select(2, UnitClass(self.unit))]
+        if (not hover) then return; end
+        health:SetStatusBarColor( hover.r, hover.g, hover.b )
+        health.colorClass = true
+    end)
+    frame:HookScript("OnLeave", function(self)
+        if (not UnitIsConnected(self.unit) or UnitIsDead(self.unit) or UnitIsGhost(self.unit)) then return; end
+        local r, g, b = ElvUF.ColorGradient(UnitHealth(self.unit), UnitHealthMax(self.unit), 4.5, 0.1, 0.1, 0.6, 0.3, 0.3, 0.2, 0.2, 0.2)
+        health:SetStatusBarColor(r, g, b)
+        health.colorClass = false
+    end)
+    frame.isMouseOverHooked = true
+end
+hooksecurefunc(UF, 'Update_RaidFrames', HoverClassColor)
