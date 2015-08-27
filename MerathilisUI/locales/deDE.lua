55ed6c67caad8dace2e49de24ec4d039773e9f7a
Merathilis <neo0608@gmail.com>
2015-08-27 11:46:10 +0200

    locales

--- MerathilisUI/locales/deDE.lua
+++ MerathilisUI/locales/deDE.lua
@@ -3,3 +3,8 @@ local AceLocale = LibStub:GetLibrary('AceLocale-3.0');
 local L = AceLocale:NewLocale('ElvUI', 'deDE');
 if not L then return; end
 
+L[".:: Welcome to MerathilisUI v"] = "Willkommen zu MerathilisUI v"
+L[" ::.\nPress OK if you want to apply my settings."] = "::.\nDrücke OK wenn du meine Einstellungen anwenden möchtest."
+L['OK'] = true
+L['No thanks'] = "Nein danke"
+
--- MerathilisUI/locales/enUS.lua
+++ MerathilisUI/locales/enUS.lua
@@ -3,3 +3,8 @@ local AceLocale = LibStub:GetLibrary('AceLocale-3.0');
 local L = AceLocale:NewLocale('ElvUI', 'enUS');
 if not L then return; end
 
+L[".:: Welcome to MerathilisUI v"] = true
+L[" ::.\nPress OK if you want to apply my settings."] = true
+L['OK'] = true
+L['No thanks'] = true
+
