a55d57799b8591ea0876d5525138cad20d40fd5f
Merathilis <neo0608@gmail.com>
2015-09-06 23:02:06 +0200

    Add an Function to Create an Profile, not yet enable (found in ElvUI_Kkthnx)

--- MerathilisUI/core/core.lua
+++ MerathilisUI/core/core.lua
@@ -2,8 +2,10 @@ local E, L, V, P, G, _ = unpack(ElvUI);
 local MER = E:NewModule('MerathilisUI', "AceConsole-3.0");
 local LSM = LibStub('LibSharedMedia-3.0');
 local EP = LibStub('LibElvUIPlugin-1.0');
+--local tinsert, pairs = tinsert, pairs
 local addon, ns = ...
 
+--E.Profiles = {}
 MER.Title = string.format('|cff00c0fa%s |r', 'MerathilisUI') -- maybe for the Future
 MER.Version = GetAddOnMetadata('MerathilisUI', 'Version') -- with this we get the addon version from toc file
 
@@ -28,6 +30,82 @@ function MER:RegisterMerMedia()
 	E['media'].MuiOnePixel = LSM:Fetch('statusbar', 'MerathilisOnePixel')
 end
 
+--[[local function CreateProfileEntry(name, profile, ace3Compatible, getPath, addon)
+	return {
+		Name = name,
+		Profile = profile,
+		Ace3 = ace3Compatible,
+		GetPath = getPath,
+		AddonName = addon
+	}
+end
+
+function C:IsInstalled()
+	return C.profiles[CharacterProfile].installed
+end
+
+function C:IsProfileInstalled(Profile)
+	return C.profiles[CharacterProfile][Profile]
+end
+
+function C:SetProfileState(Profile, State)
+	C.profiles[CharacterProfile][Profile] = State
+end
+
+function C:IsDeclined()
+	return C.profiles[CharacterProfile].declined
+end
+
+function C:SetInstallState(installed)
+	C.profiles[CharacterProfile].installed = installed
+end
+
+function C:SetDeclinedState(declined)
+	C.profiles[CharacterProfile].declined = declined
+end
+
+function E:RegisterProfile(AddonName, AddonDB, Profile, Ace3Compatible, GetPath)
+	LoadAddOn(AddonName)
+	if not IsAddOnLoaded(AddonName) then return end
+	tinsert(E.Profiles, CreateProfileEntry(AddonDB, Profile, Ace3Compatible, GetPath, AddonName))
+end
+
+function E:Install(force)
+	PlaySoundFile(InstallSound)
+	local backups = {}
+	for _, entry in pairs(E.Profiles) do
+		C:SetProfileState(entry.Name, true)
+		if entry.Ace3 then
+			if not force then
+				tinsert(backups, CreateProfileEntry(entry.Name, _G[entry.Name].profileKeys[CharacterProfile], true))
+			end
+			if not _G[entry.Name].profiles[E.Name] or force then
+				_G[entry.Name].profiles[E.Name] = entry.Profile
+			end
+			_G[entry.Name].profileKeys[CharacterProfile] = E.Name
+		else
+			if not force and not entry.GetPath then
+				tinsert(backups, CreateProfileEntry(entry.Name, _G[entry.Name], false))
+			end
+			if entry.GetPath then
+				local parent = entry.GetPath()
+				for i, v in pairs(entry.Profile) do
+					parent[i] = v
+				end
+			else
+				_G[entry.Name] = entry.Profile
+			end
+		end
+	end
+	C.Backups = backups
+	C:SetInstallState(true)
+	if IsAddOnLoaded('ElvUI') then
+		ElvUI[1]:UpdateMedia()
+		ElvUI[1]:UpdateAll(true)
+		E:PostInstall()
+	end
+end
+]]--
 function MER:Initialize()
 	self:RegisterMerMedia()
 	--self:LoadCommands() -- Run the register commands function
