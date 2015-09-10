35244aa292b3afeb066bc6b01521b04bd1307c0d
Merathilis <neo0608@gmail.com>
2015-09-10 10:03:36 +0200

    adjust the afk logo and version to fit BenikUI

--- MerathilisUI/modules/misc/afk.lua
+++ MerathilisUI/modules/misc/afk.lua
@@ -1,28 +1,28 @@
 local E, L, V, P, G, _ = unpack(ElvUI); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB, Localize Underscore
-local AFK = E:GetModule('AFK')
-local LSM = LibStub('LibSharedMedia-3.0')
+if not IsAddOnLoaded("ElvUI_BenikUI") then return end;
+
 local MER = E:GetModule('MerathilisUI');
+local AFK = E:GetModule('AFK')
 
 local SPACING = (E.PixelMode and 1 or 5)
 
 local classColor = RAID_CLASS_COLORS[E.myclass]
 
 AFK.InitializeMerAfk = AFK.Initialize
-if IsAddOnLoaded("ElvUI_BenikUI") then
-	function AFK:Initialize()
-		self:InitializeMerAfk()
-		
-		-- MerathilisUI Name
-		self.AFKMode.top.merathilisui = self.AFKMode.top:CreateFontString(nil, 'OVERLAY')
-		self.AFKMode.top.merathilisui:FontTemplate(nil, 24, 'OUTLINE')
-		self.AFKMode.top.merathilisui:SetText("MerathilisUI")
-		self.AFKMode.top.merathilisui:SetPoint("CENTER", self.AFKMode.top, "CENTER", 0, -120)
-		self.AFKMode.top.merathilisui:SetTextColor(classColor.r, classColor.g, classColor.b)
-		-- Version
-		self.AFKMode.top.btext = self.AFKMode.top:CreateFontString(nil, 'OVERLAY')
-		self.AFKMode.top.btext:FontTemplate(nil, 14)
-		self.AFKMode.top.btext:SetText(format("v%s", MER.Version))
-		self.AFKMode.top.btext:SetPoint("TOP", self.AFKMode.top.merathilisui, "BOTTOM")
-		self.AFKMode.top.btext:SetTextColor(1, 1, 1)
-	end
+function AFK:Initialize()
+	self:InitializeMerAfk()
+	
+	-- MerathilisUI Name
+	self.AFKMode.bottom.merathilisui = self.AFKMode.top:CreateFontString(nil, 'OVERLAY')
+	self.AFKMode.bottom.merathilisui:FontTemplate(nil, 24)
+	self.AFKMode.bottom.merathilisui:SetText("MerathilisUI")
+	self.AFKMode.bottom.merathilisui:SetPoint("LEFT", self.AFKMode.bottom, "LEFT", 130, 8)
+	self.AFKMode.bottom.merathilisui:SetTextColor(classColor.r, classColor.g, classColor.b)
+	-- Version
+	self.AFKMode.bottom.btext = self.AFKMode.top:CreateFontString(nil, 'OVERLAY')
+	self.AFKMode.bottom.btext:FontTemplate(nil, 10)
+	self.AFKMode.bottom.btext:SetText(format("v%s", MER.Version))
+	self.AFKMode.bottom.btext:SetPoint("TOP", self.AFKMode.bottom.merathilisui, "BOTTOM")
+	self.AFKMode.bottom.btext:SetTextColor(0.7, 0.7, 0.7)
+	--Hide BenikUI
 end
