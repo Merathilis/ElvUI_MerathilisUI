5d450319030465c4abfb52b8f41e9fe4fa3ba7f4
Merathilis <neo0608@gmail.com>
2015-09-11 11:27:53 +0200

    couldn't test this atm

--- MerathilisUI/modules/misc/misc.lua
+++ MerathilisUI/modules/misc/misc.lua
@@ -2,37 +2,35 @@ local E, L, V, P, G, _ = unpack(ElvUI);
 local MER = E:GetModule('MerathilisUI');
 
 -- Automatic achievement screenshot
-if E.db.Merathilis.Screenshot then
-	if IsAddOnLoaded('ElvUI') then
-		local function TakeScreen(delay, func, ...)
-			local waitTable = {}
-			local waitFrame = CreateFrame("Frame", "WaitFrame", UIParent)
-			waitFrame:SetScript("onUpdate", function (self, elapse)
-				local count = #waitTable
-				local i = 1
-				while (i <= count) do
-					local waitRecord = tremove(waitTable, i)
-					local d = tremove(waitRecord, 1)
-					local f = tremove(waitRecord, 1)
-					local p = tremove(waitRecord, 1)
-					if d > elapse then
-						tinsert(waitTable, i, {d-elapse, f, p})
-						i = i + 1
-					else
-						count = count - 1
-						f(unpack(p))
-					end
-				end
-			end)
-			tinsert(waitTable, {delay, func, {...}})
+local function TakeScreen(delay, func, ...)
+	if not IsAddOnLoaded("ElvUI") and not E.db.Merathilis.Screenshot then return end
+	local waitTable = {}
+	local waitFrame = CreateFrame("Frame", "WaitFrame", UIParent)
+	waitFrame:SetScript("onUpdate", function (self, elapse)
+		local count = #waitTable
+		local i = 1
+		while (i <= count) do
+			local waitRecord = tremove(waitTable, i)
+			local d = tremove(waitRecord, 1)
+			local f = tremove(waitRecord, 1)
+			local p = tremove(waitRecord, 1)
+			if d > elapse then
+				tinsert(waitTable, i, {d-elapse, f, p})
+				i = i + 1
+			else
+				count = count - 1
+				f(unpack(p))
+			end
 		end
-		
-		local function OnEvent(...) TakeScreen(1, Screenshot) end
-		
-		local frame = CreateFrame("Frame")
-		frame:RegisterEvent("ACHIEVEMENT_EARNED")
-		frame:SetScript("OnEvent", OnEvent)
-	end
+	end)
+	tinsert(waitTable, {delay, func, {...}})
+end
+
+local function OnEvent(...) TakeScreen(1, Screenshot) end
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("ACHIEVEMENT_EARNED")
+frame:SetScript("OnEvent", OnEvent)
 end
 
 -- Force readycheck warning
