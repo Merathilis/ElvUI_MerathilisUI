3260a1fbe01f37ff508cc85718da1330aac2d78a
Merathilis <neo0608@gmail.com>
2015-09-14 13:40:04 +0200

    i think this works

--- MerathilisUI/modules/unitframes/unithover.lua
+++ MerathilisUI/modules/unitframes/unithover.lua
@@ -2,25 +2,33 @@ local E, L, V, P, G, _ = unpack(ElvUI);
 local MER = E:GetModule('MerathilisUI');
 local UF = E:GetModule('UnitFrames');
 
-if not E.db.Merathilis.HoverClassColor then return end
-
 -- mouseover classcolor
 local function HoverClassColor(self, frame, db)
-    if frame.isMouseOverHooked then return; end
-    local health = frame.Health
-    frame:HookScript("OnEnter", function(self)
-        if (not UnitIsConnected(self.unit) or UnitIsDead(self.unit) or UnitIsGhost(self.unit)) then return; end
-        local hover = RAID_CLASS_COLORS[select(2, UnitClass(self.unit))]
-        if (not hover) then return; end
-        health:SetStatusBarColor( hover.r, hover.g, hover.b )
-        health.colorClass = true
-    end)
-    frame:HookScript("OnLeave", function(self)
-        if (not UnitIsConnected(self.unit) or UnitIsDead(self.unit) or UnitIsGhost(self.unit)) then return; end
-        local r, g, b = ElvUF.ColorGradient(UnitHealth(self.unit), UnitHealthMax(self.unit), 4.5, 0.1, 0.1, 0.6, 0.3, 0.3, 0.2, 0.2, 0.2)
-        health:SetStatusBarColor(r, g, b)
-        health.colorClass = false
-    end)
-    frame.isMouseOverHooked = true
+	if not E.db.Merathilis.HoverClassColor then return end
+	if frame.isMouseOverHooked then return; end
+	local health = frame.Health
+	frame:HookScript("OnEnter", function(self)
+		if (not UnitIsConnected(self.unit) or UnitIsDead(self.unit) or UnitIsGhost(self.unit)) then return; end
+		local hover = RAID_CLASS_COLORS[select(2, UnitClass(self.unit))]
+		if (not hover) then return; end
+		health:SetStatusBarColor( hover.r, hover.g, hover.b )
+		health.colorClass = true
+	end)
+	frame:HookScript("OnLeave", function(self)
+		if (not UnitIsConnected(self.unit) or UnitIsDead(self.unit) or UnitIsGhost(self.unit)) then return; end
+		local r, g, b = ElvUF.ColorGradient(UnitHealth(self.unit), UnitHealthMax(self.unit), 4.5, 0.1, 0.1, 0.6, 0.3, 0.3, 0.2, 0.2, 0.2)
+		health:SetStatusBarColor(r, g, b)
+		health.colorClass = false
+	end)
+	frame.isMouseOverHooked = true
 end
 hooksecurefunc(UF, 'Update_RaidFrames', HoverClassColor)
+
+local f = CreateFrame("Frame")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:SetScript("OnEvent",function(self, event)
+	if event == "PLAYER_ENTERING_WORLD" then
+		HoverClassColor()
+		f:UnregisterEvent("PLAYER_ENTERING_WORLD")
+	end
+end)
