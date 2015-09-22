2a39065b600cdbb4ff70121d710afc4c5921eba5
Merathilis <neo0608@gmail.com>
2015-09-22 14:59:28 +0200

    add an IsAddOnLoaded check for rarealert.lua

--- MerathilisUI/modules/misc/rarealert.lua
+++ MerathilisUI/modules/misc/rarealert.lua
@@ -2,6 +2,8 @@ local E, L, V, P, G, _ = unpack(ElvUI); --Import: Engine, Locales, PrivateDB, Pr
 local MER = E:GetModule('MerathilisUI');
 
 -- Credits to Haleth (RareAlert)
+if IsAddOnLoaded("RareAlert") then return end
+
 local blacklist = {
 	[971] = true, -- Alliance garrison
 	[976] = true, -- Horde garrison
@@ -10,9 +12,9 @@ local blacklist = {
 local f = CreateFrame("Frame")
 f:RegisterEvent("VIGNETTE_ADDED")
 f:SetScript("OnEvent", function()
-	if blacklist[GetCurrentMapAreaID()] then return end
-	
 	if E.db.Merathilis.RareAlert then
+		if blacklist[GetCurrentMapAreaID()] then return end
+
 		PlaySoundFile("Interface\\AddOns\\MerathilisUI\\media\\sounds\\warning.ogg")
 		RaidNotice_AddMessage(RaidWarningFrame, L["Rare spotted!"], ChatTypeInfo["RAID_WARNING"])
 	end
