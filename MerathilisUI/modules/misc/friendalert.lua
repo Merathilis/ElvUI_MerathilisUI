1c1de962e834bdef4574ab955a888eb80daaf050
Merathilis <neo0608@gmail.com>
2015-10-24 17:43:31 +0200

    FriendAlert: Rewrite the Code (it works at least, don't know if this is the correct Way) :D

--- MerathilisUI/modules/misc/friendalert.lua
+++ MerathilisUI/modules/misc/friendalert.lua
@@ -8,9 +8,9 @@ if IsAddOnLoaded("BattleNetFriendAlert") then return; end
 if E.db.muiMisc == nil then E.db.muiMisc = {} end -- prevent a nil Error.
 if E.db.muiMisc.FriendAlert == false then return; end
 
-MERA.interval = 3
-MERA.friends = {}
-MERA.icons = {
+local interval = 3
+local friends = {}
+local icons = {
 	["Friend"] = "|TInterface\\FriendsFrame\\UI-Toast-FriendOnlineIcon:16:16:0:0:32:32:2:30:2:30|t",
 	["App"] = "|TInterface\\CHATFRAME\\UI-ChatIcon-Battlenet:14|t",
 	["WoW"] = "|TInterface\\CHATFRAME\\UI-ChatIcon-WoW:14|t",
@@ -20,32 +20,32 @@ MERA.icons = {
 	["S2"] = "|TInterface\\CHATFRAME\\UI-ChatIcon-SC2:14|t",
 };
 
-MERA.BNPlayerLink = function(presenceName, presenceID)
+local function BNPlayerLink(presenceName, presenceID)
 	return string.format("|HBNplayer:%s:%s|h[%s]|h", presenceName, presenceID, presenceName)
 end
 
-MERA.ScanFriends = function()
+local function ScanFriends()
 	if BNConnected() then
 		for index = 1, BNGetNumFriends() do
 			local presenceID, presenceName, _, _, characterName, _, game = BNGetFriendInfo(index)
-			if game and MERA.friends[presenceID] and MERA.friends[presenceID]["game"] then
-				if game ~= MERA.friends[presenceID]["game"] then
+			if game and friends[presenceID] and friends[presenceID]["game"] then
+				if game ~= friends[presenceID]["game"] then
 					if game == "App" then
-						print(BATTLENET_FONT_COLOR_CODE .. MERA.icons["Friend"] .. string.format(L["%s stopped playing (%sIn Battle.net)."], MERA.BNPlayerLink(presenceName, presenceID), MERA.icons[game]) .. FONT_COLOR_CODE_CLOSE)
+						print(BATTLENET_FONT_COLOR_CODE .. icons["Friend"] .. string.format(L["%s stopped playing (%sIn Battle.net)."], BNPlayerLink(presenceName, presenceID), icons[game]) .. FONT_COLOR_CODE_CLOSE)
 					else
-						print(BATTLENET_FONT_COLOR_CODE .. MERA.icons["Friend"] .. string.format(L["%s is now playing (%s%s)."], MERA.BNPlayerLink(presenceName, presenceID), MERA.icons[game], characterName) .. FONT_COLOR_CODE_CLOSE)
+						print(BATTLENET_FONT_COLOR_CODE .. icons["Friend"] .. string.format(L["%s is now playing (%s%s)."], BNPlayerLink(presenceName, presenceID), icons[game], characterName) .. FONT_COLOR_CODE_CLOSE)
 						PlaySound("UI_BnetToast")
 					end
 				end
 			end
 			-- Record latest friend info
-			MERA.friends[presenceID] = {["game"] = game}
+			friends[presenceID] = {["game"] = game}
 		end
 	end
 	-- Scan again in interval seconds
-	C_Timer.After(MERA.interval, MERA.ScanFriends)
+	C_Timer.After(interval, ScanFriends)
 end
 
 local f = CreateFrame("Frame", "BNFAEventsFrame", E.UIParent)
-f:SetScript("OnEvent", function() C_Timer.After(MERA.interval, MERA.ScanFriends); end)
+f:SetScript("OnEvent", function() C_Timer.After(interval, ScanFriends); end)
 f:RegisterEvent("PLAYER_LOGIN")
