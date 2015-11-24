b818462356f6cdfc0827362f024ae73477c6ffbf
Merathilis <neo0608@gmail.com>
2015-11-24 18:31:51 +0100

    Chat: Add SLE ChatIcons for my Toons. Thanks Darth Predator!

---
+++ MerathilisUI/modules/chat/sle_chaticon.lua
@@ -0,0 +1,20 @@
+local E, L, V, P, G = unpack(ElvUI);
+local SLE = ElvUI_SLE[1]
+
+if not IsAddOnLoaded("ElvUI_SLE") then return end
+
+-- Cache global variables
+local format = format
+
+local blizzPath = [[|TInterface\ICONS\]]
+local toon = blizzPath..[[%s:12:12:0:0:64:64:4:60:4:60|t]]
+
+-- Check the Table (SLE.SpecialChatIcons)
+if not SLE.SpecialChatIcons["EU"]["Shattrath"] then SLE.SpecialChatIcons["EU"]["Shattrath"] = {} end
+if not SLE.SpecialChatIcons["EU"]["Garrosh"] then SLE.SpecialChatIcons["EU"]["Garrosh"] = {} end
+
+-- Insert it to the Table
+SLE.SpecialChatIcons["EU"]["Shattrath"]["Merathilis"] = format(toon, "inv_helmet_challengedruid_d_01")
+SLE.SpecialChatIcons["EU"]["Shattrath"]["Damará"] = format(toon, "inv_helmet_plate_challengepaladin_d_01")
+SLE.SpecialChatIcons["EU"]["Shattrath"]["Melisendra"] = format(toon, "inv_helm_cloth_challengemage_d_01")
+SLE.SpecialChatIcons["EU"]["Garrosh"]["Jahzzy"] = format(toon, "inv_helm_plate_challengedeathknight_d_01")
