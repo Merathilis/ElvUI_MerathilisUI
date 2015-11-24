a31c245375c5d83ec9ba2cb6bfcc88f8c9f7faf2
Merathilis <neo0608@gmail.com>
2015-11-24 18:34:35 +0100

    nothing

--- MerathilisUI/modules/chat/sle_chaticon.lua
+++ MerathilisUI/modules/chat/sle_chaticon.lua
@@ -9,11 +9,10 @@ local format = format
 local blizzPath = [[|TInterface\ICONS\]]
 local toon = blizzPath..[[%s:12:12:0:0:64:64:4:60:4:60|t]]
 
--- Check the Table (SLE.SpecialChatIcons)
+-- Check the Table (SLE.SpecialChatIcons) and insert it.
 if not SLE.SpecialChatIcons["EU"]["Shattrath"] then SLE.SpecialChatIcons["EU"]["Shattrath"] = {} end
 if not SLE.SpecialChatIcons["EU"]["Garrosh"] then SLE.SpecialChatIcons["EU"]["Garrosh"] = {} end
 
--- Insert it to the Table
 SLE.SpecialChatIcons["EU"]["Shattrath"]["Merathilis"] = format(toon, "inv_helmet_challengedruid_d_01")
 SLE.SpecialChatIcons["EU"]["Shattrath"]["Damará"] = format(toon, "inv_helmet_plate_challengepaladin_d_01")
 SLE.SpecialChatIcons["EU"]["Shattrath"]["Melisendra"] = format(toon, "inv_helm_cloth_challengemage_d_01")
