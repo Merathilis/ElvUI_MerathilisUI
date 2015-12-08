2d35492eb29a694bd5cd078efacbcaccdc559f23
Merathilis <neo0608@gmail.com>
2015-12-08 10:14:44 +0100

    Config: Move the datatext options to an own group

--- MerathilisUI/core/config.lua
+++ MerathilisUI/core/config.lua
@@ -67,10 +67,12 @@ local function AddOptions()
 					},
 				},
 			},
-			spacer2 = {
-				order = 6,
-				type = 'header',
-				name = '',
+			config = {
+				order = 20,
+				type = 'group',
+				name = L['Options'],
+				childGroups = 'tab',
+				args = {}
 			},
 		},
 	}
@@ -156,58 +158,65 @@ local function muiSkins()
 end
 tinsert(E.MerConfig, muiSkins)
 
-local function muiSystemDT()
-	E.Options.args.mui.args.systemdt = {
+local function muiDatatexts()
+	E.Options.args.mui.args.config.args.datatexts = {
 		order = 11,
 		type = 'group',
-		name = L["System Datatext"],
-		guiInline = true,
-		get = function(info) return E.db.muiSystemDT[info[#info]] end,
-		set = function(info, value) E.db.muiSystemDT[info[#info]] = value; end,
+		name = L["DataTexts"],
 		args = {
-			maxAddons = {
-				type = "range",
+			muiSystemDT = {
 				order = 1,
-				name = L["Max Addons"],
-				desc = L["Maximum number of addons to show in the tooltip."],
-				min = 1, max = 50, step = 1,
-			},
-			announceFreed = {
-				type = "toggle",
-				order = 2,
-				name = L["Announce Freed"],
-				desc = L["Announce how much memory was freed by the garbage collection."],
-			},
-			showFPS = {
-				type = "toggle",
-				order = 3,
-				name = L["Show FPS"],
-				desc = L["Show FPS on the datatext."],
-			},
-			showMemory = {
-				type = "toggle",
-				order = 4,
-				name = L["Show Memory"],
-				desc = L["Show total addon memory on the datatext."]
-			},
-			showMS = {
-				type = "toggle",
-				order = 5,
-				name = L["Show Latency"],
-				desc = L["Show latency on the datatext."],
-			},
-			latency = {
-				type = "select",
-				order = 6,
-				name = L["Latency Type"],
-				desc = L["Display world or home latency on the datatext. Home latency refers to your realm server. World latency refers to the current world server."],
-				disabled = function() return not E.db.muiSystemDT.showMS end,
-				values = {
-					["home"] = L["Home"],
-					["world"] = L["World"],
+				type = 'group',
+				name = L["System Datatext"],
+				guiInline = true,
+				get = function(info) return E.db.muiSystemDT[info[#info]] end,
+				set = function(info, value) E.db.muiSystemDT[info[#info]] = value; end,
+				args = {
+					maxAddons = {
+						type = "range",
+						order = 1,
+						name = L["Max Addons"],
+						desc = L["Maximum number of addons to show in the tooltip."],
+						min = 1, max = 50, step = 1,
+					},
+					announceFreed = {
+						type = "toggle",
+						order = 2,
+						name = L["Announce Freed"],
+						desc = L["Announce how much memory was freed by the garbage collection."],
+					},
+					showFPS = {
+						type = "toggle",
+						order = 3,
+						name = L["Show FPS"],
+						desc = L["Show FPS on the datatext."],
+					},
+					showMemory = {
+						type = "toggle",
+						order = 4,
+						name = L["Show Memory"],
+						desc = L["Show total addon memory on the datatext."]
+					},
+					showMS = {
+						type = "toggle",
+						order = 5,
+						name = L["Show Latency"],
+						desc = L["Show latency on the datatext."],
+					},
+					latency = {
+						type = "select",
+						order = 6,
+						name = L["Latency Type"],
+						desc = L["Display world or home latency on the datatext. Home latency refers to your realm server. World latency refers to the current world server."],
+						disabled = function() return not E.db.muiSystemDT.showMS end,
+						values = {
+							["home"] = L["Home"],
+							["world"] = L["World"],
+						},
+					},
 				},
 			},
 		},
 	}
 end
-tinsert(E.MerConfig, muiSystemDT)
+tinsert(E.MerConfig, muiDatatexts)
