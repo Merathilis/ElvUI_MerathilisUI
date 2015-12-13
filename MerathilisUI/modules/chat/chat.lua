7d8862d804f0cff17930658035076ad3e8a37ef3
Merathilis <neo0608@gmail.com>
2015-12-13 17:45:55 +0100

    Chat: Add a new Module "muiChat". For now, it only remove the Server Names in the System messages.

---
+++ MerathilisUI/modules/chat/chat.lua
@@ -0,0 +1,24 @@
+local E, L, V, P, G = unpack(ElvUI);
+local MER = E:GetModule('MerathilisUI')
+local MERC = E:NewModule('MuiChat')
+local CH = E:GetModule('Chat')
+
+-- Cache global variables
+local GetRealmName = GetRealmName
+local ChatFrame_AddMessageEventFilter = ChatFrame_AddMessageEventFilter
+
+function MERC:RemoveCurrentRealmName(self, msg, author, ...)
+	local realmName = string.gsub(GetRealmName(), " ", "")
+	
+	if msg and msg:find("-" .. realmName) then
+		return false, gsub(msg, "%-"..realmName, ""), author, ...
+	end
+end
+
+function MERC:InitializeChat()
+	-- Remove the Realm Name from system messages
+	ChatFrame_AddMessageEventFilter("CHAT_MSG_SYSTEM", MERC.RemoveCurrentRealmName)
+end
+hooksecurefunc(CH, "Initialize", MERC.InitializeChat)
+
+E:RegisterModule(MERC:GetName())
