bb66b4b9ba2bce21afeb861db8cd08d360be3fc7
Merathilis <neo0608@gmail.com>
2016-02-12 10:08:09 +0100

    Options: Create a Config Button for the future, maybe!? Options: Remove double config... wtf!?

--- MerathilisUI/core/config.lua
+++ MerathilisUI/core/config.lua
@@ -3,8 +3,30 @@ local MER = E:GetModule('MerathilisUI');
 
 if E.db.mui == nil then E.db.mui = {} end
 local tinsert = table.insert
+local select, unpack = select, unpack
 
 local function AddOptions()
+	-- Config Button for the future. maybe?!
+	--[[local ACD = LibStub("AceConfigDialog-3.0-ElvUI")
+	MER.ACD = ACD
+	
+	local function CreateButton(number, text, ...)
+		local path = {}
+		local num = select("#", ...)
+		for i = 1, num do
+			local name = select(i, ...)
+			tinsert(path, #(path)+1, name)
+		end
+		local config = {
+			order = number,
+			type = 'execute',
+			name = text,
+			func = function() ACD:SelectGroup("ElvUI", "mui", unpack(path)) end,
+		}
+		return config
+	end]]
+	
+	-- Main options
 	E.Options.args.mui = {
 		order = 9001,
 		type = 'group',
@@ -94,63 +116,3 @@ local function AddOptions()
 	}
 end
 tinsert(E.MerConfig, AddOptions)
-
-local function muiMisc()
-	E.Options.args.mui.args.misc = {
-		order = 9,
-		type = 'group',
-		name = L['Misc'],
-		guiInline = true,
-		args = {
-			TooltipIcon = {
-				order = 1,
-				type = 'toggle',
-				name = L['Tooltip Icon'],
-				desc = L['Adds an Icon for Items/Spells/Achievement on the Tooltip'],
-				get = function(info) return E.db.muiMisc[ info[#info] ] end,
-				set = function(info, value) E.db.muiMisc[ info[#info] ] = value; end,
-			},
-			HideAlertFrame = {
-				order = 2,
-				type = 'toggle',
-				name = L['Garrison Alert Frame'],
-				desc = L['Hides the Garrison Alert Frame while in combat.'],
-				get = function(info) return E.db.muiMisc[ info[#info] ] end,
-				set = function(info, value) E.db.muiMisc[ info[#info] ] = value; E:StaticPopup_Show('PRIVATE_RL'); end,
-			},
-			MailInputbox = {
-				order = 3,
-				type = 'toggle',
-				name = L['Mail Inputbox Resize'],
-				desc = L['Resize the Mail Inputbox and move the shipping cost to the Bottom'],
-				get = function(info) return E.db.muiMisc[ info[#info] ] end,
-				set = function(info, value) E.db.muiMisc[ info[#info] ] = value; E:StaticPopup_Show('PRIVATE_RL'); end,
-			},
-			FriendAlert = {
-				order = 4,
-				type = 'toggle',
-				name = L['Battle.net Alert'],
-				desc = L['Shows a Chat notification if a Battle.net Friend switch Games or goes offline.'],
-				get = function(info) return E.db.muiMisc[ info[#info] ] end,
-				set = function(info, value) E.db.muiMisc[ info[#info] ] = value; E:StaticPopup_Show('PRIVATE_RL'); end,
-			},
-			moveBlizz = {
-				order = 5,
-				type = 'toggle',
-				name = L['moveBlizz'],
-				desc = L['Make some Blizzard Frames movable.'],
-				get = function(info) return E.db.muiMisc[ info[#info] ] end,
-				set = function(info, value) E.db.muiMisc[ info[#info] ] = value; E:StaticPopup_Show('PRIVATE_RL'); end,
-			},
-			enchantScroll = {
-				order = 6,
-				type = 'toggle',
-				name = L['Enchant on Scroll'],
-				desc = L['Place a button in the Enchant Trade Window, allow you to automatically place a enchant on a scroll.'],
-				get = function(info) return E.db.muiMisc[ info[#info] ] end,
-				set = function(info, value) E.db.muiMisc[ info[#info] ] = value; E:StaticPopup_Show('PRIVATE_RL'); end,
-			},
-		},
-	}
-end
-tinsert(E.MerConfig, muiMisc)
