96d64c065014d6b122d0ceb947ad69ef0f5fc951
Merathilis <neo0608@gmail.com>
2016-03-31 10:58:01 +0200

    Unitframes: Added a new tag [num:targeting] it will display a number which indicates how many people in your group is targeting the unit. (Credits Blazeflack - ElvUI_CustomTags)

--- ElvUI_MerathilisUI/modules/unitframes/tags.lua
+++ ElvUI_MerathilisUI/modules/unitframes/tags.lua
@@ -4,9 +4,11 @@ local ElvUF = ElvUI.oUF
 
 -- Cache global variables
 -- WoW API / Variables
-local UnitIsDead, UnitIsGhost, UnitIsConnected = UnitIsDead, UnitIsGhost, UnitIsConnected
-local UnitHealth, UnitHealthMax = UnitHealth, UnitHealthMax
 local DEAD = DEAD
+local GetNumGroupMembers = GetNumGroupMembers
+local IsInGroup, IsInRaid = IsInGroup, IsInRaid
+local UnitIsDead, UnitIsGhost, UnitIsConnected, UnitIsUnit = UnitIsDead, UnitIsGhost, UnitIsConnected, UnitIsUnit
+local UnitHealth, UnitHealthMax = UnitHealth, UnitHealthMax
 
 ElvUF.Tags.Events['health:percent_short'] = 'UNIT_HEALTH_FREQUENT UNIT_MAXHEALTH UNIT_CONNECTION PLAYER_FLAGS_CHANGED'
 ElvUF.Tags.Methods['health:percent_short'] = function(unit)
@@ -17,4 +19,19 @@ ElvUF.Tags.Methods['health:percent_short'] = function(unit)
 	else
 		return MER:GetFormattedText('PERCENT_SHORT', UnitHealth(unit), UnitHealthMax(unit))
 	end
+end
+
+ElvUF.Tags.Events['num:targeting'] = "UNIT_TARGET PLAYER_TARGET_CHANGED GROUP_ROSTER_UPDATE"
+ElvUF.Tags.Methods['num:targeting'] = function(unit)
+	if not IsInGroup() then return "" end
+
+	local targetedByNum = 0
+	for i = 1, GetNumGroupMembers() do
+		local groupUnit = (IsInRaid() and "raid"..i or "party"..i);
+		if (UnitIsUnit(groupUnit.."target", unit) and not UnitIsUnit(groupUnit, "player")) or UnitIsUnit("playertarget", unit) then
+			targetedByNum = targetedByNum + 1
+		end
+	end
+
+	return (targetedByNum > 0 and targetedByNum or "")
 end
