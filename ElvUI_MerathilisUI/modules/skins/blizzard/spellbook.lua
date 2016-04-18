9840f3e3389bd46001f528670495fe206ad048d6
Merathilis <neo0608@gmail.com>
2016-04-18 15:39:33 +0200

    Skins: Overhaul the SpellBookFrame (Code taken from AddOnSkins, a little bit atleast :P)

--- ElvUI_MerathilisUI/modules/skins/blizzard/spellbook.lua
+++ ElvUI_MerathilisUI/modules/skins/blizzard/spellbook.lua
@@ -9,11 +9,55 @@ local SpellBookFrame = _G["SpellBookFrame"]
 
 
 local function styleSpellBook()
-	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.spellbook ~= true or E.private.muiSkins.blizzard.spellbook ~= true then return; end
+	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.spellbook ~= true or E.private.muiSkins.blizzard.spellbook ~= true then return end
  
 	if SpellBookFrame.pagebackdrop then
 		SpellBookFrame.pagebackdrop:Hide()
 	end
+	
+	SpellBookPageText:SetTextColor(unpack(E.media.rgbvaluecolor))
+	SpellBookCoreAbilitiesFrame.SpecName:SetTextColor(unpack(E.media.rgbvaluecolor))
+	
+	local professionheaders = {
+		"PrimaryProfession1",
+		"PrimaryProfession2",
+		"SecondaryProfession1",
+		"SecondaryProfession2",
+		"SecondaryProfession3",
+		"SecondaryProfession4"
+	}
+	
+	for _, header in pairs(professionheaders) do
+		_G[header.."Missing"]:SetTextColor(1, 0.8, 0)
+		_G[header.."Missing"]:SetShadowColor(0, 0, 0)
+		_G[header.."Missing"]:SetShadowOffset(1, -1)
+		_G[header].missingText:SetTextColor(0.6, 0.6, 0.6)
+	end
+	
+	hooksecurefunc("SpellBook_UpdateCoreAbilitiesTab", function()
+		for i = 1, #SpellBookCoreAbilitiesFrame.Abilities do
+			local button = SpellBookCoreAbilitiesFrame.Abilities[i]
+			if button and button.isSkinned ~= true then
+
+				button.EmptySlot:SetAlpha(0)
+				button.ActiveTexture:SetAlpha(0)
+				button.FutureTexture:SetAlpha(0)
+
+				button.iconTexture:SetInside()
+
+				if button.FutureTexture:IsShown() then
+					button.iconTexture:SetDesaturated(true)
+					button.Name:SetTextColor(0.6, 0.6, 0.6)
+					button.InfoText:SetTextColor(0.6, 0.6, 0.6)
+				else
+					button.Name:SetTextColor(1, 0.82, 0)
+					button.InfoText:SetTextColor(0.8, 0.8, 0.8)
+				end
+				
+				button.isSkinned = true
+			end
+		end
+	end)
 end
 
 local f = CreateFrame("Frame")
