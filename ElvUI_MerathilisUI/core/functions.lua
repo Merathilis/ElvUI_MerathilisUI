e14199726ca4222023305732dd640d63bc85f7f9
Merathilis <neo0608@gmail.com>
2016-05-01 15:09:35 +0200

    Chat: Add a special Chat Icon to my toons -- All Credits belong to Darth Predator (ElvUI_SLE)

--- ElvUI_MerathilisUI/core/functions.lua
+++ ElvUI_MerathilisUI/core/functions.lua
@@ -4,7 +4,47 @@ local MER = E:GetModule('MerathilisUI');
 -- Cache global variables
 -- Lua functions
 local print = print
-local format = format
+local format = string.format
+
+local blizzPath = [[|TInterface\ICONS\]]
+local toon = blizzPath..[[%s:12:12:0:0:64:64:4:60:4:60|t]]
+local classTable = {
+	deathknight = blizzPath..[[ClassIcon_DeathKnight:16:16|t ]],
+	-- demonhunter = blizzPath..[[ClassIcon_DemonHunter:16:16|t ]],
+	druid = blizzPath..[[ClassIcon_Druid:16:16|t ]],
+	hunter = blizzPath..[[ClassIcon_Hunter:16:16|t ]],
+	mage = blizzPath..[[ClassIcon_Mage:16:16|t ]],
+	monk = blizzPath..[[ClassIcon_Monk:16:16|t ]],
+	paladin = blizzPath..[[ClassIcon_Paladin:16:16|t ]],
+	priest = blizzPath..[[ClassIcon_Priest:16:16|t ]],
+	rogue = blizzPath..[[ClassIcon_Rogue:16:16|t ]],
+	shaman = blizzPath..[[ClassIcon_Shaman:16:16|t ]],
+	warlock = blizzPath..[[ClassIcon_Warlock:16:16|t ]],
+	warrior = blizzPath..[[ClassIcon_Warrior:16:16|t ]],
+}
+
+MER.SpecialChatIcons = {
+	["EU"] = {
+		["Shattrath"] = {
+			["Asragoth"] = format(toon, "inv_cloth_challengewarlock_d_01helm"),
+			["Brítt"] = format(toon, "inv_helm_plate_challengewarrior_d_01"),
+			["Damará"] = format(toon, "inv_helmet_plate_challengepaladin_d_01"),
+			["Jazira"] = format(toon, "inv_helmet_cloth_challengepriest_d_01"),
+			["Jústice"] = format(toon, "inv_helmet_leather_challengerogue_d_01"),
+			["Merathilis"] = format(toon, "inv_helmet_challengedruid_d_01"),
+			["Merathilîs"] = format(toon, "inv_helmet_mail_challengeshaman_d_01"),
+			["Melisendra"] = format(toon, "inv_helm_cloth_challengemage_d_01"),
+			["Róhal"] = format(toon, "inv_helmet_mail_challengehunter_d_01"),
+		},
+		["Garrosh"] = {
+			["Jahzzy"] = format(toon, "inv_helm_plate_challengedeathknight_d_01"),
+		},
+	},
+	["US"] = {},
+	["CN"] = {},
+	["KR"] = {},
+	["TW"] = {},
+}
 
 function MER:MismatchText()
 	local text = format(L["MSG_MER_ELV_OUTDATED"], MER.ElvUIV, MER.ElvUIX)
@@ -25,3 +65,33 @@ function MER:unpackColor(color)
 end
 
 MER.NewSign = "|TInterface\\OptionsFrame\\UI-OptionsFrame-NewFeatureIcon:14:14|t"
+
+-- Trying to determine the region player is in, not entirely reliable cause based on a client not an actual region id
+local GetCurrentRegion = GetCurrentRegion
+function MER:GetRegion()
+	local rid = GetCurrentRegion()
+	local region = {
+		[1] = "US",
+		[2] = "KR",
+		[3] = "EU",
+		[4] = "TW",
+		[5] = "CN",
+	}
+	MER.region = region[rid]
+	if not MER.region then 
+		MER.region = format("An error happened. Your region is unknown. Realm: %s. RID: %s. Please report your realm name and the region you are playing in to |cffff7d0aMerathilisUI|r authors.", E.myrealm, rid)
+		MER:Print(MER.region)
+		MER.region = ""
+	end
+end
+
+-- Search in a table like {"arg1", "arg2", "arg3"}
+function MER:SimpleTable(table, item)
+	for i = 1, #table do
+		if table[i] == item then  
+			return true 
+		end
+	end
+
+	return false
+end
--- ElvUI_MerathilisUI/modules/chat/chat.lua
+++ ElvUI_MerathilisUI/modules/chat/chat.lua
@@ -1,15 +1,29 @@
-local E, L, V, P, G = unpack(ElvUI);
+local E, L, V, P, G, _ = unpack(ElvUI);
 local MER = E:GetModule('MerathilisUI')
-local MERC = E:NewModule('muiChat')
+local MERC = E:NewModule('muiChat', 'AceHook-3.0', 'AceEvent-3.0', 'AceTimer-3.0')
 local CH = E:GetModule('Chat')
 
 -- Cache global variables
 -- Lua functions
 local _G = _G
-local gsub = string.gsub
+local gsub, split, strlen = string.gsub, string.split, string.len
 -- WoW API / Variables
 local GetRealmName = GetRealmName
 local ChatFrame_AddMessageEventFilter = ChatFrame_AddMessageEventFilter
+
+local leader = [[|TInterface\GroupFrame\UI-Group-LeaderIcon:12:12|t]]
+local specialChatIcons
+local lfgChannels = {
+	"PARTY_LEADER",
+	"PARTY",
+	"RAID",
+	"RAID_LEADER",
+	"INSTANCE_CHAT",
+	"INSTANCE_CHAT_LEADER",
+	"RAID_WARNING",
+}
+MERC.lfgRolesTable = {}
+MERC.PlayerRealm = gsub(E.myrealm,'[%s%-]','')
 
 function MERC:RemoveCurrentRealmName(self, msg, author, ...)
 	local realmName = gsub(GetRealmName(), " ", "")
@@ -17,6 +31,53 @@ function MERC:RemoveCurrentRealmName(self, msg, author, ...)
 	if msg and msg:find("-" .. realmName) then
 		return false, gsub(msg, "%-"..realmName, ""), author, ...
 	end
+end
+
+function MERC:GetChatIcon(sender)
+	if not specialChatIcons then 
+		MER:GetRegion()
+		specialChatIcons = MER.SpecialChatIcons[MER.region]
+	end
+	local senderName, senderRealm
+	if sender then
+		senderName, senderRealm = split('-', sender)
+	else
+		senderName = E.myname
+	end
+	senderRealm = senderRealm or MERC.PlayerRealm
+	senderRealm = gsub(senderRealm, ' ', '')
+
+	if specialChatIcons[senderRealm] and specialChatIcons[senderRealm][senderName] then
+		return specialChatIcons[senderRealm][senderName]
+	end
+end
+
+function CH:GetPluginReplacementIcon(arg2, arg6, type)
+	local icon = ""
+	if arg6 and (strlen(arg6) > 0) then
+		if ( arg6 == "GM" ) then
+			--If it was a whisper, dispatch it to the GMChat addon.
+			if ( type == "WHISPER" ) then
+				return;
+			end
+			--Add Blizzard Icon, this was sent by a GM
+			icon = "|TInterface\\ChatFrame\\UI-ChatIcon-Blizz:12:20:0:0:32:16:4:28:0:16|t ";
+		elseif ( arg6 == "DEV" ) then
+			--Add Blizzard Icon, this was sent by a Dev
+			icon = "|TInterface\\ChatFrame\\UI-ChatIcon-Blizz:12:20:0:0:32:16:4:28:0:16|t ";
+		elseif ( arg6 == "DND" or arg6 == "AFK") then
+			icon = MERC:GetChatIcon(arg2).._G["CHAT_FLAG_"..arg6]
+		else
+			icon = _G["CHAT_FLAG_"..arg6];
+		end
+	else
+		icon = MERC:GetChatIcon(arg2)
+		if(MERC.lfgRolesTable[arg2] and MER:SimpleTable(lfgChannels, type)) then
+			icon = MERC.lfgRolesTable[arg2]..icon
+		end
+	end
+	if icon == "" then icon = nil end
+	return icon, true
 end
 
 _G.ERR_FRIEND_ONLINE_SS = "|Hplayer:%s|h[%s]|h "..L["has come |cff298F00online|r."]
