862d44bd02bf31dad6aef5bb03a287cab808c057
Merathilis <neo0608@gmail.com>
2016-05-06 15:31:55 +0200

    damn globals

--- ElvUI_MerathilisUI/core/functions.lua
+++ ElvUI_MerathilisUI/core/functions.lua
@@ -1,10 +1,35 @@
 local E, L, V, P, G = unpack(ElvUI);
 local MER = E:GetModule('MerathilisUI');
+local LSM = LibStub('LibSharedMedia-3.0');
 
 -- Cache global variables
 -- Lua functions
-local print = print
+local _G = _G
+local print, select = print, select
 local format = string.format
+-- WoW API / Variables
+local AchievementFrame = _G["AchievementFrame"]
+local AchievementFrame_LoadUI = _G["AchievementFrame_LoadUI"]
+local AchievementAlertFrame_ShowAlert = _G["AchievementAlertFrame_ShowAlert"]
+local AlertFrame_AnimateIn = _G["AlertFrame_AnimateIn"]
+local AlertFrame_FixAnchors = _G["AlertFrame_FixAnchors"]
+local ChallengeModeAlertFrame_ShowAlert = _G["ChallengeModeAlertFrame_ShowAlert"]
+local CreateFrame = CreateFrame
+local CriteriaAlertFrame_ShowAlert = _G["CriteriaAlertFrame_ShowAlert"]
+local GarrisonBuildingAlertFrame_ShowAlert = _G["GarrisonBuildingAlertFrame_ShowAlert"]
+local GarrisonMissionAlertFrame = _G["GarrisonMissionAlertFrame"]
+local GetInventoryItemLink = GetInventoryItemLink
+local GetItemInfo = GetItemInfo
+local GetSpellInfo = GetSpellInfo
+local GuildChallengeAlertFrame_ShowAlert = _G["GuildChallengeAlertFrame_ShowAlert"]
+local LootUpgradeFrame_ShowAlert = _G["LootUpgradeFrame_ShowAlert"]
+local LootWonAlertFrame_ShowAlert = _G["LootWonAlertFrame_ShowAlert"]
+local MoneyWonAlertFrame_ShowAlert = _G["MoneyWonAlertFrame_ShowAlert"]
+local PlaySound = PlaySound
+local ScenarioAlertFrame1 = _G["ScenarioAlertFrame1"]
+local StorePurchaseAlertFrame_ShowAlert = _G["StorePurchaseAlertFrame_ShowAlert"]
+
+-- GLOBALS: SLASH_TEST_ACHIEVEMENT1
 
 MER.NewSign = "|TInterface\\OptionsFrame\\UI-OptionsFrame-NewFeatureIcon:14:14|t"
 
@@ -74,12 +99,13 @@ local function CreateSoftGlow(f)
 		edgeFile = LSM:Fetch('border', 'ElvUI GlowBorder'), edgeSize = E:Scale(3),
 		insets = {left = E:Scale(5), right = E:Scale(5), top = E:Scale(5), bottom = E:Scale(5)},
 	})
-	sglow:SetBackdropColor(BUI:unpackColor(E.db.general.valuecolor), 0)
-	sglow:SetBackdropBorderColor(BUI:unpackColor(E.db.general.valuecolor), 0.4)
+	sglow:SetBackdropColor(MER:unpackColor(E.db.general.valuecolor), 0)
+	sglow:SetBackdropBorderColor(MER:unpackColor(E.db.general.valuecolor), 0.4)
 	f.sglow = sglow
 end
 
 -- Test Blizz Alerts / AlertFrames
+SLASH_TEST_ACHIEVEMENT1 = "/ta"
 SlashCmdList.TEST_ACHIEVEMENT = function()
 	PlaySound("LFG_Rewards")
 	if not AchievementFrame then
@@ -100,4 +126,3 @@ SlashCmdList.TEST_ACHIEVEMENT = function()
 	-- AlertFrame_AnimateIn(GarrisonFollowerAlertFrame)
 	AlertFrame_FixAnchors()
 end
-SLASH_TEST_ACHIEVEMENT1 = "/tav"
