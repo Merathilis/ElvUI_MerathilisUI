7b57b2062ff48b8495e0abc9627d50a404fff980
Merathilis <neo0608@gmail.com>
2016-06-26 15:51:44 +0200

    Skins: Spellbook remove a function that don't exist anymore

--- ElvUI_MerathilisUI/modules/skins/blizzard/spellbook.lua
+++ ElvUI_MerathilisUI/modules/skins/blizzard/spellbook.lua
@@ -11,7 +11,6 @@ local SpellBookCoreAbilitiesFrame = _G["SpellBookCoreAbilitiesFrame"]
 local SpellBookPageText = _G["SpellBookPageText"]
 -- GLOBALS: hooksecurefunc
 
-
 local function styleSpellBook()
 	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.spellbook ~= true or E.private.muiSkins.blizzard.spellbook ~= true then return end
  
@@ -20,7 +19,6 @@ local function styleSpellBook()
 	end
 	
 	SpellBookPageText:SetTextColor(unpack(E.media.rgbvaluecolor))
-	SpellBookCoreAbilitiesFrame.SpecName:SetTextColor(unpack(E.media.rgbvaluecolor))
 	
 	local professionheaders = {
 		"PrimaryProfession1",
@@ -37,31 +35,6 @@ local function styleSpellBook()
 		_G[header.."Missing"]:SetShadowOffset(1, -1)
 		_G[header].missingText:SetTextColor(0.6, 0.6, 0.6)
 	end
-	
-	hooksecurefunc("SpellBook_UpdateCoreAbilitiesTab", function()
-		for i = 1, #SpellBookCoreAbilitiesFrame.Abilities do
-			local button = SpellBookCoreAbilitiesFrame.Abilities[i]
-			if button and button.isSkinned ~= true then
-
-				button.EmptySlot:SetAlpha(0)
-				button.ActiveTexture:SetAlpha(0)
-				button.FutureTexture:SetAlpha(0)
-
-				button.iconTexture:SetInside()
-
-				if button.FutureTexture:IsShown() then
-					button.iconTexture:SetDesaturated(true)
-					button.Name:SetTextColor(0.6, 0.6, 0.6)
-					button.InfoText:SetTextColor(0.6, 0.6, 0.6)
-				else
-					button.Name:SetTextColor(1, 0.82, 0)
-					button.InfoText:SetTextColor(0.8, 0.8, 0.8)
-				end
-				
-				button.isSkinned = true
-			end
-		end
-	end)
 end
 
 local f = CreateFrame("Frame")
