264ba76dffa85d6fee3b30779cbaa0b9ce3d28aa
Merathilis <neo0608@gmail.com>
2016-08-10 23:46:16 +0200

    v.2.10

--- ElvUI_MerathilisUI/core/changelog.lua
+++ ElvUI_MerathilisUI/core/changelog.lua
@@ -16,14 +16,14 @@ local classColor = E.myclass == 'PRIEST' and E.PriestColors or (CUSTOM_CLASS_COL
 local flat = [[Interface\AddOns\ElvUI_MerathilisUI\media\textures\Flat]]
 
 local ChangeLog = CreateFrame("frame")
-local ChangeLogData = [=[|cffff7d0av2.10|r, xx.08.2016
+local ChangeLogData = [=[|cffff7d0av2.10|r, 10.08.2016
 
 |cffff7d0aChanges:|r
  |cffff7d0a•|r Overhaul the install process for AddOns.
  |cffff7d0a•|r Some tweaks to the Layouts.
  |cffff7d0a•|r Fix a nil error if BenikUI is not loaded.
  |cffff7d0a•|r Add hearthstone position to the LocPanel menu.
- |cffff7d0a•|r Skin a progressBar in ObjectiveTracker skin.
+ |cffff7d0a•|r Skin progressBars in ObjectiveTracker skin.
  
 |cffff7d0aNotes:|r
  |cffff7d0a•|r The Heal Layout is not finished yet. Will work on it in the future.
