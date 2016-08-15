4a86d8a440bd68d45cfd91f3446b5c770e9bf165
Merathilis <neo0608@gmail.com>
2016-08-15 10:40:06 +0200

    Open/Close the Bags if Auctionhouse is shown/hidden.

---
+++ ElvUI_MerathilisUI/modules/bags/bags.lua
@@ -0,0 +1,17 @@
+local E, L, V, P, G = unpack(ElvUI); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
+local B = E:GetModule("Bags")
+
+local function EventHandler(self, event, ...)
+	if ( event == "PLAYER_ENTERING_WORLD" ) then
+		self:RegisterEvent("AUCTION_HOUSE_SHOW")
+		self:RegisterEvent("AUCTION_HOUSE_CLOSED")
+	elseif ( event == "AUCTION_HOUSE_SHOW" ) then
+		B:OpenBags()
+	elseif ( event == "AUCTION_HOUSE_CLOSED" ) then
+		B:CloseBags()
+	end
+end
+
+local f = CreateFrame("Frame")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:SetScript("OnEvent", EventHandler)
