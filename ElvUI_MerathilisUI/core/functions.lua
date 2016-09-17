943a59ed1aad50cfed127dd779ed2fa996a1819f
Merathilis <neo0608@gmail.com>
2016-09-17 12:41:38 +0200

    Move some functions to style

--- ElvUI_MerathilisUI/core/functions.lua
+++ ElvUI_MerathilisUI/core/functions.lua
@@ -1,6 +1,5 @@
 local E, L, V, P, G = unpack(ElvUI);
 local MER = E:GetModule('MerathilisUI');
-local LSM = LibStub('LibSharedMedia-3.0');
 
 -- Cache global variables
 -- Lua functions
@@ -8,26 +7,13 @@ local _G = _G
 local print, select = print, select
 local format = string.format
 -- WoW API / Variables
-local AchievementFrame = _G["AchievementFrame"]
-local AchievementFrame_LoadUI = _G["AchievementFrame_LoadUI"]
-local AchievementAlertFrame_ShowAlert = _G["AchievementAlertFrame_ShowAlert"]
-local AlertFrame_AnimateIn = _G["AlertFrame_AnimateIn"]
-local AlertFrame_FixAnchors = _G["AlertFrame_FixAnchors"]
-local ChallengeModeAlertFrame_ShowAlert = _G["ChallengeModeAlertFrame_ShowAlert"]
-local CreateFrame = CreateFrame
-local CriteriaAlertFrame_ShowAlert = _G["CriteriaAlertFrame_ShowAlert"]
-local GarrisonBuildingAlertFrame_ShowAlert = _G["GarrisonBuildingAlertFrame_ShowAlert"]
-local GarrisonMissionAlertFrame = _G["GarrisonMissionAlertFrame"]
-local GetInventoryItemLink = GetInventoryItemLink
+local GetAchievementInfo = GetAchievementInfo
 local GetItemInfo = GetItemInfo
 local GetSpellInfo = GetSpellInfo
-local GuildChallengeAlertFrame_ShowAlert = _G["GuildChallengeAlertFrame_ShowAlert"]
-local LootUpgradeFrame_ShowAlert = _G["LootUpgradeFrame_ShowAlert"]
-local LootWonAlertFrame_ShowAlert = _G["LootWonAlertFrame_ShowAlert"]
-local MoneyWonAlertFrame_ShowAlert = _G["MoneyWonAlertFrame_ShowAlert"]
-local PlaySound = PlaySound
-local ScenarioAlertFrame1 = _G["ScenarioAlertFrame1"]
-local StorePurchaseAlertFrame_ShowAlert = _G["StorePurchaseAlertFrame_ShowAlert"]
+local GetContainerItemID = GetContainerItemID
+local GetContainerNumSlots = GetContainerNumSlots
+
+--GLOBALS: NUM_BAG_SLOTS
 
 local classColor = E.myclass == 'PRIEST' and E.PriestColors or (CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[E.myclass] or RAID_CLASS_COLORS[E.myclass])
 
@@ -50,59 +36,6 @@ end
 local color = { r = 1, g = 1, b = 1, a = 1 }
 function MER:unpackColor(color)
 	return color.r, color.g, color.b, color.a
-end
-
-function MER:CreateWideShadow(f)
-	local borderr, borderg, borderb = 0, 0, 0
-	local backdropr, backdropg, backdropb = 0, 0, 0
-
-	local shadow = f.shadow or CreateFrame('Frame', nil, f) -- This way you can replace current shadows.
-	shadow:SetFrameLevel(1)
-	shadow:SetFrameStrata(f:GetFrameStrata())
-	shadow:SetOutside(f, 6, 6)
-	shadow:SetBackdrop( { 
-		edgeFile = LSM:Fetch('border', 'ElvUI GlowBorder'), edgeSize = E:Scale(6),
-		insets = {left = E:Scale(8), right = E:Scale(8), top = E:Scale(8), bottom = E:Scale(8)},
-	})
-	shadow:SetBackdropColor(backdropr, backdropg, backdropb, 0)
-	shadow:SetBackdropBorderColor(borderr, borderg, borderb, 0.5)
-	f.shadow = shadow
-end
-
-function MER:CreateSoftShadow(f)
-	local borderr, borderg, borderb = 0, 0, 0
-	local backdropr, backdropg, backdropb = 0, 0, 0
-
-	local shadow = f.shadow or CreateFrame('Frame', nil, f) -- This way you can replace current shadows.
-	shadow:SetFrameLevel(1)
-	shadow:SetFrameStrata(f:GetFrameStrata())
-	shadow:SetOutside(f, 2, 2)
-	shadow:SetBackdrop( { 
-		edgeFile = LSM:Fetch('border', 'ElvUI GlowBorder'), edgeSize = E:Scale(2),
-		insets = {left = E:Scale(5), right = E:Scale(5), top = E:Scale(5), bottom = E:Scale(5)},
-	})
-	shadow:SetBackdropColor(backdropr, backdropg, backdropb, 0)
-	shadow:SetBackdropBorderColor(borderr, borderg, borderb, 0.4)
-	f.shadow = shadow
-end
-
-function MER:CreateSoftGlow(f)
-	if f.sglow then return end
-
-	local borderr, borderg, borderb = 1, 1, .5
-	local backdropr, backdropg, backdropb = 1, 1, .5
-
-	local sglow = CreateFrame('Frame', nil, f)
-	sglow:SetFrameLevel(1)
-	sglow:SetFrameStrata(f:GetFrameStrata())
-	sglow:SetOutside(f, 2, 2)
-	sglow:SetBackdrop( { 
-		edgeFile = LSM:Fetch('border', 'ElvUI GlowBorder'), edgeSize = E:Scale(3),
-		insets = {left = E:Scale(5), right = E:Scale(5), top = E:Scale(5), bottom = E:Scale(5)},
-	})
-	sglow:SetBackdropColor(MER:unpackColor(E.db.general.valuecolor), 0)
-	sglow:SetBackdropBorderColor(MER:unpackColor(E.db.general.valuecolor), 0.4)
-	f.sglow = sglow
 end
 
 function MER:GetIconFromID(type, id)
--- ElvUI_MerathilisUI/core/style.lua
+++ ElvUI_MerathilisUI/core/style.lua
@@ -1,5 +1,6 @@
 local E, L, V, P, G = unpack(ElvUI)
 local MER = E:GetModule('MerathilisUI')
+local LSM = LibStub('LibSharedMedia-3.0');
 
 -- Cache global varables
 -- WoW API / Variables
@@ -52,5 +53,58 @@ function MER:Underline(frame, shadow, height)
 		line.Texture:SetAllPoints(line)
 	end
 	return line
+end
+
+function MER:CreateWideShadow(f)
+	local borderr, borderg, borderb = 0, 0, 0
+	local backdropr, backdropg, backdropb = 0, 0, 0
+
+	local shadow = f.shadow or CreateFrame('Frame', nil, f) -- This way you can replace current shadows.
+	shadow:SetFrameLevel(1)
+	shadow:SetFrameStrata(f:GetFrameStrata())
+	shadow:SetOutside(f, 6, 6)
+	shadow:SetBackdrop( { 
+		edgeFile = LSM:Fetch('border', 'ElvUI GlowBorder'), edgeSize = E:Scale(6),
+		insets = {left = E:Scale(8), right = E:Scale(8), top = E:Scale(8), bottom = E:Scale(8)},
+	})
+	shadow:SetBackdropColor(backdropr, backdropg, backdropb, 0)
+	shadow:SetBackdropBorderColor(borderr, borderg, borderb, 0.5)
+	f.shadow = shadow
+end
+
+function MER:CreateSoftShadow(f)
+	local borderr, borderg, borderb = 0, 0, 0
+	local backdropr, backdropg, backdropb = 0, 0, 0
+
+	local shadow = f.shadow or CreateFrame('Frame', nil, f) -- This way you can replace current shadows.
+	shadow:SetFrameLevel(1)
+	shadow:SetFrameStrata(f:GetFrameStrata())
+	shadow:SetOutside(f, 2, 2)
+	shadow:SetBackdrop( { 
+		edgeFile = LSM:Fetch('border', 'ElvUI GlowBorder'), edgeSize = E:Scale(2),
+		insets = {left = E:Scale(5), right = E:Scale(5), top = E:Scale(5), bottom = E:Scale(5)},
+	})
+	shadow:SetBackdropColor(backdropr, backdropg, backdropb, 0)
+	shadow:SetBackdropBorderColor(borderr, borderg, borderb, 0.4)
+	f.shadow = shadow
+end
+
+function MER:CreateSoftGlow(f)
+	if f.sglow then return end
+
+	local borderr, borderg, borderb = 1, 1, .5
+	local backdropr, backdropg, backdropb = 1, 1, .5
+
+	local sglow = CreateFrame('Frame', nil, f)
+	sglow:SetFrameLevel(1)
+	sglow:SetFrameStrata(f:GetFrameStrata())
+	sglow:SetOutside(f, 2, 2)
+	sglow:SetBackdrop( { 
+		edgeFile = LSM:Fetch('border', 'ElvUI GlowBorder'), edgeSize = E:Scale(3),
+		insets = {left = E:Scale(5), right = E:Scale(5), top = E:Scale(5), bottom = E:Scale(5)},
+	})
+	sglow:SetBackdropColor(MER:unpackColor(E.db.general.valuecolor), 0)
+	sglow:SetBackdropBorderColor(MER:unpackColor(E.db.general.valuecolor), 0.4)
+	f.sglow = sglow
 end
 
