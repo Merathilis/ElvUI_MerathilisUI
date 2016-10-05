bc9824cbfc1af71543dd8f5a37d5f39f617a7692
Merathilis <neo0608@gmail.com>
2016-10-05 09:19:42 +0200

    Add Vignette (for testing)

---
+++ ElvUI_MerathilisUI/modules/misc/vignette.lua
@@ -0,0 +1,29 @@
+local E, L, V, P, G = unpack(ElvUI);
+local MER = E:GetModule('MerathilisUI');
+
+-- Cache global variables
+-- Lua functions
+
+-- WoW API / Variables
+
+-- Global variables that we don't cache, list them here for the mikk's Find Globals script
+-- GLOBALS:
+
+local function OnVignetteAdded(self, event, id)
+	if not id then return end
+	self.vignettes = self.vignettes or {}
+	if self.vignettes[id] then return end
+
+	local x, y, name, icon = C_Vignettes.GetVignetteInfoFromInstanceID(id)
+	local left, right, top, bottom = GetObjectIconTextureCoords(icon)
+	PlaySoundFile("Sound\\Interface\\RaidWarning.ogg")
+	local str = "|TInterface\\MINIMAP\\ObjectIconsAtlas:0:0:0:0:256:256:"..(left*256)..":"..(right*256)..":"..(top*256)..":"..(bottom*256).."|t"
+	RaidNotice_AddMessage(RaidWarningFrame, str..name.." spotted!", ChatTypeInfo["RAID_WARNING"])
+
+	MER:Print(str..name,"spotted!")
+	self.vignettes[id] = true
+end
+
+local eventHandler = CreateFrame("Frame")
+eventHandler:RegisterEvent("VIGNETTE_ADDED")
+eventHandler:SetScript("OnEvent", OnVignetteAdded)
