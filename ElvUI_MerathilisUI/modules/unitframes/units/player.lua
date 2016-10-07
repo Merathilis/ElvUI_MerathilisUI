3b309ca5d1fff3970012546481e26eb9ea177333
Merathilis <neo0608@gmail.com>
2016-10-07 09:30:34 +0200

    Adopted latest oUF changes

--- ElvUI_MerathilisUI/modules/unitframes/units/player.lua
+++ ElvUI_MerathilisUI/modules/unitframes/units/player.lua
@@ -24,7 +24,7 @@ function MUF:ArrangePlayer()
 	-- RestIcon
 	MUF:Configure_RestingIndicator(frame)
 	
-	frame:UpdateAllElements()
+	frame:UpdateAllElements("MerathilisUI_UpdateAllElements")
 end
 
 function MUF:Configure_RestingIndicator(frame)
