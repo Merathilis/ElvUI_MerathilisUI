d8ca8eded8ecbedb65a5abda8521cb9f5cc7299a
Merathilis <neo0608@gmail.com>
2016-11-20 14:17:22 +0100

    gnarf

--- ElvUI_MerathilisUI/core/changelog.lua
+++ ElvUI_MerathilisUI/core/changelog.lua
@@ -19,7 +19,7 @@ local flat = [[Interface\AddOns\ElvUI_MerathilisUI\media\textures\Flat]]
 if E.db.mui.installed == nil then return; end
 
 local ChangeLog = CreateFrame("frame")
-local ChangeLogData = [=[|cffff7d0av2.23|r, 02.11.2016
+local ChangeLogData = [=[|cffff7d0av2.24|r, xx.11.2016
 
 |cffff7d0aChanges:|r
  |cffff7d0a•|r Changed AddOnSkins embedded settings for Skada.
