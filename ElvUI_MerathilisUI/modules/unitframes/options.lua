faedf526dde4ea07086e9563d9070767b7934f0b
Merathilis <neo0608@gmail.com>
2016-12-28 18:05:58 +0100

    Disable the configuration of an resting indicator (conflicts with BenikUI & S&L)

--- ElvUI_MerathilisUI/modules/unitframes/options.lua
+++ ElvUI_MerathilisUI/modules/unitframes/options.lua
@@ -42,28 +42,6 @@ local function UnitFramesTable()
 				type = "group",
 				name = L["Player Frame"],
 				args = {
-					rested = {
-						order = 1,
-						type = "group",
-						name = L["Rest Icon"],
-						guiInline = true,
-						get = function(info) return E.db['mui']['unitframes']['unit']['player']['rested'][ info[#info] ] end,
-						set = function(info, value) E.db['mui']['unitframes']['unit']['player']['rested'][ info[#info] ] = value; UF:Configure_RestingIndicator(_G["ElvUF_Player"]); E:StaticPopup_Show("PRIVATE_RL") end,
-						args = {
-							xoffset = { order = 1, type = 'range', name = L["X-Offset"], min = -300, max = 300, step = 1 },
-							yoffset = { order = 2, type = 'range', name = L["Y-Offset"], min = -150, max = 150, step = 1 },
-							size = { order = 3, type = 'range', name = L["Size"], min = 10, max = 60, step = 1 },
-							texture = {
-								order = 5,
-								type = "select",
-								name = L["Texture"],
-								values = {
-									["DEFAULT"] = DEFAULT,
-									["SVUI"] = "Supervillian UI",
-								},
-							},
-						},
-					},
 				},
 			},
 		},
--- ElvUI_MerathilisUI/modules/unitframes/units/player.lua
+++ ElvUI_MerathilisUI/modules/unitframes/units/player.lua
@@ -18,37 +18,11 @@ end
 function MUF:ArrangePlayer()
 	local frame = _G["ElvUF_Player"]
 
-	-- RestIcon
-	MUF:Configure_RestingIndicator(frame)
-
 	frame:UpdateAllElements("MerathilisUI_UpdateAllElements")
-end
-
-function MUF:Configure_RestingIndicator(frame)
-	local rIcon = frame.Resting
-	local db = frame.db
-	local Mdb = E.db['mui']['unitframes']['unit']['player']['rested']
-	if db.restIcon then
-		rIcon:ClearAllPoints()
-		if frame.ORIENTATION == "RIGHT" then
-			rIcon:Point("CENTER", frame.Health, "TOPLEFT", -3 + Mdb.xoffset, 6 + Mdb.yoffset)
-		else
-			if frame.USE_PORTRAIT and not frame.USE_PORTRAIT_OVERLAY then
-				rIcon:Point("CENTER", frame.Portrait, "TOPLEFT", -3 + Mdb.xoffset, 6 + Mdb.yoffset)
-			else
-				rIcon:Point("CENTER", frame.Health, "TOPLEFT", -3 + Mdb.xoffset, 6 + Mdb.yoffset)
-			end
-		end
-		rIcon:Size(Mdb.size)
-		if Mdb.texture == "DEFAULT" or Mdb.texture == "SVUI" then
-			rIcon:SetTexture(MUF.CombatTextures[Mdb.texture])
-			rIcon:SetTexCoord(0, .5, 0, .421875)
-		end
-	end
 end
 
 function MUF:InitPlayer()
 	if not E.db.unitframe.units.player.enable then return end
 	self:Construct_PlayerFrame()
-	hooksecurefunc(UF, "Configure_RestingIndicator", MUF.Configure_RestingIndicator)
+	-- hooksecurefunc(UF, "Configure_RestingIndicator", MUF.Configure_RestingIndicator)
 end
