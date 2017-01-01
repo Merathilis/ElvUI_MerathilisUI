21bce1e900e1eddcec62ed444bec35d4d4559034
Merathilis <neo0608@gmail.com>
2017-01-01 13:02:26 +0100

    Adjust a bit the logo size and version positon in flightMode

--- ElvUI_MerathilisUI/modules/misc/flightMode.lua
+++ ElvUI_MerathilisUI/modules/misc/flightMode.lua
@@ -28,7 +28,7 @@ function MFM:Initialize()
 
 		-- MerathilisUI Logo
 		BFM.FlightMode.bottom.logo = BFM.FlightMode:CreateTexture(nil, "OVERLAY")
-		BFM.FlightMode.bottom.logo:Size(256, 128)
+		BFM.FlightMode.bottom.logo:Size(200, 100)
 		BFM.FlightMode.bottom.logo:Point("CENTER", BFM.FlightMode.bottom, "CENTER", 0, 35)
 		BFM.FlightMode.bottom.logo:SetTexture("Interface\\AddOns\\ElvUI_MerathilisUI\\media\\textures\\mUI.tga")
 
@@ -36,7 +36,7 @@ function MFM:Initialize()
 		BFM.FlightMode.bottom.merathilisui = BFM.FlightMode.bottom:CreateFontString(nil, 'OVERLAY')
 		BFM.FlightMode.bottom.merathilisui:FontTemplate(nil, 10)
 		BFM.FlightMode.bottom.merathilisui:SetFormattedText("v%s", MER.Version)
-		BFM.FlightMode.bottom.merathilisui:SetPoint("TOP", BFM.FlightMode.bottom.logo, "BOTTOM", 0, 30)
+		BFM.FlightMode.bottom.merathilisui:SetPoint("BOTTOM", BFM.FlightMode.bottom.logo, "BOTTOM", 0, 5)
 		BFM.FlightMode.bottom.merathilisui:SetTextColor(1, 1, 1)
 
 		-- Pepe!
