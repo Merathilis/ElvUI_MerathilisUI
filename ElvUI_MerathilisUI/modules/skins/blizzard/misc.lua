807f2fc4d5d607aa7b900a37e86ed09c92e0b725
Merathilis <neo0608@gmail.com>
2017-02-03 11:21:32 +0100

    This should do the trick

--- ElvUI_MerathilisUI/modules/skins/blizzard/misc.lua
+++ ElvUI_MerathilisUI/modules/skins/blizzard/misc.lua
@@ -4,16 +4,38 @@ local E, L, V, P, G = unpack(ElvUI);
 -- Lua functions
 local _G = _G
 -- WoW API
-
--- Global variables that we don't cache, list th styleCharacter, CharacterStatsPane
+local WorldStateAlwaysUpFrame = WorldStateAlwaysUpFrame
+-- GLOBALS: hooksecurefunc, NUM_ALWAYS_UP_UI_FRAMES
 
 local function styleMisc()
 	if E.private.skins.blizzard.enable ~= true then return end
 
-	if _G["AlwaysUpFrame1"] then
-		_G["AlwaysUpFrame1"]:ClearAllPoints()
-		_G["AlwaysUpFrame1"]:SetPoint("CENTER", E.UIParent, "TOP", -80, -70) --e.g. Violat Hold
-	end
+	hooksecurefunc("WorldStateAlwaysUpFrame_Update", function()
+		WorldStateAlwaysUpFrame:ClearAllPoints()
+		WorldStateAlwaysUpFrame:SetPoint("TOP", E.UIParent, "TOP", 0, -40)
+
+		if _G["AlwaysUpFrame1"] then
+			local _, _, _, _, y = _G["AlwaysUpFrame1"]:GetPoint()
+			_G["AlwaysUpFrame1"]:SetPoint("TOP", WorldStateAlwaysUpFrame, "TOP", 0, y)
+		end
+
+		for i = 1, NUM_ALWAYS_UP_UI_FRAMES do
+			local frame = _G["AlwaysUpFrame"..i]
+
+			local text = _G["AlwaysUpFrame"..i.."Text"]
+			text:ClearAllPoints()
+			text:SetPoint("CENTER", frame, "CENTER", 0, 0)
+			text:SetJustifyH("CENTER")
+
+			local icon = _G["AlwaysUpFrame"..i.."Icon"]
+			icon:ClearAllPoints()
+			icon:SetPoint("RIGHT", text, "LEFT", -5, 0)
+
+			local dynamicIcon = _G["AlwaysUpFrame"..i.."DynamicIconButton"]
+			dynamicIcon:ClearAllPoints()
+			dynamicIcon:SetPoint("LEFT", text, "RIGHT", 5, 0)
+		end
+	end)
 end
 
 local f = CreateFrame("Frame")
