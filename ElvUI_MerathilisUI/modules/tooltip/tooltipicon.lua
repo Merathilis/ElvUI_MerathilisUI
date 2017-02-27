b770a4a6dda96c34e0305952c891e498fd5f22e6
Merathilis <neo0608@gmail.com>
2017-02-27 10:25:47 +0100

    Update tooltip icon code

--- ElvUI_MerathilisUI/modules/tooltip/tooltipicon.lua
+++ ElvUI_MerathilisUI/modules/tooltip/tooltipicon.lua
@@ -6,58 +6,53 @@ local _G = _G
 local select, type = select, type
 local strmatch = string.match
 -- WoW API / Variables
-local CreateFrame = CreateFrame
-local GetAchievementInfo = GetAchievementInfo
 local GetItemIcon = GetItemIcon
 local GetSpellInfo = GetSpellInfo
 
-local function AddIcon(self, icon)
+local function setTooltipIcon(self, icon)
 	if E.db.mui.misc.Tooltip ~= true then return; end
 
-	if icon then
-		local title = _G[self:GetName() .. "TextLeft1"]
-		if title and not title:GetText():find("|T" .. icon) then
-			title:SetFormattedText("|T%s:20:20:0:0:64:64:5:59:5:59:%d|t %s", icon, 20, title:GetText())
-		end
+	local title = icon and _G[self:GetName().."TextLeft1"]
+	if title then
+		title:SetFormattedText("|T%s:20:20:0:0:64:64:5:59:5:59:%d|t %s", icon, 20, title:GetText())
 	end
 end
 
--- Icon for Items
-local function hookItem(tip)
-	tip:HookScript("OnTooltipSetItem", function(self)
+local function newTooltipHooker(method, func)
+	return function(tooltip)
+		local modified = false
 
-		local link = self:GetItem()
-		local icon = link and GetItemIcon(link)
-		AddIcon(self, icon)
-	end)
+		tooltip:HookScript("OnTooltipCleared", function(self, ...)
+			modified = false
+		end)
+
+		tooltip:HookScript(method, function(self, ...)
+			if not modified then
+				modified = true
+				func(self, ...)
+			end
+		end)
+	end
 end
-hookItem(GameTooltip)
-hookItem(ItemRefTooltip)
-hookItem(ShoppingTooltip1)
-hookItem(ShoppingTooltip2)
 
--- Icon for Spells
-local function hookSpell(tip)
-	tip:HookScript("OnTooltipSetSpell", function(self)
-
-		local _, _, id = self:GetSpell()
-		if id then
-			AddIcon(self, select(3, GetSpellInfo(id)))
-		end
-	end)
-end
-hookSpell(GameTooltip)
-hookSpell(ItemRefTooltip)
-
--- Icon for Achievements (only GameTooltip)
-hooksecurefunc(GameTooltip, "SetHyperlink", function(self, link)
-	if type(link) ~= "string" then return end
-	local linkType, id = strmatch(link, "^([^:]+):(%d+)")
-	if linkType == "achievement" then
-		local _, _, _, _, _, _, _, _, _, icon, _, _, _, _ = GetAchievementInfo(id)
-		AddIcon(self, icon)
+local hookItem = newTooltipHooker("OnTooltipSetItem", function(self, ...)
+	local _, link = self:GetItem()
+	if link then
+		setTooltipIcon(self, GetItemIcon(link))
 	end
 end)
+
+local hookSpell = newTooltipHooker("OnTooltipSetSpell", function(self, ...)
+	local _, _, id = self:GetSpell()
+	if id then
+		setTooltipIcon(self, select(3, GetSpellInfo(id)))
+	end
+end)
+
+for _, tooltip in pairs{GameTooltip, ItemRefTooltip, ItemRefShoppingTooltip1, ItemRefShoppingTooltip2, ShoppingTooltip1, ShoppingTooltip2} do
+	hookItem(tooltip)
+	hookSpell(tooltip)
+end
 
 -- WorldQuest Tooltip
 hooksecurefunc("EmbeddedItemTooltip_SetItemByQuestReward", function(self)
@@ -67,12 +62,3 @@ hooksecurefunc("EmbeddedItemTooltip_SetItemByQuestReward", function(self)
 	end
 end)
 BONUS_OBJECTIVE_REWARD_WITH_COUNT_FORMAT = "|T%1$s:16:16:0:0:64:64:5:59:5:59|t |cffffffff%2$d|r %3$s"
-
-local f = CreateFrame("Frame")
-f:RegisterEvent("PLAYER_ENTERING_WORLD")
-f:SetScript("OnEvent", function(_, event)
-	if event == "PLAYER_ENTERING_WORLD" then
-		AddIcon()
-		f:UnregisterEvent("PLAYER_ENTERING_WORLD")
-	end
-end)
