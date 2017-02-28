33da2430f34a700ce6f52116e941b2fe789f977f
Merathilis <neo0608@gmail.com>
2017-02-28 11:05:58 +0100

    Update orderhall skin

--- ElvUI_MerathilisUI/modules/skins/blizzard/orderhall.lua
+++ ElvUI_MerathilisUI/modules/skins/blizzard/orderhall.lua
@@ -12,30 +12,17 @@ local ipairs = ipairs
 
 local classColor = E.myclass == "PRIEST" and E.PriestColors or (CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[E.myclass] or RAID_CLASS_COLORS[E.myclass])
 
-local function follower()
-	local index = 1
-	for i, child in ipairs({OrderHallCommandBar:GetChildren()}) do
-		if child.Icon and child.Count and child.TroopPortraitCover then
-			child.TroopPortraitCover:Hide()
-			child.Icon:SetSize(40, 20)
-			child.Count:SetFont(LSM:Fetch("font", "Merathilis Roboto-Black"), 11, nil)
-			child.Count:SetTextColor(1, 1, 1)
-			child.Count:SetShadowOffset(0, 0)
-
-			index = index + 1
-		end
-	end
-end
-
 local function styleOrderhall()
 	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.orderhall ~= true or E.private.muiSkins.blizzard.orderhall ~= true then return end
 
+	local classColor = E.myclass == 'PRIEST' and E.PriestColors or (CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[E.myclass] or RAID_CLASS_COLORS[E.myclass])
+
+	OrderHallCommandBar:SetParent(E.UIParent)
 	OrderHallCommandBar:ClearAllPoints()
 	OrderHallCommandBar:SetPoint("TOPLEFT", E.UIParent, "TOPLEFT", 2, -29)
 	OrderHallCommandBar:SetWidth(600)
 	OrderHallCommandBar:SetScript("OnEnter", onEnter)
 	OrderHallCommandBar:SetScript("OnLeave", onLeave)
-	OrderHallCommandBar:SetParent(E.UIParent)
 
 	OrderHallCommandBar.Currency:Hide()
 	OrderHallCommandBar.CurrencyIcon:Hide()
@@ -43,8 +30,6 @@ local function styleOrderhall()
 
 	OrderHallCommandBar.AreaName:ClearAllPoints()
 	OrderHallCommandBar.AreaName:Point("LEFT", OrderHallCommandBar.Currency, "RIGHT", 0, 0)
-	OrderHallCommandBar.AreaName:SetFont(LSM:Fetch("font", "Merathilis Roboto-Black"), 11, "OUTLINE")
-	OrderHallCommandBar.AreaName:SetWordWrap(true)
 
 	OrderHallCommandBar.WorldMapButton:Show()
 	OrderHallCommandBar.WorldMapButton:ClearAllPoints()
@@ -66,15 +51,14 @@ local function styleOrderhall()
 	mapButton:HookScript("OnEnter", function() mapButton.Text:SetTextColor(classColor.r, classColor.g, classColor.b) end)
 	mapButton:HookScript("OnLeave", function() mapButton.Text:SetTextColor(1, 1, 1) end)
 
-	E:CreateMover(OrderHallCommandBar, "OrderhallMover", L["Orderhall"])
+	E:CreateMover(OrderHallCommandBar, "MER_OrderhallMover", L["Orderhall"], nil, nil, "ALL, SOLO")
 end
 
 local f = CreateFrame("Frame")
 f:RegisterEvent("ADDON_LOADED")
 f:SetScript("OnEvent", function(self, event, arg1)
 	if event == "ADDON_LOADED" and arg1 == "Blizzard_OrderHallUI" then
-		E:Delay(.5, styleOrderhall)
-		E:Delay(.1, follower)
+		styleOrderhall()
 		self:UnregisterEvent("ADDON_LOADED")
 	end
 end)
