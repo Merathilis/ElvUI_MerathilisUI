d557774d432ed904b63374e7c6839f130bb1f773
Merathilis <neo0608@gmail.com>
2017-05-06 13:35:52 +0200

    Remove some code to position the AlwaysUpFrame

--- ElvUI_MerathilisUI/modules/skins/blizzard/misc.lua
+++ ElvUI_MerathilisUI/modules/skins/blizzard/misc.lua
@@ -14,28 +14,6 @@ local function styleMisc()
 	hooksecurefunc("WorldStateAlwaysUpFrame_AddFrame", function()
 		WorldStateAlwaysUpFrame:ClearAllPoints()
 		WorldStateAlwaysUpFrame:SetPoint("TOP", E.UIParent, "TOP", 0, -40)
-
-		if _G["AlwaysUpFrame1"] then
-			local _, _, _, _, y = _G["AlwaysUpFrame1"]:GetPoint()
-			_G["AlwaysUpFrame1"]:SetPoint("TOP", WorldStateAlwaysUpFrame, "TOP", 0, y)
-		end
-
-		for i = 1, NUM_ALWAYS_UP_UI_FRAMES do
-			local frame = _G["AlwaysUpFrame"..i]
-
-			local text = _G["AlwaysUpFrame"..i.."Text"]
-			text:ClearAllPoints()
-			text:SetPoint("CENTER", frame, "CENTER", 0, 0)
-			text:SetJustifyH("CENTER")
-
-			local icon = _G["AlwaysUpFrame"..i.."Icon"]
-			icon:ClearAllPoints()
-			icon:SetPoint("RIGHT", text, "LEFT", -5, 0)
-
-			local dynamicIcon = _G["AlwaysUpFrame"..i.."DynamicIconButton"]
-			dynamicIcon:ClearAllPoints()
-			dynamicIcon:SetPoint("LEFT", text, "RIGHT", 5, 0)
-		end
 	end)
 end
 
