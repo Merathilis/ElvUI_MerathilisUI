edd696eb8557f8b7eb9e75f50652953d124c046c
Merathilis <neo0608@gmail.com>
2017-05-30 10:55:17 +0200

    Skin the OrderHall TalentFrame. All credits belong to Simpy <3

--- ElvUI_MerathilisUI/modules/skins/blizzard/orderhall.lua
+++ ElvUI_MerathilisUI/modules/skins/blizzard/orderhall.lua
@@ -100,11 +100,98 @@ local function styleOrderhall()
 	end
 	MERS:CreateBD(MissionPage.RewardsFrame, .25)
 
+	-- Credits Simpy <3
 	-- Talent Frame
-	local Talent = OrderHallTalentFrame
-	if not Talent.stripes then
-		MERS:CreateStripes(Talent)
+	local TalentFrame = OrderHallTalentFrame
+	local TalentInset = ClassHallTalentInset
+	local TalentClassBG = TalentFrame.ClassBackground
+	TalentInset:CreateBackdrop("Transparent")
+	TalentInset.backdrop:SetFrameLevel(TalentInset.backdrop:GetFrameLevel()+1)
+	TalentInset.backdrop:Point("TOPLEFT", TalentClassBG, "TOPLEFT", E.Border-1, -E.Border+1)
+	TalentInset.backdrop:Point("BOTTOMRIGHT", TalentClassBG, "BOTTOMRIGHT", -E.Border+1, E.Border-1)
+	TalentClassBG:SetAtlas("orderhalltalents-background-"..E.myclass)
+	TalentClassBG:SetDrawLayer("ARTWORK")
+	TalentClassBG:SetAlpha(0.8)
+
+	local function colorBorder(child, backdrop, atlas)
+		if child.AlphaIconOverlay:IsShown() then --isBeingResearched or (talentAvailability and not selected)
+			local alpha = child.AlphaIconOverlay:GetAlpha()
+			if alpha <= 0.5 then --talentAvailability
+				backdrop:SetBackdropBorderColor(0.5, 0.5, 0.5) --[border = grey, shadow x2]
+				child.darkOverlay:SetColorTexture(0, 0, 0, 0.50)
+				child.darkOverlay:Show()
+			elseif alpha <= 0.7 then --isBeingResearched
+				backdrop:SetBackdropBorderColor(0, 1, 1) --[border = teal, shadow x1]
+				child.darkOverlay:SetColorTexture(0, 0, 0, 0.25)
+				child.darkOverlay:Show()
+			end
+		elseif atlas == "orderhalltalents-spellborder-green" then
+			backdrop:SetBackdropBorderColor(0 ,1, 0) --[border = green, no shadow]
+			child.darkOverlay:Hide()
+		elseif atlas == "orderhalltalents-spellborder-yellow" then
+			backdrop:SetBackdropBorderColor(1, 1, 0) --[border = yellow, no shadow]
+			child.darkOverlay:Hide()
+		elseif atlas == "orderhalltalents-spellborder" then
+			backdrop:SetBackdropBorderColor(unpack(E.media.bordercolor))
+			child.darkOverlay:SetColorTexture(0, 0, 0, 0.75) --[border will be default, shadow x3]
+			child.darkOverlay:Show()
+		end
 	end
+
+	TalentFrame:HookScript("OnShow", function(self)
+		if self.skinned then return end
+		for i = 1, self:GetNumChildren() do
+			local child = select(i, self:GetChildren())
+			if child and child.Icon and not child.backdrop then
+				child:StyleButton()
+				MERS:CreateBD(child, .25)
+				child:CreateBackdrop()
+				child.Border:SetAlpha(0)
+				child.Highlight:SetAlpha(0)
+				child.AlphaIconOverlay:SetTexture(nil)
+				child.Icon:SetTexCoord(unpack(E.TexCoords))
+				child.Icon:SetInside(child.backdrop)
+				child.hover:SetInside(child.backdrop)
+				child.pushed:SetInside(child.backdrop)
+				child.backdrop:SetFrameLevel(child.backdrop:GetFrameLevel()+1)
+
+				child.darkOverlay = child:CreateTexture()
+				child.darkOverlay:SetAllPoints(child.Icon)
+				child.darkOverlay:SetDrawLayer("OVERLAY")
+				child.darkOverlay:Hide()
+
+				colorBorder(child, child.backdrop, child.Border:GetAtlas())
+
+				child.TalentDoneAnim:HookScript("OnFinished", function(self)
+					child.Border:SetAlpha(0) -- clear the yellow glow border again, after it finishes the animation
+				end)
+			end
+		end
+
+		for i = 1, 8 do
+			local ticker = TalentFrame["Tick"..i]
+			if not ticker.bg then
+				local bg = CreateFrame("Frame", "OrderHallTalentFrameTick"..i.."Background", self)
+				bg:Point("LEFT", ticker, "CENTER", -20, 0)
+				MERS:CreateBD(bg, .25)
+				MERS:CreateGradient(bg)
+				bg:SetSize(320, 50)
+				ticker.bg = bg
+			end
+		end
+
+		self.choiceTexturePool:ReleaseAll()
+		hooksecurefunc(self, "RefreshAllData", function(frame)
+			frame.choiceTexturePool:ReleaseAll()
+			for i = 1, frame:GetNumChildren() do
+				local child = select(i, frame:GetChildren())
+				if child and child.Icon and child.backdrop then
+					colorBorder(child, child.backdrop, child.Border:GetAtlas())
+				end
+			end
+		end)
+		self.skinned = true
+	end)
 end
 
 local OrderHallFollower = CreateFrame("Frame")
