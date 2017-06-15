76f658a05e3166cce80f49b100816124ff4b955b
Merathilis <neo0608@gmail.com>
2017-06-15 16:42:37 +0200

    Update EncounterJournal skin

--- ElvUI_MerathilisUI/modules/skins/blizzard/encounterjournal.lua
+++ ElvUI_MerathilisUI/modules/skins/blizzard/encounterjournal.lua
@@ -18,9 +18,137 @@ function styleEncounterJournal()
 	EncounterInfo.instanceTitle:SetTextColor(unpack(E.media.rgbvaluecolor))
 	EncounterInfo.encounterTitle:SetTextColor(unpack(E.media.rgbvaluecolor))
 
+	EncounterJournalEncounterFrameInfo:DisableDrawLayer("BACKGROUND")
+	EncounterJournal:DisableDrawLayer("BORDER")
+	EncounterJournalInset:DisableDrawLayer("BORDER")
+	EncounterJournalNavBar:DisableDrawLayer("BORDER")
+	EncounterJournalSearchResults:DisableDrawLayer("BORDER")
+	EncounterJournal:DisableDrawLayer("OVERLAY")
+	EncounterJournalInstanceSelectSuggestTab:DisableDrawLayer("OVERLAY")
+	EncounterJournalInstanceSelectDungeonTab:DisableDrawLayer("OVERLAY")
+	EncounterJournalInstanceSelectRaidTab:DisableDrawLayer("OVERLAY")
+
+	EncounterJournalNavBar:GetRegions():Hide()
+	EncounterJournalNavBarOverlay:Hide()
+
+	if EJ.navBar.backdrop then
+		EJ.navBar.backdrop:Hide()
+	end
+
 	if not EJ.stripes then
 		MERS:CreateStripes(EJ)
 	end
+
+	EncounterJournalEncounterFrameInfoCreatureButton1:SetPoint("TOPLEFT", EncounterJournalEncounterFrameInfoModelFrame, 0, -35)
+
+	do
+		local numBossButtons = 1
+		local bossButton
+
+		hooksecurefunc("EncounterJournal_DisplayInstance", function()
+			bossButton = _G["EncounterJournalBossButton"..numBossButtons]
+			while bossButton do
+				MERS:Reskin(bossButton, true)
+
+				bossButton.text:SetTextColor(1, 1, 1)
+				bossButton.text.SetTextColor = MER.dummy
+
+				local hl = bossButton:GetHighlightTexture()
+				hl:SetColorTexture(MER.ClassColor.r, MER.ClassColor.g, MER.ClassColor.b, .2)
+				hl:SetPoint("TOPLEFT", 2, -1)
+				hl:SetPoint("BOTTOMRIGHT", 0, 1)
+
+				bossButton.creature:SetPoint("TOPLEFT", 0, -4)
+
+				numBossButtons = numBossButtons + 1
+				bossButton = _G["EncounterJournalBossButton"..numBossButtons]
+			end
+
+			-- move last tab
+			local _, point = EncounterJournalEncounterFrameInfoModelTab:GetPoint()
+			EncounterJournalEncounterFrameInfoModelTab:SetPoint("TOP", point, "BOTTOM", 0, 1)
+		end)
+	end
+
+	hooksecurefunc("EncounterJournal_ToggleHeaders", function(self)
+		local index = 1
+		local header = _G["EncounterJournalInfoHeader"..index]
+		while header do
+			if not header.styled then
+				header.flashAnim.Play = MER.dummy
+
+				header.descriptionBG:SetAlpha(0)
+				header.descriptionBGBottom:SetAlpha(0)
+				for i = 4, 18 do
+					select(i, header.button:GetRegions()):SetTexture("")
+				end
+
+				header.description:SetTextColor(1, 1, 1)
+				header.button.title:SetTextColor(1, 1, 1)
+				header.button.title.SetTextColor = MER.dummy
+				header.button.expandedIcon:SetTextColor(1, 1, 1)
+				header.button.expandedIcon.SetTextColor = MER.dummy
+
+				MERS:Reskin(header.button, true)
+
+				header.button.abilityIcon:SetTexCoord(.08, .92, .08, .92)
+				header.button.bg = MERS:CreateBG(header.button.abilityIcon)
+
+				if header.button.abilityIcon:IsShown() then
+					header.button.bg:Show()
+				else
+					header.button.bg:Hide()
+				end
+
+				header.styled = true
+			end
+
+			if header.button.abilityIcon:IsShown() then
+				header.button.bg:Show()
+			else
+				header.button.bg:Hide()
+			end
+
+			index = index + 1
+			header = _G["EncounterJournalInfoHeader"..index]
+		end
+	end)
+
+	hooksecurefunc("EncounterJournal_SetUpOverview", function(self, role, index)
+		local header = self.overviews[index]
+		if not header.styled then
+			header.flashAnim.Play = MER.dummy
+
+			header.descriptionBG:SetAlpha(0)
+			header.descriptionBGBottom:SetAlpha(0)
+			for i = 4, 18 do
+				select(i, header.button:GetRegions()):SetTexture("")
+			end
+
+			header.button.title:SetTextColor(1, 1, 1)
+			header.button.title.SetTextColor = MER.dummy
+			header.button.expandedIcon:SetTextColor(1, 1, 1)
+			header.button.expandedIcon.SetTextColor = MER.dummy
+
+			-- Blizzard already uses .tex for this frame, so we can't do highlights
+			MERS:Reskin(header.button, true)
+
+			header.styled = true
+		end
+	end)
+
+	hooksecurefunc("EncounterJournal_SetBullets", function(object, description)
+		local parent = object:GetParent()
+
+		if parent.Bullets then
+			for _, bullet in pairs(parent.Bullets) do
+				if not bullet.styled then
+					bullet.Text:SetTextColor(1, 1, 1)
+					bullet.styled = true
+				end
+			end
+		end
+	end)
 
 	local Tabs = {
 		_G["EncounterJournalEncounterFrameInfoBossTab"],
@@ -35,6 +163,12 @@ function styleEncounterJournal()
 		Tab:GetDisabledTexture():SetTexture(nil)
 		Tab:GetHighlightTexture():SetTexture(nil)
 		MERS:StyleOutside(Tab.backdrop)
+	end
+
+	for _, tabName in pairs({"EncounterJournalInstanceSelectSuggestTab", "EncounterJournalInstanceSelectDungeonTab", "EncounterJournalInstanceSelectRaidTab","EncounterJournalInstanceSelectLootJournalTab"}) do
+		local tab = _G[tabName]
+
+		MERS:ReskinTab(tab)
 	end
 
 	-- From Aurora
@@ -92,6 +226,121 @@ function styleEncounterJournal()
 				item.Icon:SetDrawLayer("OVERLAY")
 				MERS:CreateBG(item.Icon)
 			end
+		end
+	end)
+
+	local function rewardOnEnter()
+		for i = 1, 2 do
+			local item = EncounterJournalTooltip["Item"..i]
+			if item:IsShown() then
+				if item.IconBorder:IsShown() then
+					-- item.newBg:SetVertexColor(item.IconBorder:GetVertexColor())
+					item.IconBorder:Hide()
+				else
+					item.newBg:SetVertexColor(0, 0, 0)
+				end
+			end
+		end
+	end
+
+	local suggestFrame = EncounterJournal.suggestFrame
+
+	--[[ Suggestion 1 ]]
+	local suggestion = suggestFrame.Suggestion1
+
+	suggestion.bg:Hide()
+	MERS:CreateBD(suggestion, .25)
+
+	suggestion.icon:SetPoint("TOPLEFT", 135, -15)
+	MERS:CreateBG(suggestion.icon)
+
+	local centerDisplay = suggestion.centerDisplay
+
+	centerDisplay.title.text:SetTextColor(1, 1, 1)
+	centerDisplay.description.text:SetTextColor(.9, .9, .9)
+
+	S:HandleButton(suggestion.button)
+
+	local reward = suggestion.reward
+
+	reward:HookScript("OnEnter", rewardOnEnter)
+	reward.text:SetTextColor(.9, .9, .9)
+	reward.iconRing:Hide()
+	reward.iconRingHighlight:SetTexture("")
+	MERS:CreateBG(reward.icon)
+
+	--[[ Suggestion 2 and 3 ]]
+
+	for i = 2, 3 do
+		local suggestion = suggestFrame["Suggestion"..i]
+
+		suggestion.bg:Hide()
+
+		MERS:CreateBD(suggestion, .25)
+
+		suggestion.icon:SetPoint("TOPLEFT", 10, -10)
+		MERS:CreateBG(suggestion.icon)
+
+		local centerDisplay = suggestion.centerDisplay
+
+		centerDisplay:ClearAllPoints()
+		centerDisplay:SetPoint("TOPLEFT", 85, -10)
+		centerDisplay.title.text:SetTextColor(1, 1, 1)
+		centerDisplay.description.text:SetTextColor(.9, .9, .9)
+
+		S:HandleButton(centerDisplay.button)
+
+		local reward = suggestion.reward
+
+		reward:HookScript("OnEnter", rewardOnEnter)
+		reward.iconRing:Hide()
+		reward.iconRingHighlight:SetTexture("")
+		MERS:CreateBG(reward.icon)
+	end
+
+	-- Hook functions
+
+	hooksecurefunc("EJSuggestFrame_RefreshDisplay", function()
+		local self = suggestFrame
+
+		if #self.suggestions > 0 then
+			local suggestion = self.Suggestion1
+			local data = self.suggestions[1]
+
+			suggestion.iconRing:Hide()
+
+			if data.iconPath then
+				suggestion.icon:SetMask("")
+				suggestion.icon:SetTexture(data.iconPath)
+				suggestion.icon:SetTexCoord(.08, .92, .08, .92)
+			end
+		end
+
+		if #self.suggestions > 1 then
+			for i = 2, #self.suggestions do
+				local suggestion = self["Suggestion"..i]
+				if not suggestion then break end
+
+				local data = self.suggestions[i]
+
+				suggestion.iconRing:Hide()
+
+				if data.iconPath then
+					suggestion.icon:SetMask("")
+					suggestion.icon:SetTexture(data.iconPath)
+					suggestion.icon:SetTexCoord(.08, .92, .08, .92)
+				end
+			end
+		end
+	end)
+
+	hooksecurefunc("EJSuggestFrame_UpdateRewards", function(suggestion)
+		local rewardData = suggestion.reward.data
+		if rewardData then
+			local texture = rewardData.itemIcon or rewardData.currencyIcon or [[Interface\Icons\achievement_guildperk_mobilebanking]]
+			suggestion.reward.icon:SetMask("")
+			suggestion.reward.icon:SetTexture(texture)
+			suggestion.reward.icon:SetTexCoord(.08, .92, .08, .92)
 		end
 	end)
 end
