edfe049870acc8450c0e7b2bd46d31a1116a092d
Merathilis <neo0608@gmail.com>
2017-06-18 13:15:12 +0200

    Removed some unused code from portraits. Update locales

--- ElvUI_MerathilisUI/modules/unitframes/elements/portrait.lua
+++ ElvUI_MerathilisUI/modules/unitframes/elements/portrait.lua
@@ -74,19 +74,10 @@ function MUF:Configure_Portrait(frame, isPlayer)
 					frame.portraitmover:ClearAllPoints()
 					if frame.unit == "player" then
 						frame.portraitmover:Point("TOPRIGHT", frame, "TOPLEFT", -frame.BORDER, 0)
-						E:CreateMover(frame.portraitmover, "PlayerPortraitMover", "Player Portrait", nil, nil, nil, "ALL,SOLO")
+						E:CreateMover(frame.portraitmover, "mUIPlayerPortraitMover", L["Player Portrait"], nil, nil, nil, "ALL,SOLO")
 					elseif frame.unit == "target" then
 						frame.portraitmover:Point("TOPLEFT", frame, "TOPRIGHT", frame.BORDER, 0)
-						E:CreateMover(frame.portraitmover, "TargetPortraitMover", "Target Portrait", nil, nil, nil, "ALL,SOLO")
-					elseif frame.unit == "targettarget" then
-						frame.portraitmover:Point("TOPLEFT", frame, "TOPRIGHT", frame.BORDER, 0)
-						E:CreateMover(frame.portraitmover, "TargetTargetPortraitMover", "TargetTarget Portrait", nil, nil, nil, "ALL,SOLO")
-					elseif frame.unit == "focus" then
-						frame.portraitmover:Point("TOPLEFT", frame, "TOPRIGHT", frame.BORDER, 0)
-						E:CreateMover(frame.portraitmover, "FocusPortraitMover", "Focus Portrait", nil, nil, nil, "ALL,SOLO")
-					elseif frame.unit == "pet" then
-						frame.portraitmover:Point("TOPLEFT", frame, "TOPRIGHT", frame.BORDER, 0)
-						E:CreateMover(frame.portraitmover, "PetPortraitMover", "Pet Portrait", nil, nil, nil, "ALL,SOLO")
+						E:CreateMover(frame.portraitmover, "mUITargetPortraitMover", L["Target Portrait"], nil, nil, nil, "ALL,SOLO")
 					end
 					frame.portraitmover:ClearAllPoints()
 					frame.portraitmover:SetPoint("BOTTOMLEFT", frame.portraitmover.mover, "BOTTOMLEFT")
