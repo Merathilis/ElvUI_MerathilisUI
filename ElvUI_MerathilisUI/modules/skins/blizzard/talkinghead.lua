cbdf0beab2ec2dca59e58383bf491e99e6de1b58
Merathilis <neo0608@gmail.com>
2017-06-20 17:09:42 +0200

    I'm stupid ...

--- ElvUI_MerathilisUI/modules/skins/blizzard/talkinghead.lua
+++ ElvUI_MerathilisUI/modules/skins/blizzard/talkinghead.lua
@@ -12,6 +12,7 @@ local function styleTalkingHead()
 		frame.BackgroundFrame:StripTextures()
 		MERS:CreateBD(frame.BackgroundFrame, .25)
 		MERS:CreateBD(frame.MainFrame.Model, .25)
+		MERS:CreateGradient(frame.BackgroundFrame)
 
 		local button = frame.MainFrame.CloseButton
 		button:ClearAllPoints()
