48967bc14865545f786b855e51a70946f51c11f6
Merathilis <neo0608@gmail.com>
2017-06-25 12:18:53 +0200

    Update Auctionhouse skin

--- ElvUI_MerathilisUI/modules/skins/blizzard/auctionhouse.lua
+++ ElvUI_MerathilisUI/modules/skins/blizzard/auctionhouse.lua
@@ -13,6 +13,8 @@ local function styleAuctionhouse()
 	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.auctionhouse ~= true or E.private.muiSkins.blizzard.auctionhouse ~= true then return end
 
 	MERS:CreateGradient(AuctionFrame)
+	MERS:CreateStripes(AuctionFrame)
+
 	if AuctionFrameBrowse then
 		AuctionFrameBrowse.bg1:Hide()
 		AuctionFrameBrowse.bg2:Hide()
@@ -26,6 +28,79 @@ local function styleAuctionhouse()
 		AuctionFrameAuctions.bg1:Hide()
 		AuctionFrameAuctions.bg2:Hide()
 	end
+
+	local ABBD = CreateFrame("Frame", nil, AuctionProgressBar)
+	ABBD:Point("TOPLEFT", -1, 1)
+	ABBD:Point("BOTTOMRIGHT", 1, -1)
+	ABBD:SetFrameLevel(AuctionProgressBar:GetFrameLevel()-1)
+	MERS:CreateBD(ABBD, .25)
+
+	select(14, AuctionProgressFrameCancelButton:GetRegions()):SetPoint("CENTER", 0, 2)
+
+	local sorttabs = {
+		"BrowseQualitySort",
+		"BrowseLevelSort",
+		"BrowseDurationSort",
+		"BrowseHighBidderSort",
+		"BrowseCurrentBidSort",
+		"BidQualitySort",
+		"BidLevelSort",
+		"BidDurationSort",
+		"BidBuyoutSort",
+		"BidStatusSort",
+		"BidBidSort",
+		"AuctionsQualitySort",
+		"AuctionsDurationSort",
+		"AuctionsHighBidderSort",
+		"AuctionsBidSort",
+	}
+	for _, sorttab in pairs(sorttabs) do
+		_G[sorttab.."Left"]:Kill()
+		_G[sorttab.."Middle"]:Kill()
+		_G[sorttab.."Right"]:Kill()
+	end
+
+	hooksecurefunc("FilterButton_SetUp", function(button)
+		button:SetNormalTexture("")
+	end)
+
+	BrowseCloseButton:ClearAllPoints()
+	BrowseCloseButton:SetPoint("BOTTOMRIGHT", AuctionFrameBrowse, "BOTTOMRIGHT", 66, 13)
+	BrowseBuyoutButton:ClearAllPoints()
+	BrowseBuyoutButton:Point("RIGHT", BrowseCloseButton, "LEFT", -1, 0)
+	BrowseBidButton:ClearAllPoints()
+	BrowseBidButton:Point("RIGHT", BrowseBuyoutButton, "LEFT", -1, 0)
+	BidBuyoutButton:ClearAllPoints()
+	BidBuyoutButton:Point("RIGHT", BidCloseButton, "LEFT", -1, 0)
+	BidBidButton:ClearAllPoints()
+	BidBidButton:Point("RIGHT", BidBuyoutButton, "LEFT", -1, 0)
+	AuctionsCancelAuctionButton:ClearAllPoints()
+	AuctionsCancelAuctionButton:Point("RIGHT", AuctionsCloseButton, "LEFT", -1, 0)
+	BrowsePrevPageButton:ClearAllPoints()
+	BrowsePrevPageButton:Point("TOPLEFT", BrowseSearchButton, "BOTTOMLEFT", 0, -5)
+	BrowseNextPageButton:ClearAllPoints()
+	BrowseNextPageButton:Point("TOPRIGHT", BrowseResetButton, "BOTTOMRIGHT", 0, -5)
+
+	-- Blizz needs to be more consistent
+	BrowseBidPriceSilver:Point("LEFT", BrowseBidPriceGold, "RIGHT", 1, 0)
+	BrowseBidPriceCopper:Point("LEFT", BrowseBidPriceSilver, "RIGHT", 1, 0)
+	BidBidPriceSilver:Point("LEFT", BidBidPriceGold, "RIGHT", 1, 0)
+	BidBidPriceCopper:Point("LEFT", BidBidPriceSilver, "RIGHT", 1, 0)
+	StartPriceSilver:Point("LEFT", StartPriceGold, "RIGHT", 1, 0)
+	StartPriceCopper:Point("LEFT", StartPriceSilver, "RIGHT", 1, 0)
+	BuyoutPriceSilver:Point("LEFT", BuyoutPriceGold, "RIGHT", 1, 0)
+	BuyoutPriceCopper:Point("LEFT", BuyoutPriceSilver, "RIGHT", 1, 0)
+
+	local auctionhandler = CreateFrame("Frame")
+	auctionhandler:RegisterEvent("NEW_AUCTION_UPDATE")
+	auctionhandler:SetScript("OnEvent", function()
+		local _, _, _, _, _, _, _, _, _, _, _, _, _, AuctionsItemButtonIconTexture = AuctionsItemButton:GetRegions() -- blizzard, please name your textures
+		if AuctionsItemButtonIconTexture then
+			AuctionsItemButtonIconTexture:SetTexCoord(.08, .92, .08, .92)
+			AuctionsItemButtonIconTexture:Point("TOPLEFT", 1, -1)
+			AuctionsItemButtonIconTexture:Point("BOTTOMRIGHT", -1, 1)
+		end
+	end)
 end
 
 S:AddCallbackForAddon("Blizzard_AuctionUI", "mUIAuctionhouse", styleAuctionhouse)
