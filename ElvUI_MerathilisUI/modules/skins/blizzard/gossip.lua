573abac8614b0e5e2c26446b9c5e09e8d5c040c6
Merathilis <neo0608@gmail.com>
2017-08-10 15:34:29 +0200

    Update gossip frame. You still need AddOnSkins parchement remover enabled. =/

--- ElvUI_MerathilisUI/modules/skins/blizzard/gossip.lua
+++ ElvUI_MerathilisUI/modules/skins/blizzard/gossip.lua
@@ -21,21 +21,37 @@ local function styleGossip()
 	_G["GossipGreetingScrollFrame"]:StripTextures()
 	_G["GossipGreetingText"]:SetTextColor(1, 1, 1)
 
-	for i = 1, NUMGOSSIPBUTTONS do
-		obj = select(3, _G["GossipTitleButton"..i]:GetRegions())
-		obj:SetTextColor(1, 1, 1)
+	for i = 1, 7 do
+		select(i, GossipFrame:GetRegions()):Hide()
 	end
+	select(19, GossipFrame:GetRegions()):Hide()
+
+	GreetingText:SetTextColor(1, 1, 1)
+	GreetingText.SetTextColor = MER.dummy
+
+	GossipGreetingScrollFrame.spellTex:SetTexture('')
 
 	hooksecurefunc("GossipFrameUpdate", function()
 		for i = 1, NUMGOSSIPBUTTONS do
-			local button = _G["GossipTitleButton"..i]
-			if button:GetFontString() then
-				if button:GetFontString():GetText() and button:GetFontString():GetText():find("|cff000000") then
-					button:GetFontString():SetText(gsub(button:GetFontString():GetText(), "|cff000000", "|cffFFFF00"))
-				end
+			local text = _G["GossipTitleButton" .. i]:GetText()
+			if text then
+				text = gsub(text, "|cff......", "|cffffffff")
+				_G["GossipTitleButton"..i]:SetText(text)
 			end
 		end
 	end)
+
+	ItemTextFrame:StripTextures(true)
+	ItemTextScrollFrameScrollBar:StripTextures()
+	InboxFrameBg:Hide()
+	ItemTextPrevPageButton:GetRegions():Hide()
+	ItemTextNextPageButton:GetRegions():Hide()
+	ItemTextMaterialTopLeft:SetAlpha(0)
+	ItemTextMaterialTopRight:SetAlpha(0)
+	ItemTextMaterialBotLeft:SetAlpha(0)
+	ItemTextMaterialBotRight:SetAlpha(0)
+	ItemTextPageText:SetTextColor(1, 1, 1)
+	ItemTextPageText.SetTextColor = MER.dummy
 end
 
 S:AddCallback("mUIGossip", styleGossip)
