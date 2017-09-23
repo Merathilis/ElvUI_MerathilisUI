5dbed92a2d72d084fa9c7789b394f58ed78697b9
Merathilis <neo0608@gmail.com>
2017-09-23 22:04:05 +0200

    Add my Mage to the dev function

--- ElvUI_MerathilisUI/core/functions.lua
+++ ElvUI_MerathilisUI/core/functions.lua
@@ -148,6 +148,7 @@ MER.IsDev = {
 	["Jazira"] = true,
 	["Damará"] = true,
 	["Merathilîs"] = true,
+	["Melisendra"] = true
 }
 -- Don't forget to update realm name(s) if we ever transfer realms.
 -- If we forget it could be easly picked up by another player who matches these combinations.
