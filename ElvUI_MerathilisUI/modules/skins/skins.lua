01f3e97e06ed0b234a976e0c7e6accb3e4481619
Merathilis <neo0608@gmail.com>
2017-09-25 09:38:08 +0200

    Delete a unused function

--- ElvUI_MerathilisUI/modules/skins/skins.lua
+++ ElvUI_MerathilisUI/modules/skins/skins.lua
@@ -424,52 +424,6 @@ function MERS:SkinStatusBar(frame, ClassColor)
 	ElvUI[1]:RegisterStatusBar(Frame)
 end
 
--- Original close buttons, but desaturated. Like it used to be in ElvUI.
-function MERS:HandleCloseButton(f, point, text)
-	for i = 1, f:GetNumRegions() do
-		local region = select(i, f:GetRegions())
-		if region:GetObjectType() == "Texture" then
-			region:SetDesaturated(1)
-			for n = 1, #buttons do
-				local texture = buttons[n]
-				if region:GetTexture() == "Interface\\Buttons\\"..texture then
-					f.noBackdrop = true
-				end
-			end
-			if region:GetTexture() == "Interface\\DialogFrame\\UI-DialogBox-Corner" then
-				region:Kill()
-			end
-		end
-	end
-
-	-- Create backdrop for the few close buttons that do not use original close button
-	if not f.backdrop and not f.noBackdrop then
-		f:CreateBackdrop("Default", true)
-		f.backdrop:Point("TOPLEFT", 7, -8)
-		f.backdrop:Point("BOTTOMRIGHT", -8, 8)
-		f:HookScript("OnEnter", SetModifiedBackdrop)
-		f:HookScript("OnLeave", SetOriginalBackdrop)
-	end
-
-	-- Have to create the text, ElvUI code expects the element to be there. It won"t show up for original close buttons anyway.
-	if not f.text then
-		f.text = f:CreateFontString(nil, "OVERLAY")
-		f.text:SetFont([[Interface\AddOns\ElvUI\media\fonts\PT_Sans_Narrow.ttf]], 16, "OUTLINE")
-		f.text:SetText(text)
-		f.text:SetJustifyH("CENTER")
-		f.text:SetPoint("CENTER", f, "CENTER")
-	end
-
-	-- Hide text if button is using original skin
-	if f.text and f.noBackdrop then
-		f.text:SetAlpha(0)
-	end
-
-	if point then
-		f:Point("TOPRIGHT", point, "TOPRIGHT", 2, 2)
-	end
-end
-
 -- ClassColored ScrollBars
 function MERS:ReskinScrollBar(frame, thumbTrim)
 	if frame:GetName() then
