5f66e5b73cf74c143b0e2825d8e952f26a0bc464
Merathilis <neo0608@gmail.com>
2017-10-07 12:15:49 +0200

    Formating updates

--- ElvUI_MerathilisUI/modules/notification/notification.lua
+++ ElvUI_MerathilisUI/modules/notification/notification.lua
@@ -527,9 +527,9 @@ function NF:SocialQueueEvent(event, guid, numAddedItems)
 		fullName = format("|cff00ff00%s|r", fullName)
 		name = format("|cff00c0fa%s|r", name)
 		if name then
-			self:DisplayToast(coloredName, ((isLeader and L["is looking for members"] or L["joined a group"]).."\n"..fullName or UNKNOWN).. ": "..name:sub(1,100), _G["ToggleQuickJoinPanel"], "Interface\\Icons\\Achievement_GuildPerk_EverybodysFriend", .08, .92, .08, .92)
+			self:DisplayToast(coloredName, ((isLeader and L["is looking for members"] or L["joined a group"]).."\n".."["..fullName or UNKNOWN).."]: "..name:sub(1,100), _G["ToggleQuickJoinPanel"], "Interface\\Icons\\Achievement_GuildPerk_EverybodysFriend", .08, .92, .08, .92)
 		else
-			self:DisplayToast(coloredName, ((isLeader and L["is looking for members"] or L["joined a group"]).."\n"..fullName or UNKNOWN), _G["ToggleQuickJoinPanel"], "Interface\\Icons\\Achievement_GuildPerk_EverybodysFriend", .08, .92, .08, .92)
+			self:DisplayToast(coloredName, ((isLeader and L["is looking for members"] or L["joined a group"]).."\n".."["..fullName or UNKNOWN).."]: ", _G["ToggleQuickJoinPanel"], "Interface\\Icons\\Achievement_GuildPerk_EverybodysFriend", .08, .92, .08, .92)
 		end
 	elseif firstQueue then
 		output, outputCount, queueName, queueCount = "", "", "", 0
