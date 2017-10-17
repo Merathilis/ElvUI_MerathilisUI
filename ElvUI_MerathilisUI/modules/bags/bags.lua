0b547b745e8827e26c14be562cd10d31b05327d2
Merathilis <neo0608@gmail.com>
2017-10-17 12:03:44 +0200

    Style the ElvUI bags. This is really fucked up.

--- ElvUI_MerathilisUI/modules/bags/bags.lua
+++ ElvUI_MerathilisUI/modules/bags/bags.lua
@@ -1,5 +1,6 @@
 local MER, E, L, V, P, G = unpack(select(2, ...))
-local MERB = E:NewModule("mUIBags")
+local MERB = E:NewModule("mUIBags", "AceHook-3.0", "AceEvent-3.0")
+local MERS = E:GetModule("muiSkins")
 local B = E:GetModule("Bags")
 
 local function EventHandler(self, event)
@@ -13,14 +14,45 @@ local function EventHandler(self, event)
 	end
 end
 
+function MERB:StyleBags()
+	if ElvUI_ContainerFrame then
+		MERS:CreateGradient(ElvUI_ContainerFrame)
+		MERS:CreateStripes(ElvUI_ContainerFrame)
+		MERS:CreateGradient(ElvUI_ContainerFrameContainerHolder)
+		MERS:CreateStripes(ElvUI_ContainerFrameContainerHolder)
+	end
+
+	if ElvUIBags then
+		MERS:CreateGradient(ElvUIBags)
+		MERS:CreateStripes(ElvUIBags)
+	end
+end
+
+function MERB:OpenBankBags()
+	--this is really fucked up
+	if ElvUI_BankContainerFrame then
+		MERS:CreateGradient(ElvUI_BankContainerFrame)
+		MERS:CreateStripes(ElvUI_BankContainerFrame)
+		MERS:CreateGradient(ElvUI_BankContainerFrameContainerHolder)
+		MERS:CreateStripes(ElvUI_BankContainerFrameContainerHolder)
+	end
+end
+
+function MERB:AllInOneBags()
+	self:StyleBags()
+	self:RegisterEvent("BANKFRAME_OPENED", "OpenBankBags")
+end
+
 function MERB:Initialize()
 	if E.private.bags.enable ~= true then return end
 
 	local f = CreateFrame("Frame")
 	f:RegisterEvent("PLAYER_ENTERING_WORLD")
 	f:SetScript("OnEvent", EventHandler)
-end
 
+	self:OpenBankBags()
+	self:AllInOneBags()
+end
 
 local function InitializeCallback()
 	MERB:Initialize()
