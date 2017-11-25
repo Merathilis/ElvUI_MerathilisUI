8351bc3d9bafb75d13bf7187c8e4e40cc8b5ca2a
Merathilis <neo0608@gmail.com>
2017-11-25 14:02:11 +0100

    A little bit cleanup

--- ElvUI_MerathilisUI/core/core.lua
+++ ElvUI_MerathilisUI/core/core.lua
@@ -1,6 +1,6 @@
-local E, L, V, P, G = unpack(ElvUI);
-local LSM = LibStub("LibSharedMedia-3.0");
-local EP = LibStub("LibElvUIPlugin-1.0");
+local E, L, V, P, G = unpack(ElvUI)
+local LSM = LibStub("LibSharedMedia-3.0")
+local EP = LibStub("LibElvUIPlugin-1.0")
 local addon, Engine = ...
 
 local MER = LibStub("AceAddon-3.0"):NewAddon(addon, "AceConsole-3.0", "AceEvent-3.0", "AceTimer-3.0", "AceHook-3.0")
@@ -22,7 +22,7 @@ local SetCVar = SetCVar
 -- GLOBALS: LibStub, ElvDB, MUISplashScreen, ElvUI_SLE, hooksecurefunc, BINDING_HEADER_MER
 -- GLOBALS: MERData, MERDataPerChar
 
---Setting up table to unpack. Why? no idea
+--Setting up table to unpack.
 Engine[1] = MER
 Engine[2] = E
 Engine[3] = L
@@ -189,6 +189,7 @@ function MER:Initialize()
 	if not MERData then
 		MERData = {}
 	end
+
 	if not MERDataPerChar then
 		MERDataPerChar = {}
 	end
@@ -206,6 +207,8 @@ function MER:Initialize()
 	-- Show only Splash Screen if the install is completed
 	if (E.db.mui.installed == true and E.db.mui.general.SplashScreen) then
 		C_TimerAfter(6, ShowSplashScreen)
+	else
+		return
 	end
 
 	-- run the setup again when a profile gets deleted.
