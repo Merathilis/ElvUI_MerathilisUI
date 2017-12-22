02ea6e9ce31141361f337098a3a637b7342d231b
Merathilis <neo0608@gmail.com>
2017-12-22 11:36:41 +0100

    Not needed.

--- ElvUI_MerathilisUI/modules/blizzard/options.lua
+++ ElvUI_MerathilisUI/modules/blizzard/options.lua
@@ -64,7 +64,7 @@ local function Errors()
 	}
 
 	local listIndex = 1
-	for errorName, state in pairs(E.db.mui.errorFilters) do
+	for errorName in pairs(E.db.mui.errorFilters) do
 		E.Options.args.mui.args.errors.args.filterGroup.args[errorName] = {
 			order = listIndex,
 			type = "toggle",
