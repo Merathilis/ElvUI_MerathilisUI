b16c5ae97fc5d3818aba39550ef28dbf90fd9e89
Merathilis <neo0608@gmail.com>
2018-01-17 17:51:02 +0100

    Change the Spellbook subname color. So it is readable.

--- ElvUI_MerathilisUI/modules/skins/blizzard/spellbook.lua
+++ ElvUI_MerathilisUI/modules/skins/blizzard/spellbook.lua
@@ -8,6 +8,8 @@ local pairs, unpack = pairs, unpack
 -- WoW API / Variables
 local SpellBookFrame = _G["SpellBookFrame"]
 local SpellBookPageText = _G["SpellBookPageText"]
+local GetSpellAvailableLevel = GetSpellAvailableLevel
+local UnitLevel = UnitLevel
 -- GLOBALS: hooksecurefunc
 
 local function styleSpellBook()
@@ -35,6 +37,30 @@ local function styleSpellBook()
 		_G[header.."Missing"]:SetShadowOffset(1, -1)
 		_G[header].missingText:SetTextColor(0.6, 0.6, 0.6)
 	end
+
+	hooksecurefunc("SpellButton_UpdateButton", function(self)
+		if SpellBookFrame.bookType == BOOKTYPE_PROFESSION then return end
+
+		local slot, slotType = SpellBook_GetSpellBookSlot(self);
+		local name = self:GetName();
+		local subSpellString = _G[name.."SubSpellName"]
+
+		local isOffSpec = self.offSpecID ~= 0 and SpellBookFrame.bookType == BOOKTYPE_SPELL
+
+		subSpellString:SetTextColor(1, 1, 1)
+
+		if slotType == "FUTURESPELL" then
+			local level = GetSpellAvailableLevel(slot, SpellBookFrame.bookType)
+			if level and level > UnitLevel("player") then
+				self.SpellName:SetTextColor(.7, .7, .7)
+				subSpellString:SetTextColor(.7, .7, .7)
+			end
+		else
+			if slotType == "SPELL" and isOffSpec then
+				subSpellString:SetTextColor(.7, .7, .7)
+			end
+		end
+	end)
 end
 
 S:AddCallback("mUISpellbook", styleSpellBook)
