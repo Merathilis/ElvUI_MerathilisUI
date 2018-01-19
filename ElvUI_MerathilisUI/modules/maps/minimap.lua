88f6295031f5b4283b73651cc859ea8f05227542
Merathilis <neo0608@gmail.com>
2018-01-19 18:20:08 +0100

    Why did i ever delete the minimap blips? :thinking:

--- ElvUI_MerathilisUI/modules/maps/minimap.lua
+++ ElvUI_MerathilisUI/modules/maps/minimap.lua
@@ -6,9 +6,13 @@ MM.modName = L["Minimap"]
 --Lua functions
 local _G = _G
 --WoW API / Variables
-
+local CreateFrame = CreateFrame
 --Global variables that we don't cache, list them here for mikk's FindGlobals script
 -- GLOBALS:
+
+local function blipIcons()
+	_G["Minimap"]:SetBlipTexture("Interface\\AddOns\\ElvUI_MerathilisUI\\media\\textures\\blipIcons.tga")
+end
 
 function MM:ChangeLandingButton()
 	if _G["GarrisonLandingPageMinimapButton"] then
@@ -35,6 +39,15 @@ end
 
 function MM:Initialize()
 	self:ChangeLandingButton()
+
+	local f = CreateFrame("Frame")
+	f:RegisterEvent("PLAYER_ENTERING_WORLD")
+	f:SetScript("OnEvent", function(self, event)
+		if event == "PLAYER_ENTERING_WORLD" then
+			blipIcons()
+			f:UnregisterEvent("PLAYER_ENTERING_WORLD")
+		end
+	end)
 end
 
 local function InitializeCallback()
