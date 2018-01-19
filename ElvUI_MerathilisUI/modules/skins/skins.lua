1eb23c1b685f496b079ba95215ca870d1a80c934
Merathilis <neo0608@gmail.com>
2018-01-19 11:40:19 +0100

    missed a check

--- ElvUI_MerathilisUI/modules/skins/skins.lua
+++ ElvUI_MerathilisUI/modules/skins/skins.lua
@@ -41,7 +41,7 @@ function S:HandleCloseButton(f, point, text)
 		f:StripTextures()
 
 		--Create backdrop for the few close buttons that do not use original close button
-		if not f.backdrop then
+		if not f.backdrop and not f.noBackdrop then
 			f:CreateBackdrop("Transparent", true)
 			f.backdrop:Point("TOPLEFT", 7, -8)
 			f.backdrop:Point("BOTTOMRIGHT", -8, 8)
