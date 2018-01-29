21670283bff538a5dec19dac0e94a27ef185935d
Merathilis <neo0608@gmail.com>
2018-01-29 12:26:52 +0100

    Update BigWigs skin (no half-bar anymore)

--- ElvUI_MerathilisUI/modules/skins/addons/bigWigs.lua
+++ ElvUI_MerathilisUI/modules/skins/addons/bigWigs.lua
@@ -39,46 +39,38 @@ local function FreeStyle(bar)
 		FreeBackgrounds[#FreeBackgrounds + 1] = ibg
 	end
 
-	-- replace dummies with original method functions
-	bar.candyBarBar.SetPoint = bar.candyBarBar.OldSetPoint
-	bar.candyBarIconFrame.SetWidth = bar.candyBarIconFrame.OldSetWidth
-	bar.SetScale = bar.OldSetScale
-
 	--Reset Positions
 	--Icon
 	bar.candyBarIconFrame:ClearAllPoints()
+	bar.candyBarIconFrame.SetWidth = nil
 	bar.candyBarIconFrame:SetPoint("TOPLEFT")
 	bar.candyBarIconFrame:SetPoint("BOTTOMLEFT")
 	bar.candyBarIconFrame:SetTexCoord(0.07, 0.93, 0.07, 0.93)
 
 	--Status Bar
 	bar.candyBarBar:ClearAllPoints()
+	bar.candyBarBar.SetPoint = nil
 	bar.candyBarBar:SetPoint("TOPRIGHT")
 	bar.candyBarBar:SetPoint("BOTTOMRIGHT")
 
 	--BG
 	bar.candyBarBackground:SetAllPoints()
 
-	--Duration
-	bar.candyBarDuration:ClearAllPoints()
-	bar.candyBarDuration:SetPoint("RIGHT", bar.candyBarBar, "RIGHT", -2, 0)
+	-- Duration
+	-- bar.candyBarDuration:ClearAllPoints()
+	-- bar.candyBarDuration:SetPoint("RIGHT", bar.candyBarBar, "RIGHT", -2, 0)
 
-	--Name
-	bar.candyBarLabel:ClearAllPoints()
-	bar.candyBarLabel:SetPoint("LEFT", bar.candyBarBar, "LEFT", 2, 0)
-	bar.candyBarLabel:SetPoint("RIGHT", bar.candyBarBar, "RIGHT", -2, 0)
+	-- Name
+	-- bar.candyBarLabel:ClearAllPoints()
+	-- bar.candyBarLabel:SetPoint("LEFT", bar.candyBarBar, "LEFT", 2, 0)
+	-- bar.candyBarLabel:SetPoint("RIGHT", bar.candyBarBar, "RIGHT", -2, 0)
 end
 
 local function ApplyStyle(bar)
 	-- general bar settings
-	bar.OldHeight = bar:GetHeight()
-	bar.OldScale = bar:GetScale()
-	bar.OldSetScale = bar.SetScale
-	bar.SetScale = MER.dummy
-	bar:Height(buttonsize)
-	bar:SetScale(1)
+	bar:SetHeight(buttonsize)
 
-	local bg = nil
+	local bg
 	if #FreeBackgrounds > 0 then
 		bg = tremove(FreeBackgrounds)
 	else
@@ -111,24 +103,15 @@ local function ApplyStyle(bar)
 		bar:Set("bigwigs:MerathilisUI:ibg", ibg)
 	end
 
-	bar:SetHeight(buttonsize / 2)
-
-	bar.candyBarBar.OldPoint, bar.candyBarBar.Anchor, bar.candyBarBar.OldPoint2, bar.candyBarBar.XPoint, bar.candyBarBar.YPoint  = bar.candyBarBar:GetPoint()
 	bar.candyBarBar:ClearAllPoints()
 	bar.candyBarBar:SetAllPoints(bar)
-	bar.candyBarBar.OldSetPoint = bar.candyBarBar.SetPoint
 	bar.candyBarBar.SetPoint = MER.dummy
-	MER:SetStatusBarGradient(bar.candyBarBar, true)
 	if not bar.data["bigwigs:emphasized"] == true then
 		bar.candyBarBar:SetStatusBarColor(MER.ClassColor.r, MER.ClassColor.g, MER.ClassColor.b, 1)
 	end
 
 	bar.candyBarBackground:SetTexture(unpack(E["media"].backdropcolor))
 
-	bar.candyBarIconFrame.OldPoint, bar.candyBarIconFrame.Anchor, bar.candyBarIconFrame.OldPoint2, bar.candyBarIconFrame.XPoint, bar.candyBarIconFrame.YPoint  = bar.candyBarIconFrame:GetPoint()
-	bar.candyBarIconFrame.OldWidth = bar.candyBarIconFrame:GetWidth()
-	bar.candyBarIconFrame.OldHeight = bar.candyBarIconFrame:GetHeight()
-	bar.candyBarIconFrame.OldSetWidth = bar.candyBarIconFrame.SetWidth
 	bar.candyBarIconFrame:ClearAllPoints()
 	bar.candyBarIconFrame:SetPoint("BOTTOMRIGHT", bar, "BOTTOMLEFT", -3, 0)
 	bar.candyBarIconFrame:SetSize(buttonsize, buttonsize)
@@ -136,24 +119,24 @@ local function ApplyStyle(bar)
 	bar.candyBarIconFrame:SetTexCoord(unpack(E.TexCoords))
 
 	bar.candyBarLabel:ClearAllPoints()
-	bar.candyBarLabel:SetPoint("LEFT", bar, "LEFT", 2, 10)
-	bar.candyBarLabel:SetPoint("RIGHT", bar, "RIGHT", -2, 10)
+	bar.candyBarLabel:SetPoint("LEFT", bar, "LEFT", 2, 0)
+	bar.candyBarLabel:SetPoint("RIGHT", bar, "RIGHT", -2, 0)
 
 	bar.candyBarDuration:ClearAllPoints()
-	bar.candyBarDuration:SetPoint("LEFT", bar, "LEFT", 2, 10)
-	bar.candyBarDuration:SetPoint("RIGHT", bar, "RIGHT", -2, 10)
+	bar.candyBarDuration:SetPoint("LEFT", bar, "LEFT", 2, 0)
+	bar.candyBarDuration:SetPoint("RIGHT", bar, "RIGHT", -2, 0)
 end
 
 local f = CreateFrame("Frame")
 local function RegisterStyle()
 	if not BigWigs then return end
-	local styleName = MER.Title
+	local styleName = MER.Title or "MerathilisUI"
 	local bars = BigWigs:GetPlugin("Bars", true)
 	if not bars then return end
 	bars:RegisterBarStyle(styleName, {
 		apiVersion = 1,
 		version = 1,
-		GetSpacing = function() return 18 end,
+		GetSpacing = function() return 4 end,
 		ApplyStyle = ApplyStyle,
 		BarStopped = FreeStyle,
 		GetStyleName = function() return styleName end,
