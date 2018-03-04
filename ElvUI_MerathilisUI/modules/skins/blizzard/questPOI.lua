d2af63daab5bb31836a96f207c9f5c29794f1e5a
Merathilis <neo0608@gmail.com>
2018-03-04 10:33:04 +0100

    Stupid me >.>

--- ElvUI_MerathilisUI/modules/skins/blizzard/questPOI.lua
+++ ElvUI_MerathilisUI/modules/skins/blizzard/questPOI.lua
@@ -8,44 +8,41 @@ local next = next
 -- WoW API / Variables
 
 -- GLOBALS: hooksecurefunc, QuestPOI_GetButton, QuestPOINumericTemplate, QuestPOICompletedTemplate
-do
-	function QuestPOI_GetButton(parent, questID, style, index)
-		if E.private.skins.blizzard.enable ~= true or E.private.muiSkins.blizzard.questPOI ~= true then return; end
 
-		local poiButton
-		if style == "numeric" then
-			poiButton = parent.poiTable.numeric[index]
-			if not poiButton.IsSkinned then
-				QuestPOINumericTemplate(poiButton)
+local function QuestPOINumericTemplate(button)
+	button:SetSize(20, 20)
+	button.Number:SetSize(32, 32)
+	button.NormalTexture:SetSize(32, 32)
+	button.HighlightTexture:SetSize(32, 32)
+	button.PushedTexture:SetSize(32, 32)
+end
+
+local function QuestPOICompletedTemplate(button)
+	button:SetSize(20, 20)
+	button.FullHighlightTexture:SetSize(32, 32)
+	button.NormalTexture:SetSize(32, 32)
+	button.PushedTexture:SetSize(32, 32)
+end
+
+function QuestPOIGetButton(parent, questID, style, index)
+	if E.private.skins.blizzard.enable ~= true or E.private.muiSkins.blizzard.questPOI ~= true then return; end
+
+	local poiButton
+	if style == "numeric" then
+		poiButton = parent.poiTable.numeric[index]
+		if not poiButton.IsSkinned then
+			QuestPOINumericTemplate(poiButton)
+		end
+	else
+		for _, button in next, parent.poiTable.completed do
+			if button.questID == questID then
+				poiButton = button
+				break
 			end
-		else
-			for _, button in next, parent.poiTable.completed do
-				if button.questID == questID then
-					poiButton = button
-					break
-				end
-			end
-			if not poiButton.IsSkinned then
-				QuestPOICompletedTemplate(poiButton)
-			end
+		end
+		if not poiButton.IsSkinned then
+			QuestPOICompletedTemplate(poiButton)
 		end
 	end
 end
-hooksecurefunc("QuestPOI_GetButton", QuestPOI_GetButton)
-
-do
-	local function QuestPOINumericTemplate(button)
-		button:SetSize(20, 20)
-		button.Number:SetSize(32, 32)
-		button.NormalTexture:SetSize(32, 32)
-		button.HighlightTexture:SetSize(32, 32)
-		button.PushedTexture:SetSize(32, 32)
-	end
-
-	local function QuestPOICompletedTemplate(button)
-		button:SetSize(20, 20)
-		button.FullHighlightTexture:SetSize(32, 32)
-		button.NormalTexture:SetSize(32, 32)
-		button.PushedTexture:SetSize(32, 32)
-	end
-end
+hooksecurefunc("QuestPOI_GetButton", QuestPOIGetButton)
