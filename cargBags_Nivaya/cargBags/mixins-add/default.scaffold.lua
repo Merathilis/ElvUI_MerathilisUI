6d97118fd674db19197aa4f34d7f6ae295e308cc
Merathilis <neo0608@gmail.com>
2018-03-15 14:21:07 +0100

    Ok, this one i fucked up >.>

--- cargBags_Nivaya/cargBags/mixins-add/default.scaffold.lua
+++ cargBags_Nivaya/cargBags/mixins-add/default.scaffold.lua
@@ -250,6 +250,8 @@ local function ItemButton_UpdateQuest(self, item)
 	elseif item.rarity and item.rarity > 1 then
 		local r, g, b = GetItemQualityColor(item.rarity)
 		self.Border:SetBackdropBorderColor(r, g, b, 1)
+	else
+		self.Border:SetBackdropBorderColor(0, 0, 0, 1)
 	end
 
 	if(self.OnUpdateQuest) then self:OnUpdateQuest(item) end
