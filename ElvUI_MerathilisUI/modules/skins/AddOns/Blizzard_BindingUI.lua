f23faba2261e3727e51a35fb3e311e7727836cfb
Merathilis <neo0608@gmail.com>
2018-05-18 10:47:11 +0200

    Update Binding Skin.

--- ElvUI_MerathilisUI/modules/skins/AddOns/Blizzard_BindingUI.lua
+++ ElvUI_MerathilisUI/modules/skins/AddOns/Blizzard_BindingUI.lua
@@ -1,9 +1,11 @@
 local MER, E, L, V, P, G = unpack(select(2, ...))
+local MERS = E:GetModule("muiSkins")
 local S = E:GetModule("Skins")
 
 --Cache global variables
 --Lua functions
 local _G = _G
+local select = select
 --WoW API / Variables
 
 --Global variables that we don't cache, list them here for the mikk's Find Globals script
@@ -13,6 +15,36 @@ local function styleBinding()
 	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.binding ~= true or E.private.muiSkins.blizzard.binding ~= true then return end
 
 	_G["KeyBindingFrame"]:Styling()
+
+	local function styleBindingButton(bu)
+		local selected = bu.selectedHighlight
+
+		for i = 1, 9 do
+			select(i, bu:GetRegions()):Hide()
+		end
+
+		selected:SetTexture(E["media"].normTex)
+		selected:SetPoint("TOPLEFT", 1, -1)
+		selected:SetPoint("BOTTOMRIGHT", -1, 1)
+		selected:SetColorTexture(MER.ClassColor.r, MER.ClassColor.g, MER.ClassColor.b, .2)
+
+		MERS:Reskin(bu)
+	end
+
+	for i = 1, KEY_BINDINGS_DISPLAYED do
+		local button1 = _G["KeyBindingFrameKeyBinding"..i.."Key1Button"]
+		local button2 = _G["KeyBindingFrameKeyBinding"..i.."Key2Button"]
+
+		button2:SetPoint("LEFT", button1, "RIGHT", 1, 0)
+
+		styleBindingButton(button1)
+		styleBindingButton(button2)
+	end
+
+	local line = _G["KeyBindingFrame"]:CreateTexture(nil, "ARTWORK")
+	line:SetSize(1, 546)
+	line:SetPoint("LEFT", 205, 10)
+	line:SetColorTexture(1, 1, 1, .2)
 end
 
 S:AddCallbackForAddon("Blizzard_BindingUI", "mUIBinding", styleBinding)
