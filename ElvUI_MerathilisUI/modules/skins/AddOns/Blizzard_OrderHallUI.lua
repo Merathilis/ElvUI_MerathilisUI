074c9d21541ae918869d1ff646a1755986a0641f
Merathilis <neo0608@gmail.com>
2018-06-08 12:16:47 +0200

    Fix TalentFrame nil error in the OrderHall skin.

--- ElvUI_MerathilisUI/modules/skins/AddOns/Blizzard_OrderHallUI.lua
+++ ElvUI_MerathilisUI/modules/skins/AddOns/Blizzard_OrderHallUI.lua
@@ -106,112 +106,114 @@ local function styleOrderhall()
 	-- Credits Simpy <3
 	-- Talent Frame
 	local TalentFrame = _G["OrderHallTalentFrame"]
-	TalentFrame:StripTextures()
-	TalentFrame.LeftInset:StripTextures()
-	TalentFrame:SetTemplate("Transparent")
-	TalentFrame.CurrencyIcon:SetAtlas("legionmission-icon-currency", false)
-	S:HandleCloseButton(TalentFrame.CloseButton)
-	S:HandleButton(TalentFrame.BackButton)
-	TalentFrame.BackButton:SetFrameLevel(TalentFrame.BackButton:GetFrameLevel()+2)
-	TalentFrame.BackButton:Point('BOTTOMRIGHT', TalentFrame, 'BOTTOMRIGHT', -2, 2)
+	if TalentFrame then
+		TalentFrame:StripTextures()
+		TalentFrame.LeftInset:StripTextures()
+		TalentFrame:SetTemplate("Transparent")
+		TalentFrame.CurrencyIcon:SetAtlas("legionmission-icon-currency", false)
+		S:HandleCloseButton(TalentFrame.CloseButton)
+		S:HandleButton(TalentFrame.BackButton)
+		TalentFrame.BackButton:SetFrameLevel(TalentFrame.BackButton:GetFrameLevel()+2)
+		TalentFrame.BackButton:Point('BOTTOMRIGHT', TalentFrame, 'BOTTOMRIGHT', -2, 2)
 
-	local TalentInset = _G["ClassHallTalentInset"]
-	local TalentClassBG = TalentFrame.Background
-	TalentInset:CreateBackdrop("Transparent")
-	TalentInset.backdrop:SetFrameLevel(TalentInset.backdrop:GetFrameLevel()+1)
-	TalentInset.backdrop:Point('TOPLEFT', TalentClassBG, 'TOPLEFT', E.Border-1, -E.Border+1)
-	TalentInset.backdrop:Point('BOTTOMRIGHT', TalentClassBG, 'BOTTOMRIGHT', -E.Border+1, E.Border-1)
-	TalentClassBG:SetAtlas("orderhalltalents-background-"..E.myclass)
-	TalentClassBG:SetDrawLayer("ARTWORK")
-	TalentClassBG:SetAlpha(0.8)
+		local TalentInset = _G["ClassHallTalentInset"]
+		local TalentClassBG = TalentFrame.Background
+		TalentInset:CreateBackdrop("Transparent")
+		TalentInset.backdrop:SetFrameLevel(TalentInset.backdrop:GetFrameLevel()+1)
+		TalentInset.backdrop:Point('TOPLEFT', TalentClassBG, 'TOPLEFT', E.Border-1, -E.Border+1)
+		TalentInset.backdrop:Point('BOTTOMRIGHT', TalentClassBG, 'BOTTOMRIGHT', -E.Border+1, E.Border-1)
+		TalentClassBG:SetAtlas("orderhalltalents-background-"..E.myclass)
+		TalentClassBG:SetDrawLayer("ARTWORK")
+		TalentClassBG:SetAlpha(0.8)
 
-	local function panelBackground(self)
-		local tab8 = _G["OrderHallTalentFrame8PanelBackground"];
-		if tab8 then
-			if TalentFrame.BackButton:IsShown() then tab8:Hide() else tab8:Show() end
-		else
-			for i = 1, 8 do
-				local bg = CreateFrame("Frame", "OrderHallTalentFrame"..i.."PanelBackground", self)
-				if i == 1 then
-					bg:Point("TOPLEFT", self, "TOPLEFT", E.PixelMode and 6 or 9, -80)
-				else
-					bg:Point("TOPLEFT", "OrderHallTalentFrame"..(i-1).."PanelBackground", "BOTTOMLEFT", 0, -6)
+		local function panelBackground(self)
+			local tab8 = _G["OrderHallTalentFrame8PanelBackground"];
+			if tab8 then
+				if TalentFrame.BackButton:IsShown() then tab8:Hide() else tab8:Show() end
+			else
+				for i = 1, 8 do
+					local bg = CreateFrame("Frame", "OrderHallTalentFrame"..i.."PanelBackground", self)
+					if i == 1 then
+						bg:Point("TOPLEFT", self, "TOPLEFT", E.PixelMode and 6 or 9, -80)
+					else
+						bg:Point("TOPLEFT", "OrderHallTalentFrame"..(i-1).."PanelBackground", "BOTTOMLEFT", 0, -6)
+					end
+					bg:SetTemplate("Transparent")
+					bg:SetBackdropColor(0, 0, 0, 0.5)
+					bg:SetSize(E.PixelMode and 322 or 316, 52)
 				end
-				bg:SetTemplate("Transparent")
-				bg:SetBackdropColor(0, 0, 0, 0.5)
-				bg:SetSize(E.PixelMode and 322 or 316, 52)
+				tab8 = _G["OrderHallTalentFrame8PanelBackground"];
+				if TalentFrame.BackButton:IsShown() then tab8:Hide() else tab8:Show() end
 			end
-			tab8 = _G["OrderHallTalentFrame8PanelBackground"];
-			if TalentFrame.BackButton:IsShown() then tab8:Hide() else tab8:Show() end
 		end
-	end
 
-	local function colorBorder(child, backdrop, atlas)
-		if child.AlphaIconOverlay:IsShown() then --isBeingResearched or (talentAvailability and not selected)
-			local alpha = child.AlphaIconOverlay:GetAlpha()
-			if alpha <= 0.5 then --talentAvailability
-				backdrop:SetBackdropBorderColor(0.5, 0.5, 0.5) --[border = grey, shadow x2]
-				child.darkOverlay:SetColorTexture(0, 0, 0, 0.50)
-				child.darkOverlay:Show()
-			elseif alpha <= 0.7 then --isBeingResearched
-				backdrop:SetBackdropBorderColor(0,1,1) --[border = teal, shadow x1]
-				child.darkOverlay:SetColorTexture(0, 0, 0, 0.25)
-				child.darkOverlay:Show()
-			end
-		elseif atlas == "orderhalltalents-spellborder-green" then
-			backdrop:SetBackdropBorderColor(0,1,0) --[border = green, no shadow]
-			child.darkOverlay:Hide()
-		elseif atlas == "orderhalltalents-spellborder-yellow" then
-			backdrop:SetBackdropBorderColor(1,1,0) --[border = yellow, no shadow]
-			child.darkOverlay:Hide()
-		elseif atlas == "orderhalltalents-spellborder" then
-			backdrop:SetBackdropBorderColor(unpack(E.media.bordercolor))
-			child.darkOverlay:SetColorTexture(0, 0, 0, 0.75) --[border will be default, shadow x3]
-			child.darkOverlay:Show()
-		end
-	end
-
-	TalentFrame:HookScript("OnShow", function(self)
-		panelBackground(self) -- Chromie is the original classAgnostic talent tree
-		if self.skinned then return end
-		for i=1, self:GetNumChildren() do
-			local child = select(i, self:GetChildren())
-			if child and child.Icon and not child.backdrop then
-				child:StyleButton()
-				child:CreateBackdrop()
-				child.Border:SetAlpha(0)
-				child.Highlight:SetAlpha(0)
-				child.AlphaIconOverlay:SetTexture(nil)
-				child.Icon:SetTexCoord(unpack(E.TexCoords))
-				child.Icon:SetInside(child.backdrop)
-				child.hover:SetInside(child.backdrop)
-				child.pushed:SetInside(child.backdrop)
-				child.backdrop:SetFrameLevel(child.backdrop:GetFrameLevel()+1)
-
-				child.darkOverlay = child:CreateTexture()
-				child.darkOverlay:SetAllPoints(child.Icon)
-				child.darkOverlay:SetDrawLayer('OVERLAY')
+		local function colorBorder(child, backdrop, atlas)
+			if child.AlphaIconOverlay:IsShown() then --isBeingResearched or (talentAvailability and not selected)
+				local alpha = child.AlphaIconOverlay:GetAlpha()
+				if alpha <= 0.5 then --talentAvailability
+					backdrop:SetBackdropBorderColor(0.5, 0.5, 0.5) --[border = grey, shadow x2]
+					child.darkOverlay:SetColorTexture(0, 0, 0, 0.50)
+					child.darkOverlay:Show()
+				elseif alpha <= 0.7 then --isBeingResearched
+					backdrop:SetBackdropBorderColor(0,1,1) --[border = teal, shadow x1]
+					child.darkOverlay:SetColorTexture(0, 0, 0, 0.25)
+					child.darkOverlay:Show()
+				end
+			elseif atlas == "orderhalltalents-spellborder-green" then
+				backdrop:SetBackdropBorderColor(0,1,0) --[border = green, no shadow]
 				child.darkOverlay:Hide()
-
-				colorBorder(child, child.backdrop, child.Border:GetAtlas())
-
-				child.TalentDoneAnim:HookScript("OnFinished", function(self)
-					child.Border:SetAlpha(0) -- clear the yellow glow border again, after it finishes the animation
-				end)
+			elseif atlas == "orderhalltalents-spellborder-yellow" then
+				backdrop:SetBackdropBorderColor(1,1,0) --[border = yellow, no shadow]
+				child.darkOverlay:Hide()
+			elseif atlas == "orderhalltalents-spellborder" then
+				backdrop:SetBackdropBorderColor(unpack(E.media.bordercolor))
+				child.darkOverlay:SetColorTexture(0, 0, 0, 0.75) --[border will be default, shadow x3]
+				child.darkOverlay:Show()
 			end
 		end
-		self.choiceTexturePool:ReleaseAll()
-		hooksecurefunc(self, "RefreshAllData", function(frame)
-			frame.choiceTexturePool:ReleaseAll()
-			for i=1, frame:GetNumChildren() do
-				local child = select(i, frame:GetChildren())
-				if child and child.Icon and child.backdrop then
+
+		TalentFrame:HookScript("OnShow", function(self)
+			panelBackground(self) -- Chromie is the original classAgnostic talent tree
+			if self.skinned then return end
+			for i=1, self:GetNumChildren() do
+				local child = select(i, self:GetChildren())
+				if child and child.Icon and not child.backdrop then
+					child:StyleButton()
+					child:CreateBackdrop()
+					child.Border:SetAlpha(0)
+					child.Highlight:SetAlpha(0)
+					child.AlphaIconOverlay:SetTexture(nil)
+					child.Icon:SetTexCoord(unpack(E.TexCoords))
+					child.Icon:SetInside(child.backdrop)
+					child.hover:SetInside(child.backdrop)
+					child.pushed:SetInside(child.backdrop)
+					child.backdrop:SetFrameLevel(child.backdrop:GetFrameLevel()+1)
+
+					child.darkOverlay = child:CreateTexture()
+					child.darkOverlay:SetAllPoints(child.Icon)
+					child.darkOverlay:SetDrawLayer('OVERLAY')
+					child.darkOverlay:Hide()
+
 					colorBorder(child, child.backdrop, child.Border:GetAtlas())
+
+					child.TalentDoneAnim:HookScript("OnFinished", function(self)
+						child.Border:SetAlpha(0) -- clear the yellow glow border again, after it finishes the animation
+					end)
 				end
 			end
+			self.choiceTexturePool:ReleaseAll()
+			hooksecurefunc(self, "RefreshAllData", function(frame)
+				frame.choiceTexturePool:ReleaseAll()
+				for i=1, frame:GetNumChildren() do
+					local child = select(i, frame:GetChildren())
+					if child and child.Icon and child.backdrop then
+						colorBorder(child, child.backdrop, child.Border:GetAtlas())
+					end
+				end
+			end)
+			self.skinned = true
 		end)
-		self.skinned = true
-	end)
+	end
 end
 
 local OrderHall_eframe = CreateFrame("Frame")
