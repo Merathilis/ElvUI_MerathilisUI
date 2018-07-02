038de4a1ec179891f77c8899a1de2c16ffa5c612
Merathilis <neo0608@gmail.com>
2018-07-02 12:16:33 +0200

    Let the Sliders on the options be in class color.

--- ElvUI_MerathilisUI/modules/skins/api.lua
+++ ElvUI_MerathilisUI/modules/skins/api.lua
@@ -439,6 +439,42 @@ function MERS:ReskinScrollSlider(Slider, thumbTrim)
 	end
 end
 
+-- ClassColored Sliders
+function MERS:ReskinSliderFrame(frame)
+	assert(frame)
+
+	local orientation = frame:GetOrientation()
+	local SIZE = 12
+
+	frame:StripTextures()
+	frame:CreateBackdrop('Default')
+	frame.backdrop:SetAllPoints()
+
+	hooksecurefunc(frame, "SetBackdrop", function(slider, backdrop)
+		if backdrop ~= nil then slider:SetBackdrop(nil) end
+	end)
+
+	frame:SetThumbTexture(E["media"].normTex)
+	frame:GetThumbTexture():SetVertexColor(rgbValueColorR, rgbValueColorG, rgbValueColorB)
+	frame:GetThumbTexture():Size(SIZE-2,SIZE-2)
+
+	if orientation == 'VERTICAL' then
+		frame:Width(SIZE)
+	else
+		frame:Height(SIZE)
+
+		for i=1, frame:GetNumRegions() do
+			local region = select(i, frame:GetRegions())
+			if region and region:GetObjectType() == 'FontString' then
+				local point, anchor, anchorPoint, x, y = region:GetPoint()
+				if anchorPoint:find('BOTTOM') then
+					region:Point(point, anchor, anchorPoint, x, y - 4)
+				end
+			end
+		end
+	end
+end
+
 -- Overwrite ElvUI Tabs function to be transparent
 function MERS:ReskinTab(tab)
 	if not tab then return end
@@ -758,6 +794,7 @@ hooksecurefunc(S, "HandleButton", MERS.Reskin)
 hooksecurefunc(S, "HandleCheckBox", MERS.ReskinCheckBox)
 hooksecurefunc(S, "HandleScrollBar", MERS.ReskinScrollBar)
 hooksecurefunc(S, "HandleScrollSlider", MERS.ReskinScrollSlider)
+hooksecurefunc(S, "HandleSliderFrame", MERS.ReskinSliderFrame)
 
 -- keep the colors updated
 local function updateMedia()
