9ebc0c9f6f0949030f9c3ea616be98607a0c0df7
Merathilis <neo0608@gmail.com>
2018-07-23 11:15:10 +0200

    Update the Communities Skin.

--- ElvUI_MerathilisUI/modules/skins/AddOns/Blizzard_Communities.lua
+++ ElvUI_MerathilisUI/modules/skins/AddOns/Blizzard_Communities.lua
@@ -5,8 +5,9 @@ local S = E:GetModule("Skins")
 --Cache global variables
 --Lua functions
 local _G = _G
+local unpack = unpack
 --WoW API / Variables
-
+local CreateFrame = CreateFrame
 --Global variables that we don't cache, list them here for the mikk's Find Globals script
 -- GLOBALS: hooksecurefunc
 
@@ -14,6 +15,59 @@ local function styleCommunities()
 	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.Communities ~= true or E.private.muiSkins.blizzard.communities ~= true then return end
 
 	_G["CommunitiesFrame"]:Styling()
+
+	-- Left Communities Buttons
+	hooksecurefunc(CommunitiesListEntryMixin, "SetClubInfo", function(self, clubInfo)
+		if clubInfo then
+			-- Hide the ElvUI backdrop
+			if self.bg then
+				self.bg:Hide()
+			end
+
+			self:Styling()
+		end
+	end)
+
+	hooksecurefunc(CommunitiesListEntryMixin, "SetAddCommunity", function(self)
+		-- Hide ElvUI backdrop
+		if self.bg then
+			self.bg:Hide()
+		end
+
+		self:Styling()
+	end)
+
+	for i = 1, 5 do
+		local button = _G["CommunitiesFrameContainerButton"..i]
+		if button.backdrop then
+			button.backdrop:Hide()
+		end
+		local bg = MERS:CreateBDFrame(button, 0)
+		MERS:CreateBD(bg, .25)
+	end
+
+	-- Guild Rewards
+	hooksecurefunc("CommunitiesGuildRewards_Update", function(self)
+		local scrollFrame = self.RewardsContainer
+		local offset = HybridScrollFrame_GetOffset(scrollFrame)
+		local buttons = scrollFrame.buttons
+		local button, index
+		local numButtons = #buttons
+
+		for i = 1, numButtons do
+			button = buttons[i]
+			index = offset + i
+
+			-- Hide the ElvUI backdrop
+			if button.backdrop then
+				button.backdrop:Hide()
+			end
+
+			MERS:CreateBD(button, .25)
+
+			button.index = index
+		end
+	end)
 end
 
 S:AddCallbackForAddon("Blizzard_Communities", "mUICommunities", styleCommunities)
