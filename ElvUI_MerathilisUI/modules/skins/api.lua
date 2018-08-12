fd65fc5d72a0e950bcb9b4269ec7d872ac02a03d
Merathilis <neo0608@gmail.com>
2018-08-12 15:01:05 +0200

    Don't apply my close texture if my close button is disabled. >.>

--- ElvUI_MerathilisUI/modules/skins/api.lua
+++ ElvUI_MerathilisUI/modules/skins/api.lua
@@ -81,7 +81,7 @@ function S:HandleCloseButton(f, point, text)
 	end
 
 	-- Use a own texture for the close button.
-	if not f.tex then
+	if E.private.muiSkins.closeButton and not f.tex then
 		f.tex = f:CreateTexture(nil, "OVERLAY")
 		f.tex:Size(12)
 		f.tex:Point("CENTER", -1, 0)
