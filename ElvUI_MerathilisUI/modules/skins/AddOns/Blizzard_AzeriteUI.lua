f48dafef398ec8e3e3443c8aec707760a268e25f
Merathilis <neo0608@gmail.com>
2018-08-14 23:15:58 +0200

    Update Azerite skin.

--- ElvUI_MerathilisUI/modules/skins/AddOns/Blizzard_AzeriteUI.lua
+++ ElvUI_MerathilisUI/modules/skins/AddOns/Blizzard_AzeriteUI.lua
@@ -5,7 +5,7 @@ local S = E:GetModule("Skins")
 --Cache global variables
 --Lua functions
 local _G = _G
-local select = select
+
 --WoW API / Variables
 
 --Global variables that we don't cache, list them here for the mikk's Find Globals script
@@ -14,72 +14,8 @@ local select = select
 local function styleAzerite()
 	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.AzeriteUI ~= true or E.private.muiSkins.blizzard.AzeriteUI ~= true then return end
 
-	--[[ AddOns\Blizzard_AzeriteUI.lua ]]
-	function MERS.AzeriteEmpoweredItemUIMixin_AdjustSizeForTiers(self, numTiers)
-		if numTiers == 3 then
-			self:SetSize(474, 460)
-		else
-			self:SetSize(615, 612)
-		end
-		self.transformTree:GetRoot():SetLocalPosition(CreateVector2D(self.ClipFrame.BackgroundFrame:GetWidth() * .5, self.ClipFrame.BackgroundFrame:GetHeight() * .5));
-	end
-
-	--[[ AddOns\Blizzard_AzeriteUI.xml ]]
-
-	--[[ Blizzard_AzeriteEmpoweredItemUITemplates.xml ]]
-	function MERS:AzeriteEmpoweredItemUITemplate(Frame)
-		hooksecurefunc(Frame, "AdjustSizeForTiers", MERS.AzeriteEmpoweredItemUIMixin_AdjustSizeForTiers)
-
-		Frame:SetClipsChildren(true)
-
-		Frame.ClipFrame:SetPoint("TOPLEFT")
-		Frame.ClipFrame:SetPoint("BOTTOMRIGHT")
-
-		local holder = CreateFrame("Frame", nil, Frame.ClipFrame.BackgroundFrame.KeyOverlay)
-		holder:SetFrameLevel(holder:GetFrameLevel() + 1)
-		holder:SetAllPoints(Frame.BorderFrame.TopBorder)
-		Frame.ClipFrame.BackgroundFrame.KeyOverlay.Shadow:SetParent(holder)
-		Frame.ClipFrame.BackgroundFrame.KeyOverlay.Shadow:SetAllPoints(holder)
-	end
-
-	----====####$$$$%%%%$$$$####====----
-	--  AzeriteEmpowedItemDataSource  --
-	----====####$$$$%%%%$$$$####====----
-
-	-------------
-	-- Section --
-	-------------
-
-	--[[ Scale ]]--
-
-	----====####$$$$%%%%%%%%%%%%%%$$$$####====----
-	-- Blizzard_AzeriteEmpoweredItemUITemplates --
-	----====####$$$$%%%%%%%%%%%%%%$$$$####====----
-
-	----====####$$$$%%%%$$$$####====----
-	--     AzeritePowerLayoutInfo     --
-	----====####$$$$%%%%$$$$####====----
-
-	----====####$$$$%%%%%$$$$####====----
-	--      AzeritePowerModelInfo      --
-	----====####$$$$%%%%%$$$$####====----
-
-	----====####$$$$%%%%$$$$####====----
-	-- AzeriteEmpoweredItemPowerMixin --
-	----====####$$$$%%%%$$$$####====----
-
-	----====####$$$$%%%%%$$$$####====----
-	--  AzeriteEmpoweredItemTierMixin  --
-	----====####$$$$%%%%%$$$$####====----
-
-	----====####$$$$%%%%%$$$$####====----
-	--  AzeriteEmpoweredItemSlotMixin  --
-	----====####$$$$%%%%%$$$$####====----
-
-	----====####$$$$%%%%%$$$$####====----
-	-- Blizzard_AzeriteEmpoweredItemUI --
-	----====####$$$$%%%%%$$$$####====----
-	MERS:AzeriteEmpoweredItemUITemplate(AzeriteEmpoweredItemUI)
+	local AzeriteEmpoweredItemUI = _G["AzeriteEmpoweredItemUI"]
+	AzeriteEmpoweredItemUI:Styling()
 end
 
 S:AddCallbackForAddon("Blizzard_AzeriteUI", "mUIAzerite", styleAzerite)
