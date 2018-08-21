e54223ee3030d957210ce10d28f2f93b42f2042f
Merathilis <neo0608@gmail.com>
2018-08-21 20:02:56 +0200

    TalkingHeadFrame skin adjustments

--- ElvUI_MerathilisUI/modules/skins/AddOns/Blizzard_TalkingHeadUI.lua
+++ ElvUI_MerathilisUI/modules/skins/AddOns/Blizzard_TalkingHeadUI.lua
@@ -19,15 +19,14 @@ local function styleTalkingHead()
 	TalkingHeadFrame.MainFrame.CloseButton:ClearAllPoints()
 	TalkingHeadFrame.MainFrame.CloseButton:Point("TOPRIGHT", TalkingHeadFrame.BackgroundFrame, "TOPRIGHT", 0, -2)
 
-	hooksecurefunc("TalkingHeadFrame_PlayCurrent", function()
-		local frame = TalkingHeadFrame
-		if frame:IsShown() then
-			frame.NameFrame.Name:SetTextColor(1, .8, 0)
-			frame.NameFrame.Name:SetShadowColor(0, 0, 0, 0)
-			frame.TextFrame.Text:SetTextColor(1, 1, 1)
-			frame.TextFrame.Text:SetShadowColor(0, 0, 0, 0)
-		end
-	end)
+	TalkingHeadFrame.NameFrame.Name:SetTextColor(1, 0.82, 0.02)
+	TalkingHeadFrame.NameFrame.Name.SetTextColor = MER.dummy
+	TalkingHeadFrame.NameFrame.Name:SetShadowColor(0.0, 0.0, 0.0, 1.0)
+
+	TalkingHeadFrame.TextFrame.Text:SetTextColor(1, 1, 1)
+	TalkingHeadFrame.TextFrame.Text.SetTextColor = MER.dummy
+	TalkingHeadFrame.TextFrame.Text:SetShadowColor(0.0, 0.0, 0.0, 1.0)
+
 end
 
 S:AddCallbackForAddon("Blizzard_TalkingHeadUI", "mUITalkingHead", styleTalkingHead)
