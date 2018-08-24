c499c974c3c5b34af7235a2b57b45d37cccf2b73
Merathilis <neo0608@gmail.com>
2018-08-24 11:26:10 +0200

    Update IslandQueueUI & QuestInfo skin.

--- ElvUI_MerathilisUI/modules/skins/FrameXML/QuestInfo.lua
+++ ElvUI_MerathilisUI/modules/skins/FrameXML/QuestInfo.lua
@@ -14,6 +14,31 @@ local _G = _G
 local function styleQuestInfo()
 	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.quest ~= true or E.private.muiSkins.blizzard.quest ~= true then return; end
 
+	local r, g, b = MER.ClassColor.r, MER.ClassColor.g, MER.ClassColor.b
+
+	-- [[ Item reward highlight ]]
+	QuestInfoItemHighlight:GetRegions():Hide()
+
+	local function clearHighlight()
+		for _, button in pairs(QuestInfoRewardsFrame.RewardButtons) do
+			button.bg:SetBackdropColor(0, 0, 0, .25)
+		end
+	end
+
+	local function setHighlight(self)
+		clearHighlight()
+
+		local _, point = self:GetPoint()
+		if point then
+			point.bg:SetBackdropColor(r, g, b, .2)
+		end
+	end
+
+	hooksecurefunc(QuestInfoItemHighlight, "SetPoint", setHighlight)
+	QuestInfoItemHighlight:HookScript("OnShow", setHighlight)
+	QuestInfoItemHighlight:HookScript("OnHide", clearHighlight)
+
+	-- [[ Shared ]]
 	local function restyleSpellButton(bu)
 		local name = bu:GetName()
 		local icon = bu.Icon
@@ -23,195 +48,211 @@ local function styleQuestInfo()
 
 		icon:SetPoint("TOPLEFT", 3, -2)
 		icon:SetDrawLayer("ARTWORK")
-		icon:SetTexCoord(.08, .92, .08, .92)
+		icon:SetTexCoord(unpack(E.TexCoords))
 		MERS:CreateBG(icon)
 
-		local bg = _G.CreateFrame("Frame", nil, bu)
+		local bg = CreateFrame("Frame", nil, bu)
 		bg:SetPoint("TOPLEFT", 2, -1)
 		bg:SetPoint("BOTTOMRIGHT", 0, 14)
 		bg:SetFrameLevel(0)
 		MERS:CreateBD(bg, .25)
 	end
 
-	local function colourObjectivesText()
+	-- [[ Objectives ]]
+	restyleSpellButton(QuestInfoSpellObjectiveFrame)
+
+	local function colorObjectivesText()
 		if not QuestInfoFrame.questLog then return end
 
 		local objectivesTable = QuestInfoObjectivesFrame.Objectives
 		local numVisibleObjectives = 0
 
 		for i = 1, GetNumQuestLeaderBoards() do
-			local _, objectiveType, isCompleted = GetQuestLogLeaderBoard(i)
+			local _, type, finished = GetQuestLogLeaderBoard(i)
 
-			if (objectiveType ~= "spell" and objectiveType ~= "log" and numVisibleObjectives < MAX_OBJECTIVES) then
+			if (type ~= "spell" and type ~= "log" and numVisibleObjectives < MAX_OBJECTIVES) then
 				numVisibleObjectives = numVisibleObjectives + 1
 				local objective = objectivesTable[numVisibleObjectives]
 
-				if isCompleted then
-					objective:SetTextColor(34/255, 255/255, 0/255)
-				else
-					objective:SetTextColor(1, 1, 1)
+				if objective then
+					if finished then
+						objective:SetTextColor(34/255, 255/255, 0/255)
+					else
+						objective:SetTextColor(1, 1, 1)
+					end
 				end
 			end
 		end
 	end
 
-	local SkinQuestText
-	local function MERS_SetTextColor(self, red, green, blue)
-		if self.settingFont then return end
-		self.settingFont = true
-		self:SetTextColor(1, 1, 1)
-		self.settingFont = nil
-	end
+	hooksecurefunc("QuestMapFrame_ShowQuestDetails", colorObjectivesText)
+	hooksecurefunc("QuestInfo_Display", colorObjectivesText)
 
-	function SkinQuestText(font, hasShadow)
-		if hasShadow then
-			font:SetShadowColor(0.3, 0.3, 0.3)
+	-- [[ Quest rewards ]]
+
+	local function restyleRewardButton(bu, isMapQuestInfo)
+		bu.NameFrame:Hide()
+
+		bu.Icon:SetTexCoord(unpack(E.TexCoords))
+		bu.Icon:SetDrawLayer("BACKGROUND", 1)
+		MERS:CreateBG(bu.Icon, 1)
+		if bu.IconBorder then
+			bu.IconBorder:SetAlpha(0)
 		end
-		font:SetTextColor(1, 1, 1)
-		hooksecurefunc(font, "SetTextColor", MERS_SetTextColor)
+
+		local bg = MERS:CreateBDFrame(bu, .25)
+		bg:SetPoint("TOPLEFT", bu, 1, 1)
+
+		if isMapQuestInfo then
+			bg:SetPoint("BOTTOMRIGHT", bu, -3, 0)
+			bu.Icon:SetSize(29, 29)
+		else
+			bg:SetPoint("BOTTOMRIGHT", bu, -3, 1)
+		end
+
+		bu.bg = bg
 	end
 
-	hooksecurefunc("QuestMapFrame_ShowQuestDetails", colourObjectivesText)
+	hooksecurefunc("QuestInfo_GetRewardButton", function(rewardsFrame, index)
+		local bu = rewardsFrame.RewardButtons[index]
+
+		if not bu.restyled then
+			restyleRewardButton(bu, rewardsFrame == MapQuestInfoRewardsFrame)
+
+			bu.restyled = true
+		end
+	end)
+
+	MapQuestInfoRewardsFrame.XPFrame.Name:SetShadowOffset(0, 0)
+	for _, name in next, {"HonorFrame", "MoneyFrame", "SkillPointFrame", "XPFrame", "ArtifactXPFrame", "TitleFrame"} do
+		restyleRewardButton(MapQuestInfoRewardsFrame[name], true)
+	end
+
+	for _, name in next, {"HonorFrame", "SkillPointFrame", "ArtifactXPFrame"} do
+		restyleRewardButton(QuestInfoRewardsFrame[name])
+	end
+
+	-- Spell Rewards
+
+	local spellRewards = {QuestInfoRewardsFrame, MapQuestInfoRewardsFrame}
+	for _, rewardFrame in pairs(spellRewards) do
+		local spellRewardFrame = rewardFrame.spellRewardPool:Acquire()
+		local icon = spellRewardFrame.Icon
+		local nameFrame = spellRewardFrame.NameFrame
+
+		icon:SetTexCoord(unpack(E.TexCoords))
+		MERS:CreateBDFrame(icon)
+		nameFrame:Hide()
+		local bg = MERS:CreateBDFrame(nameFrame, .25)
+		bg:SetPoint("TOPLEFT", icon, "TOPRIGHT", 0, 2)
+		bg:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", 101, -1)
+	end
+
+	-- Title Reward
+	do
+		local frame = QuestInfoPlayerTitleFrame
+		local icon = frame.Icon
+
+		icon:SetTexCoord(unpack(E.TexCoords))
+		MERS:CreateBDFrame(icon)
+		for i = 2, 4 do
+			select(i, frame:GetRegions()):Hide()
+		end
+		local bg = MERS:CreateBDFrame(frame, .25)
+		bg:SetPoint("TOPLEFT", icon, "TOPRIGHT", 0, 2)
+		bg:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", 220, -1)
+	end
+
+	-- Follower Rewards
 	hooksecurefunc("QuestInfo_Display", function(template, parentFrame, acceptButton, material, mapView)
 		local rewardsFrame = QuestInfoFrame.rewardsFrame
 		local isQuestLog = QuestInfoFrame.questLog ~= nil
 		local isMapQuest = rewardsFrame == MapQuestInfoRewardsFrame
+		local numSpellRewards = isQuestLog and GetNumQuestLogRewardSpells() or GetNumRewardSpells()
 
-		colourObjectivesText()
-
-		if ( template.canHaveSealMaterial ) then
+		if (template.canHaveSealMaterial) then
 			local questFrame = parentFrame:GetParent():GetParent()
 			questFrame.SealMaterialBG:Hide()
 		end
 
-		local numSpellRewards = isQuestLog and GetNumQuestLogRewardSpells() or GetNumRewardSpells()
 		if numSpellRewards > 0 then
-			-- Spell Headers
-			for spellHeader in rewardsFrame.spellHeaderPool:EnumerateActive() do
-				spellHeader:SetVertexColor(1, 1, 1)
-			end
-			-- Follower Rewards
-			for followerReward in rewardsFrame.followerRewardPool:EnumerateActive() do
-				if not followerReward.isSkinned then
-					followerReward.PortraitFrame:SetScale(1)
-					followerReward.PortraitFrame:ClearAllPoints()
-					followerReward.PortraitFrame:SetPoint("TOPLEFT")
-					if isMapQuest then
-						followerReward.PortraitFrame.Portrait:SetSize(29, 29)
-					end
-
-					followerReward.BG:Hide()
-					followerReward.BG:SetPoint("TOPLEFT", followerReward.PortraitFrame, "TOPRIGHT")
-					followerReward.BG:SetPoint("BOTTOMRIGHT")
-					MERS:CreateBD(followerReward, .25)
-					followerReward:SetHeight(followerReward.PortraitFrame:GetHeight())
-
-					if not isMapQuest then
-						followerReward.Class:SetWidth(45)
-					end
-
-					followerReward.isSkinned = true
+			for reward in rewardsFrame.followerRewardPool:EnumerateActive() do
+				local portrait = reward.PortraitFrame
+				if not reward.styled then
+					portrait:ClearAllPoints()
+					portrait:SetPoint("TOPLEFT", 2, -5)
+					MERS:ReskinGarrisonPortrait(portrait)
+					reward.BG:Hide()
+					local bg = MERS:CreateBDFrame(reward, .25)
+					bg:SetPoint("TOPLEFT", 0, -3)
+					bg:SetPoint("BOTTOMRIGHT", 2, 7)
+					reward.styled = true
 				end
-				followerReward.PortraitFrame:SetBackdropBorderColor(followerReward.PortraitFrame.PortraitRingQuality:GetVertexColor())
-			end
-			-- Spell Rewards
-			for spellReward in rewardsFrame.spellRewardPool:EnumerateActive() do
-				if not spellReward.isSkinned then
-					if isMapQuest then
-						MERS:SmallItemButtonTemplate(spellReward)
-					else
-						MERS:LargeItemButtonTemplate(spellReward)
-
-						select(3, spellReward:GetRegions()):Hide() --border
-						spellReward.Icon:SetPoint("TOPLEFT", 0, 0)
-						spellReward:SetHitRectInsets(0,0,0,0)
-						spellReward:SetSize(147, 41)
-					end
-					spellReward.isSkinned = true
+				if portrait then
+					local color = BAG_ITEM_QUALITY_COLORS[portrait.quality or 1]
+					portrait.squareBG:SetBackdropBorderColor(color.r, color.g, color.b)
 				end
 			end
 		end
 	end)
 
-	hooksecurefunc("QuestInfo_GetRewardButton", function(rewardsFrame, index)
-		local button = rewardsFrame.RewardButtons[index]
+	-- [[ Change text colors ]]
 
-		if not button.restyled then
-			if rewardsFrame == MapQuestInfoRewardsFrame then
-				MERS:SmallItemButtonTemplate(button)
-			else
-				MERS:LargeItemButtonTemplate(button)
-			end
-			button.restyled = true
-		end
-	end)
-
-	restyleSpellButton(QuestInfoSpellObjectiveFrame)
-	SkinQuestText(QuestInfoSpellObjectiveLearnLabel)
-
-	--[[ QuestInfoRewardsFrame ]]
-	local QuestInfoRewardsFrame = _G["QuestInfoRewardsFrame"]
-	SkinQuestText(QuestInfoRewardsFrame.Header, true)
-	SkinQuestText(QuestInfoRewardsFrame.ItemChooseText)
-	SkinQuestText(QuestInfoRewardsFrame.ItemReceiveText)
-	SkinQuestText(QuestInfoRewardsFrame.PlayerTitleText)
-
-	for i, name in next, {"HonorFrame", "SkillPointFrame", "ArtifactXPFrame"} do
-		MERS:LargeItemButtonTemplate(QuestInfoRewardsFrame[name])
-	end
-	SkinQuestText(QuestInfoRewardsFrame.XPFrame.ReceiveText)
-
-	local QuestInfoPlayerTitleFrame = _G["QuestInfoPlayerTitleFrame"]
-	QuestInfoPlayerTitleFrame.FrameLeft:Hide()
-	QuestInfoPlayerTitleFrame.FrameCenter:Hide()
-	QuestInfoPlayerTitleFrame.FrameRight:Hide()
-
-	local titleBG = CreateFrame("Frame", nil, QuestInfoPlayerTitleFrame)
-	titleBG:SetPoint("TOPLEFT", QuestInfoPlayerTitleFrame.FrameLeft, -2, 0)
-	titleBG:SetPoint("BOTTOMRIGHT", QuestInfoPlayerTitleFrame.FrameRight, 0, -1)
-	MERS:CreateBD(titleBG, .25)
-
-	local ItemHighlight = QuestInfoRewardsFrame.ItemHighlight
-	ItemHighlight:GetRegions():Hide()
-
-	--[[ MapQuestInfoRewardsFrame ]]
-	for i, name in next, {"HonorFrame", "MoneyFrame", "SkillPointFrame", "XPFrame", "ArtifactXPFrame", "TitleFrame"} do
-		MERS:SmallItemButtonTemplate(MapQuestInfoRewardsFrame[name])
-	end
-	MapQuestInfoRewardsFrame.XPFrame.Name:SetShadowOffset(0, 0)
-
-	--[[ QuestInfoFrame ]]
-	SkinQuestText(QuestInfoTitleHeader, true)
-	SkinQuestText(QuestInfoObjectivesText)
-	SkinQuestText(QuestInfoRewardText)
-
-	hooksecurefunc(QuestInfoRequiredMoneyText, "SetTextColor", function(self, red, green, blue)
-		if red == 0 then
+	hooksecurefunc(QuestInfoRequiredMoneyText, "SetTextColor", function(self, r)
+		if r == 0 then
 			self:SetTextColor(.8, .8, .8)
-		elseif red == .2 then
+		elseif r == .2 then
 			self:SetTextColor(1, 1, 1)
 		end
 	end)
 
-	SkinQuestText(QuestInfoGroupSize)
-	SkinQuestText(QuestInfoDescriptionHeader, true)
-	SkinQuestText(QuestInfoObjectivesHeader, true)
-	SkinQuestText(QuestInfoDescriptionText)
+	QuestInfoTitleHeader:SetTextColor(1, 1, 1)
+	QuestInfoTitleHeader.SetTextColor = MER.dummy
+	QuestInfoTitleHeader:SetShadowColor(0, 0, 0)
 
-	--[= QuestInfoSealFrame =]
-	local mask = QuestInfoSealFrame:CreateMaskTexture(nil, "BACKGROUND")
-	mask:SetTexture([[Interface/QuestionFrame/answer-ChromieScenario-Chromie]], "CLAMPTOBLACKADDITIVE", "CLAMPTOBLACKADDITIVE")
-	mask:SetTexCoord(0, 0.5, 0, 0.5)
-	mask:SetPoint("TOPLEFT", QuestInfoSealFrame.Text, -28, 6)
-	mask:SetPoint("BOTTOMRIGHT", QuestInfoSealFrame.Text, 24, -5)
+	QuestInfoDescriptionHeader:SetTextColor(1, 1, 1)
+	QuestInfoDescriptionHeader.SetTextColor = MER.dummy
+	QuestInfoDescriptionHeader:SetShadowColor(0, 0, 0)
 
-	local bg = QuestInfoSealFrame:CreateTexture(nil, "BACKGROUND")
-	bg:SetColorTexture(0, 0, 0, 0.25)
-	bg:SetAllPoints(mask)
-	bg:AddMaskTexture(mask)
+	QuestInfoObjectivesHeader:SetTextColor(1, 1, 1)
+	QuestInfoObjectivesHeader.SetTextColor = MER.dummy
+	QuestInfoObjectivesHeader:SetShadowColor(0, 0, 0)
 
-	QuestInfoSealFrame.Text:SetShadowColor(103/255, 103/255, 103/255)
-	QuestInfoSealFrame.Text:SetShadowOffset(0.6, -0.6)
+	QuestInfoRewardsFrame.Header:SetTextColor(1, 1, 1)
+	QuestInfoRewardsFrame.Header.SetTextColor = MER.dummy
+	QuestInfoRewardsFrame.Header:SetShadowColor(0, 0, 0)
+
+	QuestInfoDescriptionText:SetTextColor(1, 1, 1)
+	QuestInfoDescriptionText.SetTextColor = MER.dummy
+
+	QuestInfoObjectivesText:SetTextColor(1, 1, 1)
+	QuestInfoObjectivesText.SetTextColor = MER.dummy
+
+	QuestInfoGroupSize:SetTextColor(1, 1, 1)
+	QuestInfoGroupSize.SetTextColor = MER.dummy
+
+	QuestInfoRewardText:SetTextColor(1, 1, 1)
+	QuestInfoRewardText.SetTextColor = MER.dummy
+
+	QuestInfoSpellObjectiveLearnLabel:SetTextColor(1, 1, 1)
+	QuestInfoSpellObjectiveLearnLabel.SetTextColor = MER.dummy
+
+	QuestInfoRewardsFrame.ItemChooseText:SetTextColor(1, 1, 1)
+	QuestInfoRewardsFrame.ItemChooseText.SetTextColor = MER.dummy
+
+	QuestInfoRewardsFrame.ItemReceiveText:SetTextColor(1, 1, 1)
+	QuestInfoRewardsFrame.ItemReceiveText.SetTextColor = MER.dummy
+
+	QuestInfoRewardsFrame.PlayerTitleText:SetTextColor(1, 1, 1)
+	QuestInfoRewardsFrame.PlayerTitleText.SetTextColor = MER.dummy
+
+	QuestInfoRewardsFrame.XPFrame.ReceiveText:SetTextColor(1, 1, 1)
+	QuestInfoRewardsFrame.XPFrame.ReceiveText.SetTextColor = MER.dummy
+
+	QuestInfoRewardsFrame.spellHeaderPool:Acquire():SetVertexColor(1, 1, 1)
+	QuestInfoRewardsFrame.spellHeaderPool:Acquire().SetVertexColor = MER.dummy
+
+	QuestFont:SetTextColor(1, 1, 1)
 end
 
 S:AddCallback("mUIQuestInfo", styleQuestInfo)
