7fc6993bf925d775f87ee36cc5a6a518cb7bee02
Merathilis <neo0608@gmail.com>
2018-08-26 22:43:44 +0200

    Skin updates.

--- ElvUI_MerathilisUI/modules/skins/AddOns/Blizzard_TalkingHeadUI.lua
+++ ElvUI_MerathilisUI/modules/skins/AddOns/Blizzard_TalkingHeadUI.lua
@@ -16,17 +16,23 @@ local function styleTalkingHead()
 	MERS:CreateBD(TalkingHeadFrame.BackgroundFrame, .5)
 	TalkingHeadFrame.BackgroundFrame:Styling()
 
+	TalkingHeadFrame.MainFrame.Model.ModelShadow = TalkingHeadFrame.MainFrame.Model:CreateTexture(nil, "OVERLAY", nil, 2)
+	TalkingHeadFrame.MainFrame.Model.ModelShadow:SetAtlas("Artifacts-BG-Shadow")
+	TalkingHeadFrame.MainFrame.Model.ModelShadow:SetOutside()
+	TalkingHeadFrame.MainFrame.Model.PortraitBg:Hide()
+
 	TalkingHeadFrame.MainFrame.CloseButton:ClearAllPoints()
 	TalkingHeadFrame.MainFrame.CloseButton:Point("TOPRIGHT", TalkingHeadFrame.BackgroundFrame, "TOPRIGHT", 0, -2)
 
 	TalkingHeadFrame.NameFrame.Name:SetTextColor(1, 0.82, 0.02)
 	TalkingHeadFrame.NameFrame.Name.SetTextColor = MER.dummy
 	TalkingHeadFrame.NameFrame.Name:SetShadowColor(0.0, 0.0, 0.0, 1.0)
+	TalkingHeadFrame.NameFrame.Name:SetShadowOffset(2, -2)
 
 	TalkingHeadFrame.TextFrame.Text:SetTextColor(1, 1, 1)
 	TalkingHeadFrame.TextFrame.Text.SetTextColor = MER.dummy
 	TalkingHeadFrame.TextFrame.Text:SetShadowColor(0.0, 0.0, 0.0, 1.0)
-
+	TalkingHeadFrame.TextFrame.Text:SetShadowOffset(2, -2)
 end
 
 S:AddCallbackForAddon("Blizzard_TalkingHeadUI", "mUITalkingHead", styleTalkingHead)
