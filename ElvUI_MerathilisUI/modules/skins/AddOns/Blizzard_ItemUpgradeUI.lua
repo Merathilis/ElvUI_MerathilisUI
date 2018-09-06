246d669d7ca0d83e435f3a621ab9b13628f788c7
Merathilis <neo0608@gmail.com>
2018-09-06 12:35:26 +0200

    Copy & Paste fail like a baws. >.>

--- ElvUI_MerathilisUI/modules/skins/AddOns/Blizzard_ItemUpgradeUI.lua
+++ ElvUI_MerathilisUI/modules/skins/AddOns/Blizzard_ItemUpgradeUI.lua
@@ -11,7 +11,7 @@ local _G = _G
 --Global variables that we don't cache, list them here for the mikk's Find Globals script
 -- GLOBALS:
 
-local function styleSocketing()
+local function styleItemUpgrade()
 	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.itemUpgrade ~= true or E.private.muiSkins.blizzard.itemUpgrade ~= true then return end
 
 	local ItemUpgradeFrame = _G["ItemUpgradeFrame"]
@@ -75,4 +75,4 @@ local function styleSocketing()
 	MERS:CreateBD(bg)
 end
 
-S:AddCallbackForAddon("Blizzard_ItemUpgradeUI", "mUISocketing", styleSocketing)
+S:AddCallbackForAddon("Blizzard_ItemUpgradeUI", "mUIItemUpgrade", styleItemUpgrade)
