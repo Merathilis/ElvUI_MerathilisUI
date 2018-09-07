30fe54a255ca9a7cd985404058a01ef4d28ac1bf
Merathilis <neo0608@gmail.com>
2018-09-07 19:30:45 +0200

    Moving stuff around

--- ElvUI_MerathilisUI/modules/skins/FrameXML/PaperDollFrame.lua
+++ ElvUI_MerathilisUI/modules/skins/FrameXML/PaperDollFrame.lua
@@ -16,6 +16,8 @@ local r, g, b = unpack(E["media"].rgbvaluecolor)
 local function styleCPaperDollFrame()
 	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.character ~= true or E.private.muiSkins.blizzard.character ~= true then return end
 
+	local CharacterStatsPane = _G["CharacterStatsPane"]
+
 	CharacterModelFrame:DisableDrawLayer("BACKGROUND")
 	CharacterModelFrame:DisableDrawLayer("BORDER")
 	CharacterModelFrame:DisableDrawLayer("OVERLAY")
@@ -42,6 +44,48 @@ local function styleCPaperDollFrame()
 		border:SetPoint("BOTTOMRIGHT", 1, -1)
 		border:SetDrawLayer("BACKGROUND")
 		MERS:CreateBDFrame(slot, .25)
+	end
+
+	_G["CharacterStatsPane"].ItemLevelCategory.Title:SetTextColor(unpack(E.media.rgbvaluecolor))
+	_G["CharacterStatsPane"].AttributesCategory.Title:SetTextColor(unpack(E.media.rgbvaluecolor))
+	_G["CharacterStatsPane"].EnhancementsCategory.Title:SetTextColor(unpack(E.media.rgbvaluecolor))
+
+	local function StatsPane(type)
+		CharacterStatsPane[type]:StripTextures()
+		CharacterStatsPane[type].backdrop:Hide()
+	end
+
+	local function CharacterStatFrameCategoryTemplate(Button)
+		local bg = Button.Background
+		bg:SetTexture([[Interface\LFGFrame\UI-LFG-SEPARATOR]])
+		bg:SetTexCoord(0, 0.6640625, 0, 0.3125)
+		bg:ClearAllPoints()
+		bg:SetPoint("CENTER", 0, -5)
+		bg:SetSize(210, 30)
+
+		bg:SetVertexColor(r * 0.7, g * 0.7, b * 0.7)
+	end
+
+	StatsPane("EnhancementsCategory")
+	StatsPane("ItemLevelCategory")
+	StatsPane("AttributesCategory")
+	CharacterStatFrameCategoryTemplate(CharacterStatsPane.ItemLevelCategory)
+	CharacterStatFrameCategoryTemplate(CharacterStatsPane.AttributesCategory)
+	CharacterStatFrameCategoryTemplate(CharacterStatsPane.EnhancementsCategory)
+
+	if IsAddOnLoaded("ElvUI_SLE") then
+		PaperDollFrame:HookScript("OnShow", function()
+			if _G["CharacterStatsPane"].DefenceCategory then
+				_G["CharacterStatsPane"].DefenceCategory.Title:SetTextColor(unpack(E.media.rgbvaluecolor))
+				StatsPane("DefenceCategory")
+				CharacterStatFrameCategoryTemplate(CharacterStatsPane.DefenceCategory)
+			end
+			if _G["CharacterStatsPane"].OffenseCategory then
+				_G["CharacterStatsPane"].OffenseCategory.Title:SetTextColor(unpack(E.media.rgbvaluecolor))
+				StatsPane("OffenseCategory")
+				CharacterStatFrameCategoryTemplate(CharacterStatsPane.OffenseCategory)
+			end
+		end)
 	end
 end
 
