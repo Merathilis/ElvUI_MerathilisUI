8225c6f98493c63ee8f14ff9d5861cfe6e2e67e1
Merathilis <neo0608@gmail.com>
2018-09-09 13:43:12 +0200

    This works

--- ElvUI_MerathilisUI/modules/skins/Extern/DeadlyBossMods.lua
+++ ElvUI_MerathilisUI/modules/skins/Extern/DeadlyBossMods.lua
@@ -17,25 +17,26 @@ local function DbmDecor(event)
 	local function StyleRangeFrame(self, range, filter, forceshow, redCircleNumPlayers)
 		--if DBM.Options.DontShowRangeFrame and not forceshow then return end
 
-		if DBMRangeCheckRadar and not DBMRangeCheckRadar.IsStyled then
+		if not DBMRangeCheckRadar.IsStyled then
 			DBMRangeCheckRadar:Styling()
+
 			DBMRangeCheckRadar.IsStyled = true
-			print("style")
 		end
 
-		if DBMRangeCheck and not DBMRangeCheck.IsStyled then
+		if not DBMRangeCheck.IsStyled then
 			DBMRangeCheck:SetTemplate('Transparent')
 			DBMRangeCheck:Styling()
 			DBMRangeCheck.IsStyled = true
-			print("style")
 		end
 	end
 
 	local function StyleInfoFrame(self, maxLines, event, ...)
 		if DBM.Options.DontShowInfoFrame and (event or 0) ~= "test" then return end
 
-		if DBMInfoFrame then
+		if not DBMInfoFrame.IsStyled then
 			DBMInfoFrame:Styling()
+
+			DBMInfoFrame.IsStyled = true
 		end
 	end
 
@@ -43,4 +44,4 @@ local function DbmDecor(event)
 	hooksecurefunc(DBM.InfoFrame, 'Show', StyleInfoFrame)
 end
 
-if AS:CheckAddOn('DBM-Core') then AS:RegisterSkin('DBM', DbmDecor, 2) end
+if AS:CheckAddOn('DBM-Core') then AS:RegisterSkin('DBM', DbmDecor, 'ADDON_LOADED') end
