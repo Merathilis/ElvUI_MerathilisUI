80462cb15656508adc7a0dd02858a885fb036fd1
Merathilis <neo0608@gmail.com>
2018-09-16 11:37:51 +0200

    Refactor NamePlates.

---
+++ ElvUI_MerathilisUI/modules/nameplates/nameplates.lua
@@ -0,0 +1,22 @@
+local MER, E, L, V, P, G = unpack(select(2, ...))
+local MNP = MER:NewModule('mUINamePlates', 'AceHook-3.0', 'AceEvent-3.0', 'AceTimer-3.0')
+local NP = E:GetModule('NamePlates')
+
+--Cache global variables
+--Lua functions
+--WoW API / Variables
+
+--Global variables that we don't cache, list them here for the mikk's Find Globals script
+-- GLOBALS:
+
+
+function MNP:Initialize()
+	if E.private.nameplates.enable ~= true then return end
+
+end
+
+local function InitializeCallback()
+	MNP:Initialize()
+end
+
+--MER:RegisterModule(MNP:GetName(), InitializeCallback)
