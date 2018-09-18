614dbcadbed2a463f73db0588295c54893ea149e
Merathilis <neo0608@gmail.com>
2018-09-18 11:46:11 +0200

    Update Blizzard_ChallengesUI skin Add skinning support for AngryKeystone

--- ElvUI_MerathilisUI/modules/skins/AddOns/Blizzard_ChallengesUI.lua
+++ ElvUI_MerathilisUI/modules/skins/AddOns/Blizzard_ChallengesUI.lua
@@ -5,82 +5,60 @@ local S = E:GetModule("Skins")
 -- Cache global variables
 -- Lua functions
 local _G = _G
+local ipairs, select, unpack = ipairs, select, unpack
 -- WoW API
-
+local C_ChallengeMode_GetAffixInfo = C_ChallengeMode.GetAffixInfo
+local hooksecurefunc = hooksecurefunc
+local IsAddOnLoaded = IsAddOnLoaded
 -- Global variables that we don't cache, list them here for the mikk's Find Globals script
 -- GLOBALS:
 
 local function styleChallenges()
-	if E.private.skins.blizzard.enable ~= true or E.private.muiSkins.blizzard.challenges ~= true then return end
+	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.lfg ~= true or E.private.muiSkins.blizzard.challenges ~= true then return end
 
-	--[[ AddOns\Blizzard_ChallengesUI.lua ]]
-	function MERS.ChallengesFrame_Update(self)
-		for i, icon in ipairs(self.DungeonIcons) do
-			if i > (self._skinnedIcons or 0) then
-				MERS:ChallengesDungeonIconFrameTemplate(icon)
-				self._skinnedIcons = i
+	local KeyStoneFrame = _G["ChallengesKeystoneFrame"]
+	KeyStoneFrame:Styling()
+
+	-- Copied from ElvUI
+	local function HandleAffixIcons(self)
+		for _, frame in ipairs(self.Affixes) do
+			frame.Border:SetTexture(nil)
+			frame.Portrait:SetTexture(nil)
+			if not frame.bg then
+				frame.bg = MERS:ReskinIcon(frame.Portrait)
+			end
+
+			if frame.info then
+				frame.Portrait:SetTexture(CHALLENGE_MODE_EXTRA_AFFIX_INFO[frame.info.key].texture)
+			elseif frame.affixID then
+				local _, _, filedataid = C_ChallengeMode_GetAffixInfo(frame.affixID)
+				frame.Portrait:SetTexture(filedataid)
 			end
 		end
 	end
 
-	function MERS.ChallengesKeystoneFrameMixin_Reset(self)
-		self:GetRegions():Hide()
-		self.InstructionBackground:Hide()
-	end
-
-	function MERS.ChallengesKeystoneFrameMixin_OnKeystoneSlotted(self, affixInfo)
-		for i, affix in ipairs(self.Affixes) do
-			if i > (self._skinnedAffixes or 0) then
-				MERS:ChallengesKeystoneFrameAffixTemplate(affix)
-				self._skinnedAffixes = i
+	-- Angy Keystone Skinning
+	local angryStyle
+	local function UpdateIcons(self)
+		if IsAddOnLoaded("AngryKeystones") and not angryStyle then
+			local scheduel, party = select(4, self:GetChildren())
+			scheduel:GetRegions():SetAlpha(0)
+			select(3, scheduel:GetRegions()):SetAlpha(0)
+			MERS:CreateBD(scheduel, .3)
+			if scheduel.Entries then
+				for i = 1, 3 do
+					HandleAffixIcons(scheduel.Entries[i])
+				end
 			end
 
-			affix.Portrait:SetTexture(nil)
-			if affix.info then
-				affix.Portrait:SetTexture(CHALLENGE_MODE_EXTRA_AFFIX_INFO[affix.info.key].texture)
-			elseif affix.affixID then
-				local _, _, filedataid = C_ChallengeMode.GetAffixInfo(affix.affixID)
-				affix.Portrait:SetTexture(filedataid)
-			end
+			party:GetRegions():SetAlpha(0)
+			select(3, party:GetRegions()):SetAlpha(0)
+			MERS:CreateBD(party, .3)
+
+			angryStyle = true
 		end
 	end
-
-	--[[ AddOns\Blizzard_ChallengesUI.xml ]]
-	function MERS:ChallengesDungeonIconFrameTemplate(Frame)
-		Frame:GetRegions():Hide()
-		S:CropIcon(Frame.Icon, Frame)
-	end
-
-	function MERS:ChallengesKeystoneFrameAffixTemplate(Frame)
-		Frame.Border:Hide()
-		S:CropIcon(Frame.Portrait)
-	end
-
-	hooksecurefunc("ChallengesFrame_Update", MERS.ChallengesFrame_Update)
-
-	-----------------------------
-	-- ChallengesKeystoneFrame --
-	-----------------------------
-	-- /run ChallengesKeystoneFrame:Show()
-	local ChallengesKeystoneFrame = _G["ChallengesKeystoneFrame"]
-	hooksecurefunc(ChallengesKeystoneFrame, "Reset", MERS.ChallengesKeystoneFrameMixin_Reset)
-	hooksecurefunc(ChallengesKeystoneFrame, "OnKeystoneSlotted", MERS.ChallengesKeystoneFrameMixin_OnKeystoneSlotted)
-
-	ChallengesKeystoneFrame:GetRegions():Hide()
-
-	---------------------
-	-- ChallengesFrame --
-	---------------------
-	local ChallengesFrame = _G["ChallengesFrame"]
-	MERS:InsetFrameTemplate(ChallengesFrameInset)
-
-	ChallengesFrame.WeeklyInfo:SetPoint("TOPLEFT")
-	ChallengesFrame.WeeklyInfo:SetPoint("BOTTOMRIGHT")
-	MERS:ChallengesKeystoneFrameAffixTemplate(ChallengesFrame.WeeklyInfo.Child.Affixes[1])
-
-	local bg, inset = ChallengesFrame:GetRegions()
-	bg:Hide()
-	inset:Hide()
+	hooksecurefunc("ChallengesFrame_Update", UpdateIcons)
 end
 
 S:AddCallbackForAddon("Blizzard_ChallengesUI", "mUIChallenges", styleChallenges)
