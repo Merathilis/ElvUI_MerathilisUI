6f0a675f9489c34fd79ddd9dfbd1c9ff30b9da2c
Merathilis <neo0608@gmail.com>
2018-10-04 16:35:21 +0200

    More minimap button adjustments.

--- ElvUI_MerathilisUI/core/install.lua
+++ ElvUI_MerathilisUI/core/install.lua
@@ -235,7 +235,8 @@ function MER:SetupLayout()
 	E.db["general"]["minimap"]["icons"]["mail"]["yOffset"] = -5
 	E.db["general"]["minimap"]["icons"]["ticket"]["position"] = "TOP"
 	E.db["general"]["minimap"]["icons"]["ticket"]["xOffset"] = 0
-	E.db["general"]["minimap"]["icons"]["ticket"]["yOffset"] = -5
+	E.db["general"]["minimap"]["icons"]["ticket"]["yOffset"] = 0
+	E.db["general"]["minimap"]["icons"]["ticket"]["scale"] = 0.75
 	E.db["general"]["minimap"]["resetZoom"]["enable"] = true
 	E.db["general"]["minimap"]["resetZoom"]["time"] = 5
 	E.db["general"]["minimap"]["size"] = 146
