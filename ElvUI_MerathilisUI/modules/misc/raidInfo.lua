d2d87ee326ba4edb67f6e680315013cf95f2ea29
Merathilis <neo0608@gmail.com>
2018-12-06 14:22:07 +0100

    Updated RaidInfo.

--- ElvUI_MerathilisUI/modules/misc/raidInfo.lua
+++ ElvUI_MerathilisUI/modules/misc/raidInfo.lua
@@ -5,15 +5,28 @@ local MERS = MER:GetModule("muiSkins")
 -- Cache global variables
 -- Lua functions
 local _G = _G
-local tinsert, strsplit = table.insert, string.split
+local ipairs, unpack = ipairs, unpack
+local find, strsplit = string.find, string.split
+local tinsert, tsort = table.insert, table.sort
 local next, pairs, mod = next, pairs, mod
 -- WoW API / Variables
+local C_Timer_After = C_Timer.After
+local CreateFrame = CreateFrame
+local GetInstanceInfo = GetInstanceInfo
+local GetNumGroupMembers = GetNumGroupMembers
+local GetRaidRosterInfo = GetRaidRosterInfo
+local GetReadyCheckStatus = GetReadyCheckStatus
+local GetSpellCharges = GetSpellCharges
+local GetSpellTexture = GetSpellTexture
+local GetTime = GetTime
+local IsInGroup = IsInGroup
+local IsInRaid = IsInRaid
 
 function MI:RaidInfo()
-	local header = CreateFrame("Button", nil, UIParent)
-	header:SetSize(120, 28)
+	local header = CreateFrame("Button", nil, E.UIParent)
 	header:SetFrameLevel(2)
-	header:SetPoint("TOPLEFT", E.UIParent, "TOPLEFT", 200, -15)
+	header:SetSize(120, 28)
+	header:SetPoint("TOPLEFT", E.UIParent, "TOPLEFT", 318, -17)
 	header:CreateBackdrop("Transparent")
 	header.backdrop:SetAllPoints()
 	header.backdrop:Styling()
@@ -30,7 +43,7 @@ function MI:RaidInfo()
 	end)
 
 	-- Role counts
-	local function getRaidMaxGroup()
+	local function GetRaidMaxGroup()
 		local _, instType, difficulty = GetInstanceInfo()
 		if (instType == "party" or instType == "scenario") and not IsInRaid() then
 			return 1
@@ -81,7 +94,7 @@ function MI:RaidInfo()
 	roleFrame:SetScript("OnEvent", function()
 		raidCounts = {totalTANK = 0, totalHEALER = 0, totalDAMAGER = 0}
 
-		local maxgroup = getRaidMaxGroup()
+		local maxgroup = GetRaidMaxGroup()
 		for i = 1, GetNumGroupMembers() do
 			local name, _, subgroup, _, _, _, _, online, isDead, _, _, assignedRole = GetRaidRosterInfo(i)
 			if name and online and subgroup <= maxgroup and not isDead and assignedRole ~= "NONE" then
@@ -108,7 +121,7 @@ function MI:RaidInfo()
 	res.Count = MER:CreateText(res, "OVERLAY", 16, "0")
 	res.Count:ClearAllPoints()
 	res.Count:SetPoint("LEFT", res, "RIGHT", 10, 0)
-	res.Timer = MER:CreateText(resFrame, "OVERLAY", 16, "00:00", false, "RIGHT", -5, 0)
+	res.Timer = MER:CreateText(resFrame, "OVERLAY", 16, "00:00", "white", "RIGHT", -5, 0)
 
 	res:SetScript("OnUpdate", function(self, elapsed)
 		self.elapsed = (self.elapsed or 0) + elapsed
@@ -137,4 +150,61 @@ function MI:RaidInfo()
 			self.elapsed = 0
 		end
 	end)
+
+	-- Ready check indicator
+	local rcFrame = CreateFrame("Frame", nil, header)
+	rcFrame:SetPoint("TOP", header, "BOTTOM", 0, -2)
+	rcFrame:SetSize(120, 50)
+	rcFrame:CreateBackdrop("Transparent")
+	rcFrame.backdrop:SetAllPoints()
+	rcFrame.backdrop:Styling()
+	rcFrame:Hide()
+
+	rcFrame.text = rcFrame:CreateFontString(nil, "OVERLAY")
+	rcFrame.text:SetPoint("TOP", rcFrame, 0, -8)
+	rcFrame.text:FontTemplate(nil, 14, "OUTLINE")
+	rcFrame.text:SetText(READY_CHECK)
+	rcFrame.text:SetTextColor(NORMAL_FONT_COLOR:GetRGB())
+
+	local rc = MER:CreateText(rcFrame, "OVERLAY", 12, "", nil, "TOP", 0, -28)
+
+	local count, total
+	rcFrame:RegisterEvent("READY_CHECK")
+	rcFrame:RegisterEvent("READY_CHECK_CONFIRM")
+	rcFrame:RegisterEvent("READY_CHECK_FINISHED")
+	rcFrame:SetScript("OnEvent", function(self, event)
+		if event == "READY_CHECK_FINISHED" then
+			if count == total then
+				rc:SetTextColor(0, 1, 0)
+			else
+				rc:SetTextColor(1, 0, 0)
+			end
+			C_Timer_After(5, function()
+				self:Hide()
+				rc:SetText("")
+				count, total = 0, 0
+			end)
+		else
+			count, total = 0, 0
+			self:Show()
+			local maxgroup = GetRaidMaxGroup()
+			for i = 1, GetNumGroupMembers() do
+				local name, _, subgroup, _, _, _, _, online = GetRaidRosterInfo(i)
+				if name and online and subgroup <= maxgroup then
+					total = total + 1
+					local status = GetReadyCheckStatus(name)
+					if status and status == "ready" then
+						count = count + 1
+					end
+				end
+			end
+			rc:SetText(count.." / "..total)
+			if count == total then
+				rc:SetTextColor(0, 1, 0)
+			else
+				rc:SetTextColor(1, 1, 0)
+			end
+		end
+	end)
+	rcFrame:SetScript("OnMouseUp", function(self) self:Hide() end)
 end
