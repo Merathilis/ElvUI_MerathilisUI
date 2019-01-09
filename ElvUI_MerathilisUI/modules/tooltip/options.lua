ab2dd5d72b497148bb21f2b2f05bced8e4161a48
Merathilis <neo0608@gmail.com>
2019-01-09 19:25:17 +0100

    Move the nameHover options in the tooltip section.

--- ElvUI_MerathilisUI/modules/tooltip/options.lua
+++ ElvUI_MerathilisUI/modules/tooltip/options.lua
@@ -175,6 +175,39 @@ local function Tooltip()
 					},
 				},
 			},
+			nameHover = {
+				order = 10,
+				type = "group",
+				guiInline = true,
+				name = L["Name Hover"],
+				desc = L["Shows the Unit Name on the mouse."],
+				get = function(info) return E.db.mui.nameHover[ info[#info] ] end,
+				set = function(info, value) E.db.mui.nameHover[ info[#info] ] = value; E:StaticPopup_Show("PRIVATE_RL"); end,
+				args = {
+					enable = {
+						order = 1,
+						type = "toggle",
+						name = L["Enable"],
+					},
+					fontSize = {
+						order = 2,
+						type = "range",
+						name = L["Size"],
+						min = 4, max = 24, step = 1,
+					},
+					fontOutline = {
+						order = 3,
+						type = "select",
+						name = L["Font Outline"],
+						values = {
+							["NONE"] = NONE,
+							["OUTLINE"] = "OUTLINE",
+							["MONOCHROMEOUTLINE"] = "MONOCROMEOUTLINE",
+							["THICKOUTLINE"] = "THICKOUTLINE",
+						},
+					},
+				},
+			},
 		},
 	}
 end
