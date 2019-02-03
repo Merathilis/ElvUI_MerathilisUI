09928a939964e125af3a84d499f6cce19833c313
Merathilis <neo0608@gmail.com>
2019-02-03 09:10:28 +0100

    Code formating.

--- ElvUI_MerathilisUI/modules/chat/chat.lua
+++ ElvUI_MerathilisUI/modules/chat/chat.lua
@@ -108,51 +108,51 @@ local commOpen = CreateFrame("Frame", nil, UIParent)
 commOpen:RegisterEvent("ADDON_LOADED")
 commOpen:RegisterEvent("CHANNEL_UI_UPDATE")
 commOpen:SetScript("OnEvent", function(self, event, addonName)
-if event == "ADDON_LOADED" and addonName == "Blizzard_Communities" then
-	--create overlay
-	local f = CreateFrame("Button", nil, UIParent)
-	f:SetFrameStrata("HIGH")
+	if event == "ADDON_LOADED" and addonName == "Blizzard_Communities" then
+		--create overlay
+		local f = CreateFrame("Button", nil, UIParent)
+		f:SetFrameStrata("HIGH")
 
-	f.tex = f:CreateTexture(nil, "BACKGROUND")
-	f.tex:SetAllPoints()
-	f.tex:SetColorTexture(0.1, 0.1, 0.1, 1)
+		f.tex = f:CreateTexture(nil, "BACKGROUND")
+		f.tex:SetAllPoints()
+		f.tex:SetColorTexture(0.1, 0.1, 0.1, 1)
 
-	f.text = f:CreateFontString()
-	f.text:FontTemplate(nil, 20, "OUTLINE")
-	f.text:SetShadowOffset(-2, 2)
-	f.text:SetText(L["Chat Hidden. Click to show"])
-	f.text:SetTextColor(r, g, b)
-	f.text:SetJustifyH("CENTER")
-	f.text:SetJustifyV("MIDDLE")
-	f.text:SetHeight(20)
-	f.text:SetPoint("CENTER", f, "CENTER", 0, 0)
+		f.text = f:CreateFontString()
+		f.text:FontTemplate(nil, 20, "OUTLINE")
+		f.text:SetShadowOffset(-2, 2)
+		f.text:SetText(L["Chat Hidden. Click to show"])
+		f.text:SetTextColor(r, g, b)
+		f.text:SetJustifyH("CENTER")
+		f.text:SetJustifyV("MIDDLE")
+		f.text:SetHeight(20)
+		f.text:SetPoint("CENTER", f, "CENTER", 0, 0)
 
-	f:EnableMouse(true)
-	f:RegisterForClicks("AnyUp")
-	f:SetScript("OnClick",function(...)
-		f:Hide()
-	end)
+		f:EnableMouse(true)
+		f:RegisterForClicks("AnyUp")
+		f:SetScript("OnClick",function(...)
+			f:Hide()
+		end)
 
-	--toggle
-	local function toggleOverlay()
-		if CommunitiesFrame:GetDisplayMode() == COMMUNITIES_FRAME_DISPLAY_MODES.CHAT and E.db.mui.chat.hideChat then
-			f:SetAllPoints(CommunitiesFrame.Chat.InsetFrame)
-			f:Show()
-		else
+		--toggle
+		local function toggleOverlay()
+			if CommunitiesFrame:GetDisplayMode() == COMMUNITIES_FRAME_DISPLAY_MODES.CHAT and E.db.mui.chat.hideChat then
+				f:SetAllPoints(CommunitiesFrame.Chat.InsetFrame)
+				f:Show()
+			else
+				f:Hide()
+			end
+		end
+
+		local function hideOverlay()
 			f:Hide()
 		end
-	end
+		toggleOverlay() --run once
 
-	local function hideOverlay()
-		f:Hide()
-	end
-	toggleOverlay() --run once
-
-	--hook
-	hooksecurefunc(CommunitiesFrame,"SetDisplayMode", toggleOverlay)
-	hooksecurefunc(CommunitiesFrame,"Show", toggleOverlay)
-	hooksecurefunc(CommunitiesFrame,"Hide", hideOverlay)
-	hooksecurefunc(CommunitiesFrame,"OnClubSelected", toggleOverlay)
+		--hook
+		hooksecurefunc(CommunitiesFrame,"SetDisplayMode", toggleOverlay)
+		hooksecurefunc(CommunitiesFrame,"Show", toggleOverlay)
+		hooksecurefunc(CommunitiesFrame,"Hide", hideOverlay)
+		hooksecurefunc(CommunitiesFrame,"OnClubSelected", toggleOverlay)
 	end
 end)
 
