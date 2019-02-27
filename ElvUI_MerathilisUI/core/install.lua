6399e59a1fe2f769b1dfb1118df2c9389be5bfe1
Merathilis <neo0608@gmail.com>
2019-02-27 09:18:14 +0100

    Keep this for now in. nameplateSelectedScale.

--- ElvUI_MerathilisUI/core/install.lua
+++ ElvUI_MerathilisUI/core/install.lua
@@ -43,6 +43,7 @@ local function SetupCVars()
 	SetCVar("nameplateLargerScale", 1)
 	SetCVar("nameplateMaxAlpha", 1)
 	SetCVar("nameplateTargetRadialPosition", 1)
+	SetCVar("nameplateSelectedScale", 1.1) -- keep this in, till ElvUI puts a better target Scale in
 	SetCVar("nameplateMotion", 1)
 	SetCVar("nameplateOverlapH", 0.8)
 	SetCVar("nameplateOverlapV", 1.1)
