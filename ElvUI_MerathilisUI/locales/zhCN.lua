66c95390f4bc2388dafaf73cb47d846b0cc45138
Merathilis <neo0608@gmail.com>
2019-04-04 10:08:42 +0200

    whoops

--- ElvUI_MerathilisUI/locales/zhCN.lua
+++ ElvUI_MerathilisUI/locales/zhCN.lua
@@ -1,4 +1,4 @@
--- Chinese localization file for zh
+-- Chinese localization file for zhCN
 local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
 local L = AceLocale:NewLocale("ElvUI", "zhCN")
 if not L then return end
