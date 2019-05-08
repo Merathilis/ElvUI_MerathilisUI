2fe65e4bff1ccc0c05b1782c4da3ed16dfeb7eb1
Merathilis <neo0608@gmail.com>
2019-05-08 06:25:16 +0200

    GCD Bar spark position update :thinking:

--- ElvUI_MerathilisUI/modules/unitframes/elements/gcd.lua
+++ ElvUI_MerathilisUI/modules/unitframes/elements/gcd.lua
@@ -21,7 +21,7 @@ local function OnUpdate()
 	if perc > 1 then
 		return gcdbar:Hide()
 	else
-		gcdbar.spark:SetPoint("CENTER", gcdbar, "LEFT", width * perc, 0)
+		gcdbar.spark:SetPoint("RIGHT", gcdbar, "LEFT", width * perc, 0)
 	end
 end
 
@@ -45,11 +45,8 @@ function MGCD:LoadGCDBar(frame)
 	gcdbar.Color = {1, 1, 1}
 
 	local width = _G["ElvUF_Player"]:GetWidth()
-
-	if frame then
-		gcdbar:SetWidth(width)
-		gcdbar:SetPoint("TOPLEFT", frame, "BOTTOMLEFT", 0, 0)
-	end
+	gcdbar:SetWidth(width)
+	gcdbar:SetPoint("TOPLEFT", frame, "BOTTOMLEFT", 0, 0)
 
 	gcdbar:Height(2)
 	gcdbar.width = 3
