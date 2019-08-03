5b5a8daf83423d4087a484b0eb6275f25ab8e5f8
Merathilis <neo0608@gmail.com>
2019-08-03 11:38:54 +0200

    nothing

--- ElvUI_MerathilisUI/settings/profile.lua
+++ ElvUI_MerathilisUI/settings/profile.lua
@@ -65,7 +65,7 @@ MP.misc = {
 		lfg = false,
 	},
 	paragon = true,
-	paragonColor = {r =186 / 255, g = 183 / 255, b = 107 / 255, a = 1},
+	paragonColor = {r = 186 / 255, g = 183 / 255, b = 107 / 255, a = 1},
 }
 
 MP.nameHover = {
