6b14b7bb410006897c9d6d695a53af79c7ac0ea1
Merathilis <neo0608@gmail.com>
2019-08-18 18:06:07 +0200

    Remove dead code

--- ElvUI_MerathilisUI/modules/chat/emotes.lua
+++ ElvUI_MerathilisUI/modules/chat/emotes.lua
@@ -86,39 +86,6 @@ local ShowEmoteTableButton
 local EmoteTableFrame, EmoteClubTableFrame
 
 local text, texture
-local function CreateEmoteClubTableFrame()
-	EmoteClubTableFrame = CreateFrame("Frame", "EmoteClubTableFrame", _G.CommunitiesFrame)
-	EmoteClubTableFrame:SetTemplate("Default")
-	EmoteClubTableFrame:SetWidth((ChatEmote.Config.iconSize + 2) * 12 + 4)
-	EmoteClubTableFrame:SetHeight((ChatEmote.Config.iconSize + 2) * 5 + 4)
-	EmoteClubTableFrame:SetPoint("BOTTOMLEFT", _G.CommunitiesFrame, "BOTTOMRIGHT", E:Scale(2), 0)
-	EmoteClubTableFrame:Hide()
-	EmoteClubTableFrame:SetFrameStrata("DIALOG")
-
-	local icon, row, col
-	row = 1
-	col = 1
-	for i = 1, #emotes do
-		text = emotes[i][1]
-		texture = emotes[i][2]
-		icon = CreateFrame("Frame", format("IconButton%d", i), EmoteClubTableFrame)
-		icon:SetWidth(ChatEmote.Config.iconSize)
-		icon:SetHeight(ChatEmote.Config.iconSize)
-		icon.text = text
-		icon.texture = icon:CreateTexture(nil, "ARTWORK")
-		icon.texture:SetTexture(texture)
-		icon.texture:SetAllPoints(icon)
-		icon:Show()
-		icon:SetPoint("TOPLEFT", (col - 1) * (ChatEmote.Config.iconSize + 2) + 2, -(row - 1) * (ChatEmote.Config.iconSize + 2) - 2)
-		icon:SetScript("OnMouseUp", ChatEmote.EmoteClubIconMouseUp)
-		icon:EnableMouse(true)
-		col = col + 1
-		if (col > 12) then
-			row = row + 1
-			col = 1
-		end
-	end
-end
 
 local function CreateEmoteTableFrame()
 	EmoteTableFrame = CreateFrame("Frame", "EmoteTableFrame", E.UIParent)
@@ -162,15 +129,6 @@ function ChatEmote.ToggleEmoteTable()
 		EmoteTableFrame:Hide()
 	else
 		EmoteTableFrame:Show()
-	end
-end
-
-function ChatEmote.ToggleClubEmoteTable()
-	if (not EmoteClubTableFrame) then CreateEmoteClubTableFrame() end
-	if (EmoteClubTableFrame:IsShown()) then
-		EmoteClubTableFrame:Hide()
-	else
-		EmoteClubTableFrame:Show()
 	end
 end
 
