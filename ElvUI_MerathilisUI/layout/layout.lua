Merathilis/ElvUI_MerathilisUI

commit a7370193ff1a35b3f0862c365dc67eb0e1b64f3c

Ability to make chat respect height and still have the ability to use the expand chat button

ElvUI_MerathilisUI/layout/layout.lua
@@ -91,6 +91,9 @@ end
 function MERL:CreateChatButtons()
 	if E.db.mui.chat.chatButton ~= true or E.private.chat.enable ~= true then return end
 
+	-- Maybe add option to adjust how much the chat panel expands
+	E.db.mui.chat.expandPanel = 150
+
 	local panelBackdrop = E.db.chat.panelBackdrop
 	local ChatButton = CreateFrame("Frame", "mUIChatButton", _G["LeftChatPanel"].backdrop)
 	ChatButton:ClearAllPoints()
@@ -111,11 +114,11 @@ function MERL:CreateChatButtons()
 		if InCombatLockdown() then return end
 		if btn == "LeftButton" then
 			if E.db.mui.chat.isExpanded then
-				E.db.chat.panelHeight = E.db.mui.chat.panelHeight
-				E.db.mui.chat.isExpanded = false
+				E.db.chat.panelHeight = E.db.chat.panelHeight - E.db.mui.chat.expandPanel
 				CH:PositionChat(true)
+				E.db.mui.chat.isExpanded = false
 			else
-				E.db.chat.panelHeight = 400
+				E.db.chat.panelHeight = E.db.chat.panelHeight + E.db.mui.chat.expandPanel
 				CH:PositionChat(true)
 				E.db.mui.chat.isExpanded = true
 			end
@@ -143,20 +146,6 @@ function MERL:CreateChatButtons()
 			self:SetAlpha(0.55)
 		end
 		GameTooltip:Hide()
-	end)
-
-	ChatButton:RegisterEvent("PLAYER_LEAVING_WORLD")
-	ChatButton:RegisterEvent("ADDON_LOADED")
-	ChatButton:SetScript("OnEvent", function(self, event, addon)
-		if event == "ADDON_LOADED" and addon == "ElvUI_Config" then
-			E.Options.args.chat.args.panels.args.panelHeight.set = function(info, value) E.db.chat.panelHeight = value; E.db.mui.chat.panelHeight = value; E:GetModule("Chat"):PositionChat(true); end
-			self:UnregisterEvent(event)
-		end
-		if event == "PLAYER_LEAVING_WORLD" then
-			E.db.chat.panelHeight = E.db.mui.chat.panelHeight or 146
-			E.db.mui.chat.isExpanded = false
-			CH:PositionChat(true)
-		end
 	end)
 end
 

commit 5299238e9dd51d54abbdc09c7579d4db06c3501e

Chat button bug fix that I introduced in a7370193ff1a35b3f0862c365dc67eb0e1b64f3c

ElvUI_MerathilisUI/layout/layout.lua
@@ -93,6 +93,7 @@ function MERL:CreateChatButtons()
 
 	-- Maybe add option to adjust how much the chat panel expands
 	E.db.mui.chat.expandPanel = 150
+	E.db.mui.chat.panelHeight = E.db.mui.chat.panelHeight or E.db.chat.panelHeight
 
 	local panelBackdrop = E.db.chat.panelBackdrop
 	local ChatButton = CreateFrame("Frame", "mUIChatButton", _G["LeftChatPanel"].backdrop)
@@ -132,8 +133,10 @@ function MERL:CreateChatButtons()
 		GameTooltip:ClearLines()
 		if E.db.mui.chat.isExpanded then
 			GameTooltip:AddLine(MER:cOption(L["BACK"]))
+			GameTooltip:AddLine("isExpanded is true")
 		else
 			GameTooltip:AddLine(MER:cOption(L["Expand the chat"]))
+			GameTooltip:AddLine("isExpanded is false")
 		end
 		GameTooltip:Show()
 		if InCombatLockdown() then GameTooltip:Hide() end
@@ -146,6 +149,14 @@ function MERL:CreateChatButtons()
 			self:SetAlpha(0.55)
 		end
 		GameTooltip:Hide()
+	end)
+
+	ChatButton:RegisterEvent("ADDON_LOADED")
+	ChatButton:SetScript("OnEvent", function(self, event, addon)
+		if event == "ADDON_LOADED" and addon == "ElvUI_OptionsUI" then
+			E.Options.args.chat.args.panels.args.panelHeight.set = function(info, value) E.db.chat.panelHeight = value; E.db.mui.chat.panelHeight = value; print(E.db.mui.chat.panelHeight) CH:PositionChat(true); end
+			self:UnregisterEvent(event)
+		end
 	end)
 end
 