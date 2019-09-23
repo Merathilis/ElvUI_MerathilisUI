ccca2518b976e63effb7830132da937e95ff016c
Merathilis <neo0608@gmail.com>
2019-09-23 13:46:30 +0200

    uhm.. okay

--- ElvUI_MerathilisUI/modules/misc/misc.lua
+++ ElvUI_MerathilisUI/modules/misc/misc.lua
@@ -1,6 +1,6 @@
 local MER, E, _, V, P, G = unpack(select(2, ...))
 local L = E.Libs.ACL:GetLocale('ElvUI', E.global.general.locale or 'enUS')
-local module = MER:NewModule("mUIMisc", 'AceConsole-3.0', 'AceEvent-3.0', 'AceHook-3.0', 'AceTimer-3.0')
+local module = MER:NewModule("mUIMisc", 'AceEvent-3.0', 'AceHook-3.0')
 local S = E:GetModule("Skins")
 
 -- Cache global variables
