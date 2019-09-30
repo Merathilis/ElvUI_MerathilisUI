8331c925dd97665f381b7d01d02c7a53b1b56cf1
Merathilis <neo0608@gmail.com>
2019-09-30 08:05:09 +0200

    Style more PartySync things.

--- ElvUI_MerathilisUI/modules/skins/FrameXML/WorldMapFrame.lua
+++ ElvUI_MerathilisUI/modules/skins/FrameXML/WorldMapFrame.lua
@@ -5,7 +5,7 @@ local S = E:GetModule("Skins")
 --Cache global variables
 --Lua functions
 local _G = _G
-local select = select
+local pairs, select = pairs, select
 --WoW API / Variables
 local CreateFrame = CreateFrame
 local GetNumQuestLogEntries = GetNumQuestLogEntries
@@ -45,8 +45,24 @@ local function styleWorldmap()
 	end
 
 	-- Party Sync PopUp
-	if _G.QuestSessionManager.CheckStartDialog.backdrop then
-		_G.QuestSessionManager.CheckStartDialog.backdrop:Styling()
+	local dialogs = {
+		_G.QuestSessionManager.CheckStartDialog,
+		_G.QuestSessionManager.StartDialog,
+		_G.QuestSessionManager.CheckLeavePartyDialog,
+		_G.QuestSessionManager.CheckStopDialog,
+		_G.QuestSessionManager.CheckConvertToRaidDialog,
+		_G.QuestSessionManager.ConfirmJoinGroupRequestDialog,
+		_G.QuestSessionManager.ConfirmInviteToGroupDialog,
+		_G.QuestSessionManager.ConfirmInviteToGroupReceivedDialog,
+		_G.QuestSessionManager.ConfirmRequestToJoinGroupDialog,
+		_G.QuestSessionManager.ConfirmBNJoinGroupRequestDialog,
+		_G.QuestSessionManager.ConfirmInviteTravelPassConfirmationDialog,
+	}
+
+	for _, frame in pairs(dialogs) do
+		if frame and not frame.style then
+			frame:Styling()
+		end
 	end
 end
 
