8945ae66e15606c6c1f1c736f1a16d38f7e13cdc
Merathilis <neo0608@gmail.com>
2019-11-07 09:14:46 +0100

    Added also an shadow on the NamePlates debuffs, so you can easy see the debuff type.

--- ElvUI_MerathilisUI/modules/nameplates/elements/auras.lua
+++ ElvUI_MerathilisUI/modules/nameplates/elements/auras.lua
@@ -7,8 +7,7 @@ module.modName = L["NameplateAuras"]
 -- Cache global variables
 -- Lua functions
 local _G = _G
-local select = select
-local pairs = pairs
+local pairs, select, unpack = pairs, select, unpack
 local max = math.max
 local tsort = table.sort
 -- WoW API / Variables
@@ -25,6 +24,31 @@ local RAID_CLASS_COLORS = RAID_CLASS_COLORS
 --]]
 
 function module:PostUpdateAura(unit, button)
+	if button and button.pixelBorders then
+		button:GetParent().spacing = E:Scale(4)
+		local r, g, b = E:GetBackdropBorderColor(button)
+		local br, bg, bb = E:GrabColorPickerValues(unpack(E.media.unitframeBorderColor))
+
+		if button.isDebuff then
+			if(not button.isFriend and not button.isPlayer) then
+				button.shadow:SetBackdropBorderColor(0.9, 0.1, 0.1)
+			else
+				if E.BadDispels[button.spellID] and E:IsDispellableByMe(button.dtype) then
+					button.shadow:SetBackdropBorderColor(0.05, 0.85, 0.94)
+				else
+					local color = (button.dtype and _G.DebuffTypeColor[button.dtype]) or _G.DebuffTypeColor.none
+					button.shadow:SetBackdropBorderColor(color.r, color.g, color.b)
+				end
+			end
+		else
+			if button.isStealable and not button.isFriend then
+				button.shadow:SetBackdropBorderColor(0.93, 0.91, 0.55, 1.0)
+			else
+				button.shadow:SetBackdropBorderColor(unpack(E.media.unitframeBorderColor))
+			end
+		end
+	end
+
 	if button and button.spellID then
 		local spell = E.global.unitframe.aurafilters.CCDebuffs.spells[button.spellID]
 
@@ -97,6 +121,7 @@ function module:Construct_AuraIcon(button)
 		button.cc_name:Point("BOTTOM", button, "TOP", 1, 1)
 		button.cc_name:SetJustifyH("CENTER")
 	end
+	if not button.shadow then button:CreateShadow() end
 end
 
 function module.SetPosition(element, _, to)
