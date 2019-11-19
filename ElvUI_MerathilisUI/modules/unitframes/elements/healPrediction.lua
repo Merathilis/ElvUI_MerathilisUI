1fe911e0ca64fa8fc625570a2bb40b5c97910361
fgprodigal <fgprodigal@gmail.com>
2019-11-19 17:29:41 +0800

    Add Blizzard Textures for Absorbs on the UnitFrames.

--- ElvUI_MerathilisUI/modules/unitframes/elements/healPrediction.lua
+++ ElvUI_MerathilisUI/modules/unitframes/elements/healPrediction.lua
@@ -7,6 +7,7 @@ local UF = E.UnitFrames
 local _G = _G
 local pairs = pairs
 --WoW API / Variables
+local UnitGetTotalAbsorbs = UnitGetTotalAbsorbs
 local UnitHealthMax = UnitHealthMax
 local UnitIsConnected = UnitIsConnected
 local hooksecurefunc = hooksecurefunc
@@ -21,6 +22,13 @@ function MUF:Configure_HealComm(frame)
 			local orientation = frame.db.health.orientation or frame.Health:GetOrientation()
 			local reverseFill = not not frame.db.health.reverseFill
 			local overAbsorbTexture = "Interface\\RaidFrame\\Shield-Overshield"
+
+			if healPrediction.absorbBar and not healPrediction.absorbBar.overlay then
+				healPrediction.absorbBar.overlay = healPrediction.absorbBar:CreateTexture(nil, "ARTWORK", nil, 1)
+				healPrediction.absorbBar.overlay:SetAllPoints(healPrediction.absorbBar:GetStatusBarTexture())
+				healPrediction.absorbBar.overlay:SetTexture("Interface\\RaidFrame\\Shield-Overlay", true, true)
+				healPrediction.absorbBar.overlay.tileSize = 32
+			end
 
 			if orientation == "HORIZONTAL" then
 				if healPrediction.overAbsorb then
@@ -59,7 +67,22 @@ function MUF:Configure_HealComm(frame)
 			healPrediction.overAbsorb:SetVertexColor(1, 1, 1, 1)
 			healPrediction.overAbsorb:SetParent(frame.RaisedElementParent)
 		end
+
+		if healPrediction.absorbBar then
+			healPrediction.absorbBar:SetStatusBarColor(0.66, 1, 1, 1)
+		end
 	end
+end
+
+function MUF:UpdateHealComm(unit, myIncomingHeal, otherIncomingHeal, absorb, healAbsorb, hasOverAbsorb, hasOverHealAbsorb)
+	if not self.absorbBar.overlay or not UnitIsConnected(unit) then return end
+
+	local pred = self.frame and self.frame.db and self.frame.db.healPrediction
+	local totalWidth, totalHeight = self.frame.Health:GetSize()
+	local totalMax = UnitHealthMax(unit)
+	absorb = pred and UnitGetTotalAbsorbs(unit) or absorb
+	local barSize = (absorb / totalMax) * totalWidth
+	self.absorbBar.overlay:SetTexCoord(0, barSize / self.absorbBar.overlay.tileSize, 0, totalHeight / self.absorbBar.overlay.tileSize)
 end
 
 function MUF:UpdatePredictionStatusBar(prediction, parent, name)
@@ -74,5 +97,11 @@ function MUF:HealPrediction()
 
 	hooksecurefunc(UF, "Configure_HealComm", MUF.Configure_HealComm)
 	hooksecurefunc(UF, "UpdatePredictionStatusBar", MUF.UpdatePredictionStatusBar)
+
+	for _, object in pairs(_G.ElvUF.objects) do
+        if object.HealthPrediction and object.HealthPrediction.PostUpdate then
+			hooksecurefunc(object.HealthPrediction, "PostUpdate", MUF.UpdateHealComm)
+		end
+    end
 end
 
