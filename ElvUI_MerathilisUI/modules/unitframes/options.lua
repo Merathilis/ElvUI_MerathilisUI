c7a3967b721f05799089a904018be890bece4cfe
Merathilis <neo0608@gmail.com>
2019-11-20 12:04:44 +0100

    Cleanup UnitFrames options

--- ElvUI_MerathilisUI/modules/unitframes/options.lua
+++ ElvUI_MerathilisUI/modules/unitframes/options.lua
@@ -10,7 +10,6 @@ local function UnitFramesTable()
 	E.Options.args.mui.args.modules.args.unitframes = {
 		type = "group",
 		name = E.NewSign..L["UnitFrames"],
-		childGroups = "tab",
 		disabled = function() return not E.private.unitframe.enable end,
 		args = {
 			name = {
@@ -21,20 +20,11 @@ local function UnitFramesTable()
 			general = {
 				order = 2,
 				type = "group",
-				name = L["General"],
+				name = MER:cOption(L["General"]),
+				guiInline = true,
 				args = {
-					header = {
-						order = 1,
-						type = "header",
-						name = MER:cOption(L["General"]),
-					},
-					spacer = {
-						order = 2,
-						name = "",
-						type = "description",
-					},
 					healPrediction = {
-						order = 3,
+						order = 1,
 						type = "toggle",
 						name = L["Heal Prediction"],
 						desc = L["Changes the Heal Prediction texture to the default Blizzard ones."],
@@ -42,7 +32,7 @@ local function UnitFramesTable()
 						set = function(info, value) E.db.mui.unitframes[ info[#info] ] = value; E:StaticPopup_Show("CONFIG_RL"); end,
 					},
 					auras = {
-						order = 4,
+						order = 2,
 						type = "toggle",
 						name = E.NewSign..L["Auras"],
 						desc = L["Adds a shadow to the debuffs that the debuff color is more visible."],
@@ -50,72 +40,19 @@ local function UnitFramesTable()
 						set = function(info, value) E.db.mui.unitframes[ info[#info] ] = value; E:StaticPopup_Show("CONFIG_RL"); end,
 					},
 					swing = {
-						order = 5,
+						order = 3,
 						type = "toggle",
 						name = E.NewSign..L["Swing Bar"],
 						desc = L["Creates a weapon Swing Bar"],
 						get = function(info) return E.db.mui.unitframes.swing.enable end,
 						set = function(info, value) E.db.mui.unitframes.swing.enable = value; E:StaticPopup_Show("CONFIG_RL"); end,
-					}
-				},
-			},
-			player = {
-				order = 3,
-				type = "group",
-				name = L["Player"],
-				args = {
-					header = {
-						order = 0,
-						type = "header",
-						name = MER:cOption(L["Player"]),
 					},
-					portrait = {
-						order = 1,
-						type = "execute",
-						name = L["Player Portrait"],
-						func = function() LibStub("AceConfigDialog-3.0-ElvUI"):SelectGroup("ElvUI", "unitframe", "player", "portrait") end,
-					},
-					spacer = {
-						order = 2,
-						type = "description",
-						name = "",
-					},
-				},
-			},
-			target = {
-				order = 4,
-				type = "group",
-				name = L["Target"],
-				args = {
-					header = {
-						order = 0,
-						type = "header",
-						name = MER:cOption(L["Target"]),
-					},
-					portrait = {
-						order = 1,
-						type = "execute",
-						name = L["Target Portrait"],
-						func = function() LibStub("AceConfigDialog-3.0-ElvUI"):SelectGroup("ElvUI", "unitframe", "target", "portrait") end,
-					},
-				},
-			},
-			infoPanel = {
-				type = "group",
-				order = 5,
-				name = L["InfoPanel"],
-				get = function(info) return E.db.mui.unitframes.infoPanel[ info[#info] ] end,
-				set = function(info, value) E.db.mui.unitframes.infoPanel[ info[#info] ] = value; E:StaticPopup_Show("CONFIG_RL"); end,
-				args = {
-					header = {
-						order = 0,
-						type = "header",
-						name = MER:cOption(L["InfoPanel"]),
-					},
-					style = {
-						order = 1,
+					infoPanel = {
+						order = 4,
 						type = "toggle",
 						name = L["InfoPanel Style"],
+						get = function(info) return E.db.mui.unitframes.infoPanel.style end,
+						set = function(info, value) E.db.mui.unitframes.infoPanel.style = value; E:StaticPopup_Show("CONFIG_RL"); end,
 					},
 				},
 			},
