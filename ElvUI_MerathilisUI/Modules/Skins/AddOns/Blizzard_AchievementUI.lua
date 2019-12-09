1f53316cef93b9fbee83e1f220abc485b1106acb
Merathilis <neo0608@gmail.com>
2019-12-09 08:12:20 +0100

    Small cleanup

--- ElvUI_MerathilisUI/Modules/Skins/AddOns/Blizzard_AchievementUI.lua
+++ ElvUI_MerathilisUI/Modules/Skins/AddOns/Blizzard_AchievementUI.lua
@@ -30,27 +30,7 @@ local function LoadSkin()
 		-- Hide ElvUI's backdrop
 		if bu.backdrop then bu.backdrop:SetTemplate("Transparent") end
 
-		bu:DisableDrawLayer("BORDER")
 		MERS:CreateGradient(bu)
-
-		bu.background:SetTexture(E["media"].normTex)
-		bu.background:SetVertexColor(0, 0, 0, .25)
-
-		bu.description:SetTextColor(.6, .6, .6)
-		bu.description.SetTextColor = MER.dummy
-		bu.description:SetShadowOffset(1, -1)
-		bu.description.SetShadowOffset = MER.dummy
-
-		_G["AchievementFrameAchievementsContainerButton"..i.."TitleBackground"]:Hide()
-		_G["AchievementFrameAchievementsContainerButton"..i.."Glow"]:Hide()
-		_G["AchievementFrameAchievementsContainerButton"..i.."RewardBackground"]:SetAlpha(0)
-		_G["AchievementFrameAchievementsContainerButton"..i.."PlusMinus"]:SetAlpha(0)
-		_G["AchievementFrameAchievementsContainerButton"..i.."Highlight"]:SetAlpha(0)
-		_G["AchievementFrameAchievementsContainerButton"..i.."IconOverlay"]:Hide()
-		_G["AchievementFrameAchievementsContainerButton"..i.."GuildCornerL"]:SetAlpha(0)
-		_G["AchievementFrameAchievementsContainerButton"..i.."GuildCornerR"]:SetAlpha(0)
-
-		S:HandleIcon(bu.icon.texture)
 	end
 
 	hooksecurefunc("AchievementButton_DisplayAchievement", function(button, category, achievement)
@@ -92,91 +72,23 @@ local function LoadSkin()
 				if bu.backdrop then bu.backdrop:SetTemplate("Transparent") end
 
 				MERS:CreateGradient(bu)
-				bu:DisableDrawLayer("BORDER")
-
-				local bd = _G["AchievementFrameSummaryAchievement"..i.."Background"]
-
-				bd:SetTexture(E["media"].normTex)
-				bd:SetVertexColor(0, 0, 0, .25)
-
-				_G["AchievementFrameSummaryAchievement"..i.."TitleBackground"]:Hide()
-				_G["AchievementFrameSummaryAchievement"..i.."Glow"]:Hide()
-				_G["AchievementFrameSummaryAchievement"..i.."Highlight"]:SetAlpha(0)
-				_G["AchievementFrameSummaryAchievement"..i.."IconOverlay"]:Hide()
-
-				local text = _G["AchievementFrameSummaryAchievement"..i.."Description"]
-				text:SetTextColor(.6, .6, .6)
-				text.SetTextColor = MER.dummy
-				text:SetShadowOffset(1, -1)
-				text.SetShadowOffset = MER.dummy
-
-				local ic = _G["AchievementFrameSummaryAchievement"..i.."IconTexture"]
-				ic:SetTexCoord(unpack(E.TexCoords))
-				MERS:CreateBG(ic)
 
 				bu.reskinned = true
 			end
 		end
 	end)
 
-	_G["AchievementFrame"]:HookScript("OnShow", function()
-		for i = 1, 20 do
-			local frame = _G["AchievementFrameCategoriesContainerButton"..i]
-
-			--frame:StyleButton()
-			frame:GetHighlightTexture():Point("TOPLEFT", 0, -4)
-			frame:GetHighlightTexture():Point("BOTTOMRiGHT", 0, -3)
-			frame:GetPushedTexture():Point("TOPLEFT", 0, -4)
-			frame:GetPushedTexture():Point("BOTTOMRiGHT", 0, -3)
-		end
-	end)
-
 	for i = 1, 12 do
-		local bu = _G["AchievementFrameSummaryCategoriesCategory"..i]
 		local label = _G["AchievementFrameSummaryCategoriesCategory"..i.."Label"]
 
-		bu:SetStatusBarTexture(E["media"].normTex)
 		label:SetTextColor(1, 1, 1)
-		label:Point("LEFT", bu, "LEFT", 6, 0)
-
-		local bg = CreateFrame("Frame", nil, bu)
-		bg:Point("TOPLEFT", -1, 1)
-		bg:Point("BOTTOMRIGHT", 1, -1)
-		bg:SetFrameLevel(bu:GetFrameLevel()-1)
-		MERS:CreateBD(bg, .25)
-
-		_G["AchievementFrameSummaryCategoriesCategory"..i.."Left"]:Hide()
-		_G["AchievementFrameSummaryCategoriesCategory"..i.."Middle"]:Hide()
-		_G["AchievementFrameSummaryCategoriesCategory"..i.."Right"]:Hide()
-		_G["AchievementFrameSummaryCategoriesCategory"..i.."ButtonHighlight"]:SetAlpha(0)
-		_G["AchievementFrameSummaryCategoriesCategory"..i.."Text"]:SetPoint("RIGHT", bu, "RIGHT", -5, 0)
 	end
 
-	for i = 1, 20 do
-		local bu = _G["AchievementFrameStatsContainerButton"..i]
-		bu:StripTextures()
-		bu:GetHighlightTexture():SetColorTexture(r, g, b, .2)
-		bu:GetHighlightTexture():SetBlendMode("BLEND")
-	end
+	_G.AchievementFrameSummaryCategoriesStatusBarTitle:SetTextColor(1, 1, 1)
 
-	_G.AchievementFrame.searchBox:Point("BOTTOMRIGHT", _G.AchievementFrameAchievementsContainer, "TOPRIGHT", -2, -2)
+	_G.AchievementFrame.searchBox:ClearAllPoints()
+	_G.AchievementFrame.searchBox:SetPoint("BOTTOMRIGHT", _G.AchievementFrameAchievementsContainer, "TOPRIGHT", -2, -2)
 	_G.AchievementFrame.searchBox:SetSize(100, 20)
-
-	-- Font width fix
-	hooksecurefunc("AchievementObjectives_DisplayProgressiveAchievement", function()
-		local index = 1
-		local mini = _G["AchievementFrameMiniAchievement"..index]
-		while mini do
-			if not mini.fontStyled then
-				mini.points:SetWidth(22)
-				mini.points:ClearAllPoints()
-				mini.points:SetPoint("BOTTOMRIGHT", 2, 2)
-				mini.fontStyled = true
-			end
-			index = index + 1
-			mini = _G["AchievementFrameMiniAchievement"..index]
-		end
-	end)
 end
 
 S:AddCallbackForAddon("Blizzard_AchievementUI", "mUIAchievement", LoadSkin)
