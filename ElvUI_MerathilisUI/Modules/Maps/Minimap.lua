73c9a9a5e483ea23c7f9fe6639cbac6f74c2b10a
Merathilis <neo0608@gmail.com>
2020-03-03 08:15:37 +0100

    Change Blip-Texture

--- ElvUI_MerathilisUI/Modules/Maps/Minimap.lua
+++ ElvUI_MerathilisUI/Modules/Maps/Minimap.lua
@@ -237,7 +237,7 @@ end
 function module:SetBlipTexture()
 	if E.db.mui.maps.minimap.blipTexture ~= true then return end
 
-	Minimap:SetBlipTexture("Interface\\AddOns\\ElvUI_MerathilisUI\\media\\textures\\Blip-Glass")
+	Minimap:SetBlipTexture("Interface\\AddOns\\ElvUI_MerathilisUI\\media\\textures\\Blip-Solid")
 end
 
 function module:Initialize()
