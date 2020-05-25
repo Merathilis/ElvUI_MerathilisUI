c73a2c1a97b1b484c383a9908d60e9ba6a4bf333
Merathilis <neo0608@gmail.com>
2020-05-25 22:05:46 +0200

    New create DataText Panel now gets my styling. Need to find a way to hide my styling if changing the options. Thaaaaaanks @Azilroka <3

--- ElvUI_MerathilisUI/Modules/Datatexts/DataTexts.lua
+++ ElvUI_MerathilisUI/Modules/Datatexts/DataTexts.lua
@@ -6,6 +6,7 @@ local DT = E:GetModule('DataTexts')
 local _G = _G
 --WoW API / Variables
 local CreateFrame = CreateFrame
+local hooksecurefunc = hooksecurefunc
 -- GLOBALS:
 
 local PANEL_HEIGHT = 22
@@ -20,4 +21,18 @@ DT:RegisterPanel(ChatTabFrame, 3, "ANCHOR_TOPLEFT", 3, 4)
 function MER:InitDataTexts()
 	MER_RightChatTopDT:Point("TOPRIGHT", _G.RightChatTab, "TOPRIGHT", 0, E.mult)
 	MER_RightChatTopDT:Point("BOTTOMLEFT", _G.RightChatTab, "BOTTOMLEFT", 0, E.mult)
+
+	hooksecurefunc(DT, "UpdatePanelInfo", function(DT, panelName, panel)
+		if not panel then return end
+		local db = panel.db or P.datatexts.panels[panelName] and DT.db.panels[panelName]
+		if not db then return end
+
+		-- Need to find a way to hide my styling if changing the option from a panel
+		if panel and not panel.styled then
+			if db.backdrop then
+				panel:Styling()
+			end
+			panel.styled = true
+		end
+	end)
 end
