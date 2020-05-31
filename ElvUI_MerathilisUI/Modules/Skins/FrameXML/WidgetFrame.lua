6395a951492bbc1a1f925effcb81803df2cac212
Merathilis <neo0608@gmail.com>
2020-05-31 10:08:03 +0200

    Update WidgetFrame skin

--- ElvUI_MerathilisUI/Modules/Skins/FrameXML/WidgetFrame.lua
+++ ElvUI_MerathilisUI/Modules/Skins/FrameXML/WidgetFrame.lua
@@ -15,7 +15,7 @@ local r, g, b = unpack(E["media"].rgbvaluecolor)
 local function LoadSkin()
 	if E.private.skins.blizzard.enable ~= true then return end
 
-	local atlasColors = {
+	local AtlasColors = {
 		["UI-Frame-Bar-Fill-Blue"] = {.2, .6, 1},
 		["UI-Frame-Bar-Fill-Red"] = {.9, .2, .2},
 		["UI-Frame-Bar-Fill-Yellow"] = {1, .6, 0},
@@ -23,22 +23,24 @@ local function LoadSkin()
 		["objectivewidget-bar-fill-right"] = {.9, .2, .2}
 	}
 
-	local function updateBarTexture(self, atlas)
-		if atlasColors[atlas] then
+	local function UpdateBarTexture(self, atlas)
+		if AtlasColors[atlas] then
 			self:SetStatusBarTexture(E.media.normTex)
-			self:SetStatusBarColor(unpack(atlasColors[atlas]))
+			self:SetStatusBarColor(unpack(AtlasColors[atlas]))
 		end
 	end
 
-	local doubleBarType = _G.Enum.UIWidgetVisualizationType.DoubleStatusBar
+	local Type_DoubleStatusBar = _G.Enum.UIWidgetVisualizationType.DoubleStatusBar
+	local Type_SpellDisplay = _G.Enum.UIWidgetVisualizationType.SpellDisplay
+
 	local frame = CreateFrame("Frame")
 	frame:RegisterEvent("PLAYER_ENTERING_WORLD")
 	frame:RegisterEvent("UPDATE_ALL_UI_WIDGETS")
 	frame:SetScript("OnEvent", function()
 		for _, widgetFrame in pairs(_G.UIWidgetTopCenterContainerFrame.widgetFrames) do
-			if widgetFrame.widgetType == doubleBarType then
-				for _, bar in pairs({widgetFrame.LeftBar, widgetFrame.RightBar}) do
-					if not bar.styled then
+			if widgetFrame.widgetType == Type_DoubleStatusBar then
+				if not widgetFrame.styled then
+					for _, bar in pairs({widgetFrame.LeftBar, widgetFrame.RightBar}) do
 						bar.BG:SetAlpha(0)
 						bar.BorderLeft:SetAlpha(0)
 						bar.BorderRight:SetAlpha(0)
@@ -47,36 +49,46 @@ local function LoadSkin()
 						bar.SparkGlow:SetAlpha(0)
 						bar.BorderGlow:SetAlpha(0)
 						MERS:CreateBD(bar)
-						hooksecurefunc(bar, "SetStatusBarAtlas", updateBarTexture)
-
-						bar.styled = true
+						hooksecurefunc(bar, "SetStatusBarAtlas", UpdateBarTexture)
 					end
+
+					widgetFrame.styled = true
+				end
+			elseif widgetFrame.widgetType == Type_SpellDisplay then
+				if not widgetFrame.styled then
+					local widgetSpell = widgetFrame.Spell
+					widgetSpell.IconMask:Hide()
+					widgetSpell.Border:SetTexture(nil)
+					widgetSpell.DebuffBorder:SetTexture(nil)
+					S:HandleIcon(widgetSpell.Icon)
+
+					widgetFrame.styled = true
 				end
 			end
 		end
 	end)
 
-	hooksecurefunc(_G.UIWidgetTemplateCaptureBarMixin, "Setup", function(widgetInfo)
-		widgetInfo.LeftLine:SetAlpha(0)
-		widgetInfo.RightLine:SetAlpha(0)
-		widgetInfo.BarBackground:SetAlpha(0)
-		widgetInfo.Glow1:SetAlpha(0)
-		widgetInfo.Glow2:SetAlpha(0)
-		widgetInfo.Glow3:SetAlpha(0)
+	hooksecurefunc(_G.UIWidgetTemplateCaptureBarMixin, "Setup", function(self)
+		self.LeftLine:SetAlpha(0)
+		self.RightLine:SetAlpha(0)
+		self.BarBackground:SetAlpha(0)
+		self.Glow1:SetAlpha(0)
+		self.Glow2:SetAlpha(0)
+		self.Glow3:SetAlpha(0)
 
-		widgetInfo.LeftBar:SetTexture(E.media.normTex)
-		widgetInfo.NeutralBar:SetTexture(E.media.normTex)
-		widgetInfo.RightBar:SetTexture(E.media.normTex)
+		self.LeftBar:SetTexture(E.media.normTex)
+		self.NeutralBar:SetTexture(E.media.normTex)
+		self.RightBar:SetTexture(E.media.normTex)
 
-		widgetInfo.LeftBar:SetVertexColor(.2, .6, 1)
-		widgetInfo.NeutralBar:SetVertexColor(.8, .8, .8)
-		widgetInfo.RightBar:SetVertexColor(.9, .2, .2)
+		self.LeftBar:SetVertexColor(.2, .6, 1)
+		self.NeutralBar:SetVertexColor(.8, .8, .8)
+		self.RightBar:SetVertexColor(.9, .2, .2)
 	end)
 
-	hooksecurefunc(_G.UIWidgetTemplateStatusBarMixin, "Setup", function(widgetInfo)
-		local bar = widgetInfo.Bar
+	hooksecurefunc(_G.UIWidgetTemplateStatusBarMixin, "Setup", function(self)
+		local bar = self.Bar
 		local atlas = bar:GetStatusBarAtlas()
-		updateBarTexture(bar, atlas)
+		UpdateBarTexture(bar, atlas)
 
 		if not bar.styled then
 			bar.BGLeft:SetAlpha(0)
