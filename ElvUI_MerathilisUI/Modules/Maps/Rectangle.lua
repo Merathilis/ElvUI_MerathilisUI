af57a579cf9f2200047d9da898e3c23497d1d324
Repooc <tjcooper1982@gmail.com>
2020-06-25 12:19:50 -0400

    oops

--- ElvUI_MerathilisUI/Modules/Maps/Rectangle.lua
+++ ElvUI_MerathilisUI/Modules/Maps/Rectangle.lua
@@ -14,7 +14,6 @@ local hooksecurefunc = hooksecurefunc
 local BAR_HEIGHT = 22
 
 function module:SkinMiniMap()
--- local function SkinMiniMap()
 	_G.Minimap:SetMaskTexture('Interface\\AddOns\\ElvUI_MerathilisUI\\media\\textures\\rectangle')
 	_G.Minimap:Size(E.MinimapSize, E.MinimapSize)
 	_G.Minimap:SetHitRectInsets(0, 0, (E.MinimapSize/6.1)*E.mult, (E.MinimapSize/6.1)*E.mult)
