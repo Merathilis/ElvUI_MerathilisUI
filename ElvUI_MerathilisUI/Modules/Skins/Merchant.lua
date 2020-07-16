bff704a961ca5837ab6e4856a6dff393b159a7f9
Merathilis <neo0608@gmail.com>
2020-07-16 10:37:01 +0200

    Merchant cleanup

--- ElvUI_MerathilisUI/Modules/Skins/Merchant.lua
+++ ElvUI_MerathilisUI/Modules/Skins/Merchant.lua
@@ -41,7 +41,7 @@ local SetItemButtonNormalTextureVertexColor = SetItemButtonNormalTextureVertexCo
 
 local ItemsPerSubpage, SubpagesPerPage
 local RETRIEVING_ITEM_INFO, RETRIEVING_ITEM_INFO, MISCELLANEOUS, MOUNT, ITEM_SPELL_KNOWN, SEARCH = RETRIEVING_ITEM_INFO, RETRIEVING_ITEM_INFO, MISCELLANEOUS, MOUNT, ITEM_SPELL_KNOWN, SEARCH
-local MAX_MONEY_DISPLAY_WIDTH = 120;
+local MAX_MONEY_DISPLAY_WIDTH = 120
 local RECIPE = GetItemClassInfo(_G.LE_ITEM_CLASS_RECIPE)
 local searchBox
 local searching = ""
@@ -101,7 +101,7 @@ local function SkinVendorItems(i)
 end
 
 local function UpdateButtonsPositions(isBuyBack)
-	local btn;
+	local btn
 	local vertSpacing, horizSpacing
 
 	if (isBuyBack) then
@@ -153,19 +153,19 @@ local function UpdateBuybackInfo()
 	if E.db.mui.merchant.style == "Default" then UpdateButtonsPositions(true) end
 
 	-- apply coloring
-	local btn, link, quality, r, g, b, _;
+	local btn, link, quality, r, g, b, _
 	for i = 1, _G.BUYBACK_ITEMS_PER_PAGE, 1 do
-		btn = _G["MerchantItem" .. i];
+		btn = _G["MerchantItem" .. i]
 		if (btn) then
-			link = GetBuybackItemLink(i);
+			link = GetBuybackItemLink(i)
 			if (link) then
-				_, _, quality = GetItemInfo(link);
+				_, _, quality = GetItemInfo(link)
 				if quality then
-					r, g, b = GetItemQualityColor(quality);
+					r, g, b = GetItemQualityColor(quality)
 				else
 					r, g, b = 1,1,1
 				end
-				_G["MerchantItem" .. i .. "Name"]:SetTextColor(r, g, b);
+				_G["MerchantItem" .. i .. "Name"]:SetTextColor(r, g, b)
 			end
 		end
 	end
@@ -173,7 +173,7 @@ end
 
 local function isKnown(link, itemType, itemSubType)
 	if ( not link ) then
-		return false;
+		return false
 	end
 
 	local upperLimit
@@ -185,15 +185,15 @@ local function isKnown(link, itemType, itemSubType)
 		isMount = true
 	end
 
-	_G["MER_Merchant_HiddenTooltip"]:SetOwner(UIParent, "ANCHOR_NONE");
-	_G["MER_Merchant_HiddenTooltip"]:SetHyperlink(link);
+	_G["MER_Merchant_HiddenTooltip"]:SetOwner(UIParent, "ANCHOR_NONE")
+	_G["MER_Merchant_HiddenTooltip"]:SetHyperlink(link)
 	upperLimit = isRecipe and _G["MER_Merchant_HiddenTooltip"]:NumLines() or 0
 
 	for i = 2, _G["MER_Merchant_HiddenTooltip"]:NumLines() do
 		if (isRecipe and (i <= 5 or i >= upperLimit - 3)) or isMount or not isRecipe then
-			local text = _G["MER_Merchant_HiddenTooltipTextLeft"..i];
-			local r, g, b = text:GetTextColor();
-			local gettext = text:GetText();
+			local text = _G["MER_Merchant_HiddenTooltipTextLeft"..i]
+			local r, g, b = text:GetTextColor()
+			local gettext = text:GetText()
 
 			if ( gettext and r >= 0.9 and g <= 0.2 and b <= 0.2 and gettext ~= RETRIEVING_ITEM_INFO ) then
 				if gettext == ITEM_SPELL_KNOWN then return true end
@@ -207,33 +207,32 @@ end
 local function UpdateMerchantInfo()
 	UpdateButtonsPositions()
 
-	local totalMerchantItems = GetMerchantNumItems();
+	local totalMerchantItems = GetMerchantNumItems()
 	local visibleMerchantItems = 0
 	local indexes = {}
-	local _, name, texture, price, quantity, numAvailable, isPurchasable, isUsable, extendedCost, r, g, b, notOptimal;
-	local link, quality, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemSellPrice, itemId;
+	local _, name, texture, price, quantity, numAvailable, isPurchasable, isUsable, extendedCost, r, g, b, notOptimal
+	local link, quality, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemSellPrice, itemId
 
 	for i = 1, totalMerchantItems do
-		tinsert(indexes, i);
-		visibleMerchantItems = visibleMerchantItems + 1;
+		tinsert(indexes, i)
+		visibleMerchantItems = visibleMerchantItems + 1
 	end
 
 	 -- validate current page shown
 	if (_G["MerchantFrame"].page > math_max(1, math_ceil(visibleMerchantItems / _G.MERCHANT_ITEMS_PER_PAGE))) then
-		_G["MerchantFrame"].page = math_max(1, math_ceil(visibleMerchantItems / _G.MERCHANT_ITEMS_PER_PAGE));
+		_G["MerchantFrame"].page = math_max(1, math_ceil(visibleMerchantItems / _G.MERCHANT_ITEMS_PER_PAGE))
 	end
 
 	-- Show correct page count based on number of items shown
-	_G["MerchantPageText"]:SetFormattedText(_G.MERCHANT_PAGE_NUMBER, _G["MerchantFrame"].page, math_ceil(visibleMerchantItems / _G.MERCHANT_ITEMS_PER_PAGE));
+	_G["MerchantPageText"]:SetFormattedText(_G.MERCHANT_PAGE_NUMBER, _G["MerchantFrame"].page, math_ceil(visibleMerchantItems / _G.MERCHANT_ITEMS_PER_PAGE))
 
-	--Display shit
 	for i = 1, _G.MERCHANT_ITEMS_PER_PAGE do
-		local index = ((_G["MerchantFrame"].page - 1) * _G.MERCHANT_ITEMS_PER_PAGE) + i;
-		local itemButton = _G["MerchantItem" .. i .. "ItemButton"];
-		itemButton.link = nil;
-		local merchantButton = _G["MerchantItem" .. i];
-		local merchantMoney = _G["MerchantItem" .. i .. "MoneyFrame"];
-		local merchantAltCurrency = _G["MerchantItem" .. i .. "AltCurrencyFrame"];
+		local index = ((_G["MerchantFrame"].page - 1) * _G.MERCHANT_ITEMS_PER_PAGE) + i
+		local itemButton = _G["MerchantItem" .. i .. "ItemButton"]
+		itemButton.link = nil
+		local merchantButton = _G["MerchantItem" .. i]
+		local merchantMoney = _G["MerchantItem" .. i .. "MoneyFrame"]
+		local merchantAltCurrency = _G["MerchantItem" .. i .. "AltCurrencyFrame"]
 
 		if (index <= visibleMerchantItems) then
 			-- ItemLevel
@@ -245,6 +244,7 @@ local function UpdateMerchantInfo()
 					itemButton.text:SetText("")
 				end
 
+				-- ItemLevel on MerchantFrame
 				local itemLink = GetMerchantItemLink(index)
 				if itemLink then
 					local _, _, quality, itemlevel, _, _, _, _, _, _, _, itemClassID = GetItemInfo(itemLink)
@@ -256,117 +256,117 @@ local function UpdateMerchantInfo()
 				end
 			end
 
-			name, texture, price, quantity, numAvailable, isPurchasable, isUsable, extendedCost = GetMerchantItemInfo(indexes[index]);
+			name, texture, price, quantity, numAvailable, isPurchasable, isUsable, extendedCost = GetMerchantItemInfo(indexes[index])
 			if (name ~= nil) then
-				local canAfford = CanAffordMerchantItem(index);
-				_G["MerchantItem"..i.."Name"]:SetText(name);
-				SetItemButtonCount(itemButton, quantity);
-				SetItemButtonStock(itemButton, numAvailable);
-				SetItemButtonTexture(itemButton, texture);
+				local canAfford = CanAffordMerchantItem(index)
+				_G["MerchantItem"..i.."Name"]:SetText(name)
+				SetItemButtonCount(itemButton, quantity)
+				SetItemButtonStock(itemButton, numAvailable)
+				SetItemButtonTexture(itemButton, texture)
 
 				if ( extendedCost and (price <= 0) ) then -- update item's currency info
-					itemButton.price = nil;
-					itemButton.extendedCost = true;
-					itemButton.name = name;
-					itemButton.link = GetMerchantItemLink(indexes[index]);
-					itemButton.texture = texture;
-					MerchantFrame_UpdateAltCurrency(index, i, canAfford);
-					merchantAltCurrency:ClearAllPoints();
-					merchantAltCurrency:SetPoint("BOTTOMLEFT", "MerchantItem"..i.."NameFrame", "BOTTOMLEFT", 0, 31);
-					merchantMoney:Hide();
-					merchantAltCurrency:Show();
+					itemButton.price = nil
+					itemButton.extendedCost = true
+					itemButton.name = name
+					itemButton.link = GetMerchantItemLink(indexes[index])
+					itemButton.texture = texture
+					MerchantFrame_UpdateAltCurrency(index, i, canAfford)
+					merchantAltCurrency:ClearAllPoints()
+					merchantAltCurrency:SetPoint("BOTTOMLEFT", "MerchantItem"..i.."NameFrame", "BOTTOMLEFT", 0, 31)
+					merchantMoney:Hide()
+					merchantAltCurrency:Show()
 				elseif ( extendedCost and (price > 0) ) then
-					itemButton.price = price;
-					itemButton.extendedCost = true;
-					itemButton.name = name;
-					itemButton.link = GetMerchantItemLink(indexes[index]);
-					itemButton.texture = texture;
-					local altCurrencyWidth = MerchantFrame_UpdateAltCurrency(index, i, canAfford);
-					MoneyFrame_SetMaxDisplayWidth(merchantMoney, MAX_MONEY_DISPLAY_WIDTH - altCurrencyWidth);
-					MoneyFrame_Update(merchantMoney:GetName(), price);
-					local color;
+					itemButton.price = price
+					itemButton.extendedCost = true
+					itemButton.name = name
+					itemButton.link = GetMerchantItemLink(indexes[index])
+					itemButton.texture = texture
+					local altCurrencyWidth = MerchantFrame_UpdateAltCurrency(index, i, canAfford)
+					MoneyFrame_SetMaxDisplayWidth(merchantMoney, MAX_MONEY_DISPLAY_WIDTH - altCurrencyWidth)
+					MoneyFrame_Update(merchantMoney:GetName(), price)
+					local color
 					if (canAfford == false) then
-						color = "red";
+						color = "red"
 					end
-					SetMoneyFrameColor(merchantMoney:GetName(), color);
-					merchantAltCurrency:ClearAllPoints();
-					merchantAltCurrency:SetPoint("LEFT", merchantMoney:GetName(), "RIGHT", -14, 0);
-					merchantAltCurrency:Show();
-					merchantMoney:Show();
+					SetMoneyFrameColor(merchantMoney:GetName(), color)
+					merchantAltCurrency:ClearAllPoints()
+					merchantAltCurrency:SetPoint("LEFT", merchantMoney:GetName(), "RIGHT", -14, 0)
+					merchantAltCurrency:Show()
+					merchantMoney:Show()
 				else
-					itemButton.price = price;
-					itemButton.extendedCost = nil;
-					itemButton.name = name;
-					itemButton.link = GetMerchantItemLink(indexes[index]);
-					itemButton.texture = texture;
-					MoneyFrame_SetMaxDisplayWidth(merchantMoney, MAX_MONEY_DISPLAY_WIDTH);
-					MoneyFrame_Update(merchantMoney:GetName(), price);
-					local color;
+					itemButton.price = price
+					itemButton.extendedCost = nil
+					itemButton.name = name
+					itemButton.link = GetMerchantItemLink(indexes[index])
+					itemButton.texture = texture
+					MoneyFrame_SetMaxDisplayWidth(merchantMoney, MAX_MONEY_DISPLAY_WIDTH)
+					MoneyFrame_Update(merchantMoney:GetName(), price)
+					local color
 					if (canAfford == false) then
-						color = "red";
+						color = "red"
 					end
-					SetMoneyFrameColor(merchantMoney:GetName(), color);
-					merchantAltCurrency:Hide();
-					merchantMoney:Show();
+					SetMoneyFrameColor(merchantMoney:GetName(), color)
+					merchantAltCurrency:Hide()
+					merchantMoney:Show()
 				end
 
-				MerchantFrameItem_UpdateQuality(merchantButton, itemButton.link);
+				MerchantFrameItem_UpdateQuality(merchantButton, itemButton.link)
 
-				local merchantItemID = GetMerchantItemID(index);
-				local isHeirloom = merchantItemID and C_Heirloom_IsItemHeirloom(merchantItemID);
-				local isKnownHeirloom = isHeirloom and C_Heirloom_PlayerHasHeirloom(merchantItemID);
+				local merchantItemID = GetMerchantItemID(index)
+				local isHeirloom = merchantItemID and C_Heirloom_IsItemHeirloom(merchantItemID)
+				local isKnownHeirloom = isHeirloom and C_Heirloom_PlayerHasHeirloom(merchantItemID)
 
-				itemButton.showNonrefundablePrompt = isHeirloom;
+				itemButton.showNonrefundablePrompt = isHeirloom
 
-				itemButton.hasItem = true;
-				itemButton:SetID(indexes[index]);
-				itemButton:Show();
+				itemButton.hasItem = true
+				itemButton:SetID(indexes[index])
+				itemButton:Show()
 
-				local tintRed = not isPurchasable or (not isUsable and not isHeirloom);
+				local tintRed = not isPurchasable or (not isUsable and not isHeirloom)
 
-				local colorMult = 1.0;
-				local detailColor = {};
-				local slotColor = {};
+				local colorMult = 1.0
+				local detailColor = {}
+				local slotColor = {}
 				-- unavailable items (limited stock, bought out) are darkened
 				if ( numAvailable == 0 or isKnownHeirloom) then
-					colorMult = 0.5;
+					colorMult = 0.5
 				end
 				if ( tintRed ) then
-					slotColor = {r = 1.0, g = 0, b = 0};
-					detailColor = {r = 1.0, g = 0, b = 0};
+					slotColor = {r = 1.0, g = 0, b = 0}
+					detailColor = {r = 1.0, g = 0, b = 0}
 				else
 					if not isKnown(itemButton.link, itemType, itemSubType) then
-						slotColor = {r = 1.0, g = 1.0, b = 1.0};
-						detailColor = {r = 0.5, g = 0.5, b = 0.5};
+						slotColor = {r = 1.0, g = 1.0, b = 1.0}
+						detailColor = {r = 0.5, g = 0.5, b = 0.5}
 					else
-						slotColor = {r = 1.0, g = 0, b = 0};
-						detailColor = {r = 1.0, g = 0, b = 0};
+						slotColor = {r = 1.0, g = 0, b = 0}
+						detailColor = {r = 1.0, g = 0, b = 0}
 					end
 				end
-				local alpha = 0.3;
+				local alpha = 0.3
 				if ( searching == "" or searching == SEARCH:lower() or name:lower():match(searching)
 					or ( quality and ( tostring(quality):lower():match(searching) or _G["ITEM_QUALITY"..tostring(quality).."_DESC"]:lower():match(searching) ) )
 					or ( itemType and itemType:lower():match(searching) )
 					or ( itemSubType and itemSubType:lower():match(searching) )
 					) then
-					alpha = 1;
+					alpha = 1
 				end
-				merchantButton:SetAlpha(alpha);
-				SetItemButtonNameFrameVertexColor(merchantButton, detailColor.r * colorMult, detailColor.g * colorMult, detailColor.b * colorMult);
-				SetItemButtonSlotVertexColor(merchantButton, slotColor.r * colorMult, slotColor.g * colorMult, slotColor.b * colorMult);
-				SetItemButtonTextureVertexColor(itemButton, slotColor.r * colorMult, slotColor.g * colorMult, slotColor.b * colorMult);
-				SetItemButtonNormalTextureVertexColor(itemButton, slotColor.r * colorMult, slotColor.g * colorMult, slotColor.b * colorMult);
+				merchantButton:SetAlpha(alpha)
+				SetItemButtonNameFrameVertexColor(merchantButton, detailColor.r * colorMult, detailColor.g * colorMult, detailColor.b * colorMult)
+				SetItemButtonSlotVertexColor(merchantButton, slotColor.r * colorMult, slotColor.g * colorMult, slotColor.b * colorMult)
+				SetItemButtonTextureVertexColor(itemButton, slotColor.r * colorMult, slotColor.g * colorMult, slotColor.b * colorMult)
+				SetItemButtonNormalTextureVertexColor(itemButton, slotColor.r * colorMult, slotColor.g * colorMult, slotColor.b * colorMult)
 			end
 		else
-			itemButton.price = nil;
-			itemButton.hasItem = nil;
-			itemButton.name = nil;
-			itemButton:Hide();
-			SetItemButtonNameFrameVertexColor(merchantButton, 0.5, 0.5, 0.5);
-			SetItemButtonSlotVertexColor(merchantButton,0.4, 0.4, 0.4);
-			_G["MerchantItem"..i.."Name"]:SetText("");
-			_G["MerchantItem"..i.."MoneyFrame"]:Hide();
-			_G["MerchantItem"..i.."AltCurrencyFrame"]:Hide();
+			itemButton.price = nil
+			itemButton.hasItem = nil
+			itemButton.name = nil
+			itemButton:Hide()
+			SetItemButtonNameFrameVertexColor(merchantButton, 0.5, 0.5, 0.5)
+			SetItemButtonSlotVertexColor(merchantButton,0.4, 0.4, 0.4)
+			_G["MerchantItem"..i.."Name"]:SetText("")
+			_G["MerchantItem"..i.."MoneyFrame"]:Hide()
+			_G["MerchantItem"..i.."AltCurrencyFrame"]:Hide()
 		end
 	end
 end
@@ -401,20 +401,20 @@ local function RebuildMerchantFrame()
 	_G["MerchantBuyBackItem"]:SetPoint("TOPLEFT", _G["MerchantItem10"], "BOTTOMLEFT", -14, -20)
 
 	-- move the next/previous page buttons
-	_G["MerchantPrevPageButton"]:ClearAllPoints();
-	_G["MerchantPrevPageButton"]:SetPoint("CENTER", _G["MerchantFrame"], "BOTTOM", 50, 70);
-	_G["MerchantPageText"]:ClearAllPoints();
-	_G["MerchantPageText"]:SetPoint("BOTTOM", _G["MerchantFrame"], "BOTTOM", 160, 65);
-	_G["MerchantNextPageButton"]:ClearAllPoints();
-	_G["MerchantNextPageButton"]:SetPoint("CENTER", _G["MerchantFrame"], "BOTTOM", 270, 70);
+	_G["MerchantPrevPageButton"]:ClearAllPoints()
+	_G["MerchantPrevPageButton"]:SetPoint("CENTER", _G["MerchantFrame"], "BOTTOM", 50, 70)
+	_G["MerchantPageText"]:ClearAllPoints()
+	_G["MerchantPageText"]:SetPoint("BOTTOM", _G["MerchantFrame"], "BOTTOM", 160, 65)
+	_G["MerchantNextPageButton"]:ClearAllPoints()
+	_G["MerchantNextPageButton"]:SetPoint("CENTER", _G["MerchantFrame"], "BOTTOM", 270, 70)
 
 	-- currency insets
-	_G["MerchantExtraCurrencyInset"]:ClearAllPoints();
-	_G["MerchantExtraCurrencyInset"]:SetPoint("BOTTOMRIGHT", _G["MerchantMoneyInset"], "BOTTOMLEFT", 0, 0);
-	_G["MerchantExtraCurrencyInset"]:SetPoint("TOPLEFT", _G["MerchantMoneyInset"], "TOPLEFT", -165, 0);
-	_G["MerchantExtraCurrencyBg"]:ClearAllPoints();
-	_G["MerchantExtraCurrencyBg"]:SetPoint("TOPLEFT", _G["MerchantExtraCurrencyInset"], "TOPLEFT", 3, -2);
-	_G["MerchantExtraCurrencyBg"]:SetPoint("BOTTOMRIGHT", _G["MerchantExtraCurrencyInset"], "BOTTOMRIGHT", -3, 2);
+	_G["MerchantExtraCurrencyInset"]:ClearAllPoints()
+	_G["MerchantExtraCurrencyInset"]:SetPoint("BOTTOMRIGHT", _G["MerchantMoneyInset"], "BOTTOMLEFT", 0, 0)
+	_G["MerchantExtraCurrencyInset"]:SetPoint("TOPLEFT", _G["MerchantMoneyInset"], "TOPLEFT", -165, 0)
+	_G["MerchantExtraCurrencyBg"]:ClearAllPoints()
+	_G["MerchantExtraCurrencyBg"]:SetPoint("TOPLEFT", _G["MerchantExtraCurrencyInset"], "TOPLEFT", 3, -2)
+	_G["MerchantExtraCurrencyBg"]:SetPoint("BOTTOMRIGHT", _G["MerchantExtraCurrencyInset"], "BOTTOMRIGHT", -3, 2)
 
 	searchBox = CreateFrame("EditBox", "$parentSearchBox", _G["MerchantFrame"], "InputBoxTemplate")
 	searchBox:SetWidth(_G["MerchantItem1"]:GetWidth())
@@ -452,7 +452,7 @@ local function MerchantSkinInit()
 
 	RebuildMerchantFrame()
 	UpdateButtonsPositions()
-	CreateFrame("GameTooltip", "MER_Merchant_HiddenTooltip", UIParent, "GameTooltipTemplate");
+	CreateFrame("GameTooltip", "MER_Merchant_HiddenTooltip", UIParent, "GameTooltipTemplate")
 
 	hooksecurefunc("MerchantFrame_UpdateMerchantInfo", UpdateMerchantInfo)
 end
