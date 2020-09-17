40bcc187c18ea4207607df021b738c8a6a2b039a
Merathilis <neo0608@gmail.com>
2020-09-17 12:38:15 +0200

    whoopsi

--- ElvUI_MerathilisUI/Modules/Actionbars/MicroBar.lua
+++ ElvUI_MerathilisUI/Modules/Actionbars/MicroBar.lua
@@ -1138,12 +1138,12 @@ function module:Initialize()
 
 	self:CreateMicroBar()
 	self:Template()
-	--self:Toggle()
+	self:Toggle()
 
 	function module:ForUpdateAll()
 		module.db = E.db.mui.microBar
 
-		--self:Toggle()
+		self:Toggle()
 	end
 
 	self:ForUpdateAll()
