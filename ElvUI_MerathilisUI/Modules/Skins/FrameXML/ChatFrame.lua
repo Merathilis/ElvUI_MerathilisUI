ace0e77221d9ca5bd3c2910ca576b409f0732849
Merathilis <neo0608@gmail.com>
2020-10-05 18:54:30 +0200

    ChatMenu Styling

--- ElvUI_MerathilisUI/Modules/Skins/FrameXML/ChatFrame.lua
+++ ElvUI_MerathilisUI/Modules/Skins/FrameXML/ChatFrame.lua
@@ -2,12 +2,9 @@ local MER, E, L, V, P, G = unpack(select(2, ...))
 local MERS = MER:GetModule('MER_Skins')
 local S = E:GetModule('Skins')
 
--- Cache global variables
--- Lua functions
 local _G = _G
--- WoW API
+local pairs = pairs
 local hooksecurefunc = hooksecurefunc
--- GLOBALS:
 
 local function LoadSkin()
 	if E.private.chat.enable ~= true then return; end
@@ -35,6 +32,21 @@ local function LoadSkin()
 		--ElvUI ChatButtonHolder
 		if _G.ChatButtonHolder then
 			_G.ChatButtonHolder:Styling()
+		end
+	end
+
+	do
+		local ChatMenus = {
+			_G.ChatMenu,
+			_G.EmoteMenu,
+			_G.LanguageMenu,
+			_G.VoiceMacroMenu,
+		}
+
+		for _, menu in pairs(ChatMenus) do
+			if menu then
+				menu:Styling()
+			end
 		end
 	end
 end
