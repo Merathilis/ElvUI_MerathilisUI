ee47b471915be9425ed5ada72ae23d31ecff271d
Merathilis <neo0608@gmail.com>
2020-10-13 09:53:16 +0200

    Cleanup locale

--- ElvUI_MerathilisUI/Locales/itIT.lua
+++ ElvUI_MerathilisUI/Locales/itIT.lua
@@ -185,16 +185,10 @@ L["BattleOfDazaralor"] = "Battle Of Dazaralor"
 L["CrucibleOfStorms"] = "Crucible Of Storms"
 L["FACTION"] = "Fazione"
 L["Only Icons"] = true
-L["Append to corruption stat"] = "Append to corruption stat"
 L["Use the new style tooltip."] = "Use the new style tooltip."
-L["Show summary on the corruption tooltip"] = "Show summary on the corruption tooltip"
-L["List your corruptions in the eye tooltip in the character screen."] = "List your corruptions in the eye tooltip in the character screen."
 L["Display in English"] = "Display in English"
-L["Don't translate the corruption effect names."] = "Don't translate the corruption effect names."
 L["Show icon"] = "Show icon"
 L["Show the spell icon along with the name."] = "Show the spell icon along with the name."
-L["Show corruption amount in the character screen"] = "Show corruption amount in the character screen"
-L["Show corruption stat on items in the character screen when displaying the corruption tooltip."] = "Show corruption stat on items in the character screen when displaying the corruption tooltip."
 
 -- MailInputBox
 L["Mail Inputbox Resize"] = true
--- ElvUI_MerathilisUI/Locales/ruRU.lua
+++ ElvUI_MerathilisUI/Locales/ruRU.lua
@@ -187,12 +187,7 @@ L["Only Icons"] = true
 L["I"] = "1 ур."
 L["II"] = "2 ур."
 L["III"] = "3 ур."
-L["Append to corruption stat"] = "Добавить в характеристики порчи"
-L["Use the new style tooltip."] = "Используйте новый стиль подсказки."
-L["Show summary on the corruption tooltip"] = "Показать сводку во всплывающей подсказке"
-L["List your corruptions in the eye tooltip in the character screen."] = "Отображать список эффектов порчи во всплывающей подсказке на экране персонажа."
 L["Display in English"] = "Отображать английские названия"
-L["Don't translate the corruption effect names."] = "Не переводить названия эффектов порчи."
 L["Show icon"] = "Показать значок"
 L["Show the spell icon along with the name."] = "Показать значок заклинания вместе с названием."
 
