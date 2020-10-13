0169c20f07b2ba5ec653371b08ca26f7d6d4ee48
Merathilis <neo0608@gmail.com>
2020-10-13 15:38:57 +0200

    @Repooc that's what i got so far :thinking:

--- ElvUI_MerathilisUI/Modules/Maps/Rectangle.lua
+++ ElvUI_MerathilisUI/Modules/Maps/Rectangle.lua
@@ -14,6 +14,23 @@ local hooksecurefunc = hooksecurefunc
 
 local BAR_HEIGHT = 22
 
+function module:SetupHybridMinimap()
+	local MapCanvas = _G.HybridMinimap.MapCanvas
+	MapCanvas:SetMaskTexture('Interface\\AddOns\\ElvUI_MerathilisUI\\media\\textures\\rectangle')
+	MapCanvas:Size(E.MinimapSize, E.MinimapSize)
+	MapCanvas:SetHitRectInsets(0, 0, (E.MinimapSize/6.1)*E.mult, (E.MinimapSize/6.1)*E.mult)
+	MapCanvas:SetClampRectInsets(0, 0, 0, 0)
+
+	module:UpdateHybridMoverSize()
+
+	--MapCanvas:ClearAllPoints()
+	MapCanvas:Point("TOPRIGHT", _G.MMHolder, "TOPRIGHT", -E.Border, (E.MinimapSize/6.1)+E.Border)
+
+	if MapCanvas.backdrop then
+		MapCanvas.backdrop:SetOutside(MapCanvas, 1, -(E.MinimapSize/6.1)+1)
+	end
+end
+
 function module:SkinMiniMap()
 	_G.Minimap:SetMaskTexture('Interface\\AddOns\\ElvUI_MerathilisUI\\media\\textures\\rectangle')
 	_G.Minimap:Size(E.MinimapSize, E.MinimapSize)
@@ -39,6 +56,14 @@ function module:SkinMiniMap()
 
 	if _G.Minimap.backdrop then
 		_G.Minimap.backdrop:SetOutside(_G.Minimap, 1, -(E.MinimapSize/6.1)+1)
+	end
+end
+
+function module:UpdateHybridMoverSize()
+	if E.db.datatexts.panels.MinimapPanel.enable then
+		_G.MMHolder:Height((_G.HybridMinimap.MapCanvas:GetHeight() + (_G.MinimapPanel and (_G.MinimapPanel:GetHeight() + E.Border) or 24)) + E.Spacing*3-((E.MinimapSize/6.1)))
+	else
+		_G.MMHolder:Height((_G.HybridMinimap.MapCanvas:GetHeight() + E.Border + E.Spacing*3)-(E.MinimapSize/6.1))
 	end
 end
 
@@ -89,15 +114,24 @@ function module:UpdateLocationText()
 	_G.Minimap.location:Point('TOP', _G.Minimap, 'TOP', 0, -22)
 end
 
+function module:ADDON_LOADED(_, addon)
+	if addon == 'Blizzard_HybridMinimap' then
+		module:SetupHybridMinimap()
+	end
+end
+
 function module:RectangleMinimap()
 	if not E.private.general.minimap.enable or not E.db.mui.maps.minimap.rectangle then return end
 
 	module:SkinMiniMap()
+	if _G.HybridMinimap then module:SetupHybridMinimap() end
 
 	-- Only adjust the settings for me
 	if MER:IsDeveloper() and MER:IsDeveloperRealm() then
 		module:AdjustSettings()
 	end
 
-	hooksecurefunc(MM, "UpdateSettings", module.UpdateMoverSize)
+	hooksecurefunc(MM, 'UpdateSettings', module.UpdateMoverSize)
+	--hooksecurefunc(MM, 'UpdateSettings', module.UpdateHybridMoverSize)
+	--self:RegisterEvent('ADDON_LOADED')
 end
