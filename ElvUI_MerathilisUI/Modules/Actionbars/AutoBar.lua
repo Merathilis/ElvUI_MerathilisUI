00491bc61c0f7b5817f6c7887db47417e3ea7173
Merathilis <neo0608@gmail.com>
2020-10-14 08:11:04 +0200

    AutoBar stuff... must check with masque later

--- ElvUI_MerathilisUI/Modules/Actionbars/AutoBar.lua
+++ ElvUI_MerathilisUI/Modules/Actionbars/AutoBar.lua
@@ -229,9 +229,8 @@ local function CreateButton(name, size)
 	local AutoButton = CreateFrame("Button", name, E.UIParent, "SecureActionButtonTemplate")
 	AutoButton:Size(size)
 	AutoButton:StyleButton()
-	if not useMasque then
-		AutoButton:CreateBackdrop()
-	end
+	AutoButton:CreateBackdrop()
+
 	AutoButton:SetClampedToScreen(true)
 	AutoButton:SetAttribute("type", "item")
 	AutoButton:SetAlpha(0)
@@ -379,7 +378,7 @@ function module:ScanItem(event)
 			AutoButton.ap = false
 			AutoButton.questLogIndex = QuestItemList[itemID] and QuestItemList[itemID].questLogIndex or -1
 			AutoButton.spellName = IsUsableItem(itemID)
-			--AutoButton.backdrop:SetBackdropBorderColor(nil)
+			AutoButton.backdrop:SetBackdropBorderColor(nil)
 
 			if db.questAutoButtons["questBBColorByItem"] then
 				if rarity and rarity > Enum.ItemQuality.Common then
@@ -433,7 +432,7 @@ function module:ScanItem(event)
 				local AutoButton = _G["AutoSlotButton" .. num]
 				if not AutoButton then break end
 
-				--AutoButton.backdrop:SetBackdropBorderColor(nil)
+				AutoButton.backdrop:SetBackdropBorderColor(nil)
 
 				local r, g, b, colorDB
 				if db.slotAutoButtons["slotBBColorByItem"] then
@@ -480,7 +479,7 @@ function module:ScanItem(event)
 			AutoButton.itemName = itemName
 			AutoButton.itemID = itemID
 			AutoButton.spellName = IsUsableItem(itemID)
-			--AutoButton.backdrop:SetBackdropBorderColor(nil)
+			AutoButton.backdrop:SetBackdropBorderColor(nil)
 
 			if db.usableAutoButtons["usableBBColorByItem"] then
 				if rarity and rarity > Enum.ItemQuality.Common then
