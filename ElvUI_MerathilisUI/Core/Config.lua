4da1b3cdee04c5f39f17cb195ac9a9459eac7b27
Merathilis <neo0608@gmail.com>
2020-10-15 16:39:42 +0200

    whoops

--- ElvUI_MerathilisUI/Core/Config.lua
+++ ElvUI_MerathilisUI/Core/Config.lua
@@ -80,7 +80,6 @@ local function AddOptions()
 				order = 8,
 				type = "group",
 				name = L["General"],
-				icon = MER.Media.Icons.announcement,
 				args = {
 					generalHeader = ACH:Header(MER:cOption(L["General"]), 1),
 					LoginMsg = {
