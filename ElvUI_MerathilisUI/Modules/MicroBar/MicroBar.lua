60df81f4d96b9c0926b554ae8de0562e5daa0638
Merathilis <neo0608@gmail.com>
2020-10-21 08:56:12 +0200

    Updated Tooltip Anchor.

--- ElvUI_MerathilisUI/Modules/MicroBar/MicroBar.lua
+++ ElvUI_MerathilisUI/Modules/MicroBar/MicroBar.lua
@@ -507,12 +507,10 @@ function module:ConstructTimeArea()
 		E:UIFrameFadeIn(panel.minutesHover, self.db.fadeTime, panel.minutesHover:GetAlpha(), 1)
 		E:UIFrameFadeIn(panel.text, self.db.fadeTime, panel.text:GetAlpha(), 1)
 
-		if self.db.tooltipPosition == "BOTTOM" then
-			DT.tooltip:SetOwner(panel.text, "ANCHOR_BOTTOM", 0, -5)
-		elseif self.db.tooltipPosition == "TOP" then
-			DT.tooltip:SetOwner(panel.text, "ANCHOR_TOP", 0, 40)
-		else
-			DT.tooltip:SetOwner(panel.text, "ANCHOR_BOTTOM", 0, -5)
+		if self.db.tooltipPosition == 'ANCHOR_BOTTOM' then
+			DT.tooltip:SetOwner(panel.text, self.db.tooltipPosition, 0, -5)
+		elseif self.db.tooltipPosition == 'ANCHOR_TOP' then
+			DT.tooltip:SetOwner(panel.text, self.db.tooltipPosition, 0, 50)
 		end
 
 		if IsModifierKeyDown() then
@@ -653,12 +651,10 @@ function module:ButtonOnEnter(button)
 	end
 	E:UIFrameFadeIn(button.hoverTex, self.db.fadeTime, button.hoverTex:GetAlpha(), 1)
 	if button.tooltips then
-		if self.db.tooltipPosition == "BOTTOM" then
-			DT.tooltip:SetOwner(button, "ANCHOR_BOTTOM", 0, -10)
-		elseif self.db.tooltipPosition == "TOP" then
-			DT.tooltip:SetOwner(button, "ANCHOR_TOP", 0, 10)
-		else
-			DT.tooltip:SetOwner(button, "ANCHOR_BOTTOM", 0, -10)
+		if self.db.tooltipPosition == 'ANCHOR_BOTTOM' then
+			DT.tooltip:SetOwner(button, self.db.tooltipPosition, 0, -10)
+		elseif self.db.tooltipPosition == 'ANCHOR_TOP' then
+			DT.tooltip:SetOwner(button, self.db.tooltipPosition, 0, 5)
 		end
 
 		if type(button.tooltips) == "table" then
