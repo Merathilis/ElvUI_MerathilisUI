ceb7a06348d23f3883993839824da031ac66d87b
fgprodigal <fgprodigal@gmail.com>
2020-11-26 00:25:41 +0800

    Add teleport and portal for Oribos

--- ElvUI_MerathilisUI/Modules/LocPanel/LocPanel.lua
+++ ElvUI_MerathilisUI/Modules/LocPanel/LocPanel.lua
@@ -178,6 +178,7 @@ module.Spells = {
 			[11] = {text = GetSpellInfo(176242),icon = MER:GetIconFromID("spell", 176242),secure = {buttonType = "spell",ID = 176242}, UseTooltip = true},-- TP:Warspear
 			[12] = {text = GetSpellInfo(224869),icon = MER:GetIconFromID("spell", 224869),secure = {buttonType = "spell",ID = 224869}, UseTooltip = true},-- TP:Dalaran - BI
 			[13] = {text = GetSpellInfo(281404),icon = MER:GetIconFromID("spell", 281404),secure = {buttonType = "spell",ID = 281404}, UseTooltip = true},-- TP:Dazar'alor
+			[14] = {text = GetSpellInfo(344587),icon = MER:GetIconFromID("spell", 344587),secure = {buttonType = "spell",ID = 344587}, UseTooltip = true},-- TP:Oribos
 		},
 		["Alliance"] = {
 			[1] = {text = GetSpellInfo(3561),icon = MER:GetIconFromID("spell", 3561),secure = {buttonType = "spell",ID = 3561}, UseTooltip = true},-- TP:Stormwind
@@ -193,6 +194,7 @@ module.Spells = {
 			[11] = {text = GetSpellInfo(176248),icon = MER:GetIconFromID("spell", 176248),secure = {buttonType = "spell",ID = 176248}, UseTooltip = true},-- TP:StormShield
 			[12] = {text = GetSpellInfo(224869),icon = MER:GetIconFromID("spell", 224869),secure = {buttonType = "spell",ID = 224869}, UseTooltip = true},-- TP:Dalaran - BI
 			[13] = {text = GetSpellInfo(281403),icon = MER:GetIconFromID("spell", 281403),secure = {buttonType = "spell",ID = 281403}, UseTooltip = true},-- TP:Boralus
+			[14] = {text = GetSpellInfo(344587),icon = MER:GetIconFromID("spell", 344587),secure = {buttonType = "spell",ID = 344587}, UseTooltip = true},-- TP:Oribos
 		},
 	},
 	["portals"] = {
@@ -210,6 +212,7 @@ module.Spells = {
 			[11] = {text = GetSpellInfo(176244),icon = MER:GetIconFromID("spell", 176244),secure = {buttonType = "spell",ID = 176244}, UseTooltip = true},-- P:Warspear
 			[12] = {text = GetSpellInfo(224871),icon = MER:GetIconFromID("spell", 224871),secure = {buttonType = "spell",ID = 224871}, UseTooltip = true},-- P:Dalaran - BI
 			[13] = {text = GetSpellInfo(281402),icon = MER:GetIconFromID("spell", 281402),secure = {buttonType = "spell",ID = 281402}, UseTooltip = true},-- P:Dazar'alor
+			[14] = {text = GetSpellInfo(344597),icon = MER:GetIconFromID("spell", 344597),secure = {buttonType = "spell",ID = 344597}, UseTooltip = true},-- P:Oribos
 		},
 		["Alliance"] = {
 			[1] = {text = GetSpellInfo(10059),icon = MER:GetIconFromID("spell", 10059),secure = {buttonType = "spell",ID = 10059}, UseTooltip = true},-- P:Stormwind
@@ -225,6 +228,7 @@ module.Spells = {
 			[11] = {text = GetSpellInfo(176246),icon = MER:GetIconFromID("spell", 176246),secure = {buttonType = "spell",ID = 176246}, UseTooltip = true},-- P:StormShield
 			[12] = {text = GetSpellInfo(224871),icon = MER:GetIconFromID("spell", 224871),secure = {buttonType = "spell",ID = 224871}, UseTooltip = true},-- P:Dalaran - BI
 			[13] = {text = GetSpellInfo(281400),icon = MER:GetIconFromID("spell", 281400),secure = {buttonType = "spell",ID = 281400}, UseTooltip = true},-- P:Boralus
+			[14] = {text = GetSpellInfo(344597),icon = MER:GetIconFromID("spell", 344597),secure = {buttonType = "spell",ID = 344597}, UseTooltip = true},-- P:Oribos
 		},
 	},
 	["challenge"] = {
