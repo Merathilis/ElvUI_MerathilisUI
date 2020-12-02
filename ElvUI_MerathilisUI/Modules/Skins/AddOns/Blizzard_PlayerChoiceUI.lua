db9d7f47d47a6e640b0ed3721886884f0df50159
Merathilis <neo0608@gmail.com>
2020-12-02 22:00:26 +0100

    Updated Player Choice skin

--- ElvUI_MerathilisUI/Modules/Skins/AddOns/Blizzard_PlayerChoiceUI.lua
+++ ElvUI_MerathilisUI/Modules/Skins/AddOns/Blizzard_PlayerChoiceUI.lua
@@ -1,4 +1,5 @@
 local MER, E, L, V, P, G = unpack(select(2, ...))
+local MERS = MER:GetModule('MER_Skins')
 local S = E:GetModule('Skins')
 
 local _G = _G
@@ -24,8 +25,30 @@ local function LoadSkin()
 			frame.backdrop:Styling()
 		end
 
+		if not frame.IsSkinned then
+			frame:StripTextures()
+			self.BlackBackground:SetAlpha(0)
+			self.Background:SetAlpha(0)
+			self.NineSlice:SetAlpha(0)
+			self.Title:DisableDrawLayer("BACKGROUND")
+			self.Title:CreateBackdrop('Transparent')
+			self.Title.Text:SetTextColor(1, .8, 0)
+			self.Title.Text:SetFontObject(SystemFont_Huge1)
+			self.Title.Text:SetShadowOffset(1, 1)
+			self.BorderFrame.Header:SetAlpha(0)
+		end
+
 		for i = 1, self:GetNumOptions() do
 			local option = self.Options[i]
+			option.Background:SetAlpha(0)
+			if not option.Background.backdrop then
+				option.Background:CreateBackdrop('Transparent')
+				option.Background.backdrop:Point('TOPLEFT', option.Background, 'TOPLEFT', 2, 2)
+				option.Background.backdrop:Point('BOTTOMRIGHT', option.Background, 'BOTTOMRIGHT', -2, -2)
+				MERS:CreateGradient(option.Background.backdrop)
+			end
+			option.Header.Ribbon:SetAlpha(0)
+
 			option.Header.Text:SetTextColor(1, .8, 0)
 			option.OptionText:SetTextColor(1, 1, 1)
 
@@ -52,6 +75,24 @@ local function LoadSkin()
 				end
 			end
 		end
+	end)
+
+	hooksecurefunc(frame, "SetupRewards", function(self)
+		for i = 1, self.numActiveOptions do
+			local optionFrameRewards = self.Options[i].RewardsFrame.Rewards
+			for button in optionFrameRewards.ItemRewardsPool:EnumerateActive() do
+				if not button.IsSkinned then
+					button.Name:SetTextColor(.9, .8, .5)
+					button.IconBorder:SetAlpha(0)
+
+					button.IsSkinned = true
+				end
+			end
+		end
+		--[[
+			optionFrameRewards.CurrencyRewardsPool
+			optionFrameRewards.ReputationRewardsPool
+		]]
 	end)
 end
 
