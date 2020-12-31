3c887af69b2bd8fe87723076e670ade84a620195
Merathilis <neo0608@gmail.com>
2020-12-31 14:10:37 +0100

    BNet, LootFrame skin update

--- ElvUI_MerathilisUI/Modules/Skins/FrameXML/LootFrame.lua
+++ ElvUI_MerathilisUI/Modules/Skins/FrameXML/LootFrame.lua
@@ -1,10 +1,8 @@
 local MER, E, L, V, P, G = unpack(select(2, ...))
 local S = E:GetModule('Skins')
 
---Cache global variables
 local _G = _G
---WoW API / Variables
--- GLOBALS:
+local hooksecurefunc = hooksecurefunc
 
 local function LoadSkin()
 	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.loot ~= true or E.private.muiSkins.blizzard.loot ~= true then return end
@@ -15,6 +13,18 @@ local function LoadSkin()
 	if E.private.general.loot then
 		_G.ElvLootFrame:Styling()
 	end
+
+	-- Boss Banner
+	hooksecurefunc("BossBanner_ConfigureLootFrame", function(lootFrame)
+		if not lootFrame.isSkinned then
+			local iconHitBox = lootFrame.IconHitBox
+			S:HandleIcon(lootFrame.Icon, true)
+			iconHitBox.IconBorder:SetTexture(nil)
+			S:HandleIconBorder(iconHitBox.IconBorder, lootFrame.Icon.backdrop)
+
+			lootFrame.isSkinned = true
+		end
+	end)
 end
 
 S:AddCallback("mUILoot", LoadSkin)
