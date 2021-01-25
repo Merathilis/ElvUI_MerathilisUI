d1b673c2714f683b6cd83d9224886c5685a6e652
Merathilis <neo0608@gmail.com>
2021-01-25 10:41:32 +0100

    Nope

--- ElvUI_MerathilisUI/Modules/Misc/Misc.lua
+++ ElvUI_MerathilisUI/Modules/Misc/Misc.lua
@@ -2,7 +2,6 @@ local MER, E, _, V, P, G = unpack(select(2, ...))
 local L = E.Libs.ACL:GetLocale('ElvUI', E.global.general.locale or 'enUS')
 local module = MER:GetModule('MER_Misc')
 local S = E:GetModule('Skins')
-local LCG = LibStub('LibCustomGlow-1.0')
 
 local _G = _G
 local select = select
@@ -109,44 +108,6 @@ function module:SetRole()
 	end
 end
 
-function module:WorldQuestStuff()
-	if not E.private.actionbar.enable then return end
-
-	local r, g, b = unpack(E.media.rgbvaluecolor)
-	local color = {r, g, b, 1}
-
-	local hasFound
-	local function ResetActionButtons()
-		if not hasFound then return end
-		for i = 1, 3 do
-			LCG.PixelGlow_Stop(_G["ActionButton"..i])
-		end
-		hasFound = nil
-	end
-
-	MER:RegisterEvent("CHAT_MSG_MONSTER_SAY", function(_, msg)
-		if not GetOverrideBarSkin() or not C_QuestLog.GetLogIndexForQuestID(59585) then
-			ResetActionButtons()
-			return
-		end
-
-		msg = gsub(msg, "[.%.]", "")
-
-		for i = 1, 3 do
-			local button = _G["ActionButton"..i]
-			local _, spellID = GetActionInfo(button.action)
-			local name = spellID and GetSpellInfo(spellID)
-			if name and name == msg then
-				LCG.PixelGlow_Start(button, color, nil, 0.5, nil, 1)
-			else
-				LCG.PixelGlow_Stop(button)
-			end
-		end
-
-		hasFound = true
-	end)
-end
-
 function module:Initialize()
 	local db = E.db.mui.misc
 	MER:RegisterDB(self, "misc")
@@ -165,7 +126,6 @@ function module:Initialize()
 	self:WowHeadLinks()
 	self:SplashScreen()
 	self:CreateMawWidgetFrame()
-	self:WorldQuestStuff() -- in testing
 end
 
 MER:RegisterModule(module:GetName())
