88a5b0defd2c3d3b9375cd428cbdac4838cca6ed
Merathilis <neo0608@gmail.com>
2021-03-15 21:30:02 +0100

    BNet Shadow

--- ElvUI_MerathilisUI/Modules/Skins/FrameXML/BNet.lua
+++ ElvUI_MerathilisUI/Modules/Skins/FrameXML/BNet.lua
@@ -9,6 +9,7 @@ local function LoadSkin()
 
 	local BNToastFrame = _G.BNToastFrame
 	BNToastFrame:Styling()
+	MER:CreateShadow(BNToastFrame)
 
 	-- /run BNToastFrame:AddToast(BN_TOAST_TYPE_ONLINE, 1)
 	hooksecurefunc(BNToastFrame, 'ShowToast', function(self)
