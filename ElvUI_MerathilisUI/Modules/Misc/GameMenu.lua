b30f8d4d5fd9ec2ced42cbab180212cb214fa5cc
Merathilis <neo0608@gmail.com>
2021-04-02 14:29:45 +0200

    Nothing

--- ElvUI_MerathilisUI/Modules/Misc/GameMenu.lua
+++ ElvUI_MerathilisUI/Modules/Misc/GameMenu.lua
@@ -1,7 +1,7 @@
 local MER, E, L, V, P, G = unpack(select(2, ...))
 local module = MER:GetModule('MER_GameMenu')
 local MERS = MER:GetModule('MER_Skins')
-local S = E:GetModule("Skins")
+local S = E:GetModule('Skins')
 
 -- Cache global variables
 -- Lua functions
