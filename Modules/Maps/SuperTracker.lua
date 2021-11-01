3e8f46690d977dafd534ee0ee0aa009ddf82880e
Merathilis <neo0608@gmail.com>
2021-11-01 16:08:37 +0100

    formating

--- Modules/Maps/SuperTracker.lua
+++ Modules/Maps/SuperTracker.lua
@@ -34,15 +34,11 @@ function module:HookPin()
 	if _G.WorldMapFrame:GetNumActivePinsByTemplate("WaypointLocationPinTemplate") ~= 0 then
 		for pin in _G.WorldMapFrame:EnumeratePinsByTemplate("WaypointLocationPinTemplate") do
 			if not self:IsHooked(pin, "OnMouseClickAction") then
-				self:SecureHook(
-					pin,
-					"OnMouseClickAction",
-					function(_, button)
-						if button == "RightButton" then
-							C_Map_ClearUserWaypoint()
-						end
+				self:SecureHook(pin, "OnMouseClickAction", function(_, button)
+					if button == "RightButton" then
+						C_Map_ClearUserWaypoint()
 					end
-				)
+				end)
 			end
 		end
 	end
