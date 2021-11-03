0492d63fbb303e8e5c00e1333ee7a50cf4cbbbbd
Merathilis <neo0608@gmail.com>
2021-11-03 09:06:44 +0100

    ... -.-

--- Modules/Skins/AddOns/Blizzard_ItemUpgradeUI.lua
+++ Modules/Skins/AddOns/Blizzard_ItemUpgradeUI.lua
@@ -3,15 +3,6 @@ local S = E:GetModule('Skins')
 
 local _G = _G
 
-local function ReskinCurrencyIcon(self)
-	for frame in self.iconPool:EnumerateActive() do
-		if not frame.backdrop then
-			S:HandleIcon(frame.Icon, true)
-			frame.backdrop:SetFrameLevel(2)
-		end
-	end
-end
-
 local function LoadSkin()
 	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.itemUpgrade ~= true or E.private.muiSkins.blizzard.itemUpgrade ~= true then return end
 
