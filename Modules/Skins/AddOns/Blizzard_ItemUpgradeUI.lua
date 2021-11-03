e04ab86447a64e80f6c4340ed0802bd3f0cf7eaa
Merathilis <neo0608@gmail.com>
2021-11-03 09:03:34 +0100

    Fix some unexpected errors :P

--- Modules/Skins/AddOns/Blizzard_ItemUpgradeUI.lua
+++ Modules/Skins/AddOns/Blizzard_ItemUpgradeUI.lua
@@ -1,16 +1,25 @@
 local MER, E, L, V, P, G = unpack(select(2, ...))
 local S = E:GetModule('Skins')
 
--- Cache global variables
--- Lua functions
 local _G = _G
--- WoW API
--- GLOBALS:
+
+local function ReskinCurrencyIcon(self)
+	for frame in self.iconPool:EnumerateActive() do
+		if not frame.backdrop then
+			S:HandleIcon(frame.Icon, true)
+			frame.backdrop:SetFrameLevel(2)
+		end
+	end
+end
 
 local function LoadSkin()
 	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.itemUpgrade ~= true or E.private.muiSkins.blizzard.itemUpgrade ~= true then return end
 
 	local ItemUpgradeFrame = _G.ItemUpgradeFrame
+
+	ItemUpgradeFrame.TopBG:Hide()
+
+
 	ItemUpgradeFrame:Styling()
 	MER:CreateBackdropShadow(ItemUpgradeFrame)
 end
