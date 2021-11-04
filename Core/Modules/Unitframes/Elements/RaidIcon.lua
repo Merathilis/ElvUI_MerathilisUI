dc98ab925726a91dc67d09fb8ac0a8f3a4d8e5cf
Merathilis <neo0608@gmail.com>
2021-11-04 19:33:10 +0100

    !WARNING MASSIV COMMIT! DO NOT USE IT YET

---
+++ Core/Modules/Unitframes/Elements/RaidIcon.lua
@@ -0,0 +1,23 @@
+local MER, E, L, V, P, G = unpack(select(2, ...))
+local module = MER:GetModule('MER_UnitFrames')
+
+--Cache global variables
+--WoW API / Variables
+-- GLOBALS:
+
+function module:Configure_RaidIcon(frame)
+	if E.db.mui.unitframes.raidIcons ~= true then return end
+
+	local RI = frame.RaidTargetIndicator
+	local db = frame.db
+
+	if RI.Replace then return end
+	RI.SetTexture = SetTexture
+
+	if db.raidicon.enable then
+		RI:SetTexture([[Interface\AddOns\ElvUI_MerathilisUI\Core\Media\Textures\RaidIcons\UI-RaidTargetingIcons]])
+	end
+
+	RI.Replace = true
+	RI.SetTexture = E.noop
+end
