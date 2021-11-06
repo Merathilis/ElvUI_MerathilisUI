b5a0fff35ba488b4f2e49c55d403108b9f7576b4
Merathilis <neo0608@gmail.com>
2021-11-06 09:14:26 +0100

    Updated an url

--- Core/General/Core.lua
+++ Core/General/Core.lua
@@ -143,6 +143,6 @@ function MER:Initialize()
 
 	local icon = MER:GetIconString(MER.Media.Textures.pepeSmall, 14)
 	if E.db.mui.installed and E.db.mui.general.LoginMsg then
-		print(icon..''..MER.Title..format("v|cff00c0fa%s|r", MER.Version)..L[" is loaded. For any issues or suggestions, please visit "]..MER:PrintURL("https://git.tukui.org/Merathilis/ElvUI_MerathilisUI/issues"))
+		print(icon..''..MER.Title..format("v|cff00c0fa%s|r", MER.Version)..L[" is loaded. For any issues or suggestions, please visit "]..MER:PrintURL("https://github.com/Merathilis/ElvUI_MerathilisUI/issues"))
 	end
 end
