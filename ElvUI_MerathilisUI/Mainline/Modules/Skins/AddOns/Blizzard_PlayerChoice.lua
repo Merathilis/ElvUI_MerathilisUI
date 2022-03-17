0179322cfa1b3083d182aa2545653a57aa79c963
Merathilis <neo0608@gmail.com>
2022-03-17 15:47:41 +0100

    Why isnt it styled? :thinking:

--- ElvUI_MerathilisUI/Mainline/Modules/Skins/AddOns/Blizzard_PlayerChoice.lua
+++ ElvUI_MerathilisUI/Mainline/Modules/Skins/AddOns/Blizzard_PlayerChoice.lua
@@ -13,7 +13,7 @@ local function LoadSkin()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.playerChoice) or E.private.muiSkins.blizzard.playerChoice ~= true then return end
 
 	local frame = _G.PlayerChoiceFrame
-
+	frame:Styling()
 end
 
 S:AddCallbackForAddon("Blizzard_PlayerChoice", "mUIPlayerChoice", LoadSkin)
