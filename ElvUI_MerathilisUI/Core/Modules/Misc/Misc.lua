428bea6e82240f51ec8f4926601cbbf47537f055
Merathilis <neo0608@gmail.com>
2022-05-29 11:32:27 +0200

    Prevent a taint.. fuck this was annoying to find

--- ElvUI_MerathilisUI/Core/Modules/Misc/Misc.lua
+++ ElvUI_MerathilisUI/Core/Modules/Misc/Misc.lua
@@ -2,24 +2,17 @@ local MER, F, E, _, V, P, G = unpack(select(2, ...))
 local module = MER:GetModule('MER_Misc')
 
 local _G = _G
-local pairs, select = pairs, select
+local pairs = pairs
 local twipe = table.wipe
 local tinsert = table.insert
 local strfind = string.find
 local gsub = gsub
 
-local CreateFrame = CreateFrame
 local hooksecurefunc = hooksecurefunc
 local C_FriendList_GetNumWhoResults = C_FriendList.GetNumWhoResults
 local C_FriendList_GetWhoInfo = C_FriendList.GetWhoInfo
-local GetBattlefieldStatus = GetBattlefieldStatus
 local GetGuildInfo = GetGuildInfo
 local GetQuestDifficultyColor = GetQuestDifficultyColor
-local GetLFGDungeonInfo = GetLFGDungeonInfo
-local GetLFGDungeonRewards = GetLFGDungeonRewards
-local GetLFGRandomDungeonInfo = GetLFGRandomDungeonInfo
-local GetMaxBattlefieldID = GetMaxBattlefieldID
-local GetNumRandomDungeons = GetNumRandomDungeons
 local GetNumGroupMembers = GetNumGroupMembers
 local GetRealZoneText = GetRealZoneText
 local GetSpecialization = GetSpecialization
@@ -28,77 +21,10 @@ local UnitRace = UnitRace
 local UnitGroupRolesAssigned = UnitGroupRolesAssigned
 local UnitSetRole = UnitSetRole
 local InCombatLockdown = InCombatLockdown
-local PlaySound = PlaySound
 local StaticPopupSpecial_Hide = StaticPopupSpecial_Hide
 local HybridScrollFrame_GetOffset = HybridScrollFrame_GetOffset
-local LFDQueueFrame_SetType = LFDQueueFrame_SetType
 local UIDropDownMenu_GetSelectedID = UIDropDownMenu_GetSelectedID
-local SOUNDKIT = SOUNDKIT
-local RaidNotice_AddMessage = RaidNotice_AddMessage
 local RAID_CLASS_COLORS = RAID_CLASS_COLORS
-
-function module:LoadMisc()
-	-- Force readycheck warning
-	local ShowReadyCheckHook = function(_, initiator)
-		if initiator ~= "player" then
-			PlaySound(SOUNDKIT.READY_CHECK or 8960)
-		end
-	end
-	hooksecurefunc("ShowReadyCheck", ShowReadyCheckHook)
-
-	-- Force other warning
-	local ForceWarning = CreateFrame("Frame")
-	ForceWarning:RegisterEvent("UPDATE_BATTLEFIELD_STATUS")
-	ForceWarning:RegisterEvent("PET_BATTLE_QUEUE_PROPOSE_MATCH")
-	ForceWarning:RegisterEvent("LFG_PROPOSAL_SHOW")
-	ForceWarning:SetScript("OnEvent", function(_, event)
-		if event == "UPDATE_BATTLEFIELD_STATUS" then
-			for i = 1, GetMaxBattlefieldID() do
-				local status = GetBattlefieldStatus(i)
-				if status == "confirm" then
-					PlaySound(SOUNDKIT.UI_PET_BATTLES_PVP_THROUGH_QUEUE or 36609)
-					break
-				end
-				i = i + 1
-			end
-		elseif event == "PET_BATTLE_QUEUE_PROPOSE_MATCH" then
-			PlaySound(SOUNDKIT.UI_PET_BATTLES_PVP_THROUGH_QUEUE or 36609)
-		elseif event == "LFG_PROPOSAL_SHOW" then
-			PlaySound(SOUNDKIT.READY_CHECK or 8960)
-		end
-	end)
-
-	-- Misclicks for some popups
-	_G.StaticPopupDialogs.RESURRECT.hideOnEscape = nil
-	_G.StaticPopupDialogs.AREA_SPIRIT_HEAL.hideOnEscape = nil
-	_G.StaticPopupDialogs.PARTY_INVITE.hideOnEscape = nil
-	_G.StaticPopupDialogs.CONFIRM_SUMMON.hideOnEscape = nil
-	_G.StaticPopupDialogs.ADDON_ACTION_FORBIDDEN.button1 = nil
-	_G.StaticPopupDialogs.TOO_MANY_LUA_ERRORS.button1 = nil
-	_G["PetBattleQueueReadyFrame"].hideOnEscape = nil
-
-	if (_G.PVPReadyDialog) then
-		_G.PVPReadyDialog.leaveButton:Hide()
-		_G.PVPReadyDialog.enterButton:ClearAllPoints()
-		_G.PVPReadyDialog.enterButton:SetPoint("BOTTOM", _G.PVPReadyDialog, "BOTTOM", 0, 25)
-		_G.PVPReadyDialog.label:SetPoint("TOP", 0, -22)
-	end
-
-	-- Auto select current event boss from LFD tool(EventBossAutoSelect by Nathanyel)
-	local firstLFD
-	_G["LFDParentFrame"]:HookScript("OnShow", function()
-		if not firstLFD then
-			firstLFD = 1
-			for i = 1, GetNumRandomDungeons() do
-				local id = GetLFGRandomDungeonInfo(i)
-				local isHoliday = select(15, GetLFGDungeonInfo(id))
-				if isHoliday and not GetLFGDungeonRewards(id) then
-					LFDQueueFrame_SetType(id)
-				end
-			end
-		end
-	end)
-end
 
 function module:SetRole()
 	if not E.Retail then return end
@@ -131,11 +57,6 @@ end
 
 local function diffColor(level)
 	return F.RGBToHex(GetQuestDifficultyColor(level))
-end
-
-local blizzHexColors = {}
-for class, color in pairs(RAID_CLASS_COLORS) do
-	blizzHexColors[color.colorStr] = class
 end
 
 -- Whoframe
@@ -177,20 +98,6 @@ local function UpdateWhoList()
 	end
 end
 
--- FrameXML/RaidWarning.lua
-do
-	local AddMessage = RaidNotice_AddMessage
-	RaidNotice_AddMessage = function(frame, message, ...)
-		if strfind(message, '|cff') then
-			for hex, class in pairs(blizzHexColors) do
-				local color = F.ClassColors[class]
-				message = gsub(message, hex, color.colorStr)
-			end
-		end
-		return AddMessage(frame, message, ...)
-	end
-end
-
 function module:Initialize()
 	local db = E.db.mui.misc
 
@@ -198,8 +105,6 @@ function module:Initialize()
 		E.RegisterCallback(module, "RoleChanged", "SetRole")
 		module:RegisterEvent("GROUP_ROSTER_UPDATE", "SetRole")
 
-		_G.RolePollPopup:SetScript("OnShow", function() StaticPopupSpecial_Hide(_G.RolePollPopup) end)
-		module:LoadMisc()
 		module:ItemLevel()
 		module:CreateMawWidgetFrame()
 		module:WowHeadLinks()
