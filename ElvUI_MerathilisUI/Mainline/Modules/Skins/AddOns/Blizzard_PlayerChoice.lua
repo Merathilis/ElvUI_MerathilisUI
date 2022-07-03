8bc034b2974aa588eb865dc28ced7d5f7d7b708b
Merathilis <neo0608@gmail.com>
2022-07-03 20:19:45 +0200

    Needs testing and probably a bit changes

--- ElvUI_MerathilisUI/Mainline/Modules/Skins/AddOns/Blizzard_PlayerChoice.lua
+++ ElvUI_MerathilisUI/Mainline/Modules/Skins/AddOns/Blizzard_PlayerChoice.lua
@@ -4,16 +4,30 @@ local S = E:GetModule('Skins')
 
 local _G = _G
 
+local useTextureKit = {
+	jailerstower = true,
+	cypherchoice = true
+}
+
+local function ChangeOptions(frame)
+	if frame.MERStyle then return end
+
+	local kit = useTextureKit[frame.uiTextureKit]
+
+	if not kit then
+		frame:Styling()
+	end
+
+	frame.MERStyle = true
+end
+
 local function LoadSkin()
 	if not module:CheckDB("playerChoice", "playerChoice") then
 		return
 	end
 
 	local frame = _G.PlayerChoiceFrame
-
-	if frame.backdrop then
-		frame.backdrop:Styling()
-	end
+	hooksecurefunc(frame, 'SetupOptions', ChangeOptions)
 end
 
 S:AddCallbackForAddon("Blizzard_PlayerChoice", LoadSkin)
