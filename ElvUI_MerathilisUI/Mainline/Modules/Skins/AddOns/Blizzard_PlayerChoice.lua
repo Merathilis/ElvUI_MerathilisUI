5bc67e7528d09448f5b6539d1ebe297e036f0404
Merathilis <neo0608@gmail.com>
2022-07-04 10:15:22 +0200

    Dont style the PlayerChoice frame for now

--- ElvUI_MerathilisUI/Mainline/Modules/Skins/AddOns/Blizzard_PlayerChoice.lua
+++ ElvUI_MerathilisUI/Mainline/Modules/Skins/AddOns/Blizzard_PlayerChoice.lua
@@ -4,30 +4,13 @@ local S = E:GetModule('Skins')
 
 local _G = _G
 
-local useTextureKit = {
-	jailerstower = true,
-	cypherchoice = true
-}
-
-local function ChangeOptions(frame)
-	if frame.MERStyle then return end
-
-	local kit = useTextureKit[frame.uiTextureKit]
-
-	if not kit then
-		frame:Styling()
-	end
-
-	frame.MERStyle = true
-end
-
 local function LoadSkin()
 	if not module:CheckDB("playerChoice", "playerChoice") then
 		return
 	end
 
 	local frame = _G.PlayerChoiceFrame
-	hooksecurefunc(frame, 'SetupOptions', ChangeOptions)
+	-- frame:Styling()
 end
 
-S:AddCallbackForAddon("Blizzard_PlayerChoice", LoadSkin)
+module:AddCallbackForAddon("Blizzard_PlayerChoice", LoadSkin)
