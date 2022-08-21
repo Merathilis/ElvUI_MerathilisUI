07e3dd809088347ffb447a474c202dda1495576f
Merathilis <neo0608@gmail.com>
2022-08-21 10:12:32 +0200

    v.5.19

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,3 @@
 ### Changes ###
 
-  * Fixed some troubles with my Styling
-  * Added compatibility checks with ElvUI_EltreumUI
-  * Skin Immersion (Enhance the AddOnSkin for it)
-  * Added an Option to disable the Armory Stats
-  * Skin Immersion (Enhance the AddOnSkin for it)
+  * Finally fixed now really all issues with BenikUI styling and mine
--- ElvUI_MerathilisUI/Core/General/Changelog.lua
+++ ElvUI_MerathilisUI/Core/General/Changelog.lua
@@ -12,11 +12,7 @@ local DISABLED_FONT_COLOR = DISABLED_FONT_COLOR
 
 local ChangeLogData = {
 	"Changes:",
-		"• Fixed some troubles with my Styling",
-		"• Added compatibility checks with ElvUI_EltreumUI",
-		"• Skin Immersion (Enhance the AddOnSkin for it)",
-		"• Added an Option to disable the Armory Stats",
-		"• Skin Immersion (Enhance the AddOnSkin for it)",
+		"• Finally fixed now really all issues with BenikUI styling and mine",
 
 	" ",
 	"Notes:",
