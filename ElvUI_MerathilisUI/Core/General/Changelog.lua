a4816add99d69e57c2d8fadffaa02d0cb8074d2f
Merathilis <neo0608@gmail.com>
2022-08-29 16:35:30 +0200

    v.5.21

--- ElvUI_MerathilisUI/Core/General/Changelog.lua
+++ ElvUI_MerathilisUI/Core/General/Changelog.lua
@@ -12,7 +12,12 @@ local DISABLED_FONT_COLOR = DISABLED_FONT_COLOR
 
 local ChangeLogData = {
 	"Changes:",
-		"• Finally fixed now really all issues with BenikUI styling and mine",
+		"• Added the ElvUI Gold-Datatext to the MicroBar Bag Icon. Thx Heather for the idea",
+		"• Replace default Blizzard CD Text with the ElvUI ones on WeakAuras. Credits DaveA50",
+		"• Hide some options that are not available on non Retail, forgot these",
+		"• Let the extended Merchant frame also work on non Retail Clients",
+		"• Added options to the Chat & UnitFrames to change the RoleIcon Style",
+		"• Added more options to the the ActionBars KeyFeedback",
 
 	" ",
 	"Notes:",
