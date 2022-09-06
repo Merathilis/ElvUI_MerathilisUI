44ca39dd0d386dfc324a4c6e9f4b1804fde3068d
Merathilis <neo0608@gmail.com>
2022-09-06 16:43:35 +0200

    NewSign

--- ElvUI_MerathilisUI/Core/Options/Modules/UnitFrames.lua
+++ ElvUI_MerathilisUI/Core/Options/Modules/UnitFrames.lua
@@ -54,7 +54,7 @@ options.unitframes = {
 		power = {
 			order = 3,
 			type = "group",
-			name = F.cOption(L["Power"], 'orange'),
+			name = E.NewSign..F.cOption(L["Power"], 'orange'),
 			guiInline = true,
 			get = function(info) return E.db.mui.unitframes.power[ info[#info] ] end,
 			set = function(info, value) E.db.mui.unitframes.power[info[#info]] = value; E:StaticPopup_Show("CONFIG_RL"); end,
