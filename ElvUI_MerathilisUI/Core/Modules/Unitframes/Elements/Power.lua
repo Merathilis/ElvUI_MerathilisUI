ab4045054588ac899dd01fa662ecf65031c89736
Merathilis <neo0608@gmail.com>
2022-09-06 16:20:35 +0200

    Smoll changes for Power Bar

--- ElvUI_MerathilisUI/Core/Modules/Unitframes/Elements/Power.lua
+++ ElvUI_MerathilisUI/Core/Modules/Unitframes/Elements/Power.lua
@@ -12,34 +12,34 @@ function module:Configure_Power(frame)
 	if power and not power.__MERSkin then
 		power:Styling(false, false, true)
 
-		if not power.animation and (db and db.enable) then
-			power.animation = CreateFrame("PlayerModel", "MER_PowerBarEffect")
+		power.__MERSkin = true
+	end
 
-			if db.type == "DEFAULT" then
-				if E.Retail then
-					power.animation:SetModel(1715069) -- spells/7fx_shivarracoven_frost_precasthands.m2
-					power.animation:MakeCurrentCameraCustom()
-					power.animation:SetTransform(-0.035, 0, 0, rad(270), 0, 0, 0.580)
-					power.animation:SetPortraitZoom(1)
-				else
-					power.animation:SetModel("spells/arcanepower_state_chest.m2")
-					power.animation:SetPosition(1.2, 0, 0)
-				end
-			elseif db.type == "CUSTOM" then
-				if E.Retail then
-					power.animation:SetModel(db.retailModel)
-				else
-					power.animation:SetModel(db.classicModel)
-				end
+	if not power.animation and (db and db.enable) then
+		power.animation = CreateFrame("PlayerModel", "MER_PowerBarEffect", power)
+
+		if db.type == "DEFAULT" then
+			if E.Retail then
+				power.animation:SetModel(1630153) -- spells/cfx_priest_holyprecast_precastarm.m2
+				power.animation:SetPosition(4, 0.32, 1.85, 0)
+				power.animation:ClearTransform()
+				power.animation:SetTransform(-0.035, 0, 0, rad(270), 0, 0, 0.580)
+				power.animation:SetAlpha(0.65)
+			else
+				power.animation:SetModel("spells/arcanepower_state_chest.m2")
+				power.animation:SetPosition(1.2, 0, 0)
 			end
-
-			power.animation:SetAllPoints(power:GetStatusBarTexture())
-			power.animation:SetFrameLevel(power:GetFrameLevel())
-			power.animation:SetInside(power:GetStatusBarTexture(), 0, 0)
-			power.animation:SetParent(power)
+		elseif db.type == "CUSTOM" then
+			if E.Retail then
+				power.animation:SetModel(db.retailModel)
+			else
+				power.animation:SetModel(db.classicModel)
+			end
 		end
 
-		power.__MERSkin = true
+		power.animation:SetAllPoints(power:GetStatusBarTexture())
+		power.animation:SetFrameLevel(power:GetFrameLevel())
+		power.animation:SetInside(power:GetStatusBarTexture(), 0, 0)
 	end
 end
 
