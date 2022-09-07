5fd9f5add6d35405b600f55cb0846c8638827823
Merathilis <neo0608@gmail.com>
2022-09-07 14:18:16 +0200

    Adjustments to Aninated PowerBar UF code cleanup

--- ElvUI_MerathilisUI/Core/Modules/Unitframes/Unitframes.lua
+++ ElvUI_MerathilisUI/Core/Modules/Unitframes/Unitframes.lua
@@ -34,20 +34,22 @@ function module:CreateAnimatedBars(frame)
 	if not frame then return end
 	local db = E.db.mui.unitframes.power
 
-	if not frame.animation then
-		frame.animation = CreateFrame("PlayerModel", "MER_PowerBarEffect", frame.Power)
+	if db and db.enable then
+		if not frame.animation then
+			local animation = CreateFrame("PlayerModel", "MER_PowerBarEffect", frame.Power)
+			frame.animation = animation
 
-		if db and db.enable then
 			if db.type == "DEFAULT" then
 				if E.Retail then
-					frame.animation:SetModel(1630153) -- spells/cfx_priest_holyprecast_precastarm.m2
-					frame.animation:SetPosition(4, 0.32, 1.85, 0)
-					frame.animation:ClearTransform()
+					frame.animation:SetModel(1715069)
+					frame.animation:MakeCurrentCameraCustom()
 					frame.animation:SetTransform(-0.035, 0, 0, rad(270), 0, 0, 0.580)
+					frame.animation:SetPortraitZoom(1)
 					frame.animation:SetAlpha(0.65)
 				else
 					frame.animation:SetModel("spells/arcanepower_state_chest.m2")
 					frame.animation:SetPosition(1.1, 0, 0)
+					frame.animation:SetAlpha(0.65)
 				end
 			elseif db.type == "CUSTOM" then
 				if E.Retail then
@@ -62,8 +64,31 @@ function module:CreateAnimatedBars(frame)
 			frame.animation:SetInside(frame:GetStatusBarTexture(), 0, 0)
 
 			frame.animation:Show()
+			else
+				frame.animation:Hide()
+		end
+
+		if not frame.modelFrame then
+			frame.modelFrame = CreateFrame("FRAME", nil, frame.Power)
+
+			local sparkle = CreateFrame("PlayerModel", nil, frame.modelFrame)
+			frame.modelFrame.sparkle = sparkle
+			frame.modelFrame.sparkle:SetKeepModelOnHide(true)
+
+			frame.modelFrame.sparkle:SetModel(1630153)
+			frame.modelFrame.sparkle:ClearTransform()
+			frame.modelFrame.sparkle:SetPosition(4, 0.32, 1.85, 0)
+
+			local h = frame:GetHeight()
+			frame.modelFrame.sparkle:SetPoint("RIGHT", frame.modelFrame)
+			frame.modelFrame.sparkle:SetSize(h*2, h)
+			frame.modelFrame.sparkle:SetAlpha(0.20)
+
+			frame.modelFrame:SetAllPoints(frame:GetStatusBarTexture())
+			frame.modelFrame:SetClipsChildren(true)
+			frame.modelFrame:Show()
 		else
-			frame.animation:Hide()
+			frame.modelFrame:Hide()
 		end
 	end
 end
@@ -75,56 +100,46 @@ function module:CreateUFShadows()
 	self:SecureHook(UF, "Configure_ClassBar", "UnitFrames_Configure_ClassBar")
 end
 
+function module:LoadUnits()
+	local db = E.db.mui.unitframes
+	-- Player
+	hooksecurefunc(UF, "Update_PlayerFrame", module.Update_PlayerFrame)
+	-- Target
+	hooksecurefunc(UF, "Update_TargetTargetFrame", module.Update_TargetTargetFrame)
+	-- Pet
+	hooksecurefunc(UF, "Update_PetFrame", module.Update_PetFrame)
+	-- Focus
+	hooksecurefunc(UF, "Update_FocusFrame", module.Update_FocusFrame)
+	-- FocusTarget
+	hooksecurefunc(UF, "Update_FocusTargetFrame", module.Update_FocusTargetFrame)
+	-- Party
+	hooksecurefunc(UF, "Update_PartyFrames", module.Update_PartyFrames)
+	-- Raid
+	hooksecurefunc(UF, "Update_RaidFrames", module.Update_RaidFrames)
+	-- Boss
+	hooksecurefunc(UF, "Update_BossFrames", module.Update_BossFrames)
+	-- Castbar
+	hooksecurefunc(UF, "Configure_Castbar", module.Configure_Castbar)
+	-- Power
+    hooksecurefunc(UF, "Configure_Power", module.Configure_Power)
+	-- RaidIcons
+	hooksecurefunc(UF, "Configure_RaidIcon", module.Configure_RaidIcon)
+    -- Auras
+	if db.auras then
+		module:SecureHook(UF, "PostUpdateAura", "ElvUI_PostUpdateDebuffs")
+	end
+	-- RoleIcons
+	module:Configure_RoleIcons()
+	-- Shadows
+	module:CreateUFShadows()
+end
+
 function module:Initialize()
 	if not E.private.unitframe.enable then
 		return
 	end
 
-	local db = E.db.mui.unitframes
-
-	-- Player
-	hooksecurefunc(UF, "Update_PlayerFrame", module.Update_PlayerFrame)
-
-	-- Target
-	hooksecurefunc(UF, "Update_TargetTargetFrame", module.Update_TargetTargetFrame)
-
-	-- Pet
-	hooksecurefunc(UF, "Update_PetFrame", module.Update_PetFrame)
-
-	-- Focus
-	hooksecurefunc(UF, "Update_FocusFrame", module.Update_FocusFrame)
-
-	-- FocusTarget
-	hooksecurefunc(UF, "Update_FocusTargetFrame", module.Update_FocusTargetFrame)
-
-	-- Party
-	hooksecurefunc(UF, "Update_PartyFrames", module.Update_PartyFrames)
-
-	-- Raid
-	hooksecurefunc(UF, "Update_RaidFrames", module.Update_RaidFrames)
-
-	-- Boss
-	hooksecurefunc(UF, "Update_BossFrames", module.Update_BossFrames)
-
-	-- Castbar
-	hooksecurefunc(UF, "Configure_Castbar", module.Configure_Castbar)
-
-	-- Power
-	hooksecurefunc(UF, "Configure_Power", module.Configure_Power)
-
-	-- Auras
-	if E.db.mui.unitframes.auras then
-		self:SecureHook(UF, "PostUpdateAura", "ElvUI_PostUpdateDebuffs")
-	end
-
-	-- RoleIcons
-	module:Configure_RoleIcons()
-
-	-- RaidIcons
-	hooksecurefunc(UF, "Configure_RaidIcon", module.Configure_RaidIcon)
-
-	-- Shadows
-	self:CreateUFShadows()
+	hooksecurefunc(UF, "LoadUnits", module.LoadUnits)
 
 	self:RegisterEvent("ADDON_LOADED")
 end
