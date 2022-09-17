4a90073b3ea384e4dea69f67b1ee2a73599a9ed7
Merathilis <neo0608@gmail.com>
2022-09-17 19:14:28 +0200

    Thats the real deal for the sliders

--- ElvUI_MerathilisUI/Core/Modules/Skins/Widgets/Slider.lua
+++ ElvUI_MerathilisUI/Core/Modules/Skins/Widgets/Slider.lua
@@ -18,10 +18,12 @@ function WS:HandleSliderFrame(_, slider)
 		return
 	end
 
-	if not slider.MERSkinned then
+	if not slider.MERSkinned and not slider.StripTextures_ and not slider.SetThumbTexture_ then
 		slider:SetThumbTexture(LSM:Fetch("statusbar", db.texture) or E.media.normTex)
-		-- slider.SetThumbTexture = E.noop
-
+		slider.StripTextures_ = slider.StripTextures
+		slider.StripTextures = E.noop
+		slider.SetThumbTexture_ = slider.SetThumbTexture
+		slider.SetThumbTexture = E.noop
 		slider.MERSkinned = true
 	end
 
