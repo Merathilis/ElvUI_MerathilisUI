b67332fb1c0f4b86b72dc1d5bf31545378fa870f
Merathilis <neo0608@gmail.com>
2022-09-20 18:04:38 +0200

    Use another way to style the DataText Panels

---
+++ ElvUI_MerathilisUI/Core/Modules/Skins/ElvUI/DataTexts.lua
@@ -0,0 +1,63 @@
+local MER, F, E, L, V, P, G = unpack(select(2, ...))
+local S = MER:GetModule('MER_Skins')
+local DT = E:GetModule("DataTexts")
+
+local _G = _G
+local hooksecurefunc = hooksecurefunc
+local pairs = pairs
+
+local function hookPanelSetTemplate(panel, template)
+	if not panel.shadow then
+		return
+	end
+
+	if template == "NoBackdrop" then
+		panel.shadow:Hide()
+		panel.MERstyle.stripes:Hide()
+		panel.MERstyle.gradient:Hide()
+		panel.MERstyle.mshadow:Hide()
+
+	else
+		panel.shadow:Show()
+		panel.MERstyle.stripes:Show()
+		panel.MERstyle.gradient:Show()
+		panel.MERstyle.mshadow:Show()
+	end
+end
+
+local function createPanelShadow(panel)
+	if panel.shadow and panel.shadow.__MERSkin then
+		return
+	end
+	S:CreateShadow(panel)
+	panel:Styling()
+	hooksecurefunc(panel, "SetTemplate", hookPanelSetTemplate)
+	hookPanelSetTemplate(panel, panel.template)
+end
+
+function S:ElvUI_SkinDataPanel(_, name)
+	local panel = DT:FetchFrame(name)
+	createPanelShadow(panel)
+end
+
+function S:ElvUI_DataPanels()
+	if not (E.private.mui.skins.shadow.enable) then
+		return
+	end
+
+	if DT.PanelPool.InUse then
+		for name, frame in pairs(DT.PanelPool.InUse) do
+			createPanelShadow(frame)
+		end
+	end
+
+	if DT.PanelPool.Free then
+		for name, frame in pairs(DT.PanelPool.Free) do
+			createPanelShadow(frame)
+		end
+	end
+
+	self:SecureHook(DT, "BuildPanelFrame", "ElvUI_SkinDataPanel")
+end
+
+S:AddCallback("ElvUI_DataPanels")
--- ElvUI_MerathilisUI/Core/Modules/Skins/Extern/Details.lua
+++ ElvUI_MerathilisUI/Core/Modules/Skins/Extern/Details.lua
@@ -1,23 +1,27 @@
 local MER, F, E, L, V, P, G = unpack(select(2, ...))
 local module = MER:GetModule('MER_Skins')
 
-local function LoadSkin()
-	if not E.private.mui.skins.addonSkins.enable or not E.private.mui.skins.addonSkins.dt then
-		return
-	end
-
-	hooksecurefunc(_G.Details, "InstanceRefreshRows", function(instancia)
-		if instancia.barras then
-			for _, row in ipairs(instancia.barras) do
+local function GradientBars()
+	hooksecurefunc(_detalhes, "InstanceRefreshRows", function(instancia)
+		if instancia.barras and instancia.barras[1] then
+			for _, row in next, instancia.barras do
 				if row and row.textura then
 					hooksecurefunc(row.textura, "SetVertexColor", function(_, r, g, b)
 						row.textura:SetTexture("Interface\\Addons\\ElvUI_MerathilisUI\\Core\\Media\\StatusBars\\Line4pixel")
-						row.textura:SetGradientAlpha("Horizontal", r-0.5, g-0.5, b-0.5, 0.9, r+0.2, g+0.2, b+0.2, 0.9)
+						row.textura:SetGradientAlpha("Horizontal", r - 0.5, g - 0.5, b - 0.5, 0.9, r + 0.2, g + 0.2, b + 0.2, 0.9)
 					end)
 				end
 			end
 		end
 	end)
+end
+
+local function LoadSkin()
+	if not E.private.mui.skins.addonSkins.enable or not E.private.mui.skins.addonSkins.dt then
+		return
+	end
+
+	GradientBars()
 end
 
 module:AddCallbackForAddon('Details', LoadSkin)
