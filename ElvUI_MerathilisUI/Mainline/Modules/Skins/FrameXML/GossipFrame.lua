6ec25eaf8b85a71e96d32f1cf9f6b90077ef89c3
Merathilis <neo0608@gmail.com>
2022-11-01 10:34:01 +0100

    Fix black Text on Gossip Frame if the parchment remover from ElvUI is disabled

--- ElvUI_MerathilisUI/Mainline/Modules/Skins/FrameXML/GossipFrame.lua
+++ ElvUI_MerathilisUI/Mainline/Modules/Skins/FrameXML/GossipFrame.lua
@@ -7,32 +7,30 @@ local select = select
 local strmatch = strmatch
 local gsub = string.gsub
 
-local hooksecurefunc = hooksecurefunc
-
 local function ReplaceGossipFormat(button, textFormat, text)
-	local newFormat, count = gsub(textFormat, "000000", "ffffff")
+	local newFormat, count = gsub(textFormat, '000000', 'ffffff')
 	if count > 0 then
 		button:SetFormattedText(newFormat, text)
 	end
 end
 
 local ReplacedGossipColor = {
-	["000000"] = "ffffff",
-	["414141"] = "7b8489", -- lighter color for some gossip options
+	['000000'] = 'ffffff',
+	['414141'] = '7b8489',
 }
 
 local function ReplaceGossipText(button, text)
-	if text and text ~= "" then
-		local newText, count = gsub(text, ":32:32:0:0", ":32:32:0:0:64:64:5:59:5:59") -- replace icon texture
+	if text and text ~= '' then
+		local newText, count = gsub(text, ':32:32:0:0', ':32:32:0:0:64:64:5:59:5:59')
 		if count > 0 then
 			text = newText
-			button:SetFormattedText("%s", text)
+			button:SetFormattedText('%s', text)
 		end
 
-		local colorStr, rawText = strmatch(text, "|c[fF][fF](%x%x%x%x%x%x)(.-)|r")
+		local colorStr, rawText = strmatch(text, '|c[fF][fF](%x%x%x%x%x%x)(.-)|r')
 		colorStr = ReplacedGossipColor[colorStr]
 		if colorStr and rawText then
-			button:SetFormattedText("|cff%s%s|r", colorStr, rawText)
+			button:SetFormattedText('|cff%s%s|r', colorStr, rawText)
 		end
 	end
 end
@@ -56,8 +54,29 @@ local function LoadSkin()
 
 	_G.QuestFont:SetTextColor(1, 1, 1)
 
+	_G.ItemTextPageText:SetTextColor('P', 1, 1, 1)
+	hooksecurefunc(_G.ItemTextPageText, 'SetTextColor', function(pageText, headerType, r, g, b)
+		if r ~= 1 or g ~= 1 or b ~= 1 then
+			pageText:SetTextColor(headerType, 1, 1, 1)
+		end
+	end)
+
+	hooksecurefunc(GossipFrame.GreetingPanel.ScrollBox, 'Update', function(frame)
+		for _, button in next, { frame.ScrollTarget:GetChildren() } do
+			if not button.IsSkinned then
+				local buttonText = select(3, button:GetRegions())
+				if buttonText and buttonText:IsObjectType('FontString') then
+					ReplaceGossipText(button, button:GetText())
+					hooksecurefunc(button, 'SetText', ReplaceGossipText)
+					hooksecurefunc(button, 'SetFormattedText', ReplaceGossipFormat)
+				end
+
+				button.IsSkinned = true
+			end
+		end
+	end)
+
 	MER.NPC:Register(GossipFrame)
-	-- hooksecurefunc("GossipTitleButton_OnClick", function() MER.NPC:PlayerTalksFirst() end)
 end
 
 S:AddCallback("GossipFrame", LoadSkin)
