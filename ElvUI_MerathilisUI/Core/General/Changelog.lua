76cbf8ca2ed4559ad5cfdd641c0a42185edcb4ec
Merathilis <neo0608@gmail.com>
2022-11-02 16:13:53 +0100

    v.5.32

--- ElvUI_MerathilisUI/Core/General/Changelog.lua
+++ ElvUI_MerathilisUI/Core/General/Changelog.lua
@@ -15,7 +15,11 @@ local ChangeLogData = {
 		"• Prevent a C-Stack error on the Garrison Skin",
 		"• Added Backstab as reference spell for oUF_GCD #50",
 		"• Bring back the annoying Spell Feedback, you are welcome <3",
-		"• Updated Bag Code",
+		"• Changed my code to register any Button press if up or down. >.<",
+		"• Fixed some backdrop issues on MinimapButton Bar. Thx Murava",
+		"• Replaced the Minimap Difficulty texture on the Minimap. Thx NDUI",
+		"• Zygor Guides causes some crashes with different AddOn, so i disable it!",
+		"• Updated some skins",
 
 	" ",
 	"Notes:",
