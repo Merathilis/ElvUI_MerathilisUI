bd9871d860607ec724e9b0bbf496c5ab897ea8c3
Merathilis <neo0608@gmail.com>
2022-11-27 12:24:21 +0100

    Updated Community & ClickBinding Skin

--- ElvUI_MerathilisUI/Mainline/Modules/Skins/AddOns/Blizzard_ClickBindingUI.lua
+++ ElvUI_MerathilisUI/Mainline/Modules/Skins/AddOns/Blizzard_ClickBindingUI.lua
@@ -1,23 +1,8 @@
 local MER, F, E, L, V, P, G = unpack(select(2, ...))
-local module = MER.Modules.Skins
+local module = MER:GetModule('MER_Skins')
 local S = E:GetModule('Skins')
 
 local _G = _G
-local hooksecurefunc = hooksecurefunc
-
-local function HandleScrollChild(self)
-	for i = 1, self.ScrollTarget:GetNumChildren() do
-		local child = select(i, self.ScrollTarget:GetChildren())
-		local icon = child and child.Icon
-		if icon and not icon.IsChanged then
-			if child and child.backdrop then
-				child.backdrop:SetTemplate('Transparent')
-				module:CreateGradient(child.backdrop)
-			end
-			icon.IsChanged = true
-		end
-	end
-end
 
 local function LoadSkin()
 	if not module:CheckDB("binding", "binding") then
@@ -26,8 +11,7 @@ local function LoadSkin()
 
 	local frame = _G.ClickBindingFrame
 	frame:Styling()
-
-	hooksecurefunc(frame.ScrollBox, 'Update', HandleScrollChild)
+	module:CreateShadow(frame)
 end
 
 S:AddCallbackForAddon("Blizzard_ClickBindingUI", LoadSkin)
