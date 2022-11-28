d59a9c24997bc82f23c66c4f184d39e7b3a5023a
Merathilis <neo0608@gmail.com>
2022-11-28 16:36:49 +0100

    v.5.44

--- ElvUI_MerathilisUI/Core/General/Changelog.lua
+++ ElvUI_MerathilisUI/Core/General/Changelog.lua
@@ -12,13 +12,7 @@ local DISABLED_FONT_COLOR = DISABLED_FONT_COLOR
 
 local ChangeLogData = {
 	"Changes:",
-		"• Added back MirrorTimer Style",
-		"• Added my shadow the the UF InfoPanels",
-		"• Fix a potential lua error on the LocPanel for Evokers",
-		"• Added another reference spell for Prot Paladins to oUF_GCD",
-		"• Dropped Covenant Support in Tooltips",
-		"• Updated some skins",
-		"• Added own Immersion Skin",
+		"• Fixed a C-Stack error",
 
 	" ",
 	"Notes:",
