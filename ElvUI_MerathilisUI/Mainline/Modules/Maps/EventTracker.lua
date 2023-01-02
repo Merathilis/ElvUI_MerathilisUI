6c114fdc60ee132e096e29bacd0f30b0b46f3dba
Merathilis <neo0608@gmail.com>
2023-01-02 13:27:18 +0100

    Add fishing holes from Theldraszus

--- ElvUI_MerathilisUI/Mainline/Modules/Maps/EventTracker.lua
+++ ElvUI_MerathilisUI/Mainline/Modules/Maps/EventTracker.lua
@@ -87,8 +87,10 @@ end
 
 local env = {
 	fishingNetPosition = {
-		[1] = { x = 0.63585, y = 0.75349 },
-		[2] = { x = 0.64514, y = 0.74178 }
+		[1] = { map = 2022, x = 0.63585, y = 0.75349 },
+		[2] = { map = 2022, x = 0.64514, y = 0.74178 },
+		[3] = { map = 2025, x = 0.56782, y = 0.65178 },
+		[4] = { map = 2025, x = 0.57756, y = 0.65491 },
 	}
 }
 
@@ -644,16 +646,22 @@ local eventData = {
 						end
 
 						local map = C_Map_GetBestMapForUnit("player")
-						if not map or map ~= 2022 then
+						if not map then
 							return
 						end
 
 						local position = C_Map_GetPlayerMapPosition(map, "player")
+						if not position then
+							return
+						end
+
 						local lengthMap = {}
 
 						for i, netPos in ipairs(env.fishingNetPosition) do
-							local length = math_pow(position.x - netPos.x, 2) + math_pow(position.y - netPos.y, 2)
-							lengthMap[i] = length
+							if map == netPos.map then
+								local length = math_pow(position.x - netPos.x, 2) + math_pow(position.y - netPos.y, 2)
+								lengthMap[i] = length
+							end
 						end
 
 						local min
