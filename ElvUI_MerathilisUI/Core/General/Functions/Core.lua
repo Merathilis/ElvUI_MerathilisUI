1e19b8b0d740bbea9c8491a79d56b0729a44390c
Merathilis <neo0608@gmail.com>
2023-02-04 16:51:34 +0100

    Update PlayerChoice Skin

--- ElvUI_MerathilisUI/Core/General/Functions/Core.lua
+++ ElvUI_MerathilisUI/Core/General/Functions/Core.lua
@@ -498,6 +498,14 @@ do
 			self.Show = self.Hide
 		end
 		self:Hide()
+    end
+
+	function F:ReplaceIconString(text)
+		if not text then text = self:GetText() end
+		if not text or text == "" then return end
+
+		local newText, count = gsub(text, "|T([^:]-):[%d+:]+|t", "|T%1:14:14:0:0:64:64:5:59:5:59|t")
+		if count > 0 then self:SetFormattedText("%s", newText) end
 	end
 end
 
--- ElvUI_MerathilisUI/Mainline/Modules/Skins/AddOns/Blizzard_PlayerChoice.lua
+++ ElvUI_MerathilisUI/Mainline/Modules/Skins/AddOns/Blizzard_PlayerChoice.lua
@@ -3,13 +3,48 @@ local module = MER.Modules.Skins
 local S = E:GetModule('Skins')
 
 local _G = _G
+local hooksecurefunc = hooksecurefunc
+
+local function ReskinOptionText(text, r, g, b)
+	if text then
+		text:SetTextColor(r, g, b)
+	end
+end
 
 local function LoadSkin()
-	if not module:CheckDB("playerChoice", "playerChoice") then
+	if not module:CheckDB('playerChoice', 'playerChoice') then
 		return
 	end
 
-	local frame = _G.PlayerChoiceFrame
+	hooksecurefunc(_G.PlayerChoiceFrame, "TryShow", function(self)
+		if not self.optionFrameTemplate then return end
+
+		for optionFrame in self.optionPools:EnumerateActiveByTemplate(self.optionFrameTemplate) do
+			local header = optionFrame.Header
+			if header then
+				ReskinOptionText(header.Text, 1, .8, 0)
+				if header.Contents then
+					ReskinOptionText(header.Contents.Text, 1, .8, 0)
+				end
+			end
+			ReskinOptionText(optionFrame.OptionText, 1, 1, 1)
+			F.ReplaceIconString(optionFrame.OptionText.String)
+
+			local rewards = optionFrame.Rewards
+			if rewards then
+				for rewardFrame in rewards.rewardsPool:EnumerateActiveByTemplate("PlayerChoiceBaseOptionItemRewardTemplate") do
+					ReskinOptionText(rewardFrame.Name, .9, .8, .5)
+				end
+
+				for rewardFrame in rewards.rewardsPool:EnumerateActive() do
+					local text = rewardFrame.Name or rewardFrame.Text
+					if text then
+						ReskinOptionText(text, .9, .8, .5)
+					end
+				end
+			end
+		end
+	end)
 end
 
 module:AddCallbackForAddon("Blizzard_PlayerChoice", LoadSkin)
