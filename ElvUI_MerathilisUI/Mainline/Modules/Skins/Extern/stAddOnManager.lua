d9ed7052910e64a749d0a0b86de100c57922b8d8
Merathilis <neo0608@gmail.com>
2023-03-13 10:30:56 +0100

    Updated stAM Skin

--- ElvUI_MerathilisUI/Mainline/Modules/Skins/Extern/stAddOnManager.lua
+++ ElvUI_MerathilisUI/Mainline/Modules/Skins/Extern/stAddOnManager.lua
@@ -1,26 +1,28 @@
 local MER, F, E, L, V, P, G = unpack(select(2, ...))
 local module = MER:GetModule('MER_Skins')
-if not IsAddOnLoaded("ProjectAzilroka") then return end
 
 local _G = _G
 local CreateFrame = CreateFrame
 
-local function LoadSkin()
+function module:ProjectAzilroka()
 	if not E.private.mui.skins.addonSkins.enable or not E.private.mui.skins.addonSkins.pa then return end
 
 	local f = CreateFrame("Frame")
 	f:RegisterEvent("PLAYER_ENTERING_WORLD")
-	f:SetScript("OnEvent", function(self, event)
+	f:SetScript("OnEvent", function(_, event)
 		if event then
 			local stFrame = _G.stAMFrame
-			if stFrame then
-				stFrame:Styling()
+			if stFrame and not stFrame.isSkinned then
+				stFrame:SetTemplate("Transparent")
 				stFrame.AddOns:SetTemplate("Transparent")
+				stFrame:Styling()
 				module:CreateShadow(stFrame)
+
+				stFrame.isSkinned = true
 			end
 			f:UnregisterEvent("PLAYER_ENTERING_WORLD")
 		end
 	end)
 end
 
-module:AddCallbackForAddon("ProjectAzilroka", LoadSkin)
+module:AddCallbackForAddon("ProjectAzilroka")
