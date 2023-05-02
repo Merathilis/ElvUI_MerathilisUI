5c54c510358fd156b953461c633ccbb7900f5aaf
Merathilis <neo0608@gmail.com>
2023-05-02 20:10:43 +0200

    v.5.75 for PATCH 10.1

--- ElvUI_MerathilisUI/Core/General/Changelog.lua
+++ ElvUI_MerathilisUI/Core/General/Changelog.lua
@@ -12,12 +12,7 @@ local DISABLED_FONT_COLOR = DISABLED_FONT_COLOR
 
 local ChangeLogData = {
 	"Changes:",
-		"• Fix the NameHover Option. Thanks @Murava",
-		"• Wrath: Fix double ID's on AutoButtons",
-		"• Wrath: AutoButton fix 'Inherit Global Fade Option' thanks @Naifu <3",
-		"• Added a gradient Name Option for Details. Disabled by default",
-		"• Use E:StripString on the Gradient Tooltips to fix some weird stuff",
-		"• Small Layout adjustments",
+		"• Patch 10.1 fixes",
 
 		" ",
 
