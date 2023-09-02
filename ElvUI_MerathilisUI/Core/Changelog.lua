49dfa781c1727c351ba88ca2349968465a7c4c17
Merathilis <neo0608@gmail.com>
2023-09-02 18:26:20 +0200

    v.5.86

--- ElvUI_MerathilisUI/Core/Changelog.lua
+++ ElvUI_MerathilisUI/Core/Changelog.lua
@@ -12,9 +12,11 @@ local DISABLED_FONT_COLOR = DISABLED_FONT_COLOR
 
 local ChangeLogData = {
 	"Changes:",
-		"• Fixed Timestamp on the Event Tacker for some regions",
-		"• Updated AlreadyKnown",
-		"• Update ruRU. Thx @Evgeniy-ONiX",
+		"• !!IMPORTANT!!: With this version i will only support Retail",
+		"• Changed the file structure from my Plugin",
+		"• Added a time delay option to prevent posting double Announcements",
+		"• Removed the Reminder Module",
+		"• Added more Runes/more openable items to the AutoButtons",
 		-- "• ''",
 
 		" ",
