0930213afbc3d3ae5867095cf7ed6bc3d0fa8711
Merathilis <neo0608@gmail.com>
2023-10-09 15:58:11 +0200

    v.5.90

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,3 @@
 ### Changes ###
 
-  * Updated RaidManager
-  * Updated Chat Text code
-  * Added some more openable Boxes/Items
-  * Styling update on the AFK/Game Menu frame
-  * Added a gradient style on the UnitFrame backdrop
-  * Let the BigWigs font scale with the resolution
+  * Fix WeakAuras skinning. Credits: Windtools <3
--- ElvUI_MerathilisUI/Core/Changelog.lua
+++ ElvUI_MerathilisUI/Core/Changelog.lua
@@ -12,12 +12,7 @@ local DISABLED_FONT_COLOR = DISABLED_FONT_COLOR
 
 local ChangeLogData = {
 	"Changes:",
-		"• Updated RaidManager",
-		"• Updated Chat Text code",
-		"• Added some more openable Boxes/Items",
-		"• Styling update on the AFK/Game Menu frame",
-		"• Added a gradient style on the UnitFrame backdrop",
-		"• Let the BigWigs font scale with the resolution",
+		"• Fix WeakAuras skinning. Credits: Windtools <3",
 
 		-- "• ''",
 
