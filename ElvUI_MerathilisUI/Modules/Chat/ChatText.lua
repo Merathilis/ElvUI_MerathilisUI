82a6992aa69a6cae5bfca934035d5f292231645b
Merathilis <neo0608@gmail.com>
2023-11-09 18:39:20 +0100

    Updates to ChatText

--- ElvUI_MerathilisUI/Modules/Chat/ChatText.lua
+++ ElvUI_MerathilisUI/Modules/Chat/ChatText.lua
@@ -1193,11 +1193,11 @@ function CT:ChatFrame_MessageEventHandler(frame, event, arg1, arg2, arg3, arg4, 
 			local accessID = _G.ChatHistory_GetAccessID(chatGroup, arg8)
 			local typeID = _G.ChatHistory_GetAccessID(infoType, arg8, arg12)
 
-			if E.Retail and arg1 == "YOU_CHANGED" and C_ChatInfo_GetChannelRuleset(arg8) == CHATCHANNELRULESET_MENTOR then
+			if arg1 == "YOU_CHANGED" and C_ChatInfo_GetChannelRuleset(arg8) == CHATCHANNELRULESET_MENTOR then
 				_G.ChatFrame_UpdateDefaultChatTarget(frame)
 				_G.ChatEdit_UpdateNewcomerEditBoxHint(frame.editBox)
 			else
-				if E.Retail and arg1 == "YOU_LEFT" then
+				if arg1 == "YOU_LEFT" then
 					_G.ChatEdit_UpdateNewcomerEditBoxHint(frame.editBox, arg8)
 				end
 
@@ -1298,7 +1298,7 @@ function CT:ChatFrame_MessageEventHandler(frame, event, arg1, arg2, arg3, arg4, 
 				message = format(globalstring, playerLink)
 			end
 
-			frame:AddMessage(message, info.r, info.g, info.b, info.id, nil, nil, isHistory, historyTime)
+			frame:AddMessage(message, info.r, info.g, info.b, info.id, nil, nil, nil, nil, nil, isHistory, historyTime)
 		elseif chatType == "BN_INLINE_TOAST_BROADCAST" then
 			if arg1 ~= "" then
 				arg1 = RemoveNewlines(RemoveExtraSpaces(arg1))
@@ -1401,9 +1401,11 @@ function CT:ChatFrame_MessageEventHandler(frame, event, arg1, arg2, arg3, arg4, 
 			end
 
 			-- beep boops
+			local historyType =
+				notChatHistory and not CH.SoundTimer and not strfind(event, "_INFORM") and historyTypes[event]
 			local alertType =
-				notChatHistory and not CH.SoundTimer and not strfind(event, "_INFORM") and
-				CH.db.channelAlerts[historyTypes[event]]
+				(historyType ~= "CHANNEL" and CH.db.channelAlerts[historyType]) or
+				(historyType == "CHANNEL" and CH.db.channelAlerts.CHANNEL[arg9])
 			if
 				alertType and alertType ~= "None" and arg2 ~= PLAYER_NAME and
 				(not CH.db.noAlertInCombat or not InCombatLockdown())
@@ -1562,11 +1564,9 @@ function CT:MessageFormatter(frame, info, chatType, chatGroup, chatTarget, chann
 	end
 
 	-- Player Flags
-	local pflag = GetPFlag(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15,
-		arg16, arg17)
+	local pflag = GetPFlag(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16, arg17)
 	if not isMonster then
-		local chatIcon, pluginChatIcon = specialChatIcons[arg12] or specialChatIcons[playerName],
-			CH:GetPluginIcon(arg12, playerName)
+		local chatIcon, pluginChatIcon = specialChatIcons[arg12] or specialChatIcons[playerName], CH:GetPluginIcon(arg12, playerName)
 		if type(chatIcon) == 'function' then
 			local icon, prettify, var1, var2, var3 = chatIcon()
 			if prettify and chatType ~= 'GUILD_ITEM_LOOTED' and not CH:MessageIsProtected(message) then
@@ -2020,15 +2020,13 @@ function CT:PLAYER_ENTERING_WORLD(event)
 	self:UnregisterEvent(event)
 end
 
-function CT:BN_FRIEND_INFO_CHANGED(_, friendIndex, appTexture)
-	if not appTexture then
-		if C_Texture_GetTitleIconTexture then
-			C_Texture_GetTitleIconTexture("App", TitleIconVersion_Small, function(success, texture)
-				if success then
-					self:BN_FRIEND_INFO_CHANGED(_, friendIndex, texture)
-				end
-			end)
-		end
+function CT:BN_FRIEND_INFO_CHANGED(_, friendIndex, appTexture, noRetry)
+	if not appTexture and not noRetry then
+		C_Texture_GetTitleIconTexture("App", TitleIconVersion_Small, function(success, texture)
+			if success then
+				self:BN_FRIEND_INFO_CHANGED(_, friendIndex, texture, true)
+			end
+		end)
 
 		return
 	end
@@ -2042,7 +2040,8 @@ function CT:BN_FRIEND_INFO_CHANGED(_, friendIndex, appTexture)
 		return
 	end
 
-	local displayAccountName = format("%s |cff82c5ff%s|r", BNet_GetClientEmbeddedTexture(appTexture, 32, 32, 12), accountName)
+	local appTextureString = appTexture and BNet_GetClientEmbeddedTexture(appTexture, 16, 16, 12) .. " " or ""
+	local displayAccountName = appTextureString .. format("|cff82c5ff%s|r", accountName)
 	local bnetLink = GetBNPlayerLink(accountName, displayAccountName, accountID, 0, 0, 0)
 
 	local onlineCharacters = {}
