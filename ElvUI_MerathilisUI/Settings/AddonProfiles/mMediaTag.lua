2a3dcf312e3b2d53fe7d4d29344da81c808fee91
Merathilis <neo0608@gmail.com>
2023-11-19 13:30:51 +0100

    Updated mMediaTag Profile

--- ElvUI_MerathilisUI/Settings/AddonProfiles/mMediaTag.lua
+++ ElvUI_MerathilisUI/Settings/AddonProfiles/mMediaTag.lua
@@ -2,11 +2,16 @@ local MER, F, E, L, V, P, G = unpack(ElvUI_MerathilisUI)
 
 local IsAddOnLoaded = C_AddOns.IsAddOnLoaded
 
-
-
 function MER:mMediaTag()
     if not IsAddOnLoaded("ElvUI_mMediaTag") then return end
 
     E.db["mMT"]["general"]["greeting"] = false
     E.db["mMT"]["nameplate"]["executemarker"]["enable"] = true
+    E.db["mMT"]["afk"]["misc"]["enable"] = false
+    E.db["mMT"]["afk"]["values"]["enable"] = false
+    E.db["mMT"]["afk"]["infoscreen"] = false
+    E.db["mMT"]["afk"]["garbage"] = false
+    E.db["mMT"]["afk"]["progress"]["enable"] = false
+    E.db["mMT"]["afk"]["attributes"]["enable"] = false
+    E.db["mMT"]["interruptoncd"]["enable"] = true
 end
