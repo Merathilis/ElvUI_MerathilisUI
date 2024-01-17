9608d3877f6cf503d03593d9af2740b104f79b92
Merathilis <neo0608@gmail.com>
2024-01-17 11:04:22 +0100

    nothing

--- ElvUI_MerathilisUI/Options/Core.lua
+++ ElvUI_MerathilisUI/Options/Core.lua
@@ -6,7 +6,8 @@ local format = format
 local CreateTextureMarkup = CreateTextureMarkup
 
 local newSignIgnored = [[|TInterface\OptionsFrame\UI-OptionsFrame-NewFeatureIcon:14:14|t]]
-local logo = CreateTextureMarkup("Interface/AddOns/ElvUI_MerathilisUI/Media/textures/m2", 64, 64, 20, 20, 0, 1, 0, 1, 0, -1)
+local logo = CreateTextureMarkup("Interface/AddOns/ElvUI_MerathilisUI/Media/textures/m2", 64, 64, 20, 20, 0, 1, 0, 1, 0,
+	-1)
 
 MER.options = {
 	general = {
@@ -23,7 +24,8 @@ MER.options = {
 			info = {
 				order = 1,
 				type = "description",
-				name = L["Here you find the options for all the different |cffffffffMerathilis|r|cffff8000UI|r modules."],
+				name = L
+					["Here you find the options for all the different |cffffffffMerathilis|r|cffff8000UI|r modules."],
 			},
 		},
 	},
@@ -67,26 +69,28 @@ MER.options = {
 
 function MER:OptionsCallback()
 	local icon = F.GetIconString(MER.Media.Textures.pepeSmall, 14)
-	E.Options.name = E.Options.name.." + " .. icon .. " " ..MER.Title.. format(": |cFF00c0fa%s|r", MER.Version)
+	E.Options.name = E.Options.name .. " + " .. icon .. " " .. MER.Title .. format(": |cFF00c0fa%s|r", MER.Version)
 
 	-- Main options
 	E.Options.args.mui = {
 		type = 'group',
-		name = logo..MER.Title,
+		name = logo .. MER.Title,
 		desc = L["Plugin for |cffff7d0aElvUI|r by\nMerathilis."],
 		childGroups = "tree",
-		get = function(info) return E.db.mui.general[ info[#info] ] end,
-		set = function(info, value) E.db.mui.general[ info[#info] ] = value; E:StaticPopup_Show("PRIVATE_RL"); end,
+		get = function(info) return E.db.mui.general[info[#info]] end,
+		set = function(info, value)
+			E.db.mui.general[info[#info]] = value; E:StaticPopup_Show("PRIVATE_RL");
+		end,
 		args = {
 			name = {
 				order = 1,
 				type = "header",
-				name = MER.Title..F.cOption(MER.Version, 'blue')..L["by Merathilis (|cFF00c0faEU-Shattrath|r)"],
+				name = MER.Title .. F.cOption(MER.Version, 'blue') .. L["by Merathilis (|cFF00c0faEU-Shattrath|r)"],
 			},
 			logo = {
 				order = 2,
 				type = "description",
-				name = L["MER_DESC"]..newSignIgnored,
+				name = L["MER_DESC"] .. newSignIgnored,
 				fontSize = "medium",
 				image = function() return "Interface\\AddOns\\ElvUI_MerathilisUI\\Media\\Textures\\mUI1.tga", 200, 200 end,
 			},
@@ -96,7 +100,9 @@ function MER:OptionsCallback()
 				name = L["Install"],
 				desc = L["Run the installation process."],
 				customWidth = 140,
-				func = function() E:GetModule("PluginInstaller"):Queue(MER.installTable); E:ToggleOptions() end,
+				func = function()
+					E:GetModule("PluginInstaller"):Queue(MER.installTable); E:ToggleOptions()
+				end,
 			},
 			changelog = {
 				order = 4,
@@ -104,12 +110,15 @@ function MER:OptionsCallback()
 				name = L["Changelog"],
 				desc = L["Open the changelog window."],
 				customWidth = 140,
-				func = function() MER:ToggleChangeLog(); E:ToggleOptions() end,
+				func = function()
+					MER:ToggleChangeLog(); E:ToggleOptions()
+				end,
 			},
 			discordButton = {
 				order = 5,
 				type = "execute",
-				name = L["|cffffffffMerathilis|r|cffff7d0aUI|r Discord"],
+				name = L
+					["|TInterface\\Addons\\ElvUI_MerathilisUI\\Media\\Icons\\Discord.tga:18:18:0:0:64:64|t |cffffffffMerathilis|r|cffff7d0aUI|r Discord"],
 				customWidth = 140,
 				func = function() E:StaticPopup_Show("MERATHILISUI_CREDITS", nil, nil, "https://discord.gg/28We6esE9v") end,
 			},
