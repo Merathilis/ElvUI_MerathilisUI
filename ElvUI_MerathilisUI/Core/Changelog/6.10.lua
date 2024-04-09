2feb405dd3c022eaeb6b988526e0553772e7e339
Merathilis <neo0608@gmail.com>
2024-04-09 16:31:28 +0200

    Fix a a whoospi with changelog option path

---
+++ ElvUI_MerathilisUI/Core/Changelog/6.10.lua
@@ -0,0 +1,8 @@
+local MER, F, E, I, V, P, G, L = unpack(ElvUI_MerathilisUI)
+
+MER.Changelog[610] = {
+	RELEASE_DATE = "DEV ",
+	FIXES = {
+		"[Core]: Fix a a whoospi with changelog option path",
+	},
+}
