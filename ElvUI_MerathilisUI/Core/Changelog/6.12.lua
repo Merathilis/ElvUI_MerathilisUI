faf12ef369354f08f2a699317b7688ad4a02e4cb
Merathilis <neo0608@gmail.com>
2024-04-25 10:38:15 +0200

    📖 DOC: Updated Changelogs

--- ElvUI_MerathilisUI/Core/Changelog/6.12.lua
+++ ElvUI_MerathilisUI/Core/Changelog/6.12.lua
@@ -4,12 +4,15 @@ MER.Changelog[612] = {
 	RELEASE_DATE = "TBD",
 	FIXES = {
 		"[TalentLoadoutEx]: Remove skin",
-		"[Details Skin]: Fix gradient Names",
+		"[Details Skin]: Fix gradient names and enable it by default",
 	},
-	NEW = {},
+	NEW = {
+		"[AutoButtons]: Added S4 openable boxes",
+	},
 	IMPROVEMENTS = {
 		"[Raid Manager]: Some code improvements to RaidManager",
 		"[ChatText]: Here also some code improvements",
 		"[RaiderIO Skin]: Updated a new SearchFrame",
+		"[Event Tracker]: Change the defaults on the Map Event Tracker",
 	},
 }
