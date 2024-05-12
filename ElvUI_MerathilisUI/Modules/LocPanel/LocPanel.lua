8749be39c8b7a638c294bff5bc3174d6cdb7e2fc
Merathilis <neo0608@gmail.com>
2024-05-12 17:35:13 +0200

    📦 NEW: Added Draenic Hologem to LocPanel & MicroBar

--- ElvUI_MerathilisUI/Modules/LocPanel/LocPanel.lua
+++ ElvUI_MerathilisUI/Modules/LocPanel/LocPanel.lua
@@ -101,6 +101,7 @@ module.Hearthstones = {
 	{ 208704, nil, true }, -- Deepdweller's Earthen Hearthstone
 	{ 209035, nil, true }, -- Hearthstone of the Flame
 	{ 212337, nil, true }, -- Stone of the Hearth (Hearthstone 10th Anniversary)
+	{ 210455, nil, true }, -- Draenic Hologem
 }
 
 module.PortItems = {
