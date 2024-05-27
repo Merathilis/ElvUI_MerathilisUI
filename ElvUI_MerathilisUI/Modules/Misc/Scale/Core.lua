c9f7eea347ec37668dae4ec46cc1d8cc7f92a798
Merathilis <neo0608@gmail.com>
2024-05-27 10:12:12 +0200

    👌 IMPROVE: Added more Scale options

--- ElvUI_MerathilisUI/Modules/Misc/Scale/Core.lua
+++ ElvUI_MerathilisUI/Modules/Misc/Scale/Core.lua
@@ -1,6 +1,8 @@
 local MER, F, E, I, V, P, G, L = unpack(ElvUI_MerathilisUI)
 local module = MER:GetModule("MER_Misc")
 
+local C_Timer_After = C_Timer.After
+
 local talentsHooked = false
 
 function module:SetElementScale(dbName, blizzName)
@@ -17,6 +19,51 @@ end
 function module:ScaleCollections()
 	module:SetElementScale("collections", "CollectionsJournal")
 	module:SetElementScale("wardrobe", "WardrobeFrame")
+end
+
+-- Credits to https://www.curseforge.com/wow/addons/kayrwidertransmogui
+function module:AdjustTransmogFrame()
+	if not E.db.mui.scale.transmog.enable then
+		return
+	end
+
+	local wardrobeFrame = _G["WardrobeFrame"]
+	local transmogFrame = _G["WardrobeTransmogFrame"]
+
+	local initialParentFrameWidth = wardrobeFrame:GetWidth() -- Expecting 965
+	local desiredParentFrameWidth = 1200
+	local parentFrameWidthIncrease = desiredParentFrameWidth - initialParentFrameWidth
+	wardrobeFrame:SetWidth(desiredParentFrameWidth)
+
+	local initialTransmogFrameWidth = transmogFrame:GetWidth()
+	local desiredTransmogFrameWidth = initialTransmogFrameWidth + parentFrameWidthIncrease
+	transmogFrame:SetWidth(desiredTransmogFrameWidth)
+
+	-- Calculate inset width only once
+	local modelScene = transmogFrame.ModelScene
+	local insetWidth = Round(initialTransmogFrameWidth - modelScene:GetWidth(), 0)
+	transmogFrame.Inset.BG:SetWidth(transmogFrame.Inset.BG:GetWidth() - insetWidth)
+	modelScene:SetWidth(transmogFrame:GetWidth() - insetWidth)
+
+	-- Move Slots
+	transmogFrame.HeadButton:SetPoint("TOPLEFT", 20, -60)
+	transmogFrame.HandsButton:SetPoint("TOPRIGHT", -20, -60)
+	transmogFrame.MainHandButton:SetPoint("BOTTOM", -26, 23)
+	transmogFrame.MainHandEnchantButton:SetPoint("CENTER", -26, -230)
+	transmogFrame.SecondaryHandButton:SetPoint("BOTTOM", 27, 23)
+	transmogFrame.SecondaryHandEnchantButton:SetPoint("CENTER", 27, -230)
+
+	-- Move Separate Shoulder checkbox
+	transmogFrame.ToggleSecondaryAppearanceCheckbox:SetPoint("BOTTOMLEFT", transmogFrame, "BOTTOMLEFT", 580, 15)
+
+	-- Ease constraints on zooming out
+	local function ExtendZoomDistance()
+		modelScene.activeCamera.maxZoomDistance = 6
+	end
+
+	modelScene:SetScript("OnShow", function()
+		C_Timer_After(0.01, ExtendZoomDistance)
+	end)
 end
 
 function module:ScaleInspectUI()
@@ -45,6 +92,10 @@ function module:ScaleTalents()
 	end
 end
 
+function module:ScaleAuctionHouse()
+	module:SetElementScale("auctionHouse", "AuctionHouseFrame")
+end
+
 function module:Scale()
 	-- Check if the db exist1
 	if not E.db and not E.db.mui then
@@ -62,7 +113,9 @@ function module:Scale()
 
 	module:AddCallbackOrScale("Blizzard_InspectUI", self.ScaleInspectUI)
 	module:AddCallbackOrScale("Blizzard_ClassTalentUI", self.ScaleTalents)
+	module:AddCallbackOrScale("Blizzard_AuctionHouseUI", self.ScaleAuctionHouse)
 	module:AddCallbackOrScale("Blizzard_Collections", self.ScaleCollections)
+	module:AddCallbackOrScale("Blizzard_Collections", self.AdjustTransmogFrame)
 end
 
 module:AddCallback("Scale")
