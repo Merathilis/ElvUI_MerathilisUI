1cb1118d29e19a7c0795106f95ca90d5c91a36c2
Merathilis <neo0608@gmail.com>
2024-07-21 11:01:30 +0200

    📦 NEW: Initial skin for Delves

---
+++ ElvUI_MerathilisUI/Modules/Skins/AddOns/Blizzard_Delves.lua
@@ -0,0 +1,25 @@
+local MER, F, E, I, V, P, G, L = unpack(ElvUI_MerathilisUI)
+local module = MER.Modules.Skins
+
+local _G = _G
+
+function module:Blizzard_DelvesCompanionConfiguration()
+	local CompanionConfigurationFrame = _G.DelvesCompanionConfigurationFrame
+	if not CompanionConfigurationFrame.backdrop then
+		CompanionConfigurationFrame:CreateBackdrop()
+	end
+	module:CreateShadow(CompanionConfigurationFrame)
+
+	local CompanionAbilityListFrame = _G.DelvesCompanionAbilityListFrame
+	module:CreateShadow(CompanionAbilityListFrame)
+end
+
+module:AddCallbackForAddon("Blizzard_DelvesCompanionConfiguration")
+
+function module:Blizzard_DelvesDashboardUI() end
+
+module:AddCallbackForAddon("Blizzard_DelvesDashboardUI")
+
+function module:Blizzard_DelvesDifficultyPicker() end
+
+module:AddCallbackForAddon("Blizzard_DelvesDifficultyPicker")
