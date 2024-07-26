8a5040ca5d28970614a2b6a829bb19741d0f4a05
Merathilis <neo0608@gmail.com>
2024-07-26 11:45:41 +0200

    👌 IMPROVE: Cleanup

--- ElvUI_MerathilisUI/Modules/Skins/AddOns/Blizzard_MapCanvas.lua
+++ ElvUI_MerathilisUI/Modules/Skins/AddOns/Blizzard_MapCanvas.lua
@@ -1,13 +1,13 @@
 local MER, F, E, I, V, P, G, L = unpack(ElvUI_MerathilisUI)
-local module = MER.Modules.Skins
+local module = MER:GetModule("MER_Skins")
 
 local _G = _G
 
-local C_Map_GetMapArtLayers = C_Map.GetMapArtLayers
+local GetMapArtLayers = C_Map.GetMapArtLayers
 local hooksecurefunc = hooksecurefunc
 
 local function MapCanvasDetailLayerMixin_RefreshDetailTiles(self)
-	local layers = C_Map_GetMapArtLayers(self.mapID)
+	local layers = GetMapArtLayers(self.mapID)
 	local layerInfo = layers[self.layerIndex]
 
 	for detailTile in self.detailTilePool:EnumerateActive() do
