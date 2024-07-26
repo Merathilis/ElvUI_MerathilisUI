e182b0087ac916f4e704489addf412711a690e21
Merathilis <neo0608@gmail.com>
2024-07-26 12:03:05 +0200

    📖 DOC: prep changelogs for next release

---
+++ ElvUI_MerathilisUI/Core/Changelog/6.21.lua
@@ -0,0 +1,10 @@
+local MER = unpack(ElvUI_MerathilisUI)
+
+MER.Changelog[621] = {
+	RELEASE_DATE = "26.07.2024",
+	FIXES = {
+		"[Objective Tracker]: Fix copy & paste whoooopsiii",
+	},
+	NEW = {},
+	IMPROVEMENTS = {},
+}
