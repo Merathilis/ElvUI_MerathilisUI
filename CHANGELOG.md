a81ad4463b33ffd6fab0a0d1fccd086c6462b38d
Merathilis <neo0608@gmail.com>
2024-07-27 14:16:28 +0200

    🚀 RELEASE: 6.22

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
 ### Changes ###
 
-  * [Objective Tracker]: Fix copy & paste whoooopsiii
+  * [Announcement]: Added back some Options, whoooopppsiii
+  * [Objective Tracker]: A lot of work on the Objective Tracker. Thx fang2hou and Blinkii
+  * [Widget Skin]: Some gradient look on the Widget Skin
--- ElvUI_MerathilisUI/Core/Changelog/6.22.lua
+++ ElvUI_MerathilisUI/Core/Changelog/6.22.lua
@@ -1,8 +1,13 @@
 local MER = unpack(ElvUI_MerathilisUI)
 
 MER.Changelog[622] = {
-	RELEASE_DATE = "TBD",
-	FIXES = {},
+	RELEASE_DATE = "27.07.2024",
+	FIXES = {
+		"[Announcement]: Added back some Options, whoooopppsiii",
+		"[Objective Tracker]: A lot of work on the Objective Tracker. Thx fang2hou and Blinkii",
+	},
 	NEW = {},
-	IMPROVEMENTS = {},
+	IMPROVEMENTS = {
+		"[Widget Skin]: Some gradient look on the Widget Skin",
+	},
 }
