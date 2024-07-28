40641670889445e044f115a8922c5efcd1b26ee8
Merathilis <neo0608@gmail.com>
2024-07-28 17:16:23 +0200

    🚀 RELEASE: 6.23

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 ### Changes ###
 
-  * [Announcement]: Added back some Options, whoooopppsiii
-  * [Objective Tracker]: A lot of work on the Objective Tracker. Thx fang2hou and Blinkii
-  * [Widget Skin]: Some gradient look on the Widget Skin
+  * [Objective Tracker]: Fix an error on the Scenario Tracker
+  * [Install/Layout]: Enable Rectangle Minimap by default, you might need to adjust your layout a bit
+  * [WorldMap]: Updated RawMap Data for 11.0
+  * [Instance Difficulty]: Updated Logic from Instance Difficutly
+  * [Skins]: Updated some skin
--- ElvUI_MerathilisUI/Core/Changelog/6.23.lua
+++ ElvUI_MerathilisUI/Core/Changelog/6.23.lua
@@ -1,8 +1,16 @@
 local MER = unpack(ElvUI_MerathilisUI)
 
 MER.Changelog[623] = {
-	RELEASE_DATE = "TBD",
-	FIXES = {},
-	NEW = {},
-	IMPROVEMENTS = {},
+	RELEASE_DATE = "28.07.2024",
+	FIXES = {
+		"[Objective Tracker]: Fix an error on the Scenario Tracker",
+	},
+	NEW = {
+		"[Install/Layout]: Enable Rectangle Minimap by default, you might need to adjust your layout a bit",
+	},
+	IMPROVEMENTS = {
+		"[WorldMap]: Updated RawMap Data for 11.0",
+		"[Instance Difficulty]: Updated Logic from Instance Difficutly",
+		"[Skins]: Updated some skin",
+	},
 }
