1fc943241b7dc7bb204ca1f8dad5e938a5b06d65
Merathilis <neo0608@gmail.com>
2024-07-28 14:11:51 +0200

    👌 IMPROVE: Updated Instance Difficulty

--- ElvUI_MerathilisUI/Modules/Maps/InstanceDifficulty.lua
+++ ElvUI_MerathilisUI/Modules/Maps/InstanceDifficulty.lua
@@ -9,9 +9,9 @@ local select = select
 
 local CreateFrame = CreateFrame
 local GetInstanceInfo = GetInstanceInfo
-local MinimapCluster = MinimapCluster
 local IsInInstance = IsInInstance
 
+local IsAddOnLoaded = C_AddOns.IsAddOnLoaded
 local GetActiveKeystoneInfo = C_ChallengeMode.GetActiveKeystoneInfo
 
 function module:UpdateFrame()
@@ -20,7 +20,9 @@ function module:UpdateFrame()
 	local numplayers = select(9, GetInstanceInfo())
 	local mplusdiff = select(1, GetActiveKeystoneInfo()) or ""
 
-	if instanceType == "party" or instanceType == "raid" or instanceType == "scenario" then
+	if difficulty == 0 then
+		self.frame.text:SetText("")
+	elseif instanceType == "party" or instanceType == "raid" or instanceType == "scenario" then
 		local text = module:GetTextForDifficulty(difficulty, false)
 
 		if not text then
@@ -54,7 +56,7 @@ function module:GetTextForDifficulty(difficulty, useDefault)
 		[4] = db["25-player Normal"],
 		[5] = db["10-player Heroic"],
 		[6] = db["25-player Heroic"],
-		[7] = db["Looking for Raid"],
+		[7] = db["LFR"],
 		[8] = db["Mythic Keystone"],
 		[9] = db["40-player"],
 		[11] = db["Heroic Scenario"],
@@ -62,7 +64,7 @@ function module:GetTextForDifficulty(difficulty, useDefault)
 		[14] = db["Normal Raid"],
 		[15] = db["Heroic Raid"],
 		[16] = db["Mythic Raid"],
-		[17] = db["Looking for Raid"],
+		[17] = db["LFR Raid"],
 		[18] = db["Event Scenario"],
 		[19] = db["Event Scenario"],
 		[20] = db["Event Scenario"],
@@ -71,7 +73,7 @@ function module:GetTextForDifficulty(difficulty, useDefault)
 		[25] = db["World PvP Scenario"],
 		[29] = db["PvEvP Scenario"],
 		[30] = db["Event Scenario"],
-		[32] = db["PvP"],
+		[32] = db["World PvP Scenario"],
 		[33] = db["Timewalking Raid"],
 		[34] = db["PvP Heroic"],
 		[38] = db["Normal Scenario"],
@@ -80,7 +82,21 @@ function module:GetTextForDifficulty(difficulty, useDefault)
 		[45] = db["PvP"],
 		[147] = db["Warfronts Normal"],
 		[149] = db["Warfronts Heroic"],
+		[150] = db["Normal Scaling Party"],
+		[151] = db["LFR"],
+		[152] = db["Visions of N'Zoth"],
+		[153] = db["Teeming Island"],
+		[167] = db["Torghast"],
+		[168] = db["Path of Ascension: Courage"],
+		[169] = db["Path of Ascension: Loyalty"],
+		[170] = db["Path of Ascension: Wisdom"],
+		[171] = db["Path of Ascension: Humility"],
+		[172] = db["World Boss"],
+		[192] = db["Challenge Level 1"],
+		[205] = db["Follower"],
 		[208] = db["Delves"],
+		[216] = db["Quest"],
+		[220] = db["Story"],
 	}
 
 	return text[difficulty]
@@ -127,6 +143,17 @@ function module:HideBlizzardDifficulty(difficultyFrame, isShown)
 	difficultyFrame:Hide()
 end
 
+function module:ADDON_LOADED(_, addon)
+	if addon == "Blizzard_Minimap" then
+		self:UnregisterEvent("ADDON_LOADED")
+
+		local difficulty = _G.MinimapCluster.InstanceDifficulty
+		for _, frame in pairs({ difficulty.Default, difficulty.Guild, difficulty.ChallengeMode }) do
+			frame:SetAlpha(0)
+		end
+	end
+end
+
 function module:Initialize()
 	self.db = E.db.mui.maps.instanceDifficulty
 
@@ -136,19 +163,6 @@ function module:Initialize()
 
 	self:ConstructFrame()
 
-	local difficulty = MinimapCluster.InstanceDifficulty
-	local instanceFrame = difficulty.Instance
-	local guildFrame = difficulty.Guild
-	local challengeModeFrame = difficulty.ChallengeMode
-	local default = difficulty.Default
-
-	for _, frame in pairs({ instanceFrame, guildFrame, challengeModeFrame, default }) do
-		if frame then
-			frame:Hide()
-			self:SecureHook(frame, "SetShown", "HideBlizzardDifficulty")
-		end
-	end
-
 	self:RegisterEvent("PLAYER_ENTERING_WORLD", "UpdateFrame")
 	self:RegisterEvent("CHALLENGE_MODE_START", "UpdateFrame")
 	self:RegisterEvent("CHALLENGE_MODE_COMPLETED", "UpdateFrame")
@@ -157,6 +171,12 @@ function module:Initialize()
 	self:RegisterEvent("GUILD_PARTY_STATE_UPDATED", "UpdateFrame")
 	self:RegisterEvent("ZONE_CHANGED_NEW_AREA", "UpdateFrame")
 	self:RegisterEvent("GROUP_ROSTER_UPDATE", "UpdateFrame")
+
+	if IsAddOnLoaded("Blizzard_Minimap") then
+		self:ADDON_LOADED("ADDON_LOADED", "Blizzard_Minimap")
+	else
+		self:RegisterEvent("ADDON_LOADED")
+	end
 end
 
 MER:RegisterModule(module:GetName())
