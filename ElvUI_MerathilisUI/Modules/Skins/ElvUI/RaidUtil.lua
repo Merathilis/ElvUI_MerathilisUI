cf3cc9d7a50bb4f143e134c852a47a129c01a29f
Merathilis <neo0608@gmail.com>
2024-07-28 18:00:04 +0200

    📦 NEW: Add Skin for the ElvUI Raid Utility

---
+++ ElvUI_MerathilisUI/Modules/Skins/ElvUI/RaidUtil.lua
@@ -0,0 +1,22 @@
+local MER, F, E, I, V, P, G, L = unpack(ElvUI_MerathilisUI)
+local module = MER:GetModule("MER_Skins")
+
+function module:RaidUtility()
+	if not E.private.mui.skins.enable or not E.private.general.raidUtility then
+		return
+	end
+
+	local frames = {
+		_G.RaidUtilityPanel,
+		_G.RaidUtility_ShowButton,
+		_G.RaidUtility_CloseButton,
+		_G.RaidUtilityRoleIcons,
+		_G.RaidUtilityTargetIcons,
+	}
+
+	for _, frame in pairs(frames) do
+		module:CreateShadow(frame)
+	end
+end
+
+module:AddCallback("RaidUtility")
