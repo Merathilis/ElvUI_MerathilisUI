638f1484ba365929a63875a990098953fbc7ae31
Merathilis <neo0608@gmail.com>
2024-07-29 09:04:36 +0200

    🐛 FIX: Try to avoid a C-Stack with Windtools enabled.

--- ElvUI_MerathilisUI/Modules/Skins/AddOns/Blizzard_ScenarioObjectiveTracker.lua
+++ ElvUI_MerathilisUI/Modules/Skins/AddOns/Blizzard_ScenarioObjectiveTracker.lua
@@ -121,10 +121,10 @@ local function UpdateBlock(block)
 
 			local bar = widgetFrame.TimerBar
 			if bar and not bar.__MERSkin then
-				bar.__SetStatusBarTexture = bar.SetStatusBarTexture
+				bar.__MERSetStatusBarTexture = bar.SetStatusBarTexture
 				hooksecurefunc(bar, "SetStatusBarTexture", function(frame)
-					if frame.__SetStatusBarTexture then
-						frame:__SetStatusBarTexture(E.media.normTex)
+					if frame.__MERSetStatusBarTexture then
+						frame:__MERSetStatusBarTexture(E.media.normTex)
 						frame:SetStatusBarColor(unpack(E.media.rgbvaluecolor))
 					end
 				end)
