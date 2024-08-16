1bdda03ae5c532e435313ba3557e9e809e909dca
Merathilis <neo0608@gmail.com>
2024-08-16 15:20:55 +0200

    👌 IMPROVE: Don't build my Game Menu addition on the GameMenuFrame, use E.UIParent instead.

--- ElvUI_MerathilisUI/Modules/Misc/GameMenu.lua
+++ ElvUI_MerathilisUI/Modules/Misc/GameMenu.lua
@@ -7,7 +7,6 @@ local random = random
 
 local CreateFrame = CreateFrame
 local CreateAnimationGroup = CreateAnimationGroup
-local EventRegistry = EventRegistry
 local GetScreenWidth, GetScreenHeight = GetScreenWidth, GetScreenHeight
 local UIFrameFadeIn = UIFrameFadeIn
 
@@ -35,69 +34,161 @@ MER.NPCS = {
 
 local Sequences = { 26, 52, 69, 111, 225 }
 
-local function Player_Model(self)
-	local key = random(1, 5)
-	local emote = Sequences[key]
-
-	self:ClearModel()
-	self:SetUnit("player")
-	self:SetFacing(6.5)
-	self:SetPortraitZoom(0.05)
-	self:SetCamDistanceScale(4.8)
-	self:SetAlpha(1)
-	self:SetAnimation(emote)
-	UIFrameFadeIn(self, 1, self:GetAlpha(), 1)
-end
-
-local function NPC_Model(self)
-	local npc = MER.NPCS
-	local mod = random(1, #npc)
-	local id = npc[mod]
-	local key = random(1, 5)
-	local emote = Sequences[key]
-
-	self:ClearModel()
-	self:SetCreature(id)
-	self:SetCamDistanceScale(1)
-	self:SetFacing(6)
-	self:SetAlpha(1)
-	self:SetAnimation(emote)
-	UIFrameFadeIn(self, 1, self:GetAlpha(), 1)
-end
-
 function module:GameMenu_OnShow()
 	local iconsDb = E.db.mui.armory.icons
+	local guildName = GetGuildInfo("player")
 
-	if GameMenuFrame.MUIbottomPanel.guildText and GameMenuFrame.MUIbottomPanel.levelText then
-		local guildName = GetGuildInfo("player")
+	local fallback = iconsDb and iconsDb[0] or ""
+	local specIcon
 
-		local fallback = iconsDb and iconsDb[0] or ""
-		local specIcon
+	local _, classId = UnitClassBase("player")
+	local specIndex = GetSpecialization()
+	local id = GetSpecializationInfoForClassID(classId, specIndex)
 
-		local _, classId = UnitClassBase("player")
-		local specIndex = GetSpecialization()
-		local id = GetSpecializationInfoForClassID(classId, specIndex)
-
-		if id and iconsDb then
-			specIcon = iconsDb[id]
-		end
-
-		GameMenuFrame.MUIbottomPanel.guildText:SetText(
-			guildName and F.String.FastGradientHex("<" .. guildName .. ">", "06c910", "33ff3d") or ""
-		)
-		GameMenuFrame.MUIbottomPanel.specIcon:SetText(F.String.Class(specIcon and specIcon or fallback))
-		GameMenuFrame.MUIbottomPanel.levelText:SetText("Lvl " .. E.mylevel)
-		GameMenuFrame.MUIbottomPanel.classText:SetText(F.String.GradientClass(E.myLocalizedClass, nil, true))
+	if id and iconsDb then
+		specIcon = iconsDb[id]
 	end
+
+	local mainFrame = CreateFrame("Frame", nil, E.UIParent)
+	mainFrame:SetAllPoints(E.UIParent)
+	mainFrame:SetFrameStrata("TOOLTIP")
+	mainFrame:SetFrameLevel(GameMenuFrame:GetFrameLevel())
+
+	local bottomPanel = CreateFrame("Frame", nil, mainFrame, "BackdropTemplate")
+	bottomPanel:Point("BOTTOM", E.UIParent, "BOTTOM", 0, -E.Border)
+	bottomPanel:Width(E.physicalWidth + (E.Border * 2))
+	bottomPanel:CreateBackdrop("Transparent")
+	S:CreateBackdropShadow(bottomPanel)
+
+	bottomPanel.ignoreFrameTemplates = true
+	bottomPanel.ignoreBackdropColors = true
+	E["frames"][bottomPanel] = true
+
+	bottomPanel.anim = CreateAnimationGroup(bottomPanel)
+	bottomPanel.anim.height = bottomPanel.anim:CreateAnimation("Height")
+	bottomPanel.anim.height:SetChange(E.physicalHeight * (1 / 4))
+	bottomPanel.anim.height:SetDuration(0.6)
+	bottomPanel:Height(0)
+	bottomPanel.anim.height:Play()
+
+	bottomPanel.Logo = bottomPanel:CreateTexture(nil, "OVERLAY")
+	bottomPanel.Logo:Size(100)
+	bottomPanel.Logo:SetPoint("CENTER", bottomPanel, "TOP", 0, -80)
+	bottomPanel.Logo:SetTexture(I.General.MediaPath .. "Textures\\mUI1_Shadow.tga")
+
+	bottomPanel.nameText = bottomPanel:CreateFontString(nil, "OVERLAY")
+	bottomPanel.nameText:FontTemplate(nil, 26)
+	bottomPanel.nameText:SetTextColor(1, 1, 1, 1)
+	bottomPanel.nameText:SetPoint("TOP", bottomPanel.Logo, "BOTTOM", 0, -5)
+	bottomPanel.nameText:SetText(F.String.GradientClass(E.myname))
+
+	bottomPanel.guildText = bottomPanel:CreateFontString(nil, "OVERLAY")
+	bottomPanel.guildText:FontTemplate(nil, 16)
+	bottomPanel.guildText:SetPoint("TOP", bottomPanel.nameText, "BOTTOM", 0, 0)
+	bottomPanel.guildText:SetTextColor(1, 1, 1, 1)
+	bottomPanel.guildText:SetText(
+		guildName and F.String.FastGradientHex("<" .. guildName .. ">", "06c910", "33ff3d") or ""
+	)
+
+	bottomPanel.specIcon = bottomPanel:CreateFontString(nil, "OVERLAY")
+	bottomPanel.specIcon:SetFont("Interface\\AddOns\\ElvUI_MerathilisUI\\Media\\Fonts\\ToxiUIIcons.ttf", 20, "OUTLINE")
+	bottomPanel.specIcon:SetPoint("TOP", bottomPanel.guildText, "BOTTOM", 0, -15)
+	bottomPanel.specIcon:SetTextColor(1, 1, 1, 1)
+	bottomPanel.specIcon:SetText(F.String.Class(specIcon and specIcon or fallback))
+
+	bottomPanel.levelText = bottomPanel:CreateFontString(nil, "OVERLAY")
+	bottomPanel.levelText:FontTemplate(nil, 20, "OUTLINE")
+	bottomPanel.levelText:SetPoint("RIGHT", bottomPanel.specIcon, "LEFT", -4, 0)
+	bottomPanel.levelText:SetTextColor(1, 1, 1, 1)
+	bottomPanel.levelText:SetText("Lvl " .. E.mylevel)
+
+	bottomPanel.classText = bottomPanel:CreateFontString(nil, "OVERLAY")
+	bottomPanel.classText:FontTemplate(nil, 20, "OUTLINE")
+	bottomPanel.classText:SetPoint("LEFT", bottomPanel.specIcon, "RIGHT", 4, 0)
+	bottomPanel.classText:SetTextColor(1, 1, 1, 1)
+	bottomPanel.classText:SetText(F.String.GradientClass(E.myLocalizedClass, nil, true))
+
+	local topPanel = CreateFrame("Frame", nil, mainFrame, "BackdropTemplate")
+	topPanel:Point("TOP", E.UIParent, "TOP", 0, 0)
+	topPanel:Width(E.physicalWidth + (E.Border * 2))
+	topPanel:CreateBackdrop("Transparent")
+	S:CreateBackdropShadow(topPanel)
+
+	topPanel.ignoreFrameTemplates = true
+	topPanel.ignoreBackdropColors = true
+	E["frames"][topPanel] = true
+
+	topPanel.anim = CreateAnimationGroup(topPanel)
+	topPanel.anim.height = topPanel.anim:CreateAnimation("Height")
+	topPanel.anim.height:SetChange(E.physicalHeight * (1 / 4))
+	topPanel.anim.height:SetDuration(0.6)
+	topPanel:Height(0)
+	topPanel.anim.height:Play()
+
+	topPanel.factionLogo = topPanel:CreateTexture(nil, "ARTWORK")
+	topPanel.factionLogo:Point("CENTER", topPanel, "CENTER", 0, 0)
+	topPanel.factionLogo:Size(186, 186)
+	topPanel.factionLogo:SetTexture(I.General.MediaPath .. "Textures\\ClassBanner\\CLASS-" .. E.myclass)
+
+	-- Use this frame to control the position of the model - taken from ElvUI
+	local modelHolder = CreateFrame("Frame", nil, mainFrame)
+	modelHolder:Size(150)
+	modelHolder:Point("RIGHT", GameMenuFrame, "LEFT", -300, 0)
+
+	local playerKey = random(1, 5)
+	local playerEmote = Sequences[playerKey]
+
+	local playerModel = CreateFrame("PlayerModel", nil, modelHolder)
+	playerModel:Point("CENTER", modelHolder, "CENTER")
+	playerModel:Size(E.physicalWidth * 2, E.physicalHeight * 2) --YES, double screen size. This prevents clipping of models.
+	playerModel:SetScale(0.8)
+	playerModel:SetAlpha(1)
+	playerModel:ClearModel()
+	playerModel:SetUnit("player")
+	playerModel:SetFacing(6.5)
+	playerModel:SetPortraitZoom(0.05)
+	playerModel:SetCamDistanceScale(4.8)
+	playerModel:SetAnimation(playerEmote)
+	UIFrameFadeIn(playerModel, 1, playerModel:GetAlpha(), 1)
+	playerModel.isIdle = nil
+
+	local npcHolder = CreateFrame("Frame", nil, mainFrame)
+	npcHolder:Size(150)
+	npcHolder:Point("LEFT", GameMenuFrame, "RIGHT", 300, 0)
+
+	local npc = MER.NPCS
+	local mod = random(1, #npc)
+	local npcID = npc[mod]
+	local npcKey = random(1, 5)
+	local npcEmote = Sequences[npcKey]
+
+	local npcModel = CreateFrame("PlayerModel", nil, npcHolder)
+	npcModel:Point("CENTER", npcHolder, "CENTER")
+	npcModel:Size(256)
+	npcModel:SetScale(0.8)
+	npcModel:SetAlpha(1)
+
+	npcModel:ClearModel()
+	npcModel:SetCreature(npcID)
+	npcModel:SetCamDistanceScale(1)
+	npcModel:SetFacing(6)
+	npcModel:SetAnimation(npcEmote)
+	UIFrameFadeIn(self, 1, npcModel:GetAlpha(), 1)
+	npcModel.isIdle = nil
+
+	self.mainFrame = mainFrame
+	self.mainFrame:Show()
+
+	self.modelHolder = modelHolder
+	self.modelHolder:Show()
+
+	self.npcHolder = npcHolder
+	self.npcHolder:Show()
 end
 
 function module:GameMenu_OnHide()
-	if GameMenuFrame.MUIbottomPanel then
-		GameMenuFrame.MUIbottomPanel:Hide()
-	end
-
-	if GameMenuFrame.MUItopPanel then
-		GameMenuFrame.MUItopPanel:Hide()
+	if self.mainFrame then
+		self.mainFrame:Hide()
 	end
 end
 
@@ -106,125 +197,8 @@ function module:GameMenu()
 		return
 	end
 
-	-- GameMenu Frame
-	if not GameMenuFrame.MUIbottomPanel then
-		GameMenuFrame.MUIbottomPanel = CreateFrame("Frame", nil, GameMenuFrame, "BackdropTemplate")
-		local bottomPanel = GameMenuFrame.MUIbottomPanel
-		bottomPanel:SetFrameLevel(0)
-		bottomPanel:Point("BOTTOM", E.UIParent, "BOTTOM", 0, -E.Border)
-		bottomPanel:Width(GetScreenWidth() + (E.Border * 2))
-		bottomPanel:CreateBackdrop("Transparent")
-		S:CreateBackdropShadow(bottomPanel)
-
-		bottomPanel.ignoreFrameTemplates = true
-		bottomPanel.ignoreBackdropColors = true
-		E["frames"][bottomPanel] = true
-
-		bottomPanel.anim = CreateAnimationGroup(bottomPanel)
-		bottomPanel.anim.height = bottomPanel.anim:CreateAnimation("Height")
-		bottomPanel.anim.height:SetChange(GetScreenHeight() * (1 / 4))
-		bottomPanel.anim.height:SetDuration(0.6)
-
-		bottomPanel:SetScript("OnShow", function(self)
-			self:Height(0)
-			self.anim.height:Play()
-		end)
-
-		bottomPanel.Logo = bottomPanel:CreateTexture(nil, "OVERLAY")
-		bottomPanel.Logo:Size(100)
-		bottomPanel.Logo:SetPoint("CENTER", bottomPanel, "TOP", 0, -80)
-		bottomPanel.Logo:SetTexture(I.General.MediaPath .. "Textures\\mUI1_Shadow.tga")
-
-		bottomPanel.nameText = bottomPanel:CreateFontString(nil, "OVERLAY")
-		bottomPanel.nameText:FontTemplate(nil, 26)
-		bottomPanel.nameText:SetTextColor(1, 1, 1, 1)
-		bottomPanel.nameText:SetPoint("TOP", bottomPanel.Logo, "BOTTOM", 0, -5)
-		bottomPanel.nameText:SetText(F.String.GradientClass(E.myname))
-
-		bottomPanel.guildText = bottomPanel:CreateFontString(nil, "OVERLAY")
-		bottomPanel.guildText:FontTemplate(nil, 16)
-		bottomPanel.guildText:SetPoint("TOP", bottomPanel.nameText, "BOTTOM", 0, 0)
-		bottomPanel.guildText:SetTextColor(1, 1, 1, 1)
-
-		bottomPanel.specIcon = bottomPanel:CreateFontString(nil, "OVERLAY")
-		bottomPanel.specIcon:SetFont(
-			"Interface\\AddOns\\ElvUI_MerathilisUI\\Media\\Fonts\\ToxiUIIcons.ttf",
-			20,
-			"OUTLINE"
-		)
-		bottomPanel.specIcon:SetPoint("TOP", bottomPanel.guildText, "BOTTOM", 0, -15)
-		bottomPanel.specIcon:SetTextColor(1, 1, 1, 1)
-
-		bottomPanel.levelText = bottomPanel:CreateFontString(nil, "OVERLAY")
-		bottomPanel.levelText:FontTemplate(nil, 20, "OUTLINE")
-		bottomPanel.levelText:SetPoint("RIGHT", bottomPanel.specIcon, "LEFT", -4, 0)
-		bottomPanel.levelText:SetTextColor(1, 1, 1, 1)
-
-		bottomPanel.classText = bottomPanel:CreateFontString(nil, "OVERLAY")
-		bottomPanel.classText:FontTemplate(nil, 20, "OUTLINE")
-		bottomPanel.classText:SetPoint("LEFT", bottomPanel.specIcon, "RIGHT", 4, 0)
-		bottomPanel.classText:SetTextColor(1, 1, 1, 1)
-	end
-
-	if not GameMenuFrame.MUItopPanel then
-		GameMenuFrame.MUItopPanel = CreateFrame("Frame", nil, GameMenuFrame, "BackdropTemplate")
-		local topPanel = GameMenuFrame.MUItopPanel
-		topPanel:SetFrameLevel(0)
-		topPanel:Point("TOP", E.UIParent, "TOP", 0, 0)
-		topPanel:Width(GetScreenWidth() + (E.Border * 2))
-		topPanel:CreateBackdrop("Transparent")
-		S:CreateBackdropShadow(topPanel)
-
-		topPanel.ignoreFrameTemplates = true
-		topPanel.ignoreBackdropColors = true
-		E["frames"][topPanel] = true
-
-		topPanel.anim = CreateAnimationGroup(topPanel)
-		topPanel.anim.height = topPanel.anim:CreateAnimation("Height")
-		topPanel.anim.height:SetChange(GetScreenHeight() * (1 / 4))
-		topPanel.anim.height:SetDuration(0.6)
-
-		topPanel:SetScript("OnShow", function(self)
-			self:Height(0)
-			self.anim.height:Play()
-		end)
-
-		topPanel.factionLogo = topPanel:CreateTexture(nil, "ARTWORK")
-		topPanel.factionLogo:Point("CENTER", topPanel, "CENTER", 0, 0)
-		topPanel.factionLogo:Size(186, 186)
-		topPanel.factionLogo:SetTexture(I.General.MediaPath .. "Textures\\ClassBanner\\CLASS-" .. E.myclass)
-	end
-
-	-- Use this frame to control the position of the model - taken from ElvUI
-	if not GameMenuFrame.modelHolder then
-		GameMenuFrame.modelHolder = CreateFrame("Frame", nil, GameMenuFrame)
-		GameMenuFrame.modelHolder:Size(150)
-		GameMenuFrame.modelHolder:SetPoint("RIGHT", GameMenuFrame, "LEFT", -300, 0)
-
-		local playerModel = CreateFrame("PlayerModel", nil, GameMenuFrame.modelHolder)
-		playerModel:Point("CENTER", GameMenuFrame.modelHolder, "CENTER")
-		playerModel:SetScript("OnShow", Player_Model)
-		playerModel:Size(GetScreenWidth() * 2, GetScreenHeight() * 2) --YES, double screen size. This prevents clipping of models.
-		playerModel:SetScale(0.8)
-		playerModel.isIdle = nil
-		playerModel:Show()
-	end
-
-	if not GameMenuFrame.npcHolder then
-		GameMenuFrame.npcHolder = CreateFrame("Frame", nil, GameMenuFrame)
-		GameMenuFrame.npcHolder:SetSize(150, 150)
-		GameMenuFrame.npcHolder:SetPoint("LEFT", GameMenuFrame, "RIGHT", 300, 0)
-
-		local npcModel = CreateFrame("PlayerModel", nil, GameMenuFrame.npcHolder)
-		npcModel:Point("CENTER", GameMenuFrame.npcHolder, "CENTER")
-		npcModel:SetScript("OnShow", NPC_Model)
-		npcModel.isIdle = nil
-		npcModel:Size(256)
-		npcModel:SetScale(0.8)
-		npcModel:Show()
-	end
-
 	self:SecureHookScript(GameMenuFrame, "OnShow", module.GameMenu_OnShow)
+	self:SecureHookScript(GameMenuFrame, "OnHide", module.GameMenu_OnHide)
 end
 
 module:AddCallback("GameMenu")
