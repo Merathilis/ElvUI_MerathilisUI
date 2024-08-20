9e26fe86063c67fdfa066e06eed4a56b00856b38
Merathilis <neo0608@gmail.com>
2024-08-20 19:07:39 +0200

    👌 IMPROVE: Try to avoid a lua error on Objective Tracker

--- ElvUI_MerathilisUI/Modules/Blizzard/ObjectiveTracker.lua
+++ ElvUI_MerathilisUI/Modules/Blizzard/ObjectiveTracker.lua
@@ -199,9 +199,9 @@ function module:ObjectiveTrackerModule_Update(tracker)
 		tracker.Header.Text:SetShadowOffset(1, -1)
 		tracker.Header.Text:SetShadowColor(0, 0, 0)
 
-		local r = self.db.header.classColor and MER.ClassColor.r or self.db.header.color.r
-		local g = self.db.header.classColor and MER.ClassColor.g or self.db.header.color.g
-		local b = self.db.header.classColor and MER.ClassColor.b or self.db.header.color.b
+		local r = self.db and self.db.header and self.db.header.classColor and MER.ClassColor.r or self.db.header.color.r
+		local g = self.db and self.db.header and self.db.header.classColor and MER.ClassColor.g or self.db.header.color.g
+		local b = self.db and self.db.header and self.db.header.classColor and MER.ClassColor.b or self.db.header.color.b
 
 		tracker.Header.Text:SetTextColor(r, g, b)
 		if self.db.header.shortHeader then
