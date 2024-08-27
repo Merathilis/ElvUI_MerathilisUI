ca8dbf5aa7541c0543a9a6ee3bd218878035f8ee
Merathilis <neo0608@gmail.com>
2024-08-27 19:33:03 +0200

    👌 IMPROVE: Updated Delves skin

--- ElvUI_MerathilisUI/Modules/Skins/AddOns/Blizzard_Contribution.lua
+++ ElvUI_MerathilisUI/Modules/Skins/AddOns/Blizzard_Contribution.lua
@@ -1,5 +1,5 @@
 local MER, F, E, I, V, P, G, L = unpack(ElvUI_MerathilisUI)
-local module = MER.Modules.Skins
+local module = MER:GetModule("MER_Skins")
 
 local _G = _G
 local hooksecurefunc = hooksecurefunc
--- ElvUI_MerathilisUI/Modules/Skins/AddOns/Blizzard_Delves.lua
+++ ElvUI_MerathilisUI/Modules/Skins/AddOns/Blizzard_Delves.lua
@@ -1,5 +1,6 @@
 local MER, F, E, I, V, P, G, L = unpack(ElvUI_MerathilisUI)
-local module = MER.Modules.Skins
+local module = MER:GetModule("MER_Skins")
+local S = E:GetModule("skins")
 
 local _G = _G
 
@@ -21,6 +22,17 @@ function module:Blizzard_DelvesDashboardUI() end
 
 module:AddCallbackForAddon("Blizzard_DelvesDashboardUI")
 
+local function HandleRewards(self)
+	for rewardFrame in self.rewardPool:EnumerateActive() do
+		if not rewardFrame.backdrop then
+			rewardFrame:CreateBackdrop("Transparent")
+			rewardFrame.NameFrame:SetAlpha(0)
+			S:HandleIcon(rewardFrame.Icon, true)
+			S:HandleIconBorder(rewardFrame.IconBorder, rewardFrame.Icon.backdrop)
+		end
+	end
+end
+
 function module:Blizzard_DelvesDifficultyPicker()
 	if not self:CheckDB("lfg", "delves") then
 		return
@@ -31,6 +43,9 @@ function module:Blizzard_DelvesDifficultyPicker()
 		self:CreateShadow(DifficultyPickerFrame)
 		DifficultyPickerFrame.CloseButton:ClearAllPoints()
 		DifficultyPickerFrame.CloseButton:SetPoint("TOPRIGHT", DifficultyPickerFrame, "TOPRIGHT", -3, -3)
+
+		DifficultyPickerFrame.DelveRewardsContainerFrame:HookScript("OnShow", HandleRewards)
+		hooksecurefunc(DifficultyPickerFrame.DelveRewardsContainerFrame, "SetRewards", HandleRewards)
 	end
 end
 
