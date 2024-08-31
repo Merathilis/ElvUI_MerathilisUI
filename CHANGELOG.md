3c533c95dc34b4f126f46a178f5d5b81189e2923
Merathilis <neo0608@gmail.com>
2024-08-31 11:00:52 +0200

    🚀 RELEASE: 6.37

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 ### Changes ###
 
-  * [Armory]: Updates to Armory
-  * [Skins]: Various skin tweaks & updates
-  * [Chat Text]: Sync with ElvUI changes
+  * [MicroBar]: Fix a MicroBar Notification error"
+  * [WorldMap]: Try to avoid a mapCancvas error
+  * [Event Tracker]: Added Theater Troupe Event
+  * [AutoButtons]: Added a lot of new openable items
+  * [Context Memu]: Some code adjustments and improvements
+  * [Skins]: More updates to Delves and PlayerSpells skin
--- ElvUI_MerathilisUI/Core/Changelog/6.37.lua
+++ ElvUI_MerathilisUI/Core/Changelog/6.37.lua
@@ -1,8 +1,17 @@
 local MER = unpack(ElvUI_MerathilisUI)
 
 MER.Changelog[637] = {
-	RELEASE_DATE = "TBD",
-	FIXES = {},
-	NEW = {},
-	IMPROVEMENTS = {},
+	RELEASE_DATE = "31.08.2024",
+	FIXES = {
+		"[MicroBar]: Fix a MicroBar Notification error",
+		"[WorldMap]: Try to avoid a mapCancvas error",
+	},
+	NEW = {
+		"[Event Tracker]: Added Theater Troupe Event",
+	},
+	IMPROVEMENTS = {
+		"[AutoButtons]: Added a lot of new openable items",
+		"[Context Memu]: Some code adjustments and improvements",
+		"[Skins]: More updates to Delves and PlayerSpells skin",
+	},
 }
