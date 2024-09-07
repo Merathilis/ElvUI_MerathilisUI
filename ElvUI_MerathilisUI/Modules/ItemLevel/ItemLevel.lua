74cf4f6596371096ba3713987da6a348097e898f
Merathilis <neo0608@gmail.com>
2024-09-07 13:08:24 +0200

    👌 IMPROVE: whoopsi

--- ElvUI_MerathilisUI/Modules/ItemLevel/ItemLevel.lua
+++ ElvUI_MerathilisUI/Modules/ItemLevel/ItemLevel.lua
@@ -216,6 +216,10 @@ local function ItemLevel_ReplaceGuildNews(button, _, text, name, link, ...)
 		return
 	end
 
+	if not _G.CommunitiesFrame or not _G.CommunitiesFrame.IsShown or not _G.CommunitiesFrame:IsShown() then
+		return
+	end
+
 	if not link or not strmatch(link, "|H(item:%d+:.-)|h.-|h") then
 		return
 	end
