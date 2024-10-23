a7c4b0b1f95033fd35055d54748ca7b8bc96a9c3
Merathilis <neo0608@gmail.com>
2024-10-23 07:05:09 +0200

    🚀 RELEASE: 6.43

--- ElvUI_MerathilisUI/Core/Changelog/6.43.lua
+++ ElvUI_MerathilisUI/Core/Changelog/6.43.lua
@@ -1,8 +1,9 @@
 local MER = unpack(ElvUI_MerathilisUI)
 
 MER.Changelog[643] = {
-	RELEASE_DATE = "TBD",
-	FIXES = {},
-	NEW = {},
-	IMPROVEMENTS = {},
+	RELEASE_DATE = "23.10.2024",
+	FIXES = {
+		"[AutoButtons]: Fixed an issues with the AutoButtons options",
+		"[EventTracker]: Fixed an time table option",
+	},
 }
