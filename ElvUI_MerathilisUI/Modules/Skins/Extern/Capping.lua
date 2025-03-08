81341cd11c652345e14b6731dc2bb813129a5b54
Merathilis <neo0608@gmail.com>
2025-03-08 13:52:16 +0100

    📦 NEW: Initial skin for Capping

---
+++ ElvUI_MerathilisUI/Modules/Skins/Extern/Capping.lua
@@ -0,0 +1,32 @@
+local MER, F, E, I, V, P, G, L = unpack(ElvUI_MerathilisUI)
+local module = MER:GetModule("MER_Skins")
+local S = E:GetModule("Skins")
+
+local _G = _G
+local pairs = pairs
+
+local function OnUpdate(s)
+	if not s.bars then
+		return
+	end
+
+	for bar in pairs(s.bars) do
+		if not bar._MERSkin then
+			bar:SetTemplate("Transparent")
+
+			bar._MERSkin = true
+		end
+	end
+end
+
+function module:Capping()
+	if not E.private.mui.skins.addonSkins.enable or not E.private.mui.skins.addonSkins.cap then
+		return
+	end
+
+	local frame = _G.CappingFrame
+
+	frame:HookScript("OnUpdate", OnUpdate)
+end
+
+module:AddCallbackForAddon("Capping")
