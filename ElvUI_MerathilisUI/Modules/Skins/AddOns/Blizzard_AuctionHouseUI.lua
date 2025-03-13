09ba6d411b21bb3f7a7b7164140a5fc75807c774
Merathilis <neo0608@gmail.com>
2025-03-13 11:58:08 +0100

    👌 IMPROVE: Adjust backdrops for some Editboxes on the AuctionHouse

--- ElvUI_MerathilisUI/Modules/Skins/AddOns/Blizzard_AuctionHouseUI.lua
+++ ElvUI_MerathilisUI/Modules/Skins/AddOns/Blizzard_AuctionHouseUI.lua
@@ -12,6 +12,13 @@ function module:Blizzard_AuctionHouseUI()
 	module:CreateShadow(Frame)
 	module:CreateShadow(Frame.WoWTokenResults.GameTimeTutorial)
 
+	if _G.BidAmountGold.backdrop then
+		_G.BidAmountGold.backdrop:SetPoint("BOTTOMRIGHT", 0, 0)
+	end
+	if _G.BidAmountSilver.backdrop then
+		_G.BidAmountSilver.backdrop:SetPoint("BOTTOMRIGHT", -10, 0)
+	end
+
 	local tabs = { _G.AuctionHouseFrameBuyTab, _G.AuctionHouseFrameSellTab, _G.AuctionHouseFrameAuctionsTab }
 	for _, tab in pairs(tabs) do
 		if tab then
