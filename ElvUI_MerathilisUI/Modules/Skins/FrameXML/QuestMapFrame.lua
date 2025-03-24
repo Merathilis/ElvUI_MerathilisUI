6f579cee91f51e72acf915578f1a65561d29126f
Merathilis <neo0608@gmail.com>
2025-03-24 13:32:07 +0100

    blub

--- ElvUI_MerathilisUI/Modules/Skins/FrameXML/QuestMapFrame.lua
+++ ElvUI_MerathilisUI/Modules/Skins/FrameXML/QuestMapFrame.lua
@@ -2,7 +2,6 @@ local MER, F, E, I, V, P, G, L = unpack(ElvUI_MerathilisUI)
 local module = MER:GetModule("MER_Skins")
 
 local _G = _G
-local select = select
 
 function module:QuestMapFrame()
 	if not module:CheckDB("quest", "quest") then
