a73bc4236854b58a585fdf8e666b83b641be82f3
Merathilis <neo0608@gmail.com>
2025-05-20 12:43:11 +0200

    🚀 RELEASE: 6.64

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
 ### Changes ###
 
-  * [World Map]: Fix loading of EventTracker >.>
+  * [Notification]: Fixed full bag notification
+  * [Focuser]: Added new option to shift+click a unit to set them as your focus
+  * [Misc]: Added back old ElvUI Role Icons
+  * [RBReminder]: Added more food, rune ids. Thanks @vociferoy from Discord
+  * [AutoButtons]: Added more food and flask ids
+  * [GameMenu]: Show Mythic+ infos on the GameMenu by default again
+  * [Installer]: Some minor changes to the installer
--- ElvUI_MerathilisUI/Core/Changelog/6.64.lua
+++ ElvUI_MerathilisUI/Core/Changelog/6.64.lua
@@ -1,8 +1,18 @@
 local MER = unpack(ElvUI_MerathilisUI)
 
 MER.Changelog[664] = {
-	RELEASE_DATE = "TBD",
-	FIXES = {},
-	NEW = {},
-	IMPROVEMENTS = {},
+	RELEASE_DATE = "20.05.2025",
+	FIXES = {
+		"[Notification]: Fixed full bag notification",
+	},
+	NEW = {
+		"[Focuser]: Added new option to shift+click a unit to set them as your focus",
+	},
+	IMPROVEMENTS = {
+		"[Misc]: Added back old ElvUI Role Icons",
+		"[RBReminder]: Added more food, rune ids. Thanks @vociferoy from Discord",
+		"[AutoButtons]: Added more food and flask ids",
+		"[GameMenu]: Show Mythic+ infos on the GameMenu by default again",
+		"[Installer]: Some minor changes to the installer",
+	},
 }
