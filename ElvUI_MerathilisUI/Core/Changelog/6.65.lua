d6821d750b46d7dd611ece62831282e73089d1ba
Merathilis <neo0608@gmail.com>
2025-06-03 06:18:11 +0200

    📖 DOC: Updated changelog so i dont forget

--- ElvUI_MerathilisUI/Core/Changelog/6.65.lua
+++ ElvUI_MerathilisUI/Core/Changelog/6.65.lua
@@ -2,7 +2,18 @@ local MER = unpack(ElvUI_MerathilisUI)
 
 MER.Changelog[665] = {
 	RELEASE_DATE = "TBD",
-	FIXES = {},
-	NEW = {},
-	IMPROVEMENTS = {},
+	FIXES = {
+		"[Notification]: Added the missing option for the Full Bag Alert",
+	},
+	NEW = {
+		"[Install]: BREAKING CHANGE: You have to run the Install to show my options!",
+		"[Pet Filter Tab]: Added a new tabs for pet filter settings, allowing users to customize pet-related filters more easily.",
+		"[Raid Info Frame]: Added a simple Frame which shows the current raid roles and the number of players in each role.",
+	},
+	IMPROVEMENTS = {
+		"[Enhanced NamePlate Auras]: Enhance Code a bit",
+		"[Game Menu]: Move the Game Menu Options to Misc",
+		"[Auto Buttons]: Added more openable/usable Items",
+		"[Core]: Small code improvements and optimizations",
+	},
 }
