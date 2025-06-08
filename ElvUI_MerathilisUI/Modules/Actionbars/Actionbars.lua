4925bb2b081eada3215cba4bdce04af92f1a9b68
Merathilis <neo0608@gmail.com>
2025-06-08 14:43:01 +0200

    👌 IMPROVE: Attempt to skin tdBattlePetScript ActionButton (untested)

--- ElvUI_MerathilisUI/Modules/Actionbars/Actionbars.lua
+++ ElvUI_MerathilisUI/Modules/Actionbars/Actionbars.lua
@@ -1,6 +1,19 @@
 local MER, F, E, I, V, P, G, L = unpack(ElvUI_MerathilisUI)
 local module = MER:GetModule("MER_Actionbars")
 local AB = E:GetModule("ActionBars")
+local S = E:GetModule("Skins")
+
+function module:tdBattlePetScript()
+	if not E:IsAddOnEnabled("tdBattlePetScript") then
+		return
+	end
+
+	local button = _G.tdBattlePetScriptAutoButton
+	if button then
+		S:HandleButton(button)
+		print("Button is here")
+	end
+end
 
 function module:Initialize()
 	if not E.private.actionbar.enable then
@@ -11,6 +24,7 @@ function module:Initialize()
 
 	self:CreateSpecBar()
 	self:ColorModifiers()
+	self:tdBattlePetScript()
 end
 
 MER:RegisterModule(module:GetName())
