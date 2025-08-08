cb8e8df96490021fd4f6b3f6e19108f0da1a8bc8
Merathilis <neo0608@gmail.com>
2025-08-08 14:57:38 +0200

    👌 IMPROVE: Updated AngrierWorldQuest Skinning

--- ElvUI_MerathilisUI/Modules/Skins/Extern/AngrierWorldQuests.lua
+++ ElvUI_MerathilisUI/Modules/Skins/Extern/AngrierWorldQuests.lua
@@ -1,17 +1,19 @@
 local MER, F, E, I, V, P, G, L = unpack(ElvUI_MerathilisUI)
 local module = MER:GetModule("MER_Skins")
+local AWQ = LibStub("AceAddon-3.0"):GetAddon("AngrierWorldQuests")
 
 local function HookQuestFrame_Update()
-	local button = _G.AngrierWorldQuestsHeader
-
-	if button and not button.MERSkin then
-		button:StripTextures()
-		button:CreateBackdrop("Transparent")
-		button:GetHighlightTexture():SetColorTexture(F.r, F.g, F.b, 0.25)
-		button.ButtonText:FontTemplate(nil, 16)
-
-		button.MERSkin = true
+	local headerButton = _G.AngrierWorldQuestsHeader
+	if not headerButton or headerButton.MERSkin then
+		return
 	end
+
+	headerButton:StripTextures()
+	headerButton:CreateBackdrop("Transparent")
+	headerButton:GetHighlightTexture():SetColorTexture(F.r, F.g, F.b, 0.25)
+	headerButton.ButtonText:FontTemplate(nil, 16)
+
+	headerButton.MERSkin = true
 end
 
 function module:AngrierWorldQuests()
@@ -19,7 +21,12 @@ function module:AngrierWorldQuests()
 		return
 	end
 
-	hooksecurefunc("QuestLogQuests_Update", HookQuestFrame_Update)
+	local QuestFrameModule = AWQ:GetModule("QuestFrameModule")
+	if not QuestFrameModule then
+		return
+	end
+
+	hooksecurefunc(QuestFrameModule, "QuestLog_Update", HookQuestFrame_Update)
 end
 
 module:AddCallbackForAddon("AngrierWorldQuests")
