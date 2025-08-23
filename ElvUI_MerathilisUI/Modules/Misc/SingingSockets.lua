c45d9ff02c97a995f126614d502387ab840d94a7
Merathilis <neo0608@gmail.com>
2025-08-23 16:27:36 +0200

    👌 IMPROVE: Updated Singing Socket Module to also support FIber Sockets

--- ElvUI_MerathilisUI/Modules/Misc/SingingSockets.lua
+++ ElvUI_MerathilisUI/Modules/Misc/SingingSockets.lua
@@ -31,27 +31,27 @@ local function GetGemLink(gemID)
 end
 
 function module:Socket_OnEnter()
+	local info = GetGemLink(self.gemID)
+	if not info then
+		return
+	end
+
 	GameTooltip:ClearLines()
 	GameTooltip:SetOwner(self, "ANCHOR_RIGHT", 0, 3)
-	GameTooltip:SetHyperlink(GetGemLink(self.gemID))
+	GameTooltip:SetHyperlink(info)
 	GameTooltip:Show()
 end
 
 function module:Socket_OnClick()
-	local BAG_ID, SLOT_ID
 	for bagID = 0, 4 do
 		for slotID = 1, C_Container_GetContainerNumSlots(bagID) do
-			local itemID = C_Container_GetContainerItemID(bagID, slotID)
-			if itemID == self.gemID then
-				BAG_ID = bagID
-				SLOT_ID = slotID
+			if C_Container_GetContainerItemID(bagID, slotID) == self.gemID then
+				C_Container_PickupContainerItem(bagID, slotID)
+				ClickSocketButton(self.socketID)
+				ClearCursor()
+				return
 			end
 		end
-	end
-	if BAG_ID and SLOT_ID then
-		C_Container_PickupContainerItem(BAG_ID, SLOT_ID)
-		ClickSocketButton(self.socketID)
-		ClearCursor()
 	end
 end
 
@@ -59,7 +59,6 @@ function module:CreateSingingSockets()
 	if module.SingingFrames then
 		return
 	end
-
 	module.SingingFrames = {}
 
 	for i = 1, 3 do
@@ -82,6 +81,41 @@ function module:CreateSingingSockets()
 		end
 	end
 end
+
+local fiberSockets = { 238044, 238046, 238045, 238042, 238040, 238037, 238039, 238041 }
+
+function module:CreateFiberSockets()
+	if module.FiberSockets then
+		return
+	end
+
+	local locales = { L["Crit"], L["Mastery"], L["Haste"], L["Versa"] }
+
+	local frame = CreateFrame("Frame", "MER_FiberSocket", ItemSocketingFrame)
+	frame:SetSize(iconSize * 4, iconSize * 2)
+	frame:SetPoint("TOP", _G["ItemSocketingSocket1"], "BOTTOM", 0, -50)
+	frame:CreateBackdrop("Transparent")
+
+	for index, gemID in pairs(fiberSockets) do
+		local button = S.CreateButton(frame, iconSize, iconSize, true, C_Item_GetItemIconByID(gemID))
+		button:SetPoint("TOPLEFT", mod(index - 1, 4) * (iconSize + 5), -(index > 4 and (iconSize + 5) or 0))
+		local colors = E.QualityColors[index <= 4 and 4 or 3]
+		button.backdrop:SetBackdropBorderColor(colors.r, colors.g, colors.b)
+		button.socketID = 1
+		button.gemID = gemID
+		button:SetScript("OnEnter", module.Socket_OnEnter)
+		button:SetScript("OnClick", module.Socket_OnClick)
+		button:SetScript("OnLeave", GameTooltip_Hide)
+
+		button.text = button:CreateFontString(nil, "OVERLAY")
+		button.text:FontTemplate(nil, 14, "SHADOWOUTLINE")
+		button.text:SetText(locales[mod(index - 1, 4) + 1])
+		button.text:SetTextColor(1, 0.8, 0)
+	end
+
+	module.FiberSockets = frame
+end
+
 function module:SetupSingingSockets()
 	if not E.db.mui.misc.singingSockets.enable then
 		return
@@ -92,13 +126,24 @@ function module:SetupSingingSockets()
 			return
 		end
 
-		module:CreateSingingSockets()
-
 		if module.SingingFrames then
-			local isSingingSocket = GetSocketTypes(1) == "SingingThunder"
 			for i = 1, 3 do
-				module.SingingFrames[i]:SetShown(isSingingSocket and not GetExistingSocketInfo(i))
+				module.SingingFrames[i]:Hide()
 			end
+		end
+		if module.FiberSockets then
+			module.FiberSockets:Hide()
+		end
+
+		local socketType = GetSocketTypes(1)
+		if socketType == "SingingThunder" then
+			module:CreateSingingSockets()
+			for i = 1, 3 do
+				module.SingingFrames[i]:SetShown(not GetExistingSocketInfo(i))
+			end
+		elseif socketType == "Fiber" then
+			module:CreateFiberSockets()
+			module.FiberSockets:SetShown(not GetExistingSocketInfo(1))
 		end
 	end)
 end
