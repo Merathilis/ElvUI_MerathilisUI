790f40d6d75052eac089790172d61dcdd2d51c00
Merathilis <neo0608@gmail.com>
2025-08-28 17:10:02 +0200

    🐛 FIX: The instance difficutly event handler

--- ElvUI_MerathilisUI/Modules/Maps/InstanceDifficulty.lua
+++ ElvUI_MerathilisUI/Modules/Maps/InstanceDifficulty.lua
@@ -172,10 +172,10 @@ function module:Initialize()
 	self:RegisterEvent("PLAYER_DIFFICULTY_CHANGED", "UpdateFrame")
 	self:RegisterEvent("GUILD_PARTY_STATE_UPDATED", "UpdateFrame")
 	self:RegisterEvent("ZONE_CHANGED_NEW_AREA", "UpdateFrame")
-	self:RegisterEvent("GROUP_ROSTER_UPDATE")
+	self:RegisterEvent("GROUP_ROSTER_UPDATE", "UpdateFrame")
 
 	if IsAddOnLoaded("Blizzard_Minimap") then
-		self:ADDON_LOADED("ADDON_LOADED", "Blizzard_Minimap")
+		self:ADDON_LOADED(nil, "Blizzard_Minimap")
 	else
 		self:RegisterEvent("ADDON_LOADED")
 	end
