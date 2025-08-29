d660d92031f8b2b09bc566a0b40c717011549f98
Merathilis <neo0608@gmail.com>
2025-08-29 06:36:04 +0200

    👌 IMPROVE: Update MerchantFrame skin

--- ElvUI_MerathilisUI/Modules/Skins/FrameXML/MerchantFrame.lua
+++ ElvUI_MerathilisUI/Modules/Skins/FrameXML/MerchantFrame.lua
@@ -2,6 +2,9 @@ local MER, F, E, I, V, P, G, L = unpack(ElvUI_MerathilisUI)
 local module = MER:GetModule("MER_Skins")
 
 local _G = _G
+local pairs, unpack = pairs, unpack
+
+local hooksecurefunc = hooksecurefunc
 
 function module:HandleMerchantItem(index)
 	for currencyIndex = 1, 3 do
@@ -26,9 +29,27 @@ function module:MerchantFrame()
 		self:ReskinTab(_G["MerchantFrameTab" .. i])
 	end
 
-	for i = 1, 10 do
+	for i = 1, 12 do
 		self:HandleMerchantItem(i)
 	end
+
+	for _, region in pairs({ _G.MerchantMoneyFrame.GoldButton:GetRegions() }) do
+		if region:GetObjectType() == "Texture" then
+			F.MoveFrameWithOffset(region, 0, 4)
+		end
+	end
+
+	hooksecurefunc("MerchantFrame_UpdateCurrencies", function()
+		for i = 1, 3 do
+			local token = _G["MerchantToken" .. i]
+			if token and not token.__MERSkin then
+				F.SetFontOutline(token.Count)
+				F.MoveFrameWithOffset(token.Count, -2, 0)
+				token.Icon:SetTexCoord(unpack(E.TexCoords))
+				token.__MERSkin = true
+			end
+		end
+	end)
 end
 
 module:AddCallback("MerchantFrame")
