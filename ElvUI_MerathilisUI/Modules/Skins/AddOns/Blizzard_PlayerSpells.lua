8d52b29fe2a0367d0ea2b082dbc47dee7ef53ce9
Merathilis <neo0608@gmail.com>
2025-09-11 05:45:04 +0200

    👌 IMPROVE: Remove this

--- ElvUI_MerathilisUI/Modules/Skins/AddOns/Blizzard_PlayerSpells.lua
+++ ElvUI_MerathilisUI/Modules/Skins/AddOns/Blizzard_PlayerSpells.lua
@@ -4,7 +4,6 @@ local module = MER:GetModule("MER_Skins") ---@type Skins
 local _G = _G
 
 local hooksecurefunc = hooksecurefunc
-local C_Timer_After = C_Timer.After
 
 function module:Blizzard_PlayerSpells()
 	if not module:CheckDB("talent", "talent") then
@@ -24,21 +23,8 @@ function module:Blizzard_PlayerSpells()
 
 	local SpellBookFrame = _G.PlayerSpellsFrame.SpellBookFrame
 	if SpellBookFrame then
-		local function CenterTabText(tab)
-			if tab.Text and not tab.__MERTextCentered then
-				C_Timer_After(0.05, function()
-					if tab.Text and not tab.__MERTextCentered then
-						tab.Text:ClearAllPoints()
-						tab.Text:SetPoint("CENTER", tab, "CENTER", 0, 0)
-						tab.__MERTextCentered = true
-					end
-				end)
-			end
-		end
-
 		for _, tab in next, { SpellBookFrame.CategoryTabSystem:GetChildren() } do
 			tab:SetPushedTextOffset(0, 0)
-			CenterTabText(tab)
 		end
 	end
 
