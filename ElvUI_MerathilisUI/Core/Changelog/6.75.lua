4493cb9d02dbe5286c9adeabaa259b4ef8a0e3b4
Merathilis <neo0608@gmail.com>
2025-10-08 05:57:56 +0200

    🚀 RELEASE: 6.75

--- ElvUI_MerathilisUI/Core/Changelog/6.75.lua
+++ ElvUI_MerathilisUI/Core/Changelog/6.75.lua
@@ -1,8 +1,13 @@
 local MER = unpack(ElvUI_MerathilisUI)
 
 MER.Changelog[675] = {
-	RELEASE_DATE = "TBD",
+	RELEASE_DATE = "08.10.2025",
 	FIXES = {},
-	NEW = {},
-	IMPROVEMENTS = {},
+	NEW = {
+		"[Achievement Tracker]: Added a new Achievement Tracker module.",
+	},
+	IMPROVEMENTS = {
+		"[Core]: Small updates due to 11.2.5 changes.",
+		"[Skins]: Finished Rematch skin.",
+	},
 }
