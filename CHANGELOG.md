2e360172d1dce813b417e5edb69748b7f3d2d071
Merathilis <neo0608@gmail.com>
2025-10-11 12:26:09 +0200

    🚀 RELEASE: 6.76

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 ### Changes ###
 
-  * [Core]: Small updates due to 11.2.5 changes
-  * [Achievement Tracker]: Added a new Achievement Tracker module
-  * [Skins]: Finished Rematch skin
+  * [AutoButtons]: Added Legion Remix openables
+  * [Minimap Buttons]: Added a sort feature
+  * [Skins]: Added an initial Plumber Skin
+  * [Skins]: Updated Friends List skin to also support now Recent Allies
+  * [Skins]: Updated SilverDragon skin to support the mount button
--- ElvUI_MerathilisUI/Core/Changelog/6.76.lua
+++ ElvUI_MerathilisUI/Core/Changelog/6.76.lua
@@ -1,8 +1,15 @@
 local MER = unpack(ElvUI_MerathilisUI)
 
 MER.Changelog[676] = {
-	RELEASE_DATE = "TBD",
+	RELEASE_DATE = "11.10.2025",
 	FIXES = {},
-	NEW = {},
-	IMPROVEMENTS = {},
+	NEW = {
+		"[Skins]: Added an initial Plumber Skin",
+	},
+	IMPROVEMENTS = {
+		"[AutoButtons]: Added Legion Remix openables",
+		"[Minimap Buttons]: Added a sort feature",
+		"[Skins]: Updated Friends List skin to also support now Recent Allies",
+		"[Skins]: Updated SilverDragon skin to support the mount button",
+	},
 }
