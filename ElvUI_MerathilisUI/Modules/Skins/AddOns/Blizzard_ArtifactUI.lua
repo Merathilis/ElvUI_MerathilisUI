8b283276ead316673466246a451ed4fa63c90e32
Merathilis <neo0608@gmail.com>
2025-10-14 19:12:49 +0200

    📦 NEW: Added a new RemixArtifact skin

--- ElvUI_MerathilisUI/Modules/Skins/AddOns/Blizzard_ArtifactUI.lua
+++ ElvUI_MerathilisUI/Modules/Skins/AddOns/Blizzard_ArtifactUI.lua
@@ -75,3 +75,58 @@ function module:Blizzard_ArtifactUI()
 end
 
 module:AddCallbackForAddon("Blizzard_ArtifactUI")
+
+function module:Blizzard_RemixArtifactUI()
+	if not module:CheckDB("artifact", "artifact") then
+		return
+	end
+
+	local RemixArtifactFrame = _G.RemixArtifactFrame
+	if not RemixArtifactFrame then
+		return
+	end
+
+	RemixArtifactFrame:SetTemplate("Transparent")
+	self:CreateShadow(RemixArtifactFrame)
+
+	if RemixArtifactFrame.Background then
+		RemixArtifactFrame.Background:Kill()
+	end
+	if RemixArtifactFrame.BorderContainer then
+		RemixArtifactFrame.BorderContainer:Kill()
+	end
+
+	if RemixArtifactFrame.ButtonsParent then
+		RemixArtifactFrame.ButtonsParent.Overlay:Kill()
+	end
+
+	if RemixArtifactFrame.Model then
+		F.InternalizeMethod(RemixArtifactFrame.Model, "SetLight")
+		hooksecurefunc(RemixArtifactFrame.Model, "SetLight", function(model, enabled, lightValues)
+			if lightValues.ambientIntensity then
+				lightValues.ambientIntensity = 0.3
+				F.CallMethod(model, "SetLight", enabled, lightValues)
+			end
+		end)
+	end
+
+	if RemixArtifactFrame.Currency then
+		local UnspentPointsCount = RemixArtifactFrame.Currency.UnspentPointsCount
+		F.SetFont(UnspentPointsCount)
+		F.InternalizeMethod(UnspentPointsCount, "SetText")
+		hooksecurefunc(UnspentPointsCount, "SetText", function(currency, raw)
+			local after = self:StyleTextureString(raw)
+			if after ~= raw then
+				F.CallMethod(currency, "SetText", after)
+			end
+		end)
+	end
+
+	if RemixArtifactFrame.Header then
+		F.SetFont(RemixArtifactFrame.Header.Title)
+	end
+
+	self:CreateShadow(RemixArtifactFrame)
+end
+
+module:AddCallbackForAddon("Blizzard_RemixArtifactUI")
