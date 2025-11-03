1d237bae7c9c76c810bd4548094c6293fab4a21a
Merathilis <neo0608@gmail.com>
2025-11-03 16:39:24 +0100

    📖 DOC: prep changelogs

---
+++ ElvUI_MerathilisUI/Core/Changelog/Previous/7.00.lua
@@ -0,0 +1,14 @@
+local MER = unpack(ElvUI_MerathilisUI)
+
+MER.Changelog[700] = {
+	RELEASE_DATE = "03.11.2025",
+	FIXES = {},
+	NEW = {
+		"[Core]: YOU NEED ELVUI_WINDTOOLS TO USE MERATHILISUI NOW!",
+		"[Core]: A lot of things maybe needs to be adjusted, so please report any lua errors you find!",
+	},
+	IMPROVEMENTS = {
+		"[Functions]: A lot of functions are now from WindTools, make sure you have it installed!",
+		"[Modules]: A lot of modules are now in the WindTools Options, if you only used MerathilisUI before",
+	},
+}
