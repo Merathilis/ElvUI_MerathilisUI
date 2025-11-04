0b98e40a609292a0567047acbf25c48a2f3001e6
Merathilis <neo0608@gmail.com>
2025-11-04 05:54:15 +0100

    🚀 RELEASE: 7.01

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,3 @@
 ### Changes ###
 
-      !!! READ THIS  !!!
-  * [Core]: YOU NEED ELVUI_WINDTOOLS TO USE MERATHILISUI NOW!
-  * [Core]: A lot of things maybe needs to be adjusted, so please report any lua errors you find!
-  * [Functions]: A lot of functions are now from WindTools, make sure you have it installed!
-  * [Modules]: A lot of modules are now in the WindTools Options, if you only used MerathilisUI before
-  * Please do not come in my discord if you haven't read the above!!
+  * [Core]: Fix a typo in the database conversation that broke my Plugin.
--- ElvUI_MerathilisUI/Core/Changelog/7.01.lua
+++ ElvUI_MerathilisUI/Core/Changelog/7.01.lua
@@ -1,14 +1,10 @@
 local MER = unpack(ElvUI_MerathilisUI)
 
-MER.Changelog[700] = {
-	RELEASE_DATE = "03.11.2025",
-	FIXES = {},
-	NEW = {
-		"[Core]: YOU NEED ELVUI_WINDTOOLS TO USE MERATHILISUI NOW!",
-		"[Core]: A lot of things maybe needs to be adjusted, so please report any lua errors you find!",
+MER.Changelog[701] = {
+	RELEASE_DATE = "04.11.2025",
+	FIXES = {
+		"[Core]: Fix a typo in the database conversation that broke my Plugin.",
 	},
-	IMPROVEMENTS = {
-		"[Functions]: A lot of functions are now from WindTools, make sure you have it installed!",
-		"[Modules]: A lot of modules are now in the WindTools Options, if you only used MerathilisUI before",
-	},
+	NEW = {},
+	IMPROVEMENTS = {},
 }
