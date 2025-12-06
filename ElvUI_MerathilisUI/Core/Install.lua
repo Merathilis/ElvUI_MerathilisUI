21896488f2a0770a9950a3134abf18d500da8dc0
Merathilis <neo0608@gmail.com>
2025-12-06 17:47:08 +0100

    👌 IMPROVE: Add Housing Control Mover setting to install

--- ElvUI_MerathilisUI/Core/Install.lua
+++ ElvUI_MerathilisUI/Core/Install.lua
@@ -604,6 +604,7 @@ function MER:SetupLayout()
 	E.db["movers"]["TotemBarMover"] = "BOTTOM,ElvUIParent,BOTTOM,-297,45"
 	E.db["movers"]["AddonCompartmentMover"] = "TOPRIGHT,ElvUIParent,TOPRIGHT,-213,-17"
 	E.db["movers"]["QueueStatusMover"] = "TOPRIGHT,ElvUIParent,TOPRIGHT,-7,-165"
+	E.db["movers"]["HousingControlFrameMover"] = "TOP,ElvUIParent,TOP,0,-70"
 
 	-- UIWidgets
 	E.db["movers"]["TopCenterContainerMover"] = "TOP,ElvUIParent,TOP,0,-105"
