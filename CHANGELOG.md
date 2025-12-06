d8c705069821942c44d94db32a633e37d4537d5c
Merathilis <neo0608@gmail.com>
2025-12-06 17:54:41 +0100

    🚀 RELEASE: 7.02

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ### Changes ###
 
-  * [Core]: Fix a typo in the database conversation that broke my Plugin.
+  * [Tooltip Option]: Fix that the option wasn't visble at all
+  * [Install]: Fix an error in the Chat Install step
+  * [Install]: Add Housing Control Mover settings to the install
+  * [Vehicle Bar]: Remove Vigor Bar & Speed Text
+  * [Skins]: Remove HouseBound skin
--- ElvUI_MerathilisUI/Core/Changelog/7.02.lua
+++ ElvUI_MerathilisUI/Core/Changelog/7.02.lua
@@ -1,8 +1,15 @@
 local MER = unpack(ElvUI_MerathilisUI)
 
 MER.Changelog[702] = {
-	RELEASE_DATE = "TBD",
-	FIXES = {},
+	RELEASE_DATE = "06.12.2025",
+	FIXES = {
+		"[Tooltip Option]: Fix that the option wasn't visble at all",
+		"[Install]: Fix an error in the Chat Install step",
+	},
 	NEW = {},
-	IMPROVEMENTS = {},
+	IMPROVEMENTS = {
+		"[Install]: Add HousingControlFrameMover to the install",
+		"[Vehicle Bar]: Remove Vigor Bar & Speed Text",
+		"[Skins]: Remove HomeBound skin",
+	},
 }
