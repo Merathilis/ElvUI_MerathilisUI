1c287c73e2329bf43244830f3907b040c3d97227
Merathilis <neo0608@gmail.com>
2026-01-25 17:49:57 +0100

    🚀 RELEASE (Midnight): 7.05

--- ElvUI_MerathilisUI/Core/Changelog/7.05.lua
+++ ElvUI_MerathilisUI/Core/Changelog/7.05.lua
@@ -1,8 +1,14 @@
 local MER = unpack(ElvUI_MerathilisUI)
 
 MER.Changelog[705] = {
-	RELEASE_DATE = "TBD",
+	RELEASE_DATE = "25.01.2026",
 	FIXES = {},
-	NEW = {},
-	IMPROVEMENTS = {},
+	NEW = {
+		"[Core]: Early Midnight support. Expect things to be broken or not working at all.",
+		"[Core]: YOU SHOULD COMPLETELY START FROM SCRATCH WITH YOUR LAYOUT!!",
+		"[Portraits]: Implement the Portraits from ElvUI_mMedia Tag & Tools. Disabled by default. Thx Blinkii",
+	},
+	IMPROVEMENTS = {
+		"[WindTools Profile]: Updated some settings match the latest WindTools version. WIP",
+	},
 }
