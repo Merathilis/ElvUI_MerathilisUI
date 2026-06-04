7a6a628c03e39ce3c1550ab495f540a357a994ec
Merathilis <neo0608@gmail.com>
2026-06-04 14:53:16 +0200

    🚀 RELEASE: 7.21

--- ElvUI_MerathilisUI/Core/Changelog/7.21.lua
+++ ElvUI_MerathilisUI/Core/Changelog/7.21.lua
@@ -1,8 +1,13 @@
 local MER = unpack(ElvUI_MerathilisUI)
 
 MER.Changelog[721] = {
-	RELEASE_DATE = "TBD",
-	FIXES = {},
-	NEW = {},
+	RELEASE_DATE = "04.06.2026",
+	FIXES = {
+		"[VehicleBar]: Fix an secret error in Vehicle Bar",
+		"[Armory]: Try to prevent some stats errors if you are in Combat",
+	},
+	NEW = {
+		"[NameHover]: Added an option to hide the default Game Tooltip with an override key to show it. Thx @OldDogy",
+	},
 	IMPROVEMENTS = {},
 }
