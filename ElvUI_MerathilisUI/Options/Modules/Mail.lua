cf8f51d31d208be69bad1a0b0133717d7ccfd857
Merathilis <neo0608@gmail.com>
2026-08-01 18:11:17 +0200

    👌 IMPROVE: Add Mail module and related settings

---
+++ ElvUI_MerathilisUI/Options/Modules/Mail.lua
@@ -0,0 +1,44 @@
+local MER, W, WF, F, E, I, V, P, G, L = unpack(ElvUI_MerathilisUI)
+local module = MER:GetModule("MER_Options") ---@class Options
+local Mail = MER:GetModule("MER_Mail")
+
+local options = module.options.modules.args
+
+options.mail = {
+	type = "group",
+	name = L["Mail"],
+	get = function(info)
+		return E.db.mui.mail[info[#info]]
+	end,
+	set = function(info, value)
+		E.db.mui.mail[info[#info]] = value
+		E:StaticPopup_Show("CONFIG_RL")
+	end,
+	args = {
+		header = {
+			order = 0,
+			type = "header",
+			name = F.cOption(L["Mail"], "orange"),
+		},
+		desc = {
+			order = 1,
+			type = "group",
+			inline = true,
+			name = L["Description"],
+			args = {
+				feature = {
+					order = 1,
+					type = "description",
+					name = L["Add small extras to the Mail Frame."],
+					fontSize = "medium",
+				},
+			},
+		},
+		enable = {
+			order = 2,
+			type = "toggle",
+			name = L["Enable"],
+			width = "full",
+		},
+	},
+}
--- ElvUI_MerathilisUI/Settings/Profile.lua
+++ ElvUI_MerathilisUI/Settings/Profile.lua
@@ -643,6 +643,10 @@ P.tooltip = {
 	achievement = true,
 }
 
+P.mail = {
+	enable = true,
+}
+
 P.notification = {
 	enable = true,
 	noSound = false,
