a810e128baa4ec1ccdaa422ebbdbb1cd24a49b8f
Merathilis <neo0608@gmail.com>
2026-08-15 14:18:52 +0200

    🚀 RELEASE: 7.26

--- ElvUI_MerathilisUI/Core/Changelog/7.26.lua
+++ ElvUI_MerathilisUI/Core/Changelog/7.26.lua
@@ -3,6 +3,11 @@ local MER = unpack(ElvUI_MerathilisUI)
 MER.Changelog[726] = {
 	RELEASE_DATE = "TBD",
 	FIXES = {},
-	NEW = {},
-	IMPROVEMENTS = {},
+	NEW = {
+		"[Profile]: Added a profile for Permoks Account Manager",
+	},
+	IMPROVEMENTS = {
+		"[NameHover]: Improved secret value handling to prevent potential errors",
+		"[Core]: Bumped the minimum required ElvUI version to 15.22",
+	},
 }
