e2a2fe57016ef3a994b61f2cf9d7b9cf0fb115b6
Merathilis <neo0608@gmail.com>
2026-09-19 11:09:55 +0200

    🚀 RELEASE: 7.34

--- ElvUI_MerathilisUI/Core/Changelog/7.34.lua
+++ ElvUI_MerathilisUI/Core/Changelog/7.34.lua
@@ -1,14 +1,15 @@
 local MER = unpack(ElvUI_MerathilisUI)
 
 MER.Changelog[734] = {
-	RELEASE_DATE = "TBD",
+	RELEASE_DATE = "19.09.2026",
 	FIXES = {
-		"Armory: fixed the character sheet tainting Blizzard's paperdoll code, which could cause \"Interface action failed because of an AddOn\" when opening the character frame in combat.",
+		'Armory: fixed the character sheet tainting Blizzard\'s paperdoll code, which could cause "Interface action failed because of an AddOn" when opening the character frame in combat.',
 		"Misc: fixed the Guild MOTD popup not closing on Escape.",
 		"Bags: Categorized Bags - fixed a taint error (\"tried to call the protected function 'UpgradeItem()'\") when upgrading an item at an upgrade NPC.",
 	},
 	NEW = {},
 	IMPROVEMENTS = {
 		"Mail/Loot: replaced the deprecated dropdown API in the send-mail template list and the Loot Spec Manager with Blizzard's current menu system, avoiding a known source of taint.",
+		"General: removed leftover code paths for Classic clients, since MerathilisUI is Retail only.",
 	},
 }
