94825eeef20da0029b8fbb76f39e2b3e8528c067
Merathilis <neo0608@gmail.com>
2026-09-26 10:39:18 +0200

    📖 DOC: changelog update

--- ElvUI_MerathilisUI/Core/Changelog/7.37.lua
+++ ElvUI_MerathilisUI/Core/Changelog/7.37.lua
@@ -3,7 +3,7 @@ local MER = unpack(ElvUI_MerathilisUI)
 MER.Changelog[737] = {
 	RELEASE_DATE = "TBD",
 	FIXES = {
-		"Movement Alert - fixed a Lua error in combat (\"attempt to compare a secret number value\") when a tracked spell's cooldown is restricted.",
+		'[Movement Alert]: Fixed a Lua error in combat ("attempt to compare a secret number value") when a tracked spell\'s cooldown is restricted.',
 	},
 	NEW = {
 		"[UnitFrames/NamePlates]: New Faction Indicator - shows the faction crest of players from the opposing faction on the Target, Focus and Arena frames (Target of Target and Focus Target optional) and on the NamePlates. Style, size and position can be set per frame.",
